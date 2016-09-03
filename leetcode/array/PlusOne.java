1701fdeef9a0d7bff3aa11e59a59149040e1e875
lampardchelsea <zqqsxdd@gmail.com>
2016-09-03 17:06:54 -0500

    Update PlusOne.java

--- leetcode/array/PlusOne.java
+++ leetcode/array/PlusOne.java
@@ -1,7 +1,10 @@
 /**
-*Given a non-negative number represented as an array of digits, plus one to the number.
-
-The digits are stored such that the most significant digit is at the head of the list.
-
-Subscribe to see which companies asked this question
+* Given a non-negative number represented as an array of digits, plus one to the number.
+* The digits are stored such that the most significant digit is at the head of the list.
+* Subscribe to see which companies asked this question
 */
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        
+    }
+}
