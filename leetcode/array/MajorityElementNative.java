d40193d4eecda4678d9814b4fffdb170aebd5250
lampardchelsea <zqqsxdd@gmail.com>
2016-09-04 02:19:55 -0500

    Update MajorityElementNative.java

--- leetcode/array/MajorityElementNative.java
+++ leetcode/array/MajorityElementNative.java
@@ -1,4 +1,8 @@
 /**
-* If not observe the 
-* scan once to find the longest consecutive substrings
+ * Given an array of size n, find the majority element. The majority element is the element that 
+ * appears more than ⌊ n/2 ⌋ times. You may assume that the array is non-empty and the majority
+ * element always exist in the array.
+ * 
+ * If not observe condition as this element appears more than half times and recognize it must
+ * exist on the central of sorted array. We can scan once to find the longest consecutive substrings
 */
