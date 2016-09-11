d9241a6ee7e40fcf53d3b651f2dcb852056f35df
lampardchelsea <zqqsxdd@gmail.com>
2016-09-11 15:44:41 -0500

    Update PascalTriangleIINoSpaceLimit.java

--- leetcode/array/PascalTriangleIINoSpaceLimit.java
+++ leetcode/array/PascalTriangleIINoSpaceLimit.java
@@ -8,6 +8,48 @@
  * If not limit the space, very similar to PascalTriangle, first
  * fully create the PascalTriangle, then get its corresponding row.
  * Note: Need to care about relation between rowIndex and numRows.
+ * 
+ * Below code has big issue, if rowIndex != 0, it will never create
+ * the first row (only one 1), but PascalTriangle as an integrate
+ * structure, the first row is necessary and should create in any
+ * case.
+ * 
+ * public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> pascalTriangle = new ArrayList<List<Integer>>();
+        List<Integer> result = new ArrayList<Integer>();
+        
+        if(rowIndex < 0) {
+            return result;
+        }
+
+        // This case will mistakenly skip if rowIndex != 0
+        if(rowIndex == 0) {
+            List<Integer> firstRow = new ArrayList<Integer>();
+            firstRow.add(1);
+            pascalTriangle.add(firstRow);
+        }
+        
+        for(int i = 1; i <= rowIndex; i++) {
+            List<Integer> currentRow = new ArrayList<Integer>();
+            for(int j = 0; j <= i; j++) {
+                if(j == 0 || j == i) {
+                    currentRow.add(1);
+                } else {
+                    int a = pascalTriangle.get(i - 1).get(j - 1);
+                    int b = pascalTriangle.get(i - 1).get(j);
+                    currentRow.add(a + b);
+                }
+            }
+            pascalTriangle.add(currentRow);
+        }
+        
+        result = pascalTriangle.get(rowIndex);
+        
+        return result;
+    }
+}
+ * 
 */
 public class Solution {
     public List<Integer> getRow(int rowIndex) {
