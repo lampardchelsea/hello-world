9af0f335c4b1dad86dfd2a497dd994859a4c6bdc
lampardchelsea <zqqsxdd@gmail.com>
2016-09-12 01:18:28 -0500

    Update ContainsDuplicate.java

--- leetcode/array/ContainsDuplicate.java
+++ leetcode/array/ContainsDuplicate.java
@@ -3,3 +3,18 @@
  * Your function should return true if any value appears at least twice in the array, 
  * and it should return false if every element is distinct.
 */
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int length = nums.length;
+        boolean result = false;
+        Set<Integer> set = new HashSet<Integer>();
+        
+        for(int i = 0; i < length; i++) {
+            if(!set.add(nums[i])) {
+               result = true; 
+            }
+        }
+        
+        return result;
+    }
+}
