449b359e20444460a4b6975e71e9b053fe5daf68
lampardchelsea <zqqsxdd@gmail.com>
2016-09-16 17:19:08 -0500

    Update SingleNumber.java

--- leetcode/HashTable/SingleNumber.java
+++ leetcode/HashTable/SingleNumber.java
@@ -3,3 +3,23 @@
  * Note:
  * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 */
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int length = nums.length;
+        int result = 0;
+        Set<Integer> set = new HashSet<Integer>();
+        
+        for(int i = 0; i < length; i++) {
+            if(!set.add(nums[i])) {
+                set.remove(nums[i]);
+            }
+        }
+        
+        Iterator<Integer> it = set.iterator();
+        if(it.hasNext()) {
+            result = it.next();
+        }
+        
+        return result;
+    }
+}
