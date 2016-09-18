bc2c978dc79dfe4b2cf91c723fe9471e46d67780
lampardchelsea <zqqsxdd@gmail.com>
2016-09-18 02:15:08 -0500

    Update TwoSumBruteForce.java

--- leetcode/HashTable/TwoSumBruteForce.java
+++ leetcode/HashTable/TwoSumBruteForce.java
@@ -1,3 +1,13 @@
+/**
+ * The brute force approach is simple. Loop through each element xx and find if 
+ * there is another value that equals to target - x
+ * 
+ * Complexity Analysis
+ * Time complexity : O(n^2)
+ * For each element, we try to find its complement by looping through the rest of array which takes O(n) time. 
+ * Therefore, the time complexity is O(n^2)
+ * Space complexity : O(1)
+ */
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int length = nums.length;
