c39af06c02bb0fc759fa1e0f01dc92b7fa74c893
lampardchelsea <zqqsxdd@gmail.com>
2016-09-18 02:20:54 -0500

    Update ValidSudoku.java

--- leetcode/HashTable/ValidSudoku.java
+++ leetcode/HashTable/ValidSudoku.java
@@ -1,3 +1,10 @@
 /**
- *
+ * Determine if a Sudoku is valid, according to: 
+ * Sudoku Puzzles - The Rules.
+ * http://sudoku.com.au/TheRules.aspx
+ * The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+ * A partially filled sudoku which is valid.
+ * Note:
+ * A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
+ * Subscribe to see which companies asked this question
 */
