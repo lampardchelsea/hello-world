f1858405583aefdbf8c7635929b0d3ba75561919
lampardchelsea <zqqsxdd@gmail.com>
2016-09-23 02:09:03 -0500

    Update CircularArrayQueueTwo.java

--- leetcode/array/CircularArrayQueueTwo.java
+++ leetcode/array/CircularArrayQueueTwo.java
@@ -1,3 +1,9 @@
 /**
- * If array length is N, only store N - 1 elements.
+ * Analyze
+ * The implementation class use array to implement a queue structur which attribute as FIFO and
+ * cicularly insert at the end or delete element at the start of array.
+ * Without using variable "count" to identify whether circular array is empty or full
+ * 
+ * Best example
+ * http://www.mathcs.emory.edu/~cheung/Courses/171/Syllabus/8-List/array-queue2.html
 */
