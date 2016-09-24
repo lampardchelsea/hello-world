4cbf6d16f223bd385cb34de72e25bd3d2eb37600
lampardchelsea <zqqsxdd@gmail.com>
2016-09-24 14:25:20 -0500

    Update LoggerRateLimiterCircularArray.java

--- leetcode/HashTable/LoggerRateLimiterCircularArray.java
+++ leetcode/HashTable/LoggerRateLimiterCircularArray.java
@@ -16,8 +16,8 @@ public class Logger {
     /** Returns true if the message should be printed in the given timestamp, 
     otherwise returns false. The timestamp is in seconds granularity. */
     public boolean shouldPrintMessage(int timestamp, String message) {
-        for(int t = from; t <= timestamp - 10; t ++) {
-            for(String s : buf[(t+buf.length) % buf.length]) {
+        for(int t = from; t <= timestamp - 10; t++) {
+            for(String s : buf[(t + buf.length) % buf.length]) {
                 set.remove(s);
             }
             buf[(t + buf.length) % buf.length].clear();
