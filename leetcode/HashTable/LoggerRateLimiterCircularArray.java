383c44b45e952467345375d4c116dae366dc1a54
lampardchelsea <zqqsxdd@gmail.com>
2016-09-24 21:25:27 -0500

    Update LoggerRateLimiterCircularArray.java

--- leetcode/HashTable/LoggerRateLimiterCircularArray.java
+++ leetcode/HashTable/LoggerRateLimiterCircularArray.java
@@ -1,20 +1,21 @@
 /**
- *
-*/
-public class Logger {
+ * Best example is 
+ * http://blog.csdn.net/jmspan/article/details/51697104
+ * 
+ * public class Logger {
     private List<String>[] buf = new List[10];
     private Set<String> set = new HashSet<>();
     private int from = 0;
 
-    /** Initialize your data structure here. */
+    // Initialize your data structure here.
     public Logger() {
         for(int i = 0; i < buf.length; i++) { 
           buf[i] = new ArrayList<>();
         }
     }
     
-    /** Returns true if the message should be printed in the given timestamp, 
-    otherwise returns false. The timestamp is in seconds granularity. */
+    // Returns true if the message should be printed in the given timestamp, 
+    // otherwise returns false. The timestamp is in seconds granularity.
     public boolean shouldPrintMessage(int timestamp, String message) {
         for(int t = from; t <= timestamp - 10; t++) {
             for(String s : buf[(t + buf.length) % buf.length]) {
@@ -32,9 +33,5 @@ public class Logger {
         return true;
     }
 }
+*/
 
-/**
- * Your Logger object will be instantiated and called as such:
- * Logger obj = new Logger();
- * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
- */
