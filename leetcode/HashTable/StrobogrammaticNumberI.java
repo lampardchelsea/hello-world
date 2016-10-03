ddfdc5984e2b848f7f4975e6b06108c165d090c5
lampardchelsea <zqqsxdd@gmail.com>
2016-10-03 14:14:46 -0500

    Update StrobogrammaticNumberI.java

--- leetcode/HashTable/StrobogrammaticNumberI.java
+++ leetcode/HashTable/StrobogrammaticNumberI.java
@@ -3,3 +3,10 @@
  * Write a function to determine if a number is strobogrammatic. The number is represented as a string.
  * For example, the numbers "69", "88", and "818" are all strobogrammatic.
 */
+public class Solution {
+    public boolean isStrobogrammatic(String num) {
+
+    
+    
+    }
+}
