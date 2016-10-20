7a27b06d07f718e0a79135e097c4606888340666
lampardchelsea <zqqsxdd@gmail.com>
2016-10-20 03:42:28 -0500

    Update GraphClient.java

--- leetcode/Sort_Search_DataStructure/GraphClient.java
+++ leetcode/Sort_Search_DataStructure/GraphClient.java
@@ -1,1 +1,28 @@
-
+/******************************************************************************
+ *  Compilation:  javac GraphClient.java
+ *  Execution:    java GraphClient graph.txt
+ *  Dependencies: Graph.java
+ *
+ *  Typical graph-processing code.
+ *
+ *  % java GraphClient tinyG.txt
+ *  13 13
+ *  0: 6 2 1 5 
+ *  1: 0 
+ *  2: 0 
+ *  3: 5 4 
+ *  4: 5 6 3 
+ *  5: 3 4 0 
+ *  6: 0 4 
+ *  7: 8 
+ *  8: 7 
+ *  9: 11 10 12 
+ *  10: 9 
+ *  11: 9 12 
+ *  12: 11 9 
+ *
+ *  vertex of maximum degree = 4
+ *  average degree           = 2
+ *  number of self loops     = 0
+ *  
+ ******************************************************************************/
