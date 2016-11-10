cb1c1834f262a6c37ae54b25dc99f9496039816c
lampardchelsea <zqqsxdd@gmail.com>
2016-11-10 15:55:34 -0600

    Create RequiredLengthSubsequence.java

---
+++ leetcode/Sort_Search_DataStructure/RequiredLengthSubsequence.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *  Compilation:  javac Subsequence.java
+ *  Execution:    java Subsequence s k
+ *
+ *  Print out all subsequences of the string s of length k.
+ *
+ *  % java Subsequence abcd 3
+ *  abc
+ *  abd
+ *  acd
+ *  bcd
+ *
+ *  % java Subsequence abcc 3
+ *  abc
+ *  abc
+ *  acc
+ *  bcc
+ *  
+ * Refer to comment5.news.sina.com.cn/cmnt/vote
+ ******************************************************************************/
