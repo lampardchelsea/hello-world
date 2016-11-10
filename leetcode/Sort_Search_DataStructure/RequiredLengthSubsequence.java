51676794c1962e24e5249165c1bdd35d2678b1c7
lampardchelsea <zqqsxdd@gmail.com>
2016-11-10 16:38:35 -0600

    Update RequiredLengthSubsequence.java

--- leetcode/Sort_Search_DataStructure/RequiredLengthSubsequence.java
+++ leetcode/Sort_Search_DataStructure/RequiredLengthSubsequence.java
@@ -3,6 +3,9 @@
  *  Execution:    java Subsequence s k
  *
  *  Print out all subsequences of the string s of length k.
+ *  Subsequences of a string. Write a program Subsequence.java that takes a string 
+ *  command-line argument s and an integer command-line argument k and prints out 
+ *  all subsequences of s of length k.
  *
  *  % java Subsequence abcd 3
  *  abc
