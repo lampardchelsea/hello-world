4f6c383cb2abe7bdc0c59f1f918055a27d718ec6
lampardchelsea <zqqsxdd@gmail.com>
2016-11-11 23:07:21 -0600

    Update BetterWaySumOfLeftLeaves.java

--- leetcode/Sort_Search_DataStructure/BetterWaySumOfLeftLeaves.java
+++ leetcode/Sort_Search_DataStructure/BetterWaySumOfLeftLeaves.java
@@ -29,3 +29,62 @@
  * On the bright side, what would be an infinite loop in an iterative implementation usually becomes a StackOverflowError 
  * in a recursive implementation. A buggy recursive program fails faster.
 */
+public class Solution {
+    public TreeNode root;
+
+    private static class TreeNode {
+       private String val;
+       private TreeNode left, right;
+       public TreeNode(String x) {
+	  this.val = x;
+       }
+     }
+    
+    public static int sumOfLeftLeaves(TreeNode root) {
+       if(root == null) {
+         return 0;
+       } else {
+         return sumOfLeftLeavesRec(root, false);
+       }
+    }
+    
+    public static int sumOfLeftLeavesRec(TreeNode x, boolean isLeft) {
+        if(x == null) {
+          return 0;
+        }
+      
+        int sum = 0;
+        if(x.left == null && x.right == null && isLeft) {
+          sum += x.val;
+        } else {
+          sum += sumOfLeftLeavesRec(x.left, true);
+          sum += sumOfLeftLeavesRec(x.right, false);
+        }
+      
+        return sum;
+    }
+  
+    public static void main(String[] args) {
+	/*
+	 * The tree used for test
+	 *	       1
+	 *		 /   \
+	 *		2     3
+	 *	   / \   /
+	 *	  6   5 9
+	 *	 /
+	 *	8
+	*/
+	Solution s = new Solution();
+	s.root = new TreeNode("1");
+	s.root.left = new TreeNode("2");
+	s.root.right = new TreeNode("3");
+	s.root.left.left = new TreeNode("6");
+	s.root.left.right = new TreeNode("5");
+	s.root.left.left.left = new TreeNode("8");
+	s.root.right.left = new TreeNode("9");
+	
+	int result = sumOfLeftLeaves(s.root);
+	System.out.println(result);
+    }
+}
