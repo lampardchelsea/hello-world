bac44c58a2fa4434292d3a43f31fe69fcb55b1c6
lampardchelsea <zqqsxdd@gmail.com>
2016-11-13 00:55:55 -0600

    Update SumOfLeftLeavesStackSolution.java

--- leetcode/Stack/SumOfLeftLeavesStackSolution.java
+++ leetcode/Stack/SumOfLeftLeavesStackSolution.java
@@ -13,3 +13,43 @@
  * 因为递归的实现就是潜在地使用了栈的思路。这里，我们只需要使用深度优先的方式来遍历节点，并把所有节点放入栈（push）中，
  * 之后再取出（pop）即可。 这里列出别人给出的方法：
 */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null) {
+           return 0;
+        }
+        
+        int sum = 0;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        
+        while(!stack.empty()) {
+           TreeNode x = stack.pop();
+            
+           if(x.left != null) {
+              if(x.left.left == null && x.left.right == null) {
+                  sum += x.left.val;
+              } else {
+                  stack.push(x.left);
+              }
+           } 
+            
+           if(x.right != null) {
+              if(x.right.left != null || x.right.right != null) {
+                  stack.push(x.right);
+              }
+           }
+        }
+        
+        return sum;
+    }
+}
