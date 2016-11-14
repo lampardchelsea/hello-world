ff91721fce5083ce457275c354a844594957bf98
lampardchelsea <zqqsxdd@gmail.com>
2016-11-14 13:19:34 -0600

    Update MinimumDepthofBinaryTreeQueueSolution.java

--- leetcode/Queue/MinimumDepthofBinaryTreeQueueSolution.java
+++ leetcode/Queue/MinimumDepthofBinaryTreeQueueSolution.java
@@ -1,1 +1,62 @@
-
+/**
+ * http://www.cnblogs.com/springfor/p/3879680.html
+ * https://github.com/lampardchelsea/hello-world/blob/master/leetcode/Tree/BinaryTreeLevelOrderTraversal.java
+ * This solution is very similar to Binary Tree Level Order Traversal.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public static int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        
+        Queue<TreeNode> q1 = new LinkedList<TreeNode>();
+        Queue<TreeNode> q2 = new LinkedList<TreeNode>();
+        
+        // The minimum depth is the number of nodes along the shortest path from the 
+        // root node down to the nearest leaf node. "depth" used for record levels(depth)
+        // we have encounter.
+        int depth = 1;
+        q1.add(root);
+        
+        while(!q1.isEmpty()) {
+           // Use for loop to process eech current level node
+           int size = q1.size();
+           for(int i = 0; i < size; i++) {
+               // Poll out one node
+               TreeNode x = q1.poll();
+               
+               // Check it's left child node, if exist, put into next level queue
+               if(x.left != null) {
+                  q2.add(x.left);
+               }
+           
+               // Check it's right child node, if exist, put into next level queue
+               if(x.right != null) {
+                  q2.add(x.right);
+               }
+               
+               // Return case is encountering leaf node, if yes, then return current level depth
+               if(x.left == null && x.right == null) {
+                  return depth;
+               }
+           }
+           
+           // After each level for loop, if not find leaf node to return, then depth + 1 and move
+           // to next level
+           depth++;
+           q1 = q2;
+           q2 = new LinkedList<TreeNode>();
+        }
+        
+        return depth;
+    }
+}
