583f0583cf091a09df8425fe4b2d51d15a0fd4bb
lampardchelsea <zqqsxdd@gmail.com>
2016-11-14 22:16:57 -0600

    Update SymmetricTreeStackSolution.java

--- leetcode/Stack/SymmetricTreeStackSolution.java
+++ leetcode/Stack/SymmetricTreeStackSolution.java
@@ -17,4 +17,58 @@
  *
  * Refer to 
  * http://www.acmerblog.com/leetcode-solution-symmetric-tree-6291.html
+ * https://github.com/lampardchelsea/hello-world/blob/master/leetcode/Queue/SymmetricTreeQueueSolution.java
 */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+   public boolean isSymmetric(TreeNode root) {
+      if(root == null) {
+          return true;
+      } 
+      
+      // This is very similar to the way use queue, queue is pop 2 items
+      // and push 2 items every time, but on same queue, here we separate
+      // these 2 items into two side stacks, every time push and pop for
+      // 1 item at the same time, then compare its value, the compare
+      // conditions are same as using queue.
+      // Refer to 
+      // https://github.com/lampardchelsea/hello-world/blob/master/leetcode/Queue/SymmetricTreeQueueSolution.java
+      Stack<TreeNode> left = new Stack<TreeNode>();
+      Stack<TreeNode> right = new Stack<TreeNode>();
+      
+      left.push(root.left);
+      right.push(root.right);
+      
+      while(left.size() > 0 && right.size() > 0) {
+          TreeNode p = left.pop();
+          TreeNode q = right.pop();
+          
+          if(p == null && q == null) {
+              continue;
+          }
+          
+          if(p != null && q == null || p == null && q != null) {
+              return false;
+          }
+          
+          if(p.val != q.val) {
+              return false;
+          } 
+          
+          left.push(p.left);
+          right.push(q.right);
+          left.push(p.right);
+          right.push(q.left);
+      }
+      
+      return true;
+   }
+}
