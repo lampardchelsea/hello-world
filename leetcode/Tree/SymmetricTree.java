bcf3f2fabb2dd840a1dd75afc5f289148c7e40ea
lampardchelsea <zqqsxdd@gmail.com>
2016-11-14 22:24:20 -0600

    Update SymmetricTree.java

--- leetcode/Tree/SymmetricTree.java
+++ leetcode/Tree/SymmetricTree.java
@@ -16,4 +16,10 @@
    3    3
  * Note:
  * Bonus points if you could solve it both recursively and iteratively.
+ * 
+ * Complexity Analysis
+ * Because we traverse the entire input tree once, the total run time is O(n), where nn is the total number of nodes in the tree.
+ * The number of recursive calls is bound by the height of the tree. In the worst case, 
+ * the tree is linear and the height is in O(n). Therefore, space complexity due to recursive calls on the stack is O(n)
+ * in the worst case.
 */
