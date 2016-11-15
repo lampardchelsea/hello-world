a7aa17bcf3ae972e3c422111ef748b84df4d209b
lampardchelsea <zqqsxdd@gmail.com>
2016-11-14 18:29:48 -0600

    Update SymmetricTreeQueueSolution.java

--- leetcode/Queue/SymmetricTreeQueueSolution.java
+++ leetcode/Queue/SymmetricTreeQueueSolution.java
@@ -14,7 +14,15 @@
    3    3
  * Note:
  * Bonus points if you could solve it both recursively and iteratively.
- *
+ * 
+ * Solution:
+ * Instead of recursion, we can also use iteration with the aid of a queue. Each two consecutive nodes in the 
+ * queue should be equal, and their subtrees a mirror of each other. Initially, the queue contains root and 
+ * root. Then the algorithm works similarly to BFS, with some key differences. Each time, two nodes are extracted 
+ * and their values compared. Then, the right and left children of the two nodes are inserted in the queue in 
+ * opposite order. The algorithm is done when either the queue is empty, or we detect that the tree is not symmetric 
+ * (i.e. we pull out two consecutive nodes from the queue that are unequal).
+ * 
  * Refer to 
  * https://leetcode.com/articles/symmetric-tree/
  * If test with a symmetric tree, 
@@ -68,3 +76,44 @@
        after poll null null
        after poll empty
 */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+   public boolean isSymmetric(TreeNode root) {
+      Queue<TreeNode> q = new LinkedList<TreeNode>();
+      q.add(root);
+      q.add(root);
+      
+      while(!q.isEmpty()) {
+          TreeNode x = q.poll();
+	  TreeNode y = q.poll();
+	      
+	  if(x == null && y == null) {
+	     continue;
+	  }
+	      
+	  if(x != null && y == null || x == null && y != null) {
+	     return false;
+	  }
+	      
+	  if(x.val != y.val) {
+	     return false;
+	  }
+	      
+	  q.add(x.left);
+	  q.add(y.right);
+	  q.add(x.right);
+	  q.add(y.left);
+      }
+	      
+      return true;	
+   }
+}
+
