b56c43318021215e5d786a1feb3f3d31217f8bc7
lampardchelsea <zqqsxdd@gmail.com>
2016-11-19 17:35:25 -0600

    Update WordPattern.java

--- leetcode/HashTable/WordPattern.java
+++ leetcode/HashTable/WordPattern.java
@@ -9,9 +9,42 @@
  * Notes:
  * You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
  */
+// Solution 1:
+// Refer to
+// https://segmentfault.com/a/1190000003827151
+// https://my.oschina.net/Tsybius2014/blog/514983
 public class Solution {
    public boolean wordPattern(String pattern, String str) {
+      String[] a = str.split("\\s+");
       
+      // If pattern length not match string sections array length,
+      // directly return false
+      if(a.length != pattern.length()) {
+         return false;
+      }
+      
+      Map<Character, String> map = new HashMap<Character, String>();
+      for(int i = 0; i < pattern.length(); i++) {
+         // If not contains current key
+         if(!map.containsKey(pattern.charAt(i))) {
+             // But already contains the projection value of this key,
+             // which means the value match another key not relate as
+             // one on one projection. so return false
+             if(map.containsValue(a[i])) {
+                 return false;
+             }
+             // Otherwise put this key-value pair onto map
+             map.put(pattern.charAt(i), a[i]);
+         } else {
+             // If already contains current key
+             // But the key's projection not match the expected key,
+             // so return false
+             if(!map.get(pattern.charAt(i)).equals(a[i])) {
+                 return false;
+             }
+         }
+      }
+      return true;
    }
 }
 
