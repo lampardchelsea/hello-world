8c80edcbb59e9903881ba5f80213f25e99e5c802
lampardchelsea <zqqsxdd@gmail.com>
2016-11-20 21:06:40 -0600

    Update IsomorphicStrings.java

--- leetcode/HashTable/IsomorphicStrings.java
+++ leetcode/HashTable/IsomorphicStrings.java
@@ -9,3 +9,27 @@
  * Given "foo", "bar", return false.
  * Given "paper", "title", return true.
 */
+// Refer to 
+// https://segmentfault.com/a/1190000003827151
+// https://segmentfault.com/a/1190000003709248
+// Totally same way as Word Pattern
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(!map.containsKey(s.charAt(i))) {
+                if(map.containsValue(t.charAt(i))) {
+                    return false;
+                }
+                map.put(s.charAt(i), t.charAt(i));
+            } else {
+                if(!map.get(s.charAt(i)).equals(t.charAt(i))) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
