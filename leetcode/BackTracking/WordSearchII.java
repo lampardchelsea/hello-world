47da14bf335f66fc370e3ac078d3fa2f2916626f
lampardchelsea <zqqsxdd@gmail.com>
2016-11-27 23:22:02 -0600

    Update WordSearchII.java

--- leetcode/BackTracking/WordSearchII.java
+++ leetcode/BackTracking/WordSearchII.java
@@ -21,3 +21,128 @@
  * How about a Trie? If you would like to learn how to implement a basic trie, please work on this problem: 
  * Implement Trie (Prefix Tree) first.
 */
+// Solution 1:
+// Refer to
+// http://www.programcreek.com/2014/06/leetcode-word-search-ii-java/
+public class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+    	int rows = board.length;
+    	int columns = board[0].length;
+    	
+    	boolean[][] visited = new boolean[rows][columns];
+    	
+        Trie trie = new Trie();
+        for(int i = 0; i < words.length; i++) {
+        	trie.insert(words[i]);
+        }
+        
+        List<String> result = new ArrayList<String>();
+        for(int i = 0; i < rows; i++) {
+        	for(int j = 0; j < columns; j++) {
+        		dfs(board, visited, "", trie, i, j, result);
+        	}
+        }
+
+        return result;
+    }
+    
+	public void dfs(char[][] board, boolean[][] visited, String temp, Trie trie, int rowIndex, int columnIndex, List<String> result) {
+		if(rowIndex < 0 || rowIndex >= board.length || columnIndex < 0 || columnIndex >= board[0].length) {
+			return;
+		}
+		
+		if(visited[rowIndex][columnIndex]) {
+			return;
+		}
+		
+		temp += board[rowIndex][columnIndex];
+		
+		if(!trie.startsWith(temp)) {
+			return;
+		} 
+		
+		if(trie.search(temp)) {
+		    if(!result.contains(temp)) {
+		        result.add(temp);   
+		    }
+		}
+		
+		visited[rowIndex][columnIndex] = true;
+		dfs(board, visited, temp, trie, rowIndex + 1, columnIndex, result);
+		dfs(board, visited, temp, trie, rowIndex - 1, columnIndex, result);
+		dfs(board, visited, temp, trie, rowIndex, columnIndex + 1, result);
+		dfs(board, visited, temp, trie, rowIndex, columnIndex - 1, result);
+		visited[rowIndex][columnIndex] = false;
+	}
+}
+
+class TrieNode {
+	static final int R = 26;
+	boolean isLeaf;
+	TrieNode[] next;
+	
+	public TrieNode() {
+		this.isLeaf = false;
+		this.next = new TrieNode[R];
+	}
+}
+
+class Trie {
+	private static final int lower_case_no = 26;
+	private TrieNode root;
+	
+	public Trie() {
+		root = new TrieNode();
+	}
+	
+	public void insert(String word) {
+		put(root, word, 0);
+	}
+	
+	public TrieNode put(TrieNode x, String word, int d) {
+		if(x == null) {
+			x = new TrieNode();
+		}
+		if(d == word.length()) {
+			x.isLeaf = true;
+			return x;
+		}
+		int c = (int)(word.charAt(d) - 'a');
+		x.next[c] = put(x.next[c], word, d + 1);
+		return x;
+	}
+	
+	public boolean search(String word) {
+		if(get(word) == null) {
+			return false;
+		}
+		return get(word).isLeaf;
+	}
+	
+	public TrieNode get(String word) {
+		TrieNode x = get(root, word, 0);
+		if(x == null) {
+			return null;
+		}
+		return x;
+	}
+	
+	public TrieNode get(TrieNode x, String word, int d) {
+		if(x == null) {
+			return null;
+		}
+		if(d == word.length()) {
+			return x;
+		}
+		int c = (int)(word.charAt(d) - 'a');
+		return get(x.next[c], word, d + 1);
+	}
+	
+	public boolean startsWith(String prefix) {
+		TrieNode x = get(root, prefix, 0);
+		if(x != null) {
+			return true;
+		}
+		return false;
+	}
+}
