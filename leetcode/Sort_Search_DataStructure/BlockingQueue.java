6c299dd3c5e73cd256be2caa6dea220054a56e36
lampardchelsea <zqqsxdd@gmail.com>
2016-12-01 01:47:14 -0600

    Update BlockingQueue.java

--- leetcode/Sort_Search_DataStructure/BlockingQueue.java
+++ leetcode/Sort_Search_DataStructure/BlockingQueue.java
@@ -1,3 +1,5 @@
 /**
- * 
+ * Refer to
+ * http://tutorials.jenkov.com/java-util-concurrent/blockingqueue.html
+ * http://webcache.googleusercontent.com/search?q=cache:TzsVcr70HHAJ:blog.csdn.net/chenchaofuck1/article/details/51660119+&cd=4&hl=en&ct=clnk&gl=us
 */
