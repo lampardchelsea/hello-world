d8a8326d8f42aa61de123f5bb365041fe5851285
lampardchelsea <zqqsxdd@gmail.com>
2016-12-04 14:14:07 -0600

    Update DoublyLinkedList.java

--- leetcode/Sort_Search_DataStructure/DoublyLinkedList.java
+++ leetcode/Sort_Search_DataStructure/DoublyLinkedList.java
@@ -69,6 +69,9 @@ public class DoublyLinkedList<Item> implements Iterable<Item> {
 		private Node prev;
 	}
 	
+	// Why pre.next = post; post.prev = pre;
+	// Refer to
+	// http://www.jianshu.com/p/681802a00cdf#
 	public DoublyLinkedList() {
 		pre = new Node();
 		post = new Node();
