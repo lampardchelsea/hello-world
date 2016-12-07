14312cfa34c88bd1ccbcb9bb6e8663a7c7f3bcdc
lampardchelsea <zqqsxdd@gmail.com>
2016-12-07 01:56:38 -0600

    Update BlockingQueue.java

--- leetcode/Sort_Search_DataStructure/BlockingQueue.java
+++ leetcode/Sort_Search_DataStructure/BlockingQueue.java
@@ -2,4 +2,13 @@
  * Refer to
  * http://tutorials.jenkov.com/java-util-concurrent/blockingqueue.html
  * http://webcache.googleusercontent.com/search?q=cache:TzsVcr70HHAJ:blog.csdn.net/chenchaofuck1/article/details/51660119+&cd=4&hl=en&ct=clnk&gl=us
+ *
+ * LinkedBlockingQueue Source Code Analysis
+ * http://www.jianshu.com/p/cc2281b1a6bc
+ *  
+ * ArrayBlockingQueue Source Code Analysis
+ * http://www.jianshu.com/p/9a652250e0d1
+ * 
+ * PriorityBlockingQueue Source Code Analysis
+ * http://webcache.googleusercontent.com/search?q=cache:X8oe7JZrbvoJ:blog.csdn.net/vickyway/article/details/49948701+&cd=1&hl=en&ct=clnk&gl=us
 */
