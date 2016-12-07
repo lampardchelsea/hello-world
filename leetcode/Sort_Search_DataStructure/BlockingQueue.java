09949b5ae29ec8233b1ec0012bd4639d83ceeeaa
lampardchelsea <zqqsxdd@gmail.com>
2016-12-07 01:59:28 -0600

    Update BlockingQueue.java

--- leetcode/Sort_Search_DataStructure/BlockingQueue.java
+++ leetcode/Sort_Search_DataStructure/BlockingQueue.java
@@ -1,6 +1,8 @@
 /**
  * Refer to
  * http://tutorials.jenkov.com/java-util-concurrent/blockingqueue.html
+ *
+ * Java并发编程-阻塞队列(BlockingQueue)的实现原理
  * http://webcache.googleusercontent.com/search?q=cache:TzsVcr70HHAJ:blog.csdn.net/chenchaofuck1/article/details/51660119+&cd=4&hl=en&ct=clnk&gl=us
  *
  * LinkedBlockingQueue Source Code Analysis
@@ -10,6 +12,8 @@
  * http://www.jianshu.com/p/9a652250e0d1
  * 
  * PriorityBlockingQueue Source Code Analysis
+ * JDK并发工具类源码学习系列——PriorityBlockingQueue
  * http://webcache.googleusercontent.com/search?q=cache:X8oe7JZrbvoJ:blog.csdn.net/vickyway/article/details/49948701+&cd=1&hl=en&ct=clnk&gl=us
+ * Java源码阅读之PriorityBlockingQueue
  * http://webcache.googleusercontent.com/search?q=cache:r5GkeRbvr-IJ:blog.csdn.net/evan_man/article/details/50888504+&cd=1&hl=en&ct=clnk&gl=us
 */
