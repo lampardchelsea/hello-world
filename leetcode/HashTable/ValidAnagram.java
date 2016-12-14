1f6f4386d23412e671c25419125cacb896df8351
lampardchelsea <zqqsxdd@gmail.com>
2016-12-14 01:57:19 -0600

    Update ValidAnagram.java

--- leetcode/HashTable/ValidAnagram.java
+++ leetcode/HashTable/ValidAnagram.java
@@ -8,3 +8,35 @@
  * Follow up:
  * What if the inputs contain unicode characters? How would you adapt your solution to such case?
 */
+// Solution 1: 哈希表法
+// 复杂度
+// 时间 O(K) 空间 O(1)
+// 思路
+// 变形词的本质是两个单词中，每个字母出现的次数相同，所以我们可以用一个哈希表，记录第一个单词中每个字母的个数，
+// 再遍历第二个单词，减去相应的字母出现次数，如果某个字母的计数器不为0了，则说明某个字母出现的次数不一样。
+// 这里只用了一个大小为26的数组，是假设只会出现英文字母。
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        int[] table = new int[26];
+        int length = s.length();
+        for(int i = 0; i < length; i++) {
+            int index = s.charAt(i) - 'a';
+            table[index]++;
+        }
+        for(int i = 0; i < length; i++) {
+            int index = t.charAt(i) - 'a';
+            table[index]--;
+        }
+        for(int i = 0; i < table.length; i++) {
+            if(table[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Solution 2:
