8d3f25c0e4b40c266305bc18f6b1368385d1bab5
lampardchelsea <zqqsxdd@gmail.com>
2016-12-16 00:22:08 -0600

    Update FindAllAnagramsInAString.java

--- leetcode/HashTable/FindAllAnagramsInAString.java
+++ leetcode/HashTable/FindAllAnagramsInAString.java
@@ -17,3 +17,44 @@
  * The substring with start index = 1 is "ba", which is an anagram of "ab".
  * The substring with start index = 2 is "ab", which is an anagram of "ab".
 */
+// Solution 1: Time Exceeded
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllAnagramsOfAString {
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<Integer>();
+        for(int i = 0; i <= s.length() - p.length(); i++) {
+        	String temp = s.substring(i, i + p.length());
+        	if(isAnagram(temp, p)) {
+        		result.add(i);
+        	}
+        }
+        return result;
+    }
+	
+    public static boolean isAnagram(String temp, String p) {
+    	char[] a = temp.toCharArray();
+    	char[] b = p.toCharArray();
+    	Arrays.sort(a);
+    	Arrays.sort(b);
+    	for(int i = 0; i < a.length; i++) {
+    		if(a[i] != b[i]) {
+    			return false;
+    		}
+    	}
+    	return true;
+    }
+    
+    
+	public static void main(String[] args) {
+		// s: "cbaebabacd" p: "abc"
+		String s = "cbaebabacd";
+		String p = "abc";
+//		String s = "abab";
+//		String p = "ab";
+		List<Integer> result = findAnagrams(s, p);
+		System.out.println(result);
+	}
+}
