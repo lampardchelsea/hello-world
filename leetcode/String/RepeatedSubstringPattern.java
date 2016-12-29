c6124222f1a9889b26af09b207a85fee99236244
lampardchelsea <zqqsxdd@gmail.com>
2016-12-29 15:07:53 -0600

    Update RepeatedSubstringPattern.java

--- leetcode/String/RepeatedSubstringPattern.java
+++ leetcode/String/RepeatedSubstringPattern.java
@@ -16,3 +16,12 @@
  * Output: True
  * Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
 */
+// Solution 1:
+// Refer to
+// https://discuss.leetcode.com/topic/67992/java-simple-solution-with-explanation
+// http://www.cnblogs.com/grandyang/p/6087347.html
+/**
+ * 这道题给了我们一个字符串，问其是否能拆成n个重复的子串。那么既然能拆分成多个子串，那么每个子串的长度肯定
+ * 不能大于原字符串长度的一半，那么我们可以从原字符串长度的一半遍历到1，如果当前长度能被总长度整除，说明可
+ * 以分成若干个子字符串，我们将这些子字符串拼接起来看跟原字符串是否相等。 如果拆完了都不相等，返回false。
+*/
