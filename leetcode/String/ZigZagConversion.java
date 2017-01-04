1635b7a73ef9363ee8ce4790ac194ccf9c45b5e2
lampardchelsea <zqqsxdd@gmail.com>
2017-01-04 13:21:48 -0600

    Update ZigZagConversion.java

--- leetcode/String/ZigZagConversion.java
+++ leetcode/String/ZigZagConversion.java
@@ -9,3 +9,28 @@
  * string convert(string text, int nRows);
  * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
 */
+public class Solution {
+    public String convert(String s, int numRows) {
+        char[] c = s.toCharArray();
+    	int length = c.length;
+    	StringBuffer[] sb = new StringBuffer[numRows];
+    	for(int i = 0; i < sb.length; i++) {
+    		sb[i] = new StringBuffer();
+    	}
+    	int i = 0;
+    	while(i < length) {
+        	// Vertically down
+        	for(int idx = 0; idx < numRows && i < length; idx++) {
+        		sb[idx].append(c[i++]);
+        	}
+        	// Obliquely up (Minus the first and last row, last is obliquely part)
+        	for(int idx = numRows - 2; idx >= 1 && i < length; idx--) {
+        		sb[idx].append(c[i++]);
+        	}
+    	}
+    	for(int j = 1; j < sb.length; j++) {
+    		sb[0].append(sb[j]);
+    	}
+    	return sb[0].toString();
+    }
+}
