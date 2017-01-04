b4d71628cacd9d59ffdab4894a8b06427ef0dca2
lampardchelsea <zqqsxdd@gmail.com>
2017-01-04 03:35:19 -0600

    Update ReverseString.java

--- leetcode/String/ReverseString.java
+++ leetcode/String/ReverseString.java
@@ -4,6 +4,8 @@
  * Given s = "hello", return "olleh".
 */
 // Solution 1:
+// Refer to
+// https://discuss.leetcode.com/topic/43296/java-simple-and-clean-with-explanations-6-solutions
 public class Solution {
     public String reverseString(String s) {
         char[] word = s.toCharArray();
@@ -17,5 +19,18 @@ public class Solution {
             j--;
         }
         return new String(word);
+    }
+}
+
+// Solution 2: Recursive
+public class Solution {
+    public String reverseString(String s) {
+        int length = s.length();
+        if(length <= 1) {
+          return s;  
+        }
+        String leftString = s.substring(0, length / 2);
+        String rightString = s.substring(length / 2, length);
+        return reverseString(rightString) + reverseString(leftString);
     }
 }
