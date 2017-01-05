f76962dcf4aab9f80a046909ceba28b79f390ab7
lampardchelsea <zqqsxdd@gmail.com>
2017-01-05 03:22:37 -0600

    Update LongestCommonPrefix.java

--- leetcode/String/LongestCommonPrefix.java
+++ leetcode/String/LongestCommonPrefix.java
@@ -1,3 +1,16 @@
 /**
  * Write a function to find the longest common prefix string amongst an array of strings.
 */
+// Solution 1: Binary Search
+// Refer to
+// http://www.geeksforgeeks.org/longest-common-prefix-set-4-binary-search/
+
+
+// Solution 2: Trie
+// Refer to
+// http://www.geeksforgeeks.org/longest-common-prefix-set-5-using-trie/
+
+
+// Solution 3: 
+// Refer to
+// https://discuss.leetcode.com/topic/6987/java-code-with-13-lines/6
