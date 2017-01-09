a0e99bb5095e2fc26ccc83434879561d55586a57
lampardchelsea <zqqsxdd@gmail.com>
2017-01-09 01:46:36 -0600

    Update NumberOfSegmentInAString.java

--- leetcode/String/NumberOfSegmentInAString.java
+++ leetcode/String/NumberOfSegmentInAString.java
@@ -6,3 +6,22 @@
  * Input: "Hello, my name is John"
  * Output: 5
 */
+// Wrong answer
+// If input as "               " continuous space, it will treat as length > 0 branch
+// and return wrong result
+public class Solution {
+    public int countSegments(String s) {
+        char[] chars = s.toCharArray();
+        int length = chars.length;
+        int count = 0;
+        if(length > 0) {
+            count = 1;
+            for(int i = 0; i < length; i++) {
+                if(chars[i] == ' ') {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
