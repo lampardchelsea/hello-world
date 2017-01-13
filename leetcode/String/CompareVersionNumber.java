6ca9c1df0303bc252395507dd3b4d2aef60904df
lampardchelsea <zqqsxdd@gmail.com>
2017-01-12 23:50:04 -0600

    Update CompareVersionNumber.java

--- leetcode/String/CompareVersionNumber.java
+++ leetcode/String/CompareVersionNumber.java
@@ -9,51 +9,52 @@
  * 0.1 < 1.1 < 1.2 < 13.37
 */
 // Wrong Solution:
+
 public class CompareVersionNumbers {
 	public int compareVersion(String version1, String version2) {
         String[] s1 = version1.split("\\.");
         String[] s2 = version2.split("\\.");
         
         if(s1.length == 1 && s2.length == 1) {
-        	if(compare(s1[0], s2[0]) > 0) {
+        	if(compareIntegerPart(s1[0], s2[0]) > 0) {
             	return 1;
-            } else if(compare(s1[0], s2[0]) < 0) {
+            } else if(compareIntegerPart(s1[0], s2[0]) < 0) {
             	return -1;
             } else {
             	return 0;
             }
         } else if(s1.length == 2 && s2.length == 2) {
-        	if(compare(s1[0], s2[0]) > 0) {
+        	if(compareIntegerPart(s1[0], s2[0]) > 0) {
             	return 1;
-            } else if(compare(s1[0], s2[0]) < 0) {
+            } else if(compareIntegerPart(s1[0], s2[0]) < 0) {
             	return -1;
             } else {
-            	if(compare(s1[1], s2[1]) == 0) {
+            	if(compareDecimalPart(s1[1], s2[1]) == 0) {
             		return 0;
-            	} else if(compare(s1[1], s2[1]) > 0) {
+            	} else if(compareDecimalPart(s1[1], s2[1]) > 0) {
             		return 1;
             	} else {
             		return -1;
             	}
             }
         } else if(s1.length == 1 && s2.length == 2) {
-        	if(compare(s1[0], s2[0]) > 0) {
+        	if(compareIntegerPart(s1[0], s2[0]) > 0) {
             	return 1;
-            } else if(compare(s1[0], s2[0]) == 0 && Integer.valueOf(s2[1]) == 0) {
+            } else if(compareIntegerPart(s1[0], s2[0]) == 0 && Integer.valueOf(s2[1]) == 0) {
             	return 0;
             }
         	return -1;
         } else {
-        	if(compare(s1[0], s2[0]) < 0) {
+        	if(compareIntegerPart(s1[0], s2[0]) < 0) {
             	return -1;
-            } else if(compare(s1[0], s2[0]) == 0 && Integer.valueOf(s1[1]) == 0) {
+            } else if(compareIntegerPart(s1[0], s2[0]) == 0 && Integer.valueOf(s1[1]) == 0) {
             	return 0;
             }
         	return 1;
         }
     }
 	
-	public int compare(String s1, String s2) {
+	public int compareIntegerPart(String s1, String s2) {
 		int a = Integer.valueOf(s1);
 		int b = Integer.valueOf(s2);
 		if(a == b) {
@@ -62,6 +63,34 @@ public class CompareVersionNumbers {
 			return 1;
 		} else {
 			return -1;
+		}
+	}
+	
+	public int compareDecimalPart(String s1, String s2) {
+		int s1ZeroAhead = 0;
+		int s2ZeroAhead = 0;
+		for(int i = 0; i < s1.length(); i++) {
+			if(s1.charAt(i) == '0') {
+				s1ZeroAhead++;
+			}
+		}
+		for(int j = 0; j < s2.length(); j++) {
+			if(s2.charAt(j) == '0') {
+				s2ZeroAhead++;
+			}
+		}
+//		while(s1.charAt(s1ZeroAhead) == '0') {
+//			s1ZeroAhead++;
+//		}
+//		while(s2.charAt(s2ZeroAhead) == '0') {
+//			s2ZeroAhead++;
+//		}
+		if(s1ZeroAhead > s2ZeroAhead) {
+			return -1;
+		} else if(s1ZeroAhead < s2ZeroAhead) {
+			return 1;
+		} else {
+			return compareIntegerPart(s1, s2);
 		}
 	}
 	
@@ -79,11 +108,19 @@ public class CompareVersionNumbers {
 //		String version2 = "1";
 		// Test 3: Return 0 is wrong as treat '01' same as '1'
 		// because of compare() method using Integer.valueOf()
-		String version1 = "1.01";
-		String version2 = "1.1";
+//		String version1 = "1.01";
+//		String version2 = "1.1";
+		// Test 4: Test decimal number without zero ahead
+//		String version1 = "1.15";
+//		String version2 = "1.5";
+		// Test 5: Find while loop increase index cause ArrayIndexOutOfBound
+//		String version1 = "1.1";
+//		String version2 = "1.0";
+		// Test 6: Find error on only consider one dot
+		String version1 = "1";
+		String version2 = "1.0.1";
 		CompareVersionNumbers compareVersionNumbers = new CompareVersionNumbers();
 		int result = compareVersionNumbers.compareVersion(version1, version2);
 		System.out.println(result);
 	}
 }
-
