40bfc15352641637b7a1937060223437360c93c1
lampardchelsea <zqqsxdd@gmail.com>
2017-01-31 12:55:00 -0600

    Update HeapSort.java

--- leetcode/Sort_Search_DataStructure/HeapSort.java
+++ leetcode/Sort_Search_DataStructure/HeapSort.java
@@ -12,13 +12,13 @@
  * of the node in position k are in positions 2k and 2k + 1. We can travel up and down by doing simple 
  * arithmetic on array indices: to move up the tree from a[k] we set k to k/2; to move down the tree we 
  * set k to 2*k or 2*k+1.
- * The structure we are using in this solution (Start at array position 1) match this representation
+ * The structure we are using in this solution (Start at array position 1, ignore position 0) match this representation
  * e.g
- * 0 1 2 3 4 5 6 7 8 9 10
- * X 
- *   T S 
- *       P L R A 
- *               M O E E
+ * 0 1 2 3 4 5 6 7 8 9 10 11
+ *   X 
+ *     T S 
+ *         P L R A 
+ *                  M O E E
  * 
  * X is parent of T, S
  * T is parent of P, L  |  S is parent of R, A
