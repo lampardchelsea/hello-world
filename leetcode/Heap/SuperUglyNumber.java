7fa4c79d546a47625c733850acc6b9b02657db86
lampardchelsea <zqqsxdd@gmail.com>
2017-02-11 03:34:55 -0600

    Update SuperUglyNumber.java

--- leetcode/Heap/SuperUglyNumber.java
+++ leetcode/Heap/SuperUglyNumber.java
@@ -1,1 +1,108 @@
-
+/**
+ * Solution 1: Heap
+ * Refer to
+ * https://discuss.leetcode.com/topic/34841/java-three-methods-23ms-36-ms-58ms-with-heap-performance-explained
+ * http://www.cnblogs.com/grandyang/p/5144918.html
+ * http://algs4.cs.princeton.edu/24pq/MinPQ.java.html
+ * 
+ * 这道题让我们求超级丑陋数，是之前那两道Ugly Number 丑陋数和Ugly Number II 丑陋数之二的延伸，质数集合可以任意给定，
+ * 这就增加了难度。但是本质上和Ugly Number II 丑陋数之二没有什么区别，由于我们不知道质数的个数，我们可以用一个idx数组
+ * 来保存当前的位置，然后我们从每个子链中取出一个数，找出其中最小值，然后更新idx数组对应位置，注意有可能最小值不止一个，
+ * 要更新所有最小值的位置
+*/
+public class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        // Each prime number obtains an ugly array, which
+        // constructed by previous ugly number multiple
+        // this prime number, the index_array record the 
+        // changed index on each array
+        int[] index_array = new int[primes.length];
+        // Initialize factor_array
+        int[] factor_array = new int[primes.length];
+        for(int i = 0; i < primes.length; i++) {
+            factor_array[i] = primes[i] * ugly[index_array[i]];
+        }
+        // Start loop to fill the ugly array
+        for(int i = 1; i < n; i++) {
+            int min = min(factor_array);
+            ugly[i] = min;
+            for(int j = 0; j < factor_array.length; j++) {
+                // Update index and factor in corresponding prime 
+                // number ugly array, as matched maybe not only
+                // one, need to update all matched result
+                if(min == factor_array[j]) {
+                    index_array[j] = index_array[j] + 1;
+                    factor_array[j] = primes[j] * ugly[index_array[j]];
+                }
+            }
+        }
+        return ugly[n - 1];
+    }
+    
+    public int min(int[] array) {
+        // Generate a minimum priority queue to sort and find
+        // the minimum item in current array
+        MinPQ minPQ = new MinPQ(array.length);
+        for(int i = 0; i < array.length; i++) {
+            minPQ.insert(array[i]);
+        }
+        return minPQ.delMin();
+    }
+    
+    // Minimum priority queue
+    // Refer to
+    // http://algs4.cs.princeton.edu/24pq/MinPQ.java.html
+    private class MinPQ {
+        int[] pq;
+        int n;
+        public MinPQ(int initialCapacity) {
+            pq = new int[initialCapacity + 1];
+            n = 0;
+        }
+        
+        public void insert(int x) {
+            pq[++n] = x;
+            swim(n);
+        }
+        
+        public int delMin() {
+            int min = pq[1];
+            exch(1, n--);
+            sink(1);
+            return min;
+        }
+        
+        public boolean greater(int v, int w) {
+            return (pq[v] - pq[w]) > 0;
+        }
+        
+        public void sink(int k) {
+            while(k <= n/2) {
+                int j = 2 * k;
+                if(j < n && greater(j, j + 1)) {
+                    j++;
+                }
+                if(!greater(k, j)) {
+                    break;
+                }
+                exch(k, j);
+                k = j;
+            }
+        }
+        
+        public void swim(int k) {
+            while(k > 1 && greater(k/2, k)) {
+                exch(k/2, k);
+                k = k/2;
+            }
+        }
+        
+        public void exch(int v, int w) {
+            int swap = pq[v];
+            pq[v] = pq[w];
+            pq[w] = swap;
+        }
+    }
+}
