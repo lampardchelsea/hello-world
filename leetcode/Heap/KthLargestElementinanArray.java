196d3979bd8acf4dfd1b5d21ddc61ea933242090
lampardchelsea <zqqsxdd@gmail.com>
2017-02-13 14:59:59 -0600

    Update KthLargestElementinanArray.java

--- leetcode/Heap/KthLargestElementinanArray.java
+++ leetcode/Heap/KthLargestElementinanArray.java
@@ -1,12 +1,15 @@
 /**
+ * Refer to
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/
  * Find the kth largest element in an unsorted array. Note that it is the kth largest 
  * element in the sorted order, not the kth distinct element.
    For example,
    Given [3,2,1,5,6,4] and k = 2, return 5.
  * Note: 
- * You may assume k is always valid, 1 ≤ k ≤ array's length.
+ * You may assume k is always valid, 1 ≤ k ≤ array's length
 */
 
+// Soluton 1: Max Heap
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
         int length = nums.length;
@@ -72,3 +75,10 @@ public class Solution {
         }
     }
 }
+
+// Solution 2:
+// Refer to
+// http://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array/
+
+
+
