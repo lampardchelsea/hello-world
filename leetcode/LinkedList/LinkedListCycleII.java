5e7a3a49bbce63d1071be7ccc5cc18939551afa8
lampardchelsea <zqqsxdd@gmail.com>
2017-02-27 17:23:03 -0600

    Update LinkedListCycleII.java

--- leetcode/LinkedList/LinkedListCycleII.java
+++ leetcode/LinkedList/LinkedListCycleII.java
@@ -9,3 +9,53 @@
  * and the first meeting node is m, so...s=k-m, s=nr-m, the 1st pointer starts from beginning of the list while 
  * the 2nd pointer starts from meet point, they will meet in the cycle point but the 2nd pointer walked n times of the cycle
 */
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        // Detect if exist cycle, if not return null
+        if(head == null) {
+            return null;
+        }
+        ListNode walker = head;
+        ListNode runner = head;
+        boolean isCycle = false;
+        while(runner.next != null && runner.next.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if(walker == runner) {
+                isCycle = true;
+                // Break out when find loop, otherwise will
+                // fall in infinite loop
+                break;
+            }
+        }
+        // If there exist cycle, then handle as one pointer at
+        // linklist start(head), one start at meeting place.
+        if(!isCycle) {
+            return null;
+        } else {
+            // As cycle already find, which means now 'walker' and
+            // 'runner' are meeting together, as calculate, reset
+            // 'walker'(either one is fine) back to head, and
+            // keep 'runner' at meeting position, then move forward
+            // 1 step further for both at a time, after they meet
+            // again, the position is the start of cycle
+            walker = head;
+            while(walker != runner) {
+                walker = walker.next;
+                runner = runner.next;
+            }
+        }
+        return walker;
+    }
+}
