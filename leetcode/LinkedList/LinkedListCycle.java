16e25848b3b106d74109049914c467536c9006e3
lampardchelsea <zqqsxdd@gmail.com>
2017-02-26 23:32:52 -0600

    Create LinkedListCycle.java

---
+++ leetcode/LinkedList/LinkedListCycle.java
@@ -0,0 +1,6 @@
+/**
+ * You can make use of Floyd's cycle-finding algorithm, also know as tortoise and hare algorithm. 
+ * The idea is to have two references to the list and move them at different speeds. Move one 
+ * forward by 1 node and the other by 2 nodes. If the linked list has a loop they will definitely meet.
+*/
+
