11312dd191fdd5a134367f481e250a5a3257c47b
lampardchelsea <zqqsxdd@gmail.com>
2017-03-01 01:19:48 -0600

    Update IntersectionofTwoLinkedLists.java

--- leetcode/LinkedList/IntersectionofTwoLinkedLists.java
+++ leetcode/LinkedList/IntersectionofTwoLinkedLists.java
@@ -1,4 +1,22 @@
-
+/**
+ * Refer to
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/?tab=Description
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+	For example, the following two linked lists:
+	
+	A:          a1 → a2
+	                   ↘
+	                     c1 → c2 → c3
+	                   ↗            
+	B:     b1 → b2 → b3
+	begin to intersect at node c1.
+	
+ * Notes:
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function returns.
+ * You may assume there are no cycles anywhere in the entire linked structure.
+ * Your code should preferably run in O(n) time and use only O(1) memory.
+ */
 public class IntersectionOfTwoLinkedLists {
 	 // Definition for singly-linked list.
 	 private class ListNode {
@@ -141,4 +159,3 @@ public class IntersectionOfTwoLinkedLists {
 		System.out.println(result1);
 	}
 }
-
