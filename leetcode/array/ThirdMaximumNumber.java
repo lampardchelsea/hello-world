a2a4ea665bd6a4f966c4f25bf396c4b54c97b040
lampardchelsea <zqqsxdd@gmail.com>
2017-03-18 18:59:32 -0500

    Update ThirdMaximumNumber.java

--- leetcode/array/ThirdMaximumNumber.java
+++ leetcode/array/ThirdMaximumNumber.java
@@ -121,7 +121,7 @@ public class ThirdMaximumNumber {
             pq[w] = temp;
         }
     }
-    
+   
     public static void main(String[] args) {
     	int[] nums = {-2147483648, 1, 1};
     	ThirdMaximumNumber t = new ThirdMaximumNumber();
@@ -129,4 +129,32 @@ public class ThirdMaximumNumber {
     	System.out.println(result);
     }
 }
+
+
+// Refer to
+// https://discuss.leetcode.com/topic/63715/java-neat-and-easy-understand-solution-o-n-time-o-1-space
+public class Solution {
+    public int thirdMax(int[] nums) {
+        Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
+        for(Integer n : nums) {
+            if(n.equals(max1) || n.equals(max2) || n.equals(max3)) {
+                continue;
+            }
+            if(max1 == null || n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if(max2 == null || n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if(max3 == null || n > max3) {
+                max3 = n;
+            }
+        }
+        return max3 == null ? max1 : max3;
+    }
+}
+
 
