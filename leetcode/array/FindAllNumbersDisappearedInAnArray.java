1020f75ade79b5fd2b9407e4473a0feb325099ff
lampardchelsea <zqqsxdd@gmail.com>
2017-03-31 16:52:27 -0500

    Update FindAllNumbersDisappearedInAnArray.java

--- leetcode/array/FindAllNumbersDisappearedInAnArray.java
+++ leetcode/array/FindAllNumbersDisappearedInAnArray.java
@@ -48,13 +48,55 @@ public class FindAllNumbersDisappearedInAnArray {
         return result;
     }
     
+    // Solution 2:
+    // Refer to
+    // https://discuss.leetcode.com/topic/65738/java-accepted-simple-solution/30
+    /**
+     * A more detailed explanation for those who might still be confused:
+     * This solution is using the relation between array index ([0, n-1]) and the given value range [1,n].
+     * Each time when a new value X is read, it changes the corresponding Xth number (value at index X-1) 
+     * into negative, indicating value X is read for the first time.
+     * For example. using the given test case [4,3,2,7,8,2,3,1], when it comes to i = 2 in the first loop, 
+     * this solution marks the 2nd number (index = 1), indicating we've found number 2 for the first time.
+     * When we encounter a redundant number Y, because we've marked the Yth position (index Y -1) when we 
+     * saw Y for the first time, the if clause won't let us flip it again. This leaves the already marked 
+     * Yth number (number at index Y-1) negative.
+     * For example, in the given test case, when i = 5, val = |2| - 1 = 1, nums[1] = -3 < 0. No flip operation 
+     * is needed because we've found value 2 before.
+     * Looping through the 1st loop takes O(n) time, flipping signs won't take extra space.
+     * The second loop checks the signs of the values at indices. If the sign at index P is negative, it means 
+     * value P + 1 is in the array. e.g. nums[0] = -4, so value 0+1 = 1 is in the array. If the value at index 
+     * Q is positive, then value Q + 1 is not in the array. e.g. nums[4] = 8 > 0, value 4 + 1 = 5, we add 5 
+     * into the ret list.
+     */
+    public List<Integer> findDisappearedNumbers2(int[] nums) {
+        List<Integer> result = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i++) {
+        	// Must use abs, otherwise if encounter duplicate values
+        	// which already set to negative value by previous
+        	// operation, will calculate to negative index which is
+        	// absolutely wrong
+            int index =  Math.abs(nums[i]) - 1;
+            //int index =  nums[i] - 1;
+            if(nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+        for(int i = 0; i < nums.length; i++) {
+        	if(nums[i] > 0) {
+        		result.add(i + 1);
+        	}
+        }
+        return result;
+    }
+    
+    
     public static void main(String[] args) {
     	int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
     	FindAllNumbersDisappearedInAnArray f = new FindAllNumbersDisappearedInAnArray();
-    	List<Integer> result = f.findDisappearedNumbers(nums);
+    	List<Integer> result = f.findDisappearedNumbers2(nums);
     	for(Integer i : result) {
     		System.out.print(i + " ");
     	}
     }
 }
-
