6cfe5ce830d88e7c2bbe505befe24866c231eae4
lampardchelsea <zqqsxdd@gmail.com>
2017-04-02 16:29:32 -0500

    Update Triangle.java

--- leetcode/array/Triangle.java
+++ leetcode/array/Triangle.java
@@ -58,3 +58,22 @@
  * 递推公式是： dp[i][j] = dp[i+1][j] + dp[i+1][j+1] ，当前这个点的最小值，由他下面那一行临近的2个点的最小值与当前点的值相加得到。
  * 由于是三角形，且历史数据只在计算最小值时应用一次，所以无需建立二维数组，每次更新1维数组值，最后那个值里存的就是最终结果。
 */
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle.size()==1) {
+            return triangle.get(0).get(0); 
+        }
+        int[] dp = new int[triangle.size()];
+        //initial by last row 
+        for (int i = 0; i < triangle.get(triangle.size() - 1).size(); i++) {
+            dp[i] = triangle.get(triangle.size() - 1).get(i);
+        }
+        // iterate from last second row
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+}
