13aa511508b6cff5e0ba65cfa3b99b1732445495
lampardchelsea <zqqsxdd@gmail.com>
2017-04-16 02:26:45 -0500

    Update RemoveDuplicatesFromSortedArrayII.java

--- leetcode/array/RemoveDuplicatesFromSortedArrayII.java
+++ leetcode/array/RemoveDuplicatesFromSortedArrayII.java
@@ -1,1 +1,18 @@
-
+// Refer to
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/#/description
+/**
+ * Follow up for "Remove Duplicates":
+ * What if duplicates are allowed at most twice?
+ * For example,
+ * Given sorted array nums = [1,1,1,2,2,3],
+ * Your function should return length = 5, with the first five elements of nums being 
+ * 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
+*/
+// Solution
+// Refer to
+// https://discuss.leetcode.com/topic/46519/short-and-simple-java-solution-easy-to-understand
+/**
+ * Question wants us to return the length of new array after removing duplicates and that 
+ * we don't care about what we leave beyond new length , hence we can use i to keep track 
+ * of the position and update the array.
+*/
