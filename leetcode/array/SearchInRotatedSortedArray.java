06354460ce71b1bedda28373480f4166f16a2875
lampardchelsea <zqqsxdd@gmail.com>
2017-04-15 22:16:17 -0500

    Update SearchInRotatedSortedArray.java

--- leetcode/array/SearchInRotatedSortedArray.java
+++ leetcode/array/SearchInRotatedSortedArray.java
@@ -70,6 +70,31 @@ public class Solution {
     }
    另外，判断左半边是否有序的条件是：nums[min] <= nums[mid]，而判断是否在某一个有序区间，则要包含那个区间较远的那一头
 */
-
+public class Solution {
+    public int search(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo)/2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[lo] <= nums[mid]) {
+                if(nums[lo] <= target && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if(nums[mid] < target && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
 
 
