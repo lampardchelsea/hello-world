e216198570b495e8885b7b5825e9a6d14fbb1c6b
lampardchelsea <zqqsxdd@gmail.com>
2017-05-17 04:41:27 -0500

    Update MinimumSizeSubarraySum.java

--- leetcode/array/MinimumSizeSubarraySum.java
+++ leetcode/array/MinimumSizeSubarraySum.java
@@ -1,21 +1,20 @@
 /**
  * Refer to
- * https://leetcode.com/problems/find-all-duplicates-in-an-array/#/description
- * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear 
- * twice and others appear once.
- * Find all the elements that appear twice in this array.
- * Could you do it without extra space and in O(n) runtime?
- * Example:
-	Input:
-	[4,3,2,7,8,2,3,1]
-	
-	Output:
-	[2,3]
+ * https://leetcode.com/problems/minimum-size-subarray-sum/#/description
+ *  Given an array of n positive integers and a positive integer s, find the minimal length 
+ *  of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+ *  For example, given the array [2,3,1,2,4,3] and s = 7,
+ *  the subarray [4,3] has the minimal length under the problem constraint.
+ *  click to show more practice.
+ *  
+ *  More practice:
+ *  If you have figured out the O(n) solution, try coding another solution of which the time 
+ *  complexity is O(n log n).
  * 
  * Solution
  * https://discuss.leetcode.com/topic/18583/accepted-clean-java-o-n-solution-two-pointers
  */
-public class FindAllDuplicatesInAnArray {
+public class MinimumSizeSubarraySum {
     public int minSubArrayLen(int s, int[] nums) {
         int len = nums.length;
         if(len == 0) return 0;
@@ -37,4 +36,3 @@ public class FindAllDuplicatesInAnArray {
     	
     }
 }
-
