838f276950804ee9489e9f32eeb96f8a8c2d9f38
lampardchelsea <zqqsxdd@gmail.com>
2017-05-29 17:53:16 -0500

    Update ReverseStringII.java

--- leetcode/String/ReverseStringII.java
+++ leetcode/String/ReverseStringII.java
@@ -17,6 +17,25 @@
  * https://discuss.leetcode.com/topic/82626/java-concise-solution
  * Using int j = Math.min(i + k - 1, n - 1) and just swap the specific positions of array is 
  * very tricky which can avoid many end conditions determination
+ *    
+   public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int i = 0;
+        while(i < n) {
+            int j = Math.min(i + k - 1, n - 1);
+            swap(arr, i, j);
+            i += 2 * k;
+        }
+        return String.valueOf(arr);
+    }
+    private void swap(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+    }
 */
 
 public class Solution {
