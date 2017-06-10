4f810df64b9311062e08fcb384507139f3e32c74
lampardchelsea <zqqsxdd@gmail.com>
2017-06-10 17:53:22 -0500

    Create StringToInteger.java

--- leetcode/String/StringToInteger.java
+++ leetcode/String/StringToInteger.java
@@ -1,1 +1,55 @@
-
+/**
+ * Refer to
+ * 
+ * Implement atoi to convert a string to an integer.
+ * Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself 
+ * what are the possible input cases.
+ * Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to 
+ * gather all the input requirements up front.
+ * Update (2015-02-10):
+ * The signature of the C++ function had been updated. If you still see your function signature accepts a const 
+ * char * argument, please click the reload button to reset your code definition.
+ * spoilers alert... click to show requirements for atoi.
+ * Requirements for atoi:
+ * The function first discards as many whitespace characters as necessary until the first non-whitespace character 
+ * is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many 
+ * numerical digits as possible, and interprets them as a numerical value.
+ * The string can contain additional characters after those that form the integral number, which are ignored and have 
+ * no effect on the behavior of this function.
+ * If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence 
+ * exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+ * If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of 
+ * representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
+ * 
+ * Solution
+ * https://discuss.leetcode.com/topic/2666/my-simple-solution/14
+ * 
+*/
+public class Solution {
+    public int myAtoi(String str) {
+        // I think we only need to handle four cases:
+        // (1) discards all leading whitespaces
+        // (2) sign of the number
+        // (3) overflow
+        // (4) invalid input
+        int i = 0;
+        int sign = 1;
+        int base = 0;
+        if(str.isEmpty()) {
+           return 0; 
+        }
+        while(str.charAt(i) == ' ') {
+            i++;
+        }
+        if(str.charAt(i) == '-' || str.charAt(i) == '+') {
+            sign = str.charAt(i++) == '-' ? -1 : 1;
+        }
+        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            if(base > Integer.MAX_VALUE / 10 || (base == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > 7)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            base = base * 10 + (str.charAt(i++) - '0');
+        }
+        return base * sign;
+    }
+}
