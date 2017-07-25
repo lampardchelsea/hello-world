55cf11c47e20be031237a8ddafe1cb3816037e9d
lampardchelsea <zqqsxdd@gmail.com>
2017-07-25 11:51:06 -0500

    Update FirstPositionOfTarget.java

--- lintcode/BinarySearch/FirstPositionOfTarget.java
+++ lintcode/BinarySearch/FirstPositionOfTarget.java
@@ -1,1 +1,43 @@
-
+class Solution {
+    /**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearch(int[] nums, int target) {
+        // Check on null and empty case
+        if(nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while(start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) {
+                // Not directly return here, we need to find first happening
+                // of one value in array, always keep removing the second
+                // half of current subset, its tendency is try to find most
+                // previous one even we already find one item matching target
+                // (Since there are duplicates, so even find a matching item
+                // still need continuous process previous section, should
+                // not return directly)
+                end = mid;
+            } else if(nums[mid] < target) {
+                // Use this template should not write as 
+                // 'start = mid + 1' or 'end = mid - 1',
+                // because if we find the 'mid' position
+                // item is the last one matching target,
+                start = mid;
+            } else if(nums[mid] > target) {
+                end = mid;
+            }
+        }
+        if(nums[start] == target) {
+            return start;
+        }
+        if(nums[end] == target) {
+            return end;
+        }
+        return -1;
+    }
+}
