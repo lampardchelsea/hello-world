969570c18fc4b1a5b7bd46e35578bbad79237d88
lampardchelsea <zqqsxdd@gmail.com>
2017-07-26 00:26:42 -0500

    Update SmallestRectangleEnclosingBlackPixels.java

--- lintcode/BinarySearch/VideoExamples/SmallestRectangleEnclosingBlackPixels.java
+++ lintcode/BinarySearch/VideoExamples/SmallestRectangleEnclosingBlackPixels.java
@@ -1,1 +1,25 @@
+/**
+ * An image is represented by a binary matrix with 0 as a white pixel and 1 as a black pixel. 
+ * The black pixels are connected, i.e., there is only one black region. Pixels are connected 
+ * horizontally and vertically. Given the location (x, y) of one of the black pixels, return 
+ * the area of the smallest (axis-aligned) rectangle that encloses all black pixels.
+    For example, given the following image:
+    [
+      "0010",
+      "0110",
+      "0100"
+    ]
+    and x = 0, y = 2,
+    Return 6
+*/
 
+public class Solution {
+    /**
+     * @param image a binary matrix with '0' and '1'
+     * @param x, y the location of one of the black pixels
+     * @return an integer
+     */
+    public int minArea(char[][] image, int x, int y) {
+        
+    }
+}
