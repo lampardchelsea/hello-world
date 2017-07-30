7fba1e679fb92ccfc12d9423fb66d5c846ef4d9f
lampardchelsea <zqqsxdd@gmail.com>
2017-07-29 21:59:28 -0500

    Update MinimumSubtree.java

--- lintcode/BinaryTree__DivideAndConquer/VideoExamples/MinimumSubtree.java
+++ lintcode/BinaryTree__DivideAndConquer/VideoExamples/MinimumSubtree.java
@@ -1,1 +1,18 @@
+/**
+ * Refer to
+ * http://blog.leanote.com/post/westcode/%5B%E5%88%B7%E9%A2%98%E7%AC%94%E8%AE%B0%5D-LeetCode-LintCode-3
+ * Given a binary tree, find the subtree with minimum sum. Return the root of the subtree.
+    Example
+    Given a binary tree:
 
+             1
+           /   \
+         -5     2
+         / \   /  \
+        0   2 -4  -5 
+    return the node 1.
+* 
+* Solution
+* http://www.jiuzhang.com/solutions/minimum-subtree/
+* http://blog.leanote.com/post/westcode/%5B%E5%88%B7%E9%A2%98%E7%AC%94%E8%AE%B0%5D-LeetCode-LintCode-3
+*/
