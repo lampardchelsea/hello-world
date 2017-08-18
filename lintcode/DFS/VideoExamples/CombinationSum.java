21f2079e60ac6d0d6890413f114c34b29ce2d452
lampardchelsea <zqqsxdd@gmail.com>
2017-08-18 01:55:56 -0500

    Update CombinationSum.java

--- lintcode/DFS/VideoExamples/CombinationSum.java
+++ lintcode/DFS/VideoExamples/CombinationSum.java
@@ -1,1 +1,50 @@
-
+/**
+ * Refer to
+ *  https://leetcode.com/problems/combination-sum/#/description
+ *  Given a set of candidate numbers (C) (without duplicates) and a target number (T), 
+ *  find all unique combinations in C where the candidate numbers sums to T.
+ *  
+ *  The same repeated number may be chosen from C unlimited number of times.
+ *  Note:
+    All numbers (including target) will be positive integers.
+    The solution set must not contain duplicate combinations.
+ * 
+ *  For example, given candidate set [2, 3, 6, 7] and target 7,
+ *  A solution set is:
+	[
+	  [7],
+	  [2, 2, 3]
+	]
+*  
+* Solution
+* https://discuss.leetcode.com/topic/46159/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partitioning/2
+*/
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(target < 0 || candidates == null || candidates.length == 0) {
+            return result;
+        }
+        Arrays.sort(candidates);
+        List<Integer> combination = new ArrayList<Integer>();
+        helper(candidates, target, result, combination, 0);
+        return result;
+    }
+    
+    private void helper(int[] candidates, int remain, List<List<Integer>> result, List<Integer> combination, int startIndex) {
+        if(remain < 0) {
+            return;
+        } else if(remain == 0) {
+            result.add(new ArrayList<Integer>(combination));    
+        } else {
+            for(int i = startIndex; i < candidates.length; i++) {
+                combination.add(candidates[i]);
+                // Not i + 1 because we can reuse same elements
+                // If use i + 1, then given [2, 3, 6, 7] and target = 7, will only
+                // have combination as [7], and miss the combination [2, 2, 3]
+                helper(candidates, remain - candidates[i], result, combination, i);
+                combination.remove(combination.size() - 1);
+            }
+        }
+    }
+}
