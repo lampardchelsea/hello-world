64d56195d413b34d31c4c5e300b8a6b102d09d05
lampardchelsea <zqqsxdd@gmail.com>
2017-08-18 04:16:53 -0500

    Update PermutationsII.java

--- lintcode/DFS/VideoExamples/PermutationsII.java
+++ lintcode/DFS/VideoExamples/PermutationsII.java
@@ -1,1 +1,53 @@
+/**
+ * Refer to
+ * https://leetcode.com/problems/permutations-ii/description/
+ * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
 
+    For example,
+    [1,1,2] have the following unique permutations:
+    [
+      [1,1,2],
+      [1,2,1],
+      [2,1,1]
+    ]
+ *
+ * Solution
+ * https://discuss.leetcode.com/topic/46159/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partitioning
+ * http://www.jiuzhang.com/solutions/permutations-ii/
+*/
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(nums == null || nums.length == 0) {
+            return result;
+        }
+        Arrays.sort(nums);
+        List<Integer> combination = new ArrayList<Integer>();
+        helper(nums, result, combination, new boolean[nums.length]);
+        return result;
+    }
+    
+    private void helper(int[] nums, List<List<Integer>> result, List<Integer> combination, boolean[] used) {
+        if(combination.size() == nums.length) {
+            result.add(new ArrayList<Integer>(combination));
+        }
+        for(int i = 0; i < nums.length; i++) {
+            /*
+            判断主要是为了去除重复元素影响。
+            比如，给出一个排好序的数组，[1,2,2]，那么第一个2和第二2如果在结果中互换位置，
+            我们也认为是同一种方案，所以我们强制要求相同的数字，原来排在前面的，在结果
+            当中也应该排在前面，这样就保证了唯一性。所以当前面的2还没有使用的时候，就
+            不应该让后面的2使用。
+            */
+            if(used[i] || (i > 0 && !used[i - 1] && nums[i] == nums[i - 1])) {
+                continue;
+            }
+            used[i] = true;
+            combination.add(nums[i]);
+            helper(nums, result, combination, used);
+            combination.remove(combination.size() - 1);
+            // Don't forget to reset 'used' boolean flag back to false
+            used[i] = false;
+        }        
+    }
+}
