40de5a42013a47f080c1d302688130a4acb85aa2
lampardchelsea <zqqsxdd@gmail.com>
2017-08-27 02:53:25 -0500

    Update QuickSort.java

--- leetcode/Sort_Search_DataStructure/QuickSort.java
+++ leetcode/Sort_Search_DataStructure/QuickSort.java
@@ -4,25 +4,25 @@
  * http://blog.csdn.net/morewindows/article/details/6684558
 */
 public class QuickSort {
-    public static void quickSort(int[] arr){
-        qsort(arr, 0, arr.length-1);
+    public static void quickSort(int[] arr) {
+        qsort(arr, 0, arr.length - 1);
     }
   
-    private static void qsort(int[] arr, int low, int high){
+    private static void qsort(int[] arr, int low, int high) {
         if (low < high){
-            int pivot=partition(arr, low, high);        //将数组分为两部分
-            qsort(arr, low, pivot-1);                   //递归排序左子数组
-            qsort(arr, pivot+1, high);                  //递归排序右子数组
+            int pivot = partition(arr, low, high);        //将数组分为两部分
+            qsort(arr, low, pivot - 1);                   //递归排序左子数组
+            qsort(arr, pivot + 1, high);                  //递归排序右子数组
         }
     }
   
-    private static int partition(int[] arr, int low, int high){
+    private static int partition(int[] arr, int low, int high) {
         int pivot = arr[low];     //枢轴记录
         while(low < high) {
             while(low < high && arr[high] >= pivot) {
                 --high;            
             }
-            arr[low] = arr[high];             //交换比枢轴小的记录到左端
+            arr[low] = arr[high];            //交换比枢轴小的记录到左端
             while(low < high && arr[low] <= pivot) {
                 ++low;           
             }
