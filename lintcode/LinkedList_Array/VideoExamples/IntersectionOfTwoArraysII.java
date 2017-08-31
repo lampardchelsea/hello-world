70f101774229cbe39d3f486cb7ec21ff002e07a9
lampardchelsea <zqqsxdd@gmail.com>
2017-08-31 11:13:33 -0500

    Update IntersectionOfTwoArraysII.java

--- lintcode/LinkedList_Array/VideoExamples/IntersectionOfTwoArraysII.java
+++ lintcode/LinkedList_Array/VideoExamples/IntersectionOfTwoArraysII.java
@@ -1,1 +1,39 @@
-
+/**
+ * Refer to
+ * http://www.lintcode.com/en/problem/intersection-of-two-arrays-ii/
+ * 
+ * Solution
+ * http://www.jiuzhang.com/solutions/intersection-of-two-arrays-ii/
+*/
+public class Solution {
+    /**
+     * @param nums1 an integer array
+     * @param nums2 an integer array
+     * @return an integer array
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int a : nums1) {
+            if(!map.containsKey(a)) {
+                map.put(a, 1);
+            } else {
+                map.put(a, map.get(a) + 1);
+            }
+        }
+        
+        List<Integer> list = new ArrayList<Integer>();
+        for(int b : nums2) {
+            if(map.containsKey(b) && map.get(b) > 0) {
+                list.add(b);
+                map.put(b, map.get(b) - 1);
+            }
+        }
+        
+        int[] result = new int[list.size()];
+        int i = 0;
+        for(int c : list) {
+            result[i++] = c;
+        }
+        return result;
+    }
+}
