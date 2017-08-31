c8206fe6bba53e09d46cbc9aedb9d0c247a15564
lampardchelsea <zqqsxdd@gmail.com>
2017-08-31 13:02:09 -0500

    Update IntersectionOfTwoArraysII.java

--- lintcode/LinkedList_Array/VideoExamples/IntersectionOfTwoArraysII.java
+++ lintcode/LinkedList_Array/VideoExamples/IntersectionOfTwoArraysII.java
@@ -11,14 +11,48 @@
     Follow up:
     What if the given array is already sorted? How would you optimize your algorithm?
     What if nums1's size is small compared to nums2's size? Which algorithm is better?
-    What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once? 
- *
- *
+    What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements 
+    into the memory at once? 
  *
  * Solution
  * http://www.jiuzhang.com/solutions/intersection-of-two-arrays-ii/
  * https://discuss.leetcode.com/topic/45992/solution-to-3rd-follow-up-question
- * 
+ * https://discuss.leetcode.com/topic/68883/solution-with-three-follow-ups
+ * Compared to 349. Intersection of Two Arrays, which uses a hash to flag if element exists, 
+   we turn to use a hash to count how many elements exist.
+
+      def intersect(nums1, nums2)
+        hash = nums1.reduce(Hash.new(0)) {|ha, num| ha[num] += 1; ha }
+
+        nums2.reduce([]) do |ar, num|
+          if hash[num] > 0
+            hash[num] -= 1
+            ar << num
+          else
+            ar
+          end
+        end
+      end
+    
+    Q. What if the given array is already sorted? How would you optimize your algorithm?
+       If both arrays are sorted, I would use two pointers to iterate, which somehow resembles the merge process in merge sort.
+    
+    Q. What if nums1's size is small compared to nums2's size? Which algorithm is better?
+       Suppose lengths of two arrays are N and M, the time complexity of my solution is O(N+M) and the space 
+       complexity if O(N) considering the hash. So it's better to use the smaller array to construct the counter hash.
+       Well, as we are calculating the intersection of two arrays, the order of array doesn't matter. 
+       We are totally free to swap to arrays.
+
+    Q. What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into 
+       the memory at once?
+       Divide and conquer. Repeat the process frequently: Slice nums2 to fit into memory, process (calculate intersections), 
+       and write partial results to memory.
+       
+       Another version
+       If only nums2 cannot fit in memory, put all elements of nums1 into a HashMap, read chunks of array that fit into the 
+       memory, and record the intersections.
+       If both nums1 and nums2 are so huge that neither fit into the memory, sort them individually (external sort), then 
+       read 2 elements from each array at a time in memory, record intersections.
  * 
 */
 public class Solution {
