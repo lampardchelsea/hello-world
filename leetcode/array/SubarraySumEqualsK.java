a70a885b9cc502608d8a49b9c67887cfa7dfb80c
lampardchelsea <zqqsxdd@gmail.com>
2017-09-02 03:58:09 -0500

    Update SubarraySumEqualsK.java

--- leetcode/array/SubarraySumEqualsK.java
+++ leetcode/array/SubarraySumEqualsK.java
@@ -86,3 +86,40 @@ class Solution {
         return count;
     }
 }
+
+// Solution 3: HashMap
+/**
+ * Approach #4 Using hashmap [Accepted]
+   Algorithm
+   The idea behind this approach is as follows: If the cumulative sum(repreesnted by sum[i] for sum upto ith index) 
+   upto two indices is the same, the sum of the elements lying in between those indices is zero. Extending the same 
+   thought further, if the cumulative sum upto two indices, say i and j is at a difference of k 
+   i.e. if sum[i] - sum[j] = k, the sum of elements lying between indices i and j is k.
+   Based on these thoughts, we make use of a hashmap map which is used to store the cumulative sum upto all the 
+   indices possible along with the number of times the same sum occurs. We store the data in the form: 
+   (sum_i, no. of occurences of sum_i) We traverse over the array nums and keep on finding the cumulative sum. 
+   Every time we encounter a new sum, we make a new entry in the hashmap corresponding to that sum. If the same 
+   sum occurs again, we increment the count corresponding to that sum in the hashmap. 
+   Further, for every sum encountered, we also determine the number of times the sum sum-k has occured already, 
+   since it will determine the number of times a subarray with sum k has occured upto the current index. 
+   We increment the countcount by the same amount.
+   After the complete array has been traversed, the count gives the required result.
+*/
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0, 1);
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if(map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
+
+
