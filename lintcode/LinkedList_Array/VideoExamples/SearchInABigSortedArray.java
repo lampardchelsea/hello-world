7c1c4c4a23d028f6504a0920377ae235f5461f68
lampardchelsea <zqqsxdd@gmail.com>
2017-09-02 12:01:25 -0500

    Update SearchInABigSortedArray.java

--- lintcode/LinkedList_Array/VideoExamples/SearchInABigSortedArray.java
+++ lintcode/LinkedList_Array/VideoExamples/SearchInABigSortedArray.java
@@ -30,3 +30,43 @@
  * 
  * http://xuyiruan.com/2016/06/28/Search-in-a-Big-Sorted-Array/
 */
+/** 
+ * Definition of ArrayReader: 
+ *  
+ * class ArrayReader { 
+ *      // get the number at index, return -1 if index is less than zero. 
+ *      public int get(int index); 
+ * } 
+ */ 
+public class Solution { 
+    /** 
+     * @param reader: An instance of ArrayReader. 
+     * @param target: An integer 
+     * @return : An integer which is the index of the target number 
+     */ 
+    public int searchBigSortedArray(ArrayReader reader, int target) { 
+        int index = 1;
+        while(reader.get(index - 1) <= target) {
+		    index = index * 2;
+		}
+		int start = 0;
+		int end = index;
+		while(start + 1 < end) {
+		    int mid = start + (end - start) / 2;
+			if(reader.get(mid) == target) {
+			   end = mid;
+			} else if(reader.get(mid) > target) {
+			   end = mid;
+			} else {
+			   start = mid;
+			}
+		}
+		if(reader.get(start) == target) {
+		    return start;
+		}
+		if(reader.get(end) == target) {
+		    return end;
+		}
+		return -1;
+    } 
+} 
