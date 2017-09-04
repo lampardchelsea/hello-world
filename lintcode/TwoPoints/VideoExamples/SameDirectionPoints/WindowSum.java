8dd75e9594e4af2b3286e8ccc6fffe1548043bf4
lampardchelsea <zqqsxdd@gmail.com>
2017-09-03 23:31:18 -0500

    Update WindowSum.java

--- lintcode/TwoPoints/VideoExamples/SameDirectionPoints/WindowSum.java
+++ lintcode/TwoPoints/VideoExamples/SameDirectionPoints/WindowSum.java
@@ -1,1 +1,17 @@
-
+/**
+ * Refer to
+ * https://yeqiuquan.blogspot.com/2017/03/lintcode-604-window-sum.html
+ * Description
+   Given an array of n integer, and a moving window(size k), move the window at each iteration 
+   from the start of the array, find the sum of the element inside the window at each moving.
+   
+    Example
+    For array [1,2,7,8,5], moving window size k = 3.
+    1 + 2 + 7 = 10
+    2 + 7 + 8 = 17
+    7 + 8 + 5 = 20
+    return [10,17,20]
+ * 
+ * Solution
+ * https://yeqiuquan.blogspot.com/2017/03/lintcode-604-window-sum.html
+*/
