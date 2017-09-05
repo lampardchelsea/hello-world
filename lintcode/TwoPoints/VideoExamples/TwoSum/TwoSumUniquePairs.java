cd776a56bc207930ad192daee6f24dd12bc2231b
lampardchelsea <zqqsxdd@gmail.com>
2017-09-05 03:45:41 -0500

    Update TwoSumUniquePairs.java

--- lintcode/TwoPoints/VideoExamples/TwoSum/TwoSumUniquePairs.java
+++ lintcode/TwoPoints/VideoExamples/TwoSum/TwoSumUniquePairs.java
@@ -14,3 +14,40 @@
  * http://www.jiuzhang.com/solutions/two-sum-unique-pairs
  * 
 */
+public class TwoSum {
+    public int twoSumUniquePairs(int[] nums, int target) {
+    	if(nums == null || nums.length < 2) {
+            return 0;
+        }
+    	Arrays.sort(nums);
+    	int start = 0;
+    	int end = nums.length - 1;
+    	int count = 0;
+    	while(start < end) {
+    		if(nums[start] + nums[end] == target) {
+    		    start++;
+    		    end--;
+    		    count++;
+    		    while(start < end && nums[start] == nums[start - 1]) {
+    		    	start++;
+    		    }
+    		    while(start < end && nums[end] == nums[end + 1]) {
+    		    	end--;
+    		    }
+    		} else if(nums[start] + nums[end] < target) {
+    			start++;
+    		} else {
+    			end--;
+    		}
+    	}
+    	return count;
+    }
+   
+    public static void main(String[] args) {
+    	TwoSum t = new TwoSum();
+    	int[] numbers = {1,1,2,45,45,46};
+    	int target = 47;
+    	int result = t.twoSumUniquePairs(numbers, target);
+    	System.out.println(result);
+    }
+}
