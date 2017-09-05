13b1296e2937db3a1a4ce17abe29f033d2227e53
lampardchelsea <zqqsxdd@gmail.com>
2017-09-04 20:35:07 -0500

    Update TwoSum.java

--- lintcode/TwoPoints/VideoExamples/TwoSum/TwoSum.java
+++ lintcode/TwoPoints/VideoExamples/TwoSum/TwoSum.java
@@ -1,9 +1,22 @@
 /**
  * Refer to
- *
- * 
+ * http://www.lintcode.com/en/problem/two-sum/
+ * Given an array of integers, find two numbers such that they add up to a specific target number.
+
+   The function twoSum should return indices of the two numbers such that they add up to the target, 
+   where index1 must be less than index2. Please note that your returned answers (both index1 and index2) 
+   are NOT zero-based.
+   
+    Notice
+    You may assume that each input would have exactly one solution
+    Have you met this question in a real interview? Yes
+    Example
+    numbers=[2, 7, 11, 15], target=9
+
+    return [1, 2]
+
  * Solution
- * 
+ * https://discuss.leetcode.com/topic/2447/accepted-java-o-n-solution/3?page=1
  * 
 */
 public class Solution {
