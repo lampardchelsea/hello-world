3797894ce5e3f55c7e52a3912ce42cafddefd983
lampardchelsea <zqqsxdd@gmail.com>
2017-09-06 01:43:53 -0500

    Update TwoSumDifferenceEqualsToTarget.java

--- lintcode/TwoPoints/VideoExamples/TwoSum/TwoSumDifferenceEqualsToTarget.java
+++ lintcode/TwoPoints/VideoExamples/TwoSum/TwoSumDifferenceEqualsToTarget.java
@@ -1,1 +1,29 @@
+/**
+ * Refer to
+ * https://yeqiuquan.blogspot.com/2017/04/lintcode-610-two-sum-difference-equals.html
+ * Description
+   Given an array of integers, find two numbers that their difference equals to a target value.
+   where index1 must be less than index2. Please note that your returned answers 
+   (both index1 and index2) are NOT zero-based.
+   Notice
+   It's guaranteed there is only one available solution
 
+   Example
+   Given nums = [2, 7, 15, 24], target = 5
+   return [1, 2] (7 - 2 = 5)
+ *
+ * Solution
+ * https://yeqiuquan.blogspot.com/2017/04/lintcode-610-two-sum-difference-equals.html
+ * 思路
+ * 排序以后两个指针同时从左往右走。
+*/
+public class Solution {
+    /*
+     * @param nums an array of Integer
+     * @param target an integer
+     * @return [index1 + 1, index2 + 1] (index1 < index2)
+     */
+    public int[] twoSum7(int[] nums, int target) {
+    
+    }
+}
