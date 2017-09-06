501cb40e3a25cd8c3b550c70aa780fe3093d7920
lampardchelsea <zqqsxdd@gmail.com>
2017-09-06 04:01:27 -0500

    Update TwoSumDifferenceEqualsToTarget.java

--- lintcode/TwoPoints/VideoExamples/TwoSum/TwoSumDifferenceEqualsToTarget.java
+++ lintcode/TwoPoints/VideoExamples/TwoSum/TwoSumDifferenceEqualsToTarget.java
@@ -23,7 +23,50 @@ public class Solution {
      * @param target an integer
      * @return [index1 + 1, index2 + 1] (index1 < index2)
      */
+    private class Pair {
+    	int index;
+    	int value;
+    	public Pair(int index, int value) {
+    		this.index = index;
+    		this.value = value;
+    	}
+    }
+	
     public int[] twoSumDifferenceEqualsToTarget(int[] nums, int target) {
-    
+        if(nums == null || nums.length == 0) {
+        	return new int[0];
+        }
+        int[] result = new int[2];
+        // Because we need to return original nums indexes, cannot sort
+        // on original nums as that will change the original order, but
+        // still need to sort nums based on value, so we need to create
+        // private class Pair to implement just sort value and keep its
+        // original index
+        Pair[] pairs = new Pair[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+        	pairs[i] = new Pair(i, nums[i]);
+        }
+        // Sort based on value, no change on original index
+        Arrays.sort(pairs, new Comparator<Pair>() {
+			@Override
+			public int compare(Pair x, Pair y) {
+				return x.value - y.value;
+			}
+        });
+        int j = 0;
+        for(int i = 0; i < pairs.length; i++) {
+        	if(j == i) {
+        		j++;
+        	}
+        	while(j < pairs.length && pairs[j].value - pairs[i].value < target) {
+        		j++;
+        	}
+        	if(j < pairs.length && pairs[j].value - pairs[i].value == target) {
+        		result[0] = Math.min(pairs[i].index, pairs[j].index) + 1;
+        		result[1] = Math.max(pairs[i].index, pairs[j].index) + 1;
+        		return result;
+        	}
+        }
+        return result;
     }
 }
