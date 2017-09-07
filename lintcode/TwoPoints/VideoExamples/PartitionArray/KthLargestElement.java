603be03434539dea6eda868a98beff107937b3cc
lampardchelsea <zqqsxdd@gmail.com>
2017-09-07 14:04:46 -0500

    Update KthLargestElement.java

--- lintcode/TwoPoints/VideoExamples/PartitionArray/KthLargestElement.java
+++ lintcode/TwoPoints/VideoExamples/PartitionArray/KthLargestElement.java
@@ -1,7 +1,14 @@
 /**
  * Refer to
- *
- *
+ * http://www.lintcode.com/en/problem/kth-largest-element/
+ * Find K-th largest element in an array.
+    Notice
+    You can swap elements in the array
+
+    Have you met this question in a real interview? Yes
+    Example
+    In array [9,3,2,4,8], the 3rd largest element is 4.
+    In array [1,2,3,4,5], the 1st largest element is 5, 2nd largest element is 4, 3rd largest element is 3 and etc.
  *
  * Solution
  * http://www.jiuzhang.com/solution/kth-largest-element
