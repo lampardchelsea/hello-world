709b36c35b0c6005b275a481743ccf4dfb454fa3
lampardchelsea <zqqsxdd@gmail.com>
2017-09-07 02:59:54 -0500

    Update KthSmallestNumbersInUnsortedArray.java

--- lintcode/TwoPoints/VideoExamples/PartitionArray/KthSmallestNumbersInUnsortedArray.java
+++ lintcode/TwoPoints/VideoExamples/PartitionArray/KthSmallestNumbersInUnsortedArray.java
@@ -1,1 +1,26 @@
+/**
+ * Refer to
+ * https://yeqiuquan.blogspot.com/2017/03/lintcode-461-kth-smallest-numbers-in.html
+ * Description
+    Find the kth smallest numbers in an unsorted integer array.
 
+    Example
+    Given [3, 4, 1, 2, 5], k = 3, the 3rd smallest numbers are [1, 2, 3].
+
+    Challenge 
+    An O(nlogn) algorithm is acceptable, if you can do it in O(n), that would be great.
+ *
+ * Solution
+ * https://www.jiuzhang.com/solutions/kth-smallest-numbers-in-unsorted-array/
+ * http://blog.csdn.net/sinat_32547403/article/details/55259566
+*/
+class Solution {
+    /*
+     * @param k an integer
+     * @param nums an integer array
+     * @return kth smallest element
+     */
+    public int kthSmallest(int k, int[] nums) {
+    
+    }
+}
