ceeb6678fb17be1f327cb56d558d4dcd1a8e1c26
lampardchelsea <zqqsxdd@gmail.com>
2017-09-06 21:01:23 -0500

    Update BubbleSort_JiuZhang.java

--- lintcode/TwoPoints/VideoExamples/PartitionArray/BubbleSort_JiuZhang.java
+++ lintcode/TwoPoints/VideoExamples/PartitionArray/BubbleSort_JiuZhang.java
@@ -1,1 +1,33 @@
+/**
+ * Refer to
+ * http://www.lintcode.com/en/problem/sort-integers/
+ * Given an integer array, sort it in ascending order. Use selection sort, bubble sort, 
+   insertion sort or any O(n2) algorithm.
 
+    Have you met this question in a real interview? Yes
+    Example
+    Given [3, 2, 1, 4, 5], return [1, 2, 3, 4, 5].
+ *
+ * Solution
+ * http://www.jiuzhang.com/solution/sort-integers/
+*/
+public class Solution {
+    /*
+     * @param A: an integer array
+     * @return: 
+     */
+    public void sortIntegers(int[] A) {
+        if(A == null || A.length == 0) {
+            return;
+        }
+        for(int i = 0; i < A.length; i++) {
+            for(int j = i + 1; j < A.length; j++) {
+                if(A[j] < A[i]) {
+                    int temp = A[j];
+                    A[j] = A[i];
+                    A[i] = temp;
+                }
+            }
+        }
+    }
+}
