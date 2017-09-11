66e85def2e9aba1a1e1e73fd138650f66481cffd
lampardchelsea <zqqsxdd@gmail.com>
2017-09-11 15:38:20 -0500

    Update TopKLaregstNumber.java

--- lintcode/DataStructure/VideoExamples/Heap/TopKLaregstNumber.java
+++ lintcode/DataStructure/VideoExamples/Heap/TopKLaregstNumber.java
@@ -1,1 +1,37 @@
+/**
+ * Refer to
+ * https://aaronice.gitbooks.io/lintcode/content/data_structure/top_k_largest_numbers.html
+ * Question
 
+  Given an integer array, find the top k largest numbers in it.
+  Example
+  Given [3,10,1000,-99,4,100] and k = 3. Return [1000, 100, 10].
+ *
+ *
+ * Solution
+ * https://aaronice.gitbooks.io/lintcode/content/data_structure/top_k_largest_numbers.html
+*/
+// Solution 1: Max Heap
+class Solution {
+    /*
+     * @param nums an integer array
+     * @param k an integer
+     * @return the top k largest numbers in array
+     */
+     public int[] topk(int[] nums, int k) {
+     
+     }
+}
+
+
+// Solution 2: Min Heap
+class Solution {
+    /*
+     * @param nums an integer array
+     * @param k an integer
+     * @return the top k largest numbers in array
+     */
+    public int[] topk(int[] nums, int k) {
+    
+    }
+}
