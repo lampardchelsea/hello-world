8ef4e45cbe163a489ee514c5025aa5e97be95e94
lampardchelsea <zqqsxdd@gmail.com>
2017-09-11 16:31:37 -0500

    Update UglyNumberII.java

--- lintcode/DataStructure/VideoExamples/Heap/UglyNumberII.java
+++ lintcode/DataStructure/VideoExamples/Heap/UglyNumberII.java
@@ -15,7 +15,7 @@
  * Solution
  * http://www.jiuzhang.com/solutions/ugly-number-ii/
 */
-// Solution 1: Scan
+// Solution 1: Scan (O(n) time)
 public class Solution {
     /*
      * @param n: An integer
@@ -47,3 +47,43 @@ public class Solution {
     }
 }
 
+
+// Solution 2: HashSet + PriorityQueue (O(nlogn) time)
+public class Solution {
+    /**
+	 * For using Long instead of Integer, because in solution 2 is little different
+	 * than solution 1, the 'num * primes[j]' in solution 2 will over Integer boundary
+	 * in big n case, e.g when n = 1407, similar but different in solution 1 is not
+	 * same case, because this 'num' in solution 2 is exactly same one that will 
+	 * multiple 2/3/5, but in solution 1, this 'num' is not exactly same one, e.g
+	 * because limitation of 'num' = result.get(p2/p3/p5) * primes[j] <= lastNum', here,
+	 * p2/p3/p5 may different, and cause 'num' also different 
+	 */
+	public int nthUglyNumber3(int n) {
+        Queue<Long> queue = new PriorityQueue<Long>();
+        // Set is necessary to filter duplicate numbers
+        // e.g given n = 9, expected num = 10, but return 9,
+        //     because duplicate 6 adding onto queue based
+        //     on first time 2 * 3 and later 3 * 2
+        Set<Long> set = new HashSet<Long>();
+        int[] primes = new int[3];
+        primes[0] = 2;
+        primes[1] = 3;
+        primes[2] = 5;
+        for(int i = 0; i < 3; i++) {
+            queue.offer(Long.valueOf(primes[i]));
+            set.add(Long.valueOf(primes[i]));
+        }
+        Long num = 1L;
+        for(int i = 1; i < n; i++) {
+            num = queue.poll();
+            for(int j = 0; j < 3; j++) {
+                if(!set.contains(num * primes[j])) {
+                    queue.offer(num * primes[j]);
+                    set.add(num * primes[j]);    
+                }
+            }
+        }
+        return num.intValue();
+    }
+}
