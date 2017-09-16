29e373edab3fed71ed0b0853eb994593f599b551
lampardchelsea <zqqsxdd@gmail.com>
2017-09-16 18:52:02 -0500

    Update ExpressionExpand_DFS.java

--- lintcode/DFS/VideoExamples/ExpressionExpand_DFS.java
+++ lintcode/DFS/VideoExamples/ExpressionExpand_DFS.java
@@ -16,7 +16,48 @@
 // Base on definition here we implement Traversal + Divide and Conquer way together on this problem as
 // we use native method (Divide and Conquer) and return void (Traversal), also set i as global variable
 // to control the loop condition (Traversal), they are both DFS recursive way
-
+public class Solution {
+    /*
+     * @param s: an expression includes numbers, letters and brackets
+     * @return: a string
+     */
+    int i = 0;
+    public String expressionExpand(String s) {
+        if(s == null || s.length() == 0) {
+            return "";
+        }
+        // Initial global StringBuilder to record each recursion temparay
+        // result as DFS Traversal way
+        StringBuilder sb = new StringBuilder();
+        while(i < s.length()) {
+            if(Character.isDigit(s.charAt(i))) {
+                int start = i;
+                // Loop until character not numeric number, based
+                // on problem description this character is '[',
+                // need to skip
+                while(Character.isDigit(s.charAt(i))) {
+                    i++;
+                }
+                int val = Integer.parseInt(s.substring(start, i));
+                // Skip '[' just after numeric value
+                i++;
+                // DFS to get nested substring in current '[]'
+                String nested = expressionExpand(s);
+                while(val-- > 0) {
+                    sb.append(nested);
+                } 
+            } else if(Character.isLetter(s.charAt(i))) {
+                sb.append(s.charAt(i));
+                i++;
+            } else if(s.charAt(i) == ']') {
+                // Skip ']'
+                i++;
+                return sb.toString();
+            }
+        }
+        return sb.toString();
+    }
+}
 
 
 
