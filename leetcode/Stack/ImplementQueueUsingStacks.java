e46be29a21c50283e0cfa123cb66b09da338b9e2
lampardchelsea <zqqsxdd@gmail.com>
2017-09-16 00:30:03 -0500

    Update ImplementQueueUsingStacks.java

--- leetcode/Stack/ImplementQueueUsingStacks.java
+++ leetcode/Stack/ImplementQueueUsingStacks.java
@@ -16,8 +16,16 @@
     called on an empty queue).
 
  *
- * Solution
+ * Solution (Time Complexity O(1))
  * https://discuss.leetcode.com/topic/17974/short-o-1-amortized-c-java-ruby/2
+ * I have one input stack, onto which I push the incoming elements, and one output stack, 
+   from which I peek/pop. I move elements from input stack to output stack when needed, 
+   i.e., when I need to peek/pop but the output stack is empty. When that happens, I move 
+   all elements from input to output stack, thereby reversing the order so it's the correct 
+   order for peek/pop.
+   The loop in peek does the moving from input to output stack. Each element only ever gets 
+   moved like that once, though, and only after we already spent time pushing it, so the 
+   overall amortized cost for each operation is O(1).
 */
 class MyQueue {
     Stack<Integer> input;
