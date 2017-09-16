f58e16affd177a9813fa518767eed1ae5322f34f
lampardchelsea <zqqsxdd@gmail.com>
2017-09-16 15:50:30 -0500

    Update ExpressionExpand_DFS.java

--- lintcode/DFS/VideoExamples/ExpressionExpand_DFS.java
+++ lintcode/DFS/VideoExamples/ExpressionExpand_DFS.java
@@ -1,1 +1,12 @@
-
+/**
+ * Refer to
+ * http://www.lintcode.com/en/problem/expression-expand/
+ * https://leetcode.com/problems/decode-string/description/
+ *
+ * Solution
+ * Based on definition here we implement Traversal + Divide and Conquer way together on this problem as
+ * we create new helper method (Traversal) and return value not void (Divide and Conquer), they are both
+ * DFS recursive way
+ * https://github.com/lampardchelsea/hello-world/tree/master/lintcode/BinaryTree__DivideAndConquer/VideoExamples
+ * http://www.cnblogs.com/lishiblog/p/5874147.html
+*/
