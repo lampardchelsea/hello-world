5218927630c888fe0c747ba011f015f904684657
lampardchelsea <zqqsxdd@gmail.com>
2017-09-16 19:30:09 -0500

    Update ExpressionExpand.java

--- lintcode/DataStructure/VideoExamples/Stack/ExpressionExpand.java
+++ lintcode/DataStructure/VideoExamples/Stack/ExpressionExpand.java
@@ -1,1 +1,13 @@
-
+/**
+ * Refer to
+ * http://www.lintcode.com/en/problem/expression-expand/
+ * https://leetcode.com/problems/decode-string/description/
+ *
+ * Solution
+ * Style 1
+ * http://www.cnblogs.com/Dylan-Java-NYC/p/6751689.html
+ * Style 2
+ * https://github.com/lampardchelsea/hello-world/blob/master/leetcode/Stack/DecodeString.java
+ * Style 3
+ * 
+*/
