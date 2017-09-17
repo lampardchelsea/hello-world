cdd9e36af1a81142eef31e7d8a8cd90eb239e30f
lampardchelsea <zqqsxdd@gmail.com>
2017-09-16 21:44:09 -0500

    Update ExpressionExpand.java

--- lintcode/DataStructure/VideoExamples/Stack/ExpressionExpand.java
+++ lintcode/DataStructure/VideoExamples/Stack/ExpressionExpand.java
@@ -22,3 +22,51 @@
  * Style 3
  * 
 */
+// Style 1
+
+
+
+// Style 2
+// Refer to
+// https://github.com/lampardchelsea/hello-world/blob/master/leetcode/Stack/DecodeString.java
+public class Solution {
+    /*
+     * @param s: an expression includes numbers, letters and brackets
+     * @return: a string
+     */
+    public String expressionExpand(String s) {
+        if(s == null || s.length() == 0) {
+            return "";
+        }
+        Stack<Integer> countStack = new Stack<Integer>();
+        Stack<String> result = new Stack<String>();
+        result.push("");
+        int i = 0;
+        while(i < s.length()) {
+            char c = s.charAt(i);
+            if(Character.isDigit(c)) {
+                int val = 0;
+                int start = i;
+                while(s.charAt(i + 1) >= '0' && s.charAt(i + 1) <= '9') {
+                    i++;
+                }
+                int currCount = Integer.parseInt(s.substring(start, i + 1));
+                countStack.push(currCount);
+            } else if(c == '[') {
+                result.push("");
+            } else if(Character.isLetter(c)) {
+                result.push(result.pop() + c);
+            } else if(c == ']') {
+                StringBuilder sb = new StringBuilder();
+                int times = countStack.pop();
+                String str = result.pop();
+                for(int j = 0; j < times; j++) {
+                    sb.append(str);
+                }
+                result.push(result.pop() + sb.toString());
+            }
+            i++;
+        }
+        return result.pop();
+    }
+}
