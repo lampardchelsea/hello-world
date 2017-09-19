7f689fe7acc2e9f504bad1d86842e829b949dd94
lampardchelsea <zqqsxdd@gmail.com>
2017-09-19 03:56:53 -0500

    Update MaximalRectangle.java

--- leetcode/Stack/MaximalRectangle.java
+++ leetcode/Stack/MaximalRectangle.java
@@ -1,1 +1,82 @@
 
+import java.util.Stack;
+
+/**
+ * Refer to
+ * https://leetcode.com/problems/maximal-rectangle/description/
+ * http://www.lintcode.com/en/problem/maximal-rectangle/
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle 
+ * containing only 1's and return its area.
+
+	For example, given the following matrix:
+	
+	1 0 1 0 0
+	1 0 1 1 1
+	1 1 1 1 1
+	1 0 0 1 0
+	Return 6.
+ *  
+ * 
+ * Solution
+ * https://discuss.leetcode.com/topic/1634/a-o-n-2-solution-based-on-largest-rectangle-in-histogram/23?page=2 
+ * https://siddontang.gitbooks.io/leetcode-solution/content/array/maximal_rectangle.html
+ * 这题是一道难度很大的题目，至少我刚开始的时候完全不知道怎么做，也是google了才知道的。
+
+	这题要求在一个矩阵里面求出全部包含1的最大矩形面积，譬如这个：
+	
+	    0 0 0 0
+	    1 1 1 1
+	    1 1 1 0
+	    0 1 0 0
+	我们可以知道，最大的矩形面积为6。也就是下图中虚线包围的区域。那么我们如何得到这个区域呢？
+	
+	    0  0  0  0
+	   |--------|
+	   |1  1  1 |1
+	   |1  1  1 |0
+	   |--------|
+	    0  1  0  0
+	对于上面哪一题，我们先去掉最下面的一行，然后就可以发现，它可以转化成一个直方图，数据为[2, 2, 2, 0]，我们认为1就是高度，
+	如果碰到0，譬如上面最右列，则高度为0，而计算这个直方图最大矩形面积就很容易了，
+	我们已经在Largest Rectangle in Histogram处理了。
+	
+	所以我们可以首先得到每一行的直方图，分别求出改直方图的最大区域，最后就能得到结果了。 
+	
+	Refer to
+	https://github.com/lampardchelsea/hello-world/blob/master/lintcode/DataStructure/VideoExamples/Stack/LargestRectanguleInHistogram.java
+	Solution 3 is suitable
+ */
+public class MaximalRectangle {
+    public int maximalRectangle(char[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] heights = new int[cols + 1];
+        // Set up global variable 'max' to record final maximum rectangle after compare each row
+        int max = 0;
+        for(int i = 0; i < rows; i++) {
+        	// Set up stack to find maximum rectangle in current row
+            Stack<Integer> stack = new Stack<Integer>();
+            for(int j = 0; j < heights.length; j++) {
+            	// Set up the height for each column, if contains '0', height drop back to 0
+                if(j < cols) {
+                    if(matrix[i][j] == '1') {
+                        heights[j] += 1;
+                    } else {
+                        heights[j] = 0;
+                    }                    
+                }
+                // Largest Rectangle In Histogram section
+                while(!stack.isEmpty() && heights[j] <= heights[stack.peek()]) {
+                    int h = heights[stack.pop()];
+                    int w = stack.isEmpty() ? j : j - stack.peek() - 1;
+                    max = Math.max(max, h * w);
+                }
+                stack.push(j);
+            }
+        }
+        return max;
+    }
+}
