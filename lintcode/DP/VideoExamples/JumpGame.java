0e0809b2cabbdfdc3ff7d0e3d1f5b8207e7f6fc3
lampardchelsea <zqqsxdd@gmail.com>
2017-10-10 16:37:17 -0500

    Update JumpGame.java

--- lintcode/DP/VideoExamples/JumpGame.java
+++ lintcode/DP/VideoExamples/JumpGame.java
@@ -24,6 +24,24 @@
  *
  * Solution
  * https://leetcode.com/articles/jump-game/
+ * Top-down to bottom-up conversion is done by eliminating recursion. In practice, 
+   this achieves better performance as we no longer have the method stack overhead 
+   and might even benefit from some caching. More importantly, this step opens up 
+   possibilities for future optimization. The recursion is usually eliminated by 
+   trying to reverse the order of the steps from the top-down approach.
+   
+   The observation to make here is that we only ever jump to the right. This means 
+   that if we start from the right of the array, every time we will query a position 
+   to our right, that position has already be determined as being GOOD or BAD. This 
+   means we don't need to recurse anymore, as we will always hit the memo table.
+   
+   Complexity Analysis
+   Time complexity : O(n^2) 
+   For every element in the array, say i, we are looking at the next nums[i] elements 
+   to its right aiming to find a GOOD index. nums[i] can be at most nn, where nn is 
+   the length of array nums.
+   Space complexity : O(n). This comes from the usage of the memo table.
+ *
  * https://www.jiuzhang.com/solution/jump-game/
 */
 class Solution {
