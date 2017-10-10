b3bcbc59f07c94eb4fefcec62439bcc4c83d363c
lampardchelsea <zqqsxdd@gmail.com>
2017-10-10 12:53:50 -0500

    Update ClimbingStairs.java

--- lintcode/DP/VideoExamples/ClimbingStairs.java
+++ lintcode/DP/VideoExamples/ClimbingStairs.java
@@ -1,1 +1,29 @@
-
+/**
+ * Refer to
+ * https://leetcode.com/problems/climbing-stairs/description/
+ * http://www.lintcode.com/en/problem/climbing-stairs/#
+ *
+ * Solution
+ * https://leetcode.com/articles/climbing-stairs/
+*/
+class Solution {
+    public int climbStairs(int n) {
+        // state
+        int[] f = new int[n + 1];
+        
+        // intialize
+        if(n <= 1) {
+            return 1;
+        }
+        f[1] = 1;
+        f[2] = 2;
+        
+        // function
+        for(int i = 3; i <= n; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        
+        // answer
+        return f[n];
+    }
+}
