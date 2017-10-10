5532253a15b5f80b1685f5a1c6ea905ee16aec64
lampardchelsea <zqqsxdd@gmail.com>
2017-10-09 23:14:51 -0500

    Update Triangle_DFS.java

--- lintcode/DFS/VideoExamples/Triangle_DFS.java
+++ lintcode/DFS/VideoExamples/Triangle_DFS.java
@@ -1,1 +1,24 @@
-
+/**
+ * Refer to
+ * http://www.lintcode.com/en/problem/triangle/
+ * 
+ * Solution
+ * https://github.com/lampardchelsea/hello-world/blob/master/lintcode/DP/VideoExamples/Triangle.java
+*/
+public class Triangle {
+    public int minPath;
+    public int minimumTotal(int[][] triangle) {
+        minPath = Integer.MAX_VALUE;
+        traverse(triangle, 0, 0, 0);
+        return minPath;
+    }
+  
+    private void traverse(int[][] triangle, int x, int y, int sum) {
+        if(x == triangle.length) {
+            minPath = Math.min(minPath, sum);
+            return minPath;
+        }
+        traverse(triangle, x + 1, y, sum + triangle[x + 1][y]);
+        traverse(triangle, x + 1, y + 1, sum + triangle[x + 1][y + 1]);
+    }
+}
