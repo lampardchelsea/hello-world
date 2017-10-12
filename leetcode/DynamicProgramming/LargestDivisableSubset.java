489eecee3004351d1ffdc402a85a2a41910c3088
lampardchelsea <zqqsxdd@gmail.com>
2017-10-12 15:35:22 -0500

    Update LargestDivisableSubset.java

--- leetcode/DynamicProgramming/LargestDivisableSubset.java
+++ leetcode/DynamicProgramming/LargestDivisableSubset.java
@@ -23,6 +23,15 @@ import java.util.List;
  * 
  * Solution
  * http://www.cnblogs.com/grandyang/p/5625209.html
+ * 这道题给了我们一个数组，让我们求这样一个子集合，集合中的任意两个数相互取余均为0，而且提示中说明了要使用DP来解。那么我们考虑，
+ * 较小数对较大数取余一定不为0，那么问题就变成了看较大数能不能整除这个较小数。那么如果数组是无序的，处理起来就比较麻烦，所以我们
+ * 首先可以先给数组排序，这样我们每次就只要看后面的数字能否整除前面的数字。定义一个动态数组dp，其中dp[i]表示到数字nums[i]
+ * 位置最大可整除的子集合的长度，还需要一个一维数组pre，来保存上一个能整除的数字的位置，两个整型变量mx和mx_idx分别表示
+ * 最大子集合的长度和起始数字的位置，我们可以从后往前遍历数组，对于某个数字再遍历到末尾，在这个过程中，如果nums[j]能整除
+ * nums[i], 且dp[i] < dp[j] + 1的话，更新dp[i]和pre[i]，如果dp[i]大于mx了，还要更新mx和mx_idx，
+ * 最后循环结束后，我们来填res数字，根据parent数组来找到每一个数字
+ * 
+ * 
  * http://blog.csdn.net/yeqiuzs/article/details/51773890
  * https://discuss.leetcode.com/topic/49652/classic-dp-solution-similar-to-lis-o-n-2
  * https://discuss.leetcode.com/topic/49456/c-solution-with-explanations
@@ -35,6 +44,8 @@ public class LargestDivisibleSubset {
         }
         
         // sort array
+        // 那么我们考虑，较小数对较大数取余一定不为0，那么问题就变成了看较大数能不能整除这个较小数。那么如果数组是无序的，
+        // 处理起来就比较麻烦，所以我们首先可以先给数组排序，这样我们每次就只要看后面的数字能否整除前面的数字
         Arrays.sort(nums);
         
         // state:
