86b07a2fac616e0a899e648be25ef565b618a57d
lampardchelsea <zqqsxdd@gmail.com>
2017-10-12 02:19:02 -0500

    Update PerfectSquare.java

--- leetcode/DynamicProgramming/PerfectSquare.java
+++ leetcode/DynamicProgramming/PerfectSquare.java
@@ -9,6 +9,28 @@
  *
  * Solution
  * https://discuss.leetcode.com/topic/26400/an-easy-understanding-dp-solution-in-java
+ * dp[n] indicates that the perfect squares count of the given n, and we have:
+
+   dp[0] = 0 
+   dp[1] = dp[0]+1 = 1
+   dp[2] = dp[1]+1 = 2
+   dp[3] = dp[2]+1 = 3
+   dp[4] = Min{ dp[4-1*1]+1, dp[4-2*2]+1 } 
+         = Min{ dp[3]+1, dp[0]+1 } 
+         = 1				
+   dp[5] = Min{ dp[5-1*1]+1, dp[5-2*2]+1 } 
+         = Min{ dp[4]+1, dp[1]+1 } 
+         = 2
+                     .
+                     .
+                     .
+   dp[13] = Min{ dp[13-1*1]+1, dp[13-2*2]+1, dp[13-3*3]+1 } 
+          = Min{ dp[12]+1, dp[9]+1, dp[4]+1 } 
+          = 2
+                     .
+                     .
+                     .
+   dp[n] = Min{ dp[n - i*i] + 1 },  n - i*i >=0 && i >= 1
 */
 class Solution {
     public int numSquares(int n) {    
