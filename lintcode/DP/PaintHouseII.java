2eb6c706dd97a3bb44393664187121dbc0415607
lampardchelsea <zqqsxdd@gmail.com>
2017-10-21 16:57:29 -0700

    Update PaintHouseII.java

--- lintcode/DP/PaintHouseII.java
+++ lintcode/DP/PaintHouseII.java
@@ -8,6 +8,10 @@
    The cost of painting each house with a certain color is represented by a n x k cost matrix. 
    For example, costs[0][0] is the cost of painting house 0 with color 0; costs[1][2] is the 
    cost of painting house 1 with color 2, and so on... Find the minimum cost to paint all houses.
+   
+   Example
+   Given n = 3, k = 3, costs = [[14,2,11],[11,14,5],[14,3,10]] return 10
+   house 0 is color 2, house 1 is color 3, house 2 is color 2, 2 + 5 + 3 = 10
  *
  * Solution
  * http://www.cnblogs.com/grandyang/p/5322870.html
