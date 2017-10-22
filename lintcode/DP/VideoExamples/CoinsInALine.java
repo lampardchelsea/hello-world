9d51c2365497971b51023d3289e6df59cb3de3bd
lampardchelsea <zqqsxdd@gmail.com>
2017-10-22 12:50:05 -0700

    Update CoinsInALine.java

--- lintcode/DP/VideoExamples/CoinsInALine.java
+++ lintcode/DP/VideoExamples/CoinsInALine.java
@@ -1,1 +1,18 @@
-
+/**
+ * Refer to
+ * http://www.lintcode.com/en/problem/coins-in-a-line/
+ * There are n coins in a line. Two players take turns to take one or two coins from right side 
+   until there are no more coins left. The player who take the last coin wins.
+   Could you please decide the first play will win or lose?
+   
+   Example
+    n = 1, return true.
+    n = 2, return true.
+    n = 3, return false.
+    n = 4, return true.
+    n = 5, return true.
+ * 
+ * Solution
+ * https://aaronice.gitbooks.io/lintcode/content/dynamic_programming/coins_in_a_line.html
+ * https://www.jiuzhang.com/solution/coins-in-a-line/
+*/
