75ef087fc20d78eb3dffff2e568d14b0885e3373
lampardchelsea <zqqsxdd@gmail.com>
2017-10-22 12:03:03 -0700

    Update PaintFence.java

--- lintcode/DP/PaintFence.java
+++ lintcode/DP/PaintFence.java
@@ -43,3 +43,49 @@ public class Solution {
         return dp[3];
     }
 }
+
+// Solution 2
+// Refer to
+// https://discuss.leetcode.com/topic/24432/java-dp-solution
+// https://discuss.leetcode.com/topic/24432/java-dp-solution/3
+public class Solution {
+    /*
+     * @param n: non-negative integer, n posts
+     * @param k: non-negative integer, k colors
+     * @return: an integer, the total number of ways
+     */
+    public int numWays(int n, int k) {
+        if(n == 0 || k == 0) {
+            return 0;
+        }
+        if(n == 1) {
+            return k;
+        }
+        // state
+        // same[i] means the ith post has the same color with the (i-1)th post.
+        int[] same = new int[n];
+        // diff[i] means the ith post has a different color with the (i-1)th post.
+        int[] diff = new int[n];
+        // initialize
+        same[0] = same[1] = k;
+        diff[0] = k;
+        diff[1] = k * (k - 1);
+        // function
+        for(int i = 2; i < n; i++) {
+            // the i-th in same should be equal the previous one in diff since 
+            // only two consectutive same are allowed
+            same[i] = diff[i - 1];
+            // the i-th in diff should be either different from its previous 
+            // one or from the one before the previous one
+            diff[i] = (k - 1) * (diff[i - 1] + same[i - 1]);
+        }
+        // answer
+        return diff[n - 1] + same[n - 1];
+    }
+}
+
+
+
+
+
+
