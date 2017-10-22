b38d10f5d611630a6d21110d8bcd3d7247f6b123
lampardchelsea <zqqsxdd@gmail.com>
2017-10-21 23:51:19 -0700

    Update PaintHouseII.java

--- lintcode/DP/PaintHouseII.java
+++ lintcode/DP/PaintHouseII.java
@@ -18,6 +18,7 @@
  * https://segmentfault.com/a/1190000003903965
  * https://discuss.leetcode.com/topic/25489/fast-dp-java-solution-runtime-o-nk-space-o-1
  * https://discuss.leetcode.com/topic/22580/ac-java-solution-without-extra-space
+ * https://discuss.leetcode.com/topic/22580/ac-java-solution-without-extra-space/12
 */
 // Solution 1:
 // Refer to
@@ -66,5 +67,56 @@ public class Solution {
 // 这道题不能用之前那题的解法，会TLE。这题的解法的思路还是用DP，但是在找不同颜色的最小值不是遍历所有不同颜色，
 // 而是用min1和min2来记录之前房子的最小和第二小的花费的颜色，如果当前房子颜色和min1相同，那么我们用min2对应
 // 的值计算，反之我们用min1对应的值，这种解法实际上也包含了求次小值的方法，感觉也是一种很棒的解题思路
-
+public class Solution {
+    /*
+     * @param costs: n x k cost matrix
+     * @return: an integer, the minimum cost to paint all houses
+     */
+    public int minCostII(int[][] costs) {
+        if(costs == null || costs.length == 0) {
+            return 0;
+        }
+        int n = costs.length;
+        int k = costs[0].length;
+        // initialize
+        // Caution: we initialize as dp = costs, later statement
+        // of '+=' don't need dp[i][j] = cost[i][j] + dp[i - 1][...],
+        // as dp[i][j] already represent cost[i][j]
+        int[][] dp = costs;
+        // min1 is index of 1st minimum cost till the previous house
+        // min2 is index of 2nd minimum cost till the previous house
+        int min1 = -1;
+        int min2 = -1;
+        for(int i = 0; i < n; i++) {
+            // For each row, you need last1 and last2 to keep track of the 
+            // least and second least costs of previous row, while min1 
+            // min2 to get the least and second least values of current row.
+            int last1 = min1;
+            int last2 = min2;
+            min1 = -1;
+            min2 = -1;
+            for(int j = 0; j < k; j++) {
+                // For first row
+                if(last1 == -1) {
+                    dp[i][j] += 0;
+                } else if(j == last1) {
+                    // If same as previous index(means same color), only plus second least cost
+                    dp[i][j] += dp[i - 1][last2];
+                } else {
+                    // If not same as previous index(means same color), plus first least cost
+                    dp[i][j] += dp[i - 1][last1];
+                }
+                // If index j can get first minimum value, update original first minimum
+                // index to j and second minimum index to first minimum index
+                if(min1 == -1 || dp[i][j] < dp[i][min1]) {
+                    min2 = min1;
+                    min1 = j;
+                } else if(min2 == -1 || dp[i][j] < dp[i][min2]) {
+                    min2 = j;
+                }
+            }
+        }
+        return dp[n - 1][min1];
+    }
+}
 
