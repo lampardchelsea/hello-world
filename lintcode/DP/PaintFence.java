0945bbe82c0e12820f153b1b9a51187df8066106
lampardchelsea <zqqsxdd@gmail.com>
2017-10-22 01:31:08 -0700

    Update PaintFence.java

--- lintcode/DP/PaintFence.java
+++ lintcode/DP/PaintFence.java
@@ -35,6 +35,7 @@ public class Solution {
             return dp[n];
         }
         for(int i = 2; i < n; i++) {
+            // 递推式：第三根柱子要么根第一个柱子不是一个颜色，要么跟第二根柱子不是一个颜色
             dp[3] = (k - 1) * (dp[1] + dp[2]);
             dp[1] = dp[2];
             dp[2] = dp[3];
