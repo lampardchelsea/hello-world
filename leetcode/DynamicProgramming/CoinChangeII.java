1d68c1e05d164606220e9cf03e0f163b0118b1e6
lampardchelsea <zqqsxdd@gmail.com>
2017-10-23 17:47:04 -0700

    Update CoinChangeII.java

--- leetcode/DynamicProgramming/CoinChangeII.java
+++ leetcode/DynamicProgramming/CoinChangeII.java
@@ -70,3 +70,58 @@
             return dp[amount];
         }
 */
+class Solution {
+    public int change(int amount, int[] coins) {
+        // state: dp[i][j] : the number of combinations to make up amount j by using the first i types of coins
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        // initialize
+        // Question: 
+        // Refer to
+        // https://discuss.leetcode.com/topic/79071/knapsack-problem-java-solution-with-thinking-process-o-nm-time-and-o-m-space/8
+        // A question on why we have Initialization: dp[i][0] = 1, as described by tankztc, 
+        // dp[i][j] represent the number of combinations to make up amount j by the first i 
+        // types of coins, so dp[i][0] should represent the number of combinations to make 
+        // up 0 by the first i types of coins, the number of combinations is 0, 
+        // so dp[i][0] = 0 right ? Why initialize as 1 ?
+        dp[0][0] = 1;
+        for(int i = 1; i < coins.length + 1; i++) {
+            dp[i][0] = 1;
+        }
+        // function
+        for(int i = 1; i < coins.length + 1; i++) {
+            for(int j = 1; j < amount + 1; j++) {
+                // State transition:
+                // (1) not using the ith coin, only using the first i-1 coins to 
+                //     make up amount j, then we have dp[i-1][j] ways.
+                // (2) using the ith coin, since we can use unlimited same coin, 
+                //     we need to know how many way to make up amount j - coins[i] 
+                //     by using first i coins(including ith), which is dp[i][j-coins[i]]
+                dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
+            }
+        }
+        // answer
+        return dp[coins.length][amount];
+    }
+}
+
+
+// More concies solution
+class Solution {
+    public int change(int amount, int[] coins) {
+        // Now we can see that dp[i][j] only rely on dp[i-1][j] and dp[i][j-coins[i]], 
+        // then we can optimize the space by only using one-dimension array.
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for(int coin : coins) {
+            for(int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
+
+
+
+
+
