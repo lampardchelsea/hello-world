0d12c613837e3561842c52b011702c755178213a
lampardchelsea <zqqsxdd@gmail.com>
2017-10-28 01:02:45 -0700

    Update MinimumSizeSubarraySum.java

--- leetcode/array/MinimumSizeSubarraySum.java
+++ leetcode/array/MinimumSizeSubarraySum.java
@@ -19,6 +19,49 @@
  * https://lefttree.gitbooks.io/leetcode-categories/twoPointer/minSizeSubarraySum.html
  */
 // Solution 1:
+class Solution {
+    /**
+     Approach #1 Brute force [Time Limit Exceeded]
+     Intuition
+     Do as directed in question. Find the sum for all the possible subarrays and update 
+     the ans as and when we get a better subarray that fulfill the requirements sum ≥ s.
+     
+     Algorithm
+     Initialize ans=INT_MAX
+     Iterate the array from left to right using i:
+     Iterate from the current element to the end of vector using j:
+     Find the sum of elements from index i to j
+     If sum is greater then s:
+     Update ans=min(ans,(j−i+1))
+     Start the next iith iteration, since, we got the smallest subarray with
+     sum≥s starting from the current index.
+    */
+    public int minSubArrayLen(int s, int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int result = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i; j < nums.length; j++) {
+                int sum = 0;
+                for(int k = i; k <= j; k++) {
+                    sum += nums[k];
+                }
+                if(sum >= s) {
+                    result = Math.min(result, j - i + 1);
+                    // Must break here to get the minimum length
+                    // as from now on sum always >= s, and the
+                    // first case means minimum length
+                    break;
+                }
+            }
+        }
+        // Return 0 if not able to reach target >= s
+        // e.g s = 3, nums = {1,1}
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+}
+
 
 // Solution 2:
 
