c27ada91f7bff668c1170d6007b13b5412dfab21
lampardchelsea <zqqsxdd@gmail.com>
2017-10-28 04:40:14 -0700

    Update MinimumSizeSubarraySum.java

--- leetcode/array/MinimumSizeSubarraySum.java
+++ leetcode/array/MinimumSizeSubarraySum.java
@@ -35,6 +35,13 @@ class Solution {
      Update ans=min(ans,(j−i+1))
      Start the next iith iteration, since, we got the smallest subarray with
      sum≥s starting from the current index.
+     
+     Complexity Analysis
+     Time complexity: O(n^3). For each element of array, we find all the subarrays starting 
+                              from that index which is O(n^2) Time complexity to find the 
+                              sum of each subarray is O(n). Thus, the total time complexity: 
+                              O(n^2 * n) = O(n^3)
+     Space complexity: O(1) extra space.
     */
     public int minSubArrayLen(int s, int[] nums) {
         if(nums == null || nums.length == 0) {
@@ -82,6 +89,12 @@ class Solution {
      Update sums[i]=sums[i−1]+nums[i]
      Sum of subarray from i to j is calculated as: sum=sums[j]−sums[i]+nums[i], 
      wherein sums[j]−sums[i] is the sum from (i+1)th element to the jth element.
+     
+     Complexity analysis
+     Time complexity: O(n^2) Time complexity to find all the subarrays is O(n^2).
+                             Sum of the subarrays is calculated in O(1) time.
+                             Thus, the total time complexity: O(n^2 * 1)
+     Space complexity: O(n) extra space. Additional O(n) space for sums vector than in Approach #1.
     */
     public int minSubArrayLen(int s, int[] nums) {
         if(nums == null || nums.length == 0) {
@@ -112,6 +125,96 @@ class Solution {
 
 
 // Solution 3:
+class Solution {
+    /**
+      Approach #3 Using Binary search [Accepted]
+      Intuition
+      We could further improve the Approach #2 using the binary search. Notice that we find the 
+      subarray with sum>=s starting with an index i in O(n) time. But, we could reduce the time 
+      to O(log(n) using binary search. Note that in Approach #2, we search for subarray starting 
+      with index i, until we find sum=sums[j]−sums[i]+nums[i] that is greater than s. So, instead 
+      of iterating linearly to find the sum, we could use binary search to find the index that 
+      is not lower than s−sums[i] in the sums, which can be done using lower_bound function in 
+      C++ STL or could be implemented manually.
+      
+      Algorithm
+      Create vector sums of size n+1 with: sums[0]=0, sums[i]=sums[i−1]+nums[i−1]
+      Iterate from i=1 to n:
+      Find the value to_find sum required for minimum subarray starting from index i to have 
+      sum greater than s, that is: to_find=s+sums[i−1]
+      Find the index in sums such that value at that index is not lower than the to_find value, 
+      say bound
+      If we find the to_find in sums, then: Size of current subarray is given by: bound−(sums.begin()+i−1)
+      Compare ansans with the current subarray size and store minimum in ansans
+      
+      Complexity analysis
+      Time complexity: O(nlog(n)).
+      For each element in the vector, find the subarray starting from that index, and having sum 
+      greater than ss using binary search. Hence, the time required is O(n) for iteration over the 
+      vector and O(log(n)) for finding the subarray for each index using binary search.
+      Therefore, total time complexity = O(n∗log(n))
+      Space complexity: O(n). Additional O(n) space for sums vector
+      
+      Refer to
+      http://likesky3.iteye.com/blog/2229475
+      将问题转化为在递增数列中查找某个数。建立辅助数组sum[], sum[i]表示 num数组的前 i 个
+      元素的加和。对于每个sum[i]，在 i 后面查找子数组右边界位置使得子数组的加和 >= s, 
+      也就是在 i 位置后面寻找位置 j 满足 sum[j] >= sum[i] + s, 满足这个关系表明 num[i] 
+      到 num[j - 1]这段子数组加和>=s。因为sum[]是递增数组，可使用二分法查找满足要求的下标。
+      注意到实现中的binarySearchNotLess和经典的二分算法区别就在于while循环外面的return，
+      这里是return left，如果sum数组中找不到target，会返回第一个比target大元素的下标，
+      如果没有则返回sum.length + 1, 调用处据此判断某个元素后面是否有加和为s的子数组
+    */
+    public int minSubArrayLen(int s, int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        // size = n + 1 for easier calculate
+        int[] preSum = new int[n + 1];
+        // preSum[0] = 0 means it is the sum for first 0 elements
+        preSum[0] = 0;
+        // preSum[1] = A[0] means the sum for first 1 elements
+        for(int i = 1; i < n + 1; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
+        }
+        int result = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++) {
+            // Try from preSum[0] to the end as preSum[n]
+            int target = s + preSum[i];
+            int right_bound = binarySearch(i, n, target, preSum);
+            if(right_bound != preSum.length + 1) {
+                result = Math.min(result, right_bound - i);
+            }
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+    
+    private int binarySearch(int start, int end, int target, int[] preSum) {
+        while(start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            // As try to find minimum length, cut second half
+            // and keep on search in first half by end = mid
+            if(preSum[mid] == target) {
+                end = mid;
+            } else if(preSum[mid] > target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if(preSum[start] >= target) {
+            return start;
+        } else if(preSum[end] >= target) {
+            return end;
+        } else {
+            return preSum.length + 1;
+        }
+    }
+}
+
+
+
 
 // Solution 4: Two Points
 public class MinimumSizeSubarraySum {
