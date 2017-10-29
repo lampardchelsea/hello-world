3fb918740d448f474731113cf266aa5b3f6e514a
lampardchelsea <zqqsxdd@gmail.com>
2017-10-29 12:33:34 -0700

    Update LongestSubstringWithAtMostKDistinctCharacters.java

--- leetcode/String/LongestSubstringWithAtMostKDistinctCharacters.java
+++ leetcode/String/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -1,12 +1,9 @@
 /**
  * Refer to
- * https://zhengyang2015.gitbooks.io/lintcode/longest_substring_with_at_most_k_distinct_characte.html
- * Given a string s, find the length of the longest substring T that contains at most k distinct characters.
-    Example
-    For example, Given s = "eceba", k = 3,
-    T is "eceb" which its length is 4.
-    Challenge
-    O(n), n is the size of the string s.
+ * http://www.cnblogs.com/grandyang/p/5351347.html
+ * Given a string, find the length of the longest substring T that contains at most k distinct characters.
+   For example, Given s = “eceba” and k = 2,
+   T is "ece" which its length is 3.
  *
  * Solution
  * https://zhengyang2015.gitbooks.io/lintcode/longest_substring_with_at_most_k_distinct_characte.html
@@ -20,11 +17,6 @@
  * https://discuss.leetcode.com/topic/41671/15-lines-java-solution-using-slide-window/2
 */
 public class Solution {
-    /**
-     * @param s : A string
-     * @return : The length of the longest substring 
-     *           that contains at most k distinct characters.
-     */
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
         
     }
