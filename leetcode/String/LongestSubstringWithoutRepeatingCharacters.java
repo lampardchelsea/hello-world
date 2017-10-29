a27bf212a572d58383e02aaca1d9454fedafb9cb
lampardchelsea <zqqsxdd@gmail.com>
2017-10-28 21:59:07 -0700

    Update LongestSubstringWithoutRepeatingCharacters.java

--- leetcode/String/LongestSubstringWithoutRepeatingCharacters.java
+++ leetcode/String/LongestSubstringWithoutRepeatingCharacters.java
@@ -14,7 +14,77 @@ import java.util.Map;
  * If the character is already in the hashmap, then move the left 
  * pointer to the right of the same character last found. 
  * Note that the two pointers can only move forward.
+ *
+ * https://leetcode.com/articles/longest-substring-without-repeating-characters/
  */
+
+// Solution 1:
+class Solution {
+    /**
+     Approach #1 Brute Force [Time Limit Exceeded]
+     Intuition
+     Check all the substring one by one to see if it has no duplicate character.
+     
+     Algorithm
+     Suppose we have a function boolean allUnique(String substring) which will return 
+     true if the characters in the substring are all unique, otherwise false. We can 
+     iterate through all the possible substrings of the given string s and call the 
+     function allUnique. If it turns out to be true, then we update our answer of the 
+     maximum length of substring without duplicate characters.
+     
+     Now let's fill the missing parts:
+     To enumerate all substrings of a given string, we enumerate the start and end 
+     indices of them. Suppose the start and end indices are i and j, respectively. 
+     Then we have n0 ≤ i < j ≤ n (here end index j is exclusive by convention). 
+     Thus, using two nested loops with i from 0 to n−1 and j from i+1 to n, we can 
+     enumerate all the substrings of s.
+     
+     To check if one string has duplicate characters, we can use a set. We iterate 
+     through all the characters in the string and put them into the set one by one. 
+     Before putting one character, we check if the set already contains it. If so, 
+     we return false. After the loop, we return true.
+     
+     Complexity Analysis
+     Time complexity : O(n^3)
+     Space complexity : O(min(n,m)). We need O(k) space for checking a substring 
+                        has no duplicate characters, where k is the size of the Set. 
+                        The size of the Set is upper bounded by the size of the 
+                        string n and the size of the charset/alphabet m.
+    */
+    public int lengthOfLongestSubstring(String s) {
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
+        int result = 0;
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
+            // Note: The char at index 'j' exclusive in substring
+            // based on String.substring() method, and substring length
+            // is (j - i), not (j - i + 1) as char at 'j' not include
+            for(int j = 1; j <= n; j++) {
+                if(allUnique(s, i, j)) {
+                    result = Math.max(result, j - i);
+                }
+            }
+        }
+        return result;
+    }
+    
+    private boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<Character>();
+        for(int i = start; i < end; i++) {
+            Character c = s.charAt(i);
+            if(set.contains(c)) {
+                return false;
+            }
+            set.add(c);
+        }
+        return true;
+    }
+}
+
+
+
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
         if(s.length() == 0) {
