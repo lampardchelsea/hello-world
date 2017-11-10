bbb1a8d40e7341213c53bb3283130e1eda84ad4e
lampardchelsea <zqqsxdd@gmail.com>
2017-11-09 23:37:57 -0800

    Update ShortestWordDistanceIII.java

--- lintcode/TwoPoints/ShortestWordDistanceIII.java
+++ lintcode/TwoPoints/ShortestWordDistanceIII.java
@@ -13,3 +13,40 @@
  * Solution
  * https://segmentfault.com/a/1190000003906667
 */
+
+public class ShortestWordDistanceIII {
+	public int shortestWordDistance(String[] words, String word1, String word2) {
+		// Solution 1: HashMap 
+		
+		// Solution 2: Two Points
+		int p1 = -1;
+		int p2 = -1;
+		int distance = Integer.MAX_VALUE;
+		boolean isSame = word1.equals(word2);
+		for(int i = 0; i < words.length; i++) {
+			if(words[i].equals(word1)) {
+				p1 = i;
+			} else if(words[i].equals(word2)) {
+				p2 = i;
+			} else {
+				continue;
+			}
+			if(p1 >= 0 && p2 >= 0) {
+				distance = Math.min(distance, Math.abs(p1 - p2));
+			}
+			if(isSame) {
+				p2 = p1;
+			}
+		}
+		return distance;
+	}
+	
+	public static void main(String[] args) {
+		String[] words = {"practice", "makes", "perfect", "running", "sitting", "makes", "coding", "makes"};
+		String word1 = "makes";
+		String word2 = "makes";
+		ShortestWordDistanceIII s = new ShortestWordDistanceIII();
+		int result = s.shortestWordDistance(words, word1, word2);
+		System.out.println(result);
+	}
+}
