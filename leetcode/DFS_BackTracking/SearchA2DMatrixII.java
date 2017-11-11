d40177b597a8f71847f06a7778aff986b67617ac
lampardchelsea <zqqsxdd@gmail.com>
2017-11-10 22:35:15 -0800

    Update SearchA2DMatrixII.java

--- leetcode/DFS_BackTracking/SearchA2DMatrixII.java
+++ leetcode/DFS_BackTracking/SearchA2DMatrixII.java
@@ -1,4 +1,55 @@
+/**
+ * Refer to
+ * https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+ * Write an efficient algorithm that searches for a value in an m x n matrix. 
+   This matrix has the following properties:
 
+    Integers in each row are sorted in ascending from left to right.
+    Integers in each column are sorted in ascending from top to bottom.
+    For example,
+
+    Consider the following matrix:
+    [
+      [1,   4,  7, 11, 15],
+      [2,   5,  8, 12, 19],
+      [3,   6,  9, 16, 22],
+      [10, 13, 14, 17, 24],
+      [18, 21, 23, 26, 30]
+    ]
+    Given target = 5, return true.
+    Given target = 20, return false.
+ *
+ * 
+ * Solution
+ * https://discuss.leetcode.com/topic/33240/java-an-easy-to-understand-divide-and-conquer-method?page=1
+ * The coding seems to be much more complex than those smart methods such as this one, but the idea behind 
+   is actually quite straightforward. Unfortunately, it is not as fast as the smart ones.
+
+    First, we divide the matrix into four quarters as shown below:
+
+      zone 1      zone 2
+    *  *  *  * | *  *  *  *
+    *  *  *  * | *  *  *  *
+    *  *  *  * | *  *  *  *
+    *  *  *  * | *  *  *  *
+    -----------------------
+    *  *  *  * | *  *  *  *
+    *  *  *  * | *  *  *  *
+    *  *  *  * | *  *  *  *
+    *  *  *  * | *  *  *  *
+      zone 3      zone 4
+    We then compare the element in the center of the matrix with the target. There are three possibilities:
+    center < target. In this case, we discard zone 1 because all elements in zone 1 are less than target.
+    center > target. In this case, we discard zone 4.
+    center == target. return true.
+
+    For time complexity, if the matrix is a square matrix of size nxn, then for the worst case,
+    T(nxn) = 3T(n/2 x n/2)
+    which makes
+    T(nxn) = O(n^log3)
+ * 
+ * https://discuss.leetcode.com/topic/33240/java-an-easy-to-understand-divide-and-conquer-method/3?page=1
+*/
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
