c4422fc384ceb2cafbcaa5eb6f295914259540e9
lampardchelsea <zqqsxdd@gmail.com>
2017-12-11 13:59:06 -0800

    Update FactorialTrailingZeros.java

--- leetcode/array/FactorialTrailingZeros.java
+++ leetcode/array/FactorialTrailingZeros.java
@@ -6,4 +6,29 @@
  *
  * Solution
  * https://discuss.leetcode.com/topic/6513/simple-c-c-solution-with-detailed-explaination
+ * https://discuss.leetcode.com/topic/6513/simple-c-c-solution-with-detailed-explaination/4
+ * http://www.purplemath.com/modules/factzero.htm
 */
+public class FactorialTrailingZeros {
+	public int trailingZeros(int n) {
+        int divisor = 5;
+        int result = 0;
+        // Directly use 'n', don't need to calculate n!
+        while(n / divisor > 0) {
+            result += n / divisor;
+            // Don't use multiply, as n > 1808548329 will cause overflow issue
+            // on 'divisor * 5' because of 'divisor * 5 > Integer.MAX_VALUE / 5'
+            // and it is still valid.
+            //divisor *= 5; 
+            n = n / divisor;
+        }
+        return result;
+	}
+	
+	public static void main(String[] args) {
+		FactorialTrailingZeros f = new FactorialTrailingZeros();
+		int n = 3;
+		int result = f.trailingZeros(n);
+		System.out.println(result);
+	}
+}
