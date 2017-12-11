faa0f2e16f3b1effed451309504bb23558684580
lampardchelsea <zqqsxdd@gmail.com>
2017-12-11 13:07:58 -0800

    Create FactorialTrailingZeros.java

---
+++ leetcode/array/FactorialTrailingZeros.java
@@ -0,0 +1,9 @@
+/**
+ * Refer to
+ * https://leetcode.com/problems/factorial-trailing-zeroes/description/
+ * Given an integer n, return the number of trailing zeroes in n!.
+   Note: Your solution should be in logarithmic time complexity.
+ *
+ * Solution
+ * https://discuss.leetcode.com/topic/6513/simple-c-c-solution-with-detailed-explaination
+*/
