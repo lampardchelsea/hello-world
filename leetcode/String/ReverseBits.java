bd847803297f3747ebbedac7b9d9662481e8f782
lampardchelsea <zqqsxdd@gmail.com>
2017-12-16 18:04:51 -0800

    Update ReverseBits.java

--- leetcode/String/ReverseBits.java
+++ leetcode/String/ReverseBits.java
@@ -1,1 +1,13 @@
+/**
+ * Refer to
+ * https://leetcode.com/problems/reverse-bits/description/
+ * Reverse bits of a given 32 bits unsigned integer.
+    For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), 
+    return 964176192 (represented in binary as 00111001011110000010100101000000).
 
+    Follow up:
+    If this function is called many times, how would you optimize it?
+ *
+ * Solution
+ * https://discuss.leetcode.com/topic/9764/java-solution-and-optimization
+*/
