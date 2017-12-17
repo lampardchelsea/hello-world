cd317d78b39b0fdd4c8f62ca94091c8ecd93b72c
lampardchelsea <zqqsxdd@gmail.com>
2017-12-16 21:14:35 -0800

    Update ReverseBits.java

--- leetcode/String/ReverseBits.java
+++ leetcode/String/ReverseBits.java
@@ -11,3 +11,18 @@
  * Solution
  * https://discuss.leetcode.com/topic/9764/java-solution-and-optimization
 */
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        int mask = 1;
+        for(int i = 0; i < 32; i++) {
+            result += (n & mask);
+            n >>= 1;
+            if(i < 31) {
+                result <<= 1;
+            }
+        }
+        return result;
+    }
+}
