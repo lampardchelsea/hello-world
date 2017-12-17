ba156bf935b8533306fec5e275903c5a72305a5f
lampardchelsea <zqqsxdd@gmail.com>
2017-12-16 18:32:21 -0800

    Update NumberOf1Bits.java

--- leetcode/String/NumberOf1Bits.java
+++ leetcode/String/NumberOf1Bits.java
@@ -48,6 +48,7 @@ public class Solution {
         int ones = 0;
         while(n != 0) {
             ones += (n & 1);
+            // In this style we shift n
             n = n >>> 1;
         }
         return ones;
