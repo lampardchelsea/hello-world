28fbf3643a95436087dd60dcbc4bd4743bca4b84
lampardchelsea <zqqsxdd@gmail.com>
2017-12-16 22:44:05 -0800

    Update ReverseBits.java

--- leetcode/String/ReverseBits.java
+++ leetcode/String/ReverseBits.java
@@ -17,10 +17,23 @@ public class Solution {
         int result = 0;
         int mask = 1;
         for(int i = 0; i < 32; i++) {
+            // Not like 'Number of 1 Bits', don't need
+            // to judge on whether its '1' or '0',
+            // just need add to result is fine, then
+            // shift n to rightward for next digit
             result += (n & mask);
             n >>= 1;
+            // Don't do the leftward shift as 'mask <<= 1',
+            // because we should consider when i = 30,
+            // mask = -2147483648, this is not same as
+            // 'Number of 1 Bits' because that only consider
+            // '1' appearance, but here we should consider
+            // overflow
+            //mask <<= 1;
+            // For the last time shift we don't need to
+            // move to leftward
             if(i < 31) {
-                result <<= 1;
+                result <<= 1; 
             }
         }
         return result;
