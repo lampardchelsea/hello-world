d7ce991ea5f2b5b07e361b2f870643a2319d2ded
lampardchelsea <zqqsxdd@gmail.com>
2017-12-19 04:12:51 -0800

    Update BoundaryOfBinaryTree.java

--- leetcode/Tree/BoundaryOfBinaryTree.java
+++ leetcode/Tree/BoundaryOfBinaryTree.java
@@ -61,7 +61,7 @@
  * http://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
 */
 public class BoundaryOfBinaryTree {
-private class TreeNode {
+       private class TreeNode {
 		TreeNode left;
 		TreeNode right;
 		int val;
@@ -70,36 +70,67 @@ private class TreeNode {
 		}
 	}
 	
+        // 这道题给了我们一棵二叉树，让我们以逆时针的顺序来输出树的边界，按顺序分别为左边界，
+	// 叶结点和右边界。题目中给的例子也能让我们很清晰的明白哪些算是边界上的结点。那么最直接
+	// 的方法就是分别按顺序求出左边界结点，叶结点，和右边界结点。那么如何求的，对于树的操作
+	// 肯定是用递归最简洁啊，所以我们可以写分别三个递归函数来分别求左边界结点，叶结点，和右边界结点。
 	public List<Integer> boundaryOfBinaryTree(TreeNode root) {  
-	    List<Integer> res = new ArrayList<>();  
-	    if (root == null) return res;  
-	    if (root.left != null || root.right != null) res.add(root.val);  
-	    left_bound(root.left, res);  
-	    leaves(root, res);  
-	    right_bound(root.right, res);  
-	     
-	    return res;  
+		List<Integer> result = new ArrayList<Integer>();
+		if(root == null) {
+			return result;
+		}
+		// 首先我们先要处理根结点的情况，当根结点没有左右子结点时，其也是一个叶结点，
+		// 那么我们一开始就将其加入结果res中，那么再计算叶结点的时候又会再加入一次，
+		// 这样不对。所以我们判断如果根结点至少有一个子结点，我们才提前将其加入结果res中。
+		if(root.left != null || root.right != null) {
+			result.add(root.val);
+		}
+		left_bound(root.left, result);
+		leaves(root, result);
+		right_bound(root.right, result);
+		return result;
 	}  
 	  
-	private void left_bound(TreeNode root, List<Integer> res) {  
-	    if (root == null || (root.left == null && root.right == null)) return;  
-	    res.add(root.val);  
-	    if (root.left == null) left_bound(root.right, res);  
-	    else left_bound(root.left, res);  
+	// pre-order traverse
+	// 然后看如果左子结点存在，就对其调用递归函数，反之如果左子结点不存在，那么对右子结点调用递归函数。
+	private void left_bound(TreeNode root, List<Integer> result) {  
+        if(root == null || (root.left == null && root.right == null)) {
+        	return;
+        }
+        result.add(root.val);
+        if(root.left == null) {
+        	left_bound(root.right, result);
+        } else {
+        	left_bound(root.left, result);
+        }
 	}  
 	  
-	private void right_bound(TreeNode root, List<Integer> res) {  
-	    if (root == null || (root.left == null && root.right == null)) return;  
-	    if (root.right == null) right_bound(root.left, res);  
-	    else right_bound(root.right, res);  
-	    res.add(root.val);  
+	// post-order traverse
+	// 那么对右子结点调用递归函数。而对于求右边界结点的函数就反过来了，如果右子结点存在，就对其调用递归函数，
+	// 反之如果右子结点不存在，就对左子结点调用递归函数，注意在调用递归函数之后才将结点值加入结果res，
+	// 因为我们是需要按逆时针的顺序输出。
+	private void right_bound(TreeNode root, List<Integer> result) {  
+        if(root == null || (root.left == null && root.right == null)) {
+        	return;
+        }
+        if(root.right == null) {
+        	right_bound(root.left, result);
+        } else {
+        	right_bound(root.right, result);
+        }
+        result.add(root.val);
 	}  
-	  
-	private void leaves(TreeNode root, List<Integer> res) {  
-	    if (root == null) return;  
-	    if (root.left == null && root.right == null) res.add(root.val);  
-	    leaves(root.left, res);  
-	    leaves(root.right, res);  
+	
+	// 最后就来看求叶结点的函数，没什么可说的，就是看没有子结点存在了就加入结果res，然后对左右子结点分别调用递归即可
+	private void leaves(TreeNode root, List<Integer> result) {  
+        if(root == null) {
+        	return;
+        }
+        if(root.left == null && root.right == null) {
+        	result.add(root.val);
+        }
+        leaves(root.left, result);
+        leaves(root.right, result);
 	}
    
    public static void main(String[] args) {
