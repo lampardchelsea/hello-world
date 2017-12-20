4dbf22af85e9327c0129c3f17a2ed7fcf38ee466
lampardchelsea <zqqsxdd@gmail.com>
2017-12-20 03:01:17 -0800

    Update VerifyPreorderSequenceInBinarySearchTree.java

--- leetcode/Tree/VerifyPreorderSequenceInBinarySearchTree.java
+++ leetcode/Tree/VerifyPreorderSequenceInBinarySearchTree.java
@@ -1,1 +1,11 @@
-
+/**
+ * Refer to
+ * https://segmentfault.com/a/1190000003874375
+ * Given an array of numbers, verify whether it is the correct preorder traversal sequence of 
+   a binary search tree.
+   You may assume each number in the sequence is unique.
+   Follow up: Could you do it using only constant space complexity?
+ *
+ * Solution
+ * 
+*/
