6843da3d7589f943c4bef34fa7f585897476463c
lampardchelsea <zqqsxdd@gmail.com>
2017-12-23 15:24:57 -0800

    Update AddDigits.java

--- leetcode/DFS_BackTracking/AddDigits.java
+++ leetcode/DFS_BackTracking/AddDigits.java
@@ -12,7 +12,9 @@
  * 
  * Solution
  * https://stackoverflow.com/questions/4968323/java-parse-int-value-from-a-char
+ * http://www.cnblogs.com/grandyang/p/4741028.html
 */
+// Solution 1: DFS
 class Solution {
     int result = 0;
     public int addDigits(int num) {
@@ -34,3 +36,53 @@ class Solution {
         }
     }
 }
+
+// Solution 2: 
+// Refer to
+// http://www.cnblogs.com/grandyang/p/4741028.html
+/** 
+  但是这个解法在出题人看来又trivial又naive，需要想点高逼格的解法，一行搞定碉堡了，
+  那么我们先来观察1到20的所有的树根：
+  
+    1    1
+    2    2
+    3    3
+    4    4
+    5    5
+    6    6
+    7    7
+    8    8    
+    9    9    
+    10    1
+    11    2
+    12    3    
+    13    4
+    14    5
+    15    6
+    16    7
+    17    8
+    18    9
+    19    1
+    20    2
+
+    根据上面的列举，我们可以得出规律，每9个一循环，所有大于9的数的树根都是对9取余，
+    那么对于等于9的数对9取余就是0了，为了得到其本身，而且同样也要对大于9的数适用，
+    我们就用(n-1)%9+1这个表达式来包括所有的情况
+*/
+class Solution {
+    public int addDigits(int num) {
+        return (num - 1) % 9 + 1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
