898e162f3728834ac6279c2329d63a431813d681
lampardchelsea <zqqsxdd@gmail.com>
2017-12-30 15:49:02 -0800

    Update ZigzagIterator.java

--- leetcode/array/ZigzagIterator.java
+++ leetcode/array/ZigzagIterator.java
@@ -84,20 +84,80 @@ public class ZigzagIterator {
             // for next time calling next2() method, will start with list -> ((3,4,5,6),(2))
             list.add(poll);
         }
-        return list;
+        return result;
     }
     
     public boolean hasNext2() {
         return !list.isEmpty();
-    }
-     
-    // Follow up: 
-    // What if you are given k 1d vectors? How well can your code be extended to such cases?
-    
- 
+    } 
 }
 
 
+// Follow up: 
+// What if you are given k 1d vectors? How well can your code be extended to such cases?
+// Refer to
+// https://discuss.leetcode.com/topic/26654/simple-java-solution-for-k-vector
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ZigzagIterator {
+    LinkedList<Iterator> itrlist;	
+	public ZigzagIterator(List<List<Integer>> lists) {
+		itrlist = new LinkedList<Iterator>();
+		// add all given iterates into list
+	    for(List<Integer> list : lists) {
+	        Iterator itr = list.iterator();
+	    	if(itr.hasNext()) {
+	    		itrlist.add(itr);
+	    	} 
+	    }	
+	}
+	
+	public int next() {
+		Iterator poll = itrlist.remove();
+		int result = (Integer)poll.next();
+		if(poll.hasNext()) {
+			itrlist.add(poll);
+		}
+		return result;
+	}
+	
+	public boolean hasNext() {
+		return !itrlist.isEmpty();
+	}
+	
+	public static void main(String[] args) {
+		/**
+		 * [1,2,3]
+		 * [4,5,6,7]
+		 * [8,9]
+		 * It should return [1,4,8,2,5,9,3,6,7].
+		 */
+		List<List<Integer>> lists = new ArrayList<List<Integer>>();
+		List<Integer> list1 = new ArrayList<Integer>();
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+		List<Integer> list2 = new ArrayList<Integer>();
+		list2.add(4);
+		list2.add(5);
+		list2.add(6);
+		list2.add(7);
+		List<Integer> list3 = new ArrayList<Integer>();
+		list3.add(8);
+		list3.add(9);
+		lists.add(list1);
+		lists.add(list2);
+		lists.add(list3);
+		ZigzagIterator z = new ZigzagIterator(lists);
+		while(z.hasNext()) {
+			System.out.print(z.next() + " ");
+		}
+	}
+
+}
 
 
 
