e51afa85ee08c01585753e73ea02e321d5ef9f48
lampardchelsea <zqqsxdd@gmail.com>
2018-01-02 11:15:00 -0800

    Update RangeSumQueryImmutable.java

--- leetcode/array/RangeSumQueryImmutable.java
+++ leetcode/array/RangeSumQueryImmutable.java
@@ -1,1 +1,35 @@
+/**
+ * Refer to
+ * https://leetcode.com/problems/range-sum-query-immutable/
+ *
+ * 
+ * Solution
+ * 
+ * 
+*/
 
+// Solution 1: Time Limited Exceeded
+class NumArray {
+    List<Integer> list;
+    
+    public NumArray(int[] nums) {
+        list = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            list.add(nums[i]);
+        }
+    }
+    
+    public int sumRange(int i, int j) {
+        int sum = 0;
+        for(int k = i; k <= j; k++) {
+            sum += list.get(k);
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
