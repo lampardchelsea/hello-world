6aa50cb4a39fa39a7280651d2f13378d051944a9
lampardchelsea <zqqsxdd@gmail.com>
2018-01-04 01:02:09 -0800

    Update BestTimeToBuyAndSellStockWithCooldown.java

--- leetcode/DynamicProgramming/BestTimeToBuyAndSellStockWithCooldown.java
+++ leetcode/DynamicProgramming/BestTimeToBuyAndSellStockWithCooldown.java
@@ -18,7 +18,6 @@
  * Solution
  * Refer to
  * https://discuss.leetcode.com/topic/30421/share-my-thinking-process?page=1
- 
    The series of problems are typical dp. The key for dp is to find the variables to represent the 
    states and deduce the transition function.
 
@@ -31,11 +30,8 @@
    Then the transaction sequences can end with any of these three states.
 
    For each of them we make an array, buy[n], sell[n] and rest[n].
-
    buy[i] means before day i what is the maxProfit for any sequence end with buy.
-
    sell[i] means before day i what is the maxProfit for any sequence end with sell.
-
    rest[i] means before day i what is the maxProfit for any sequence end with rest.
 
    Then we want to deduce the transition functions for buy sell and rest. By definition we have:
@@ -43,10 +39,11 @@
    buy[i]  = max(rest[i-1]-price, buy[i-1]) 
    sell[i] = max(buy[i-1]+price, sell[i-1])
    rest[i] = max(sell[i-1], buy[i-1], rest[i-1])
+   
    Where price is the price of day i. All of these are very straightforward. They simply represents :
-
    (1) We have to `rest` before we `buy` and 
    (2) we have to `buy` before we `sell`
+   
    One tricky point is how do you make sure you sell before you buy, since from the equations it 
    seems that [buy, rest, buy] is entirely possible.
 
@@ -54,23 +51,23 @@
    rest[i] = max(sell[i-1], rest[i-1]). That made sure [buy, rest, buy] is never occurred.
 
    A further observation is that and rest[i] <= sell[i] is also true therefore
-
+   rest[i-1] <= sell[i-1], and max(sell[i-1], rest[i-1]) = sell[i-1], so
    rest[i] = sell[i-1]
+   
    Substitute this in to buy[i] we now have 2 functions instead of 3:
-
    buy[i] = max(sell[i-2]-price, buy[i-1])
    sell[i] = max(buy[i-1]+price, sell[i-1])
-   This is better than 3, but
-
-   we can do even better
-
+   
+   This is better than 3, but we can do even better
    Since states of day i relies only on i-1 and i-2 we can reduce the O(n) space to O(1). 
    And here we are at our final solution:
-
-   Java
+*/
 
    public int maxProfit(int[] prices) {
-       int sell = 0, prev_sell = 0, buy = Integer.MIN_VALUE, prev_buy;
+       int sell = 0;
+       int prev_sell = 0;
+       int buy = Integer.MIN_VALUE;
+       int prev_buy;
        for (int price : prices) {
            prev_buy = buy;
            buy = Math.max(prev_sell - price, prev_buy);
@@ -79,4 +76,3 @@
        }
        return sell;
    }
-*/
