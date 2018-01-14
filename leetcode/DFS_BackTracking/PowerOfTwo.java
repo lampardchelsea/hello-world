54ab55783e719786fe833a0e1bb938437dcaff9e
lampardchelsea <zqqsxdd@gmail.com>
2018-01-14 13:33:39 -0800

    Update PowerOfTwo.java

--- leetcode/DFS_BackTracking/PowerOfTwo.java
+++ leetcode/DFS_BackTracking/PowerOfTwo.java
@@ -1,1 +1,42 @@
+/**
+ * Refer to
+ * https://leetcode.com/problems/power-of-two/description/
+ * Given an integer, write a function to determine if it is a power of two.
+ *
+ * Solution
+ * https://discuss.leetcode.com/topic/47195/4-different-ways-to-solve-iterative-recursive-bit-operation-math
+*/
+// Solution 1: Iterative
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n == 0) {
+            return false;
+        }
+        while(n % 2 == 0) {
+            n = n / 2;
+        }
+        return n == 1;
+    }
+}
+
+// Solution 2: DFS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
