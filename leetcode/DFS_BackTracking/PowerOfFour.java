fa2823e807fa770ab964f3aad0154e6af9c78e42
lampardchelsea <zqqsxdd@gmail.com>
2018-01-15 19:00:09 -0800

    Update PowerOfFour.java

--- leetcode/DFS_BackTracking/PowerOfFour.java
+++ leetcode/DFS_BackTracking/PowerOfFour.java
@@ -1,1 +1,27 @@
+/**
+ * Refer to
+ * https://leetcode.com/problems/power-of-four/description/
+ * 
+    Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
 
+    Example:
+    Given num = 16, return true. Given num = 5, return false.
+ *
+ * Solution
+*/
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        if(num == 0) {
+            return false;
+        }
+        return helper(num);
+    }
+    
+    private boolean helper(int num) {
+        if(num == 1) {
+            return true;
+        } else {
+            return num % 4 == 0 && helper(num / 4);
+        }
+    }
+}
