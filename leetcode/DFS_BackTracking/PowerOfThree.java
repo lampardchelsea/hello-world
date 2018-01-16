a123866ae40c82259949e20a197ab66ad4ba1da1
lampardchelsea <zqqsxdd@gmail.com>
2018-01-15 18:58:10 -0800

    Update PowerOfThree.java

--- leetcode/DFS_BackTracking/PowerOfThree.java
+++ leetcode/DFS_BackTracking/PowerOfThree.java
@@ -1,1 +1,22 @@
+/**
+ * Refer to
+ * https://leetcode.com/problems/power-of-three/description/
+ * Given an integer, write a function to determine if it is a power of three.
 
+    Follow up:
+    Could you do it without using any loop / recursion?
+ *
+ *
+ * Solution
+*/
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n == 0) {
+            return false;
+        }
+        while(n % 3 == 0) {
+            n = n / 3;
+        }
+        return n == 1;
+    }
+}
