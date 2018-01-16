8be9b89f672d2e958269b90bdce109ad1edba681
lampardchelsea <zqqsxdd@gmail.com>
2018-01-15 18:32:34 -0800

    Update PowerOfTwo.java

--- leetcode/DFS_BackTracking/PowerOfTwo.java
+++ leetcode/DFS_BackTracking/PowerOfTwo.java
@@ -40,8 +40,22 @@ class Solution {
 }
 
 
-
-
+// Solution 3: Bit Operation
+/**
+    If n is the power of two:
+    n = 2 ^ 0 = 1 = 0b0000...00000001, and (n - 1) = 0 = 0b0000...0000.
+    n = 2 ^ 1 = 2 = 0b0000...00000010, and (n - 1) = 1 = 0b0000...0001.
+    n = 2 ^ 2 = 4 = 0b0000...00000100, and (n - 1) = 3 = 0b0000...0011.
+    n = 2 ^ 3 = 8 = 0b0000...00001000, and (n - 1) = 7 = 0b0000...0111.
+    we have n & (n-1) == 0b0000...0000 == 0
+    Otherwise, n & (n-1) != 0.
+    For example, n =14 = 0b0000...1110, and (n - 1) = 13 = 0b0000...1101.
+*/
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return n>0 && ((n & (n-1)) == 0);
+    }
+}
 
 
 
