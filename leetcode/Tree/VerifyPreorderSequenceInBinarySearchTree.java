78c91098fb703e3ede715b2028aba17f537a4eda
lampardchelsea <zqqsxdd@gmail.com>
2018-01-18 13:06:17 -0800

    Update VerifyPreorderSequenceInBinarySearchTree.java

--- leetcode/Tree/VerifyPreorderSequenceInBinarySearchTree.java
+++ leetcode/Tree/VerifyPreorderSequenceInBinarySearchTree.java
@@ -52,27 +52,28 @@ public class VerifyPreorderSequenceInBinarySearchTree {
 	    helper(result, node.right);
 	}
 	
-	public boolean verifyPreorder(int[] preorder) {
-        Stack<Integer> stk = new Stack<Integer>();
-        // 初始化最小值为最小整数
-        int min = Integer.MIN_VALUE;
-        for(int num : preorder){
-            // 违反最小值限定则是无效的
-            if(num < min) return false;
-            // 将路径中所有小于当前的数pop出来并更新最小值
-            while(!stk.isEmpty() && num > stk.peek()){
-                min = stk.pop();
+	// preorder = 10, 5, 2, 6, 12
+        public boolean verifyPreorder(int[] preorder) {
+	    Stack<Integer> stk = new Stack<Integer>();
+	    // 初始化最小值为最小整数
+	    int min = Integer.MIN_VALUE;
+	    for(int num : preorder){
+                // 违反最小值限定则是无效的
+		if(num < min) return false;
+		// 将路径中所有小于当前的数pop出来并更新最小值
+		while(!stk.isEmpty() && num > stk.peek()){
+	            min = stk.pop();
+		}
+		// 将当前值push进去
+		stk.push(num);
             }
-            // 将当前值push进去
-            stk.push(num);
-        }
-        return true;
-    }
+	    return true;
+	}
 	
 	public static void main(String[] args) {
-		VerifyPreorderSequenceInBinarySearchTree v = new VerifyPreorderSequenceInBinarySearchTree();
-		/**
-		    10
+	    VerifyPreorderSequenceInBinarySearchTree v = new VerifyPreorderSequenceInBinarySearchTree();
+	    /**
+		10
 	       /  \
 	      5    12
 	     / \
