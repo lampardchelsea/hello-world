57c27cf7db587de77249c2b684ba93ec46be54bc
lampardchelsea <zqqsxdd@gmail.com>
2018-01-18 21:08:41 -0800

    Update VerifyPreorderSerializationOfABinaryTree.java

--- leetcode/Tree/VerifyPreorderSerializationOfABinaryTree.java
+++ leetcode/Tree/VerifyPreorderSerializationOfABinaryTree.java
@@ -95,3 +95,18 @@ class Solution {
         return count == 0;
     }
 }
+
+
+// Solution 2: Leaves Nonleaves
+// Refer to
+/**
+ Nice solution. My solution is quite similar to yours.
+ If we treat null's as leaves, then the binary tree will always be full. A full binary tree has 
+ a good property that # of leaves = # of nonleaves + 1. Since we are given a pre-order serialization, 
+ we just need to find the shortest prefix of the serialization sequence satisfying the property above. 
+ If such prefix does not exist, then the serialization is definitely invalid; otherwise, the serialization 
+ is valid if and only if the prefix is the entire sequence.
+*/
+
+
+
