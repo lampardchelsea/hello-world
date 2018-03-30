a82f0793e07e00f1028d62637feb4d2fdd0df41d
lampardchelsea <zqqsxdd@gmail.com>
2018-03-30 03:13:24 -0700

    Update PartitionLabels.java

--- leetcode/HashTable/PartitionLabels.java
+++ leetcode/HashTable/PartitionLabels.java
@@ -45,7 +45,32 @@ class Solution {
 }
 
 // Solution 2: Java 2 pass O(n) time O(1) space, extending end pointer solution
+// (1) traverse the string record the last index of each char.
+// (2) using pointer to record end of the current sub string.
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        if(S == null || S.length() == 0){
+            return null;
+        }
+        List<Integer> list = new ArrayList<>();
+        int[] map = new int[26];  // record the last index of the each char
 
+        for(int i = 0; i < S.length(); i++){
+            map[S.charAt(i)-'a'] = i;
+        }
+        // record the end index of the current sub string
+        int last = 0;
+        int start = 0;
+        for(int i = 0; i < S.length(); i++){
+            last = Math.max(last, map[S.charAt(i)-'a']);
+            if(last == i){
+                list.add(last - start + 1);
+                start = last + 1;
+            }
+        }
+        return list;
+    }
+}
 
 
 
