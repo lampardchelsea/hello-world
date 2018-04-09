a99ec22aa2657d98b54e8ded5470a4f72632185a
lampardchelsea <zqqsxdd@gmail.com>
2018-04-08 17:24:19 -0700

    Update MinimumWindowSubstring.java

--- leetcode/String/MinimumWindowSubstring.java
+++ leetcode/String/MinimumWindowSubstring.java
@@ -28,9 +28,72 @@ import java.util.Map;
  * 用一个数组来保存每个字符出现的次数，比哈希表容易
  * 保存结果子串的起始点初值为-1，方便最后判断是否有正确结果
  * 
+ * https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-10-line-template-that-can-solve-most-'substring'-problems/25861
+ * https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-10-line-template-that-can-solve-most-'substring'-problems/25848
  * https://discuss.leetcode.com/topic/30941/here-is-a-10-line-template-that-can-solve-most-substring-problems
  * 
  */
+class Solution {
+    public String minWindow(String s, String t) {
+        /**
+         1. Use two pointers: start and end to represent a window.
+         2. Move end to find a valid window.
+         3. When a valid window is found, move start to find a smaller window.
+        */
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        // Use s and t together to fill map with all characters frequency
+        // charcater only present in s will set frequency as 0
+        for(char c : s.toCharArray()) {
+            map.put(c, 0);
+        }
+        for(char c : t.toCharArray()) {
+            if(map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);    
+            }
+        }
+        int start = 0;
+        int end = 0;
+        int minStart = 0;
+        // count represents the number of chars of t to be found in s.
+        int count = t.length();
+        int minLen = Integer.MAX_VALUE;
+        while(end < s.length()) {
+            // If char in s exists in t, decrease counter
+            if(map.get(s.charAt(end)) > 0) {
+                count--;
+            }
+            // Decrease map.get(s.charAt(end)). If character not exist in t, 
+            // s.charAt(end) in map will be -1.
+            map.put(s.charAt(end), map.get(s.charAt(end)) - 1);
+            end++;
+            // When we found a valid window, move start to find smaller window.
+            while(count == 0) {
+                if(end - start < minLen) {
+                    minLen = end - start;
+                    minStart = start;
+                }
+                map.put(s.charAt(start), map.get(s.charAt(start)) + 1);
+                // For character not exist in t, s.charAt(start) + 1 = (-1) + 1 = 0,
+                // only character exist in t, will be positive
+                if(map.get(s.charAt(start)) > 0) {
+                    count++;
+                }
+                start++;
+            }
+        }
+        if(minLen != Integer.MAX_VALUE) {
+            return s.substring(minStart, minStart + minLen);
+        }
+        return "";
+    }
+}
+
+
+
+
+
+
+
 public class MinimumWindowSubstring {
 	public String minWindow(String s, String t) {
 		char[] tChars = t.toCharArray();
