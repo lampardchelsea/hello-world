e3e8021b482a8138433f8150a84f80901b11452b
lampardchelsea <zqqsxdd@gmail.com>
2018-06-21 11:58:20 -0700

    Update PopulatingNextRightPointersInEachNode.java

--- leetcode/Tree/PopulatingNextRightPointersInEachNode.java
+++ leetcode/Tree/PopulatingNextRightPointersInEachNode.java
@@ -121,8 +121,37 @@ public class Solution {
     }
 }
 
-
-
+// More concise way and eliminate 'start' node as redundant
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if(root == null) {
+            return;
+        }
+        while(root != null) {
+            // Traverse start as 'cur' on current level
+            TreeLinkNode cur = root;
+            while(cur != null) {
+                if(cur.left != null) {
+                    cur.left.next = cur.right;
+                }
+                if(cur.next != null && cur.right != null) {
+                    cur.right.next = cur.next.left;
+                }
+                cur = cur.next;
+            }
+            // Move to next level
+            root = root.left;
+        }
+    }
+}
 
 
 
