c8f38d6d056857f0bd32ae9acd2702188fe06895
lampardchelsea <zqqsxdd@gmail.com>
2018-07-08 22:50:42 -0700

    Create SetMismatch.java

---
+++ leetcode/HashTable/SetMismatch.java
@@ -0,0 +1,40 @@
+/**
+ * Refer to
+ * https://leetcode.com/problems/set-mismatch/description/
+ * The set S originally contains numbers from 1 to n. But unfortunately, due to the data error, 
+   one of the numbers in the set got duplicated to another number in the set, which results in 
+   repetition of one number and loss of another number.
+
+    Given an array nums representing the data status of this set after the error. Your task is 
+    to firstly find the number occurs twice and then find the number that is missing. Return 
+    them in the form of an array.
+
+    Example 1:
+    Input: nums = [1,2,2,4]
+    Output: [2,3]
+    Note:
+    The given array size will in the range [2, 10000].
+    The given array's numbers won't have any order.
+
+ * 
+ * Solution
+ * https://leetcode.com/problems/set-mismatch/discuss/112425/Java-HashMap-Solution-O(n)-time-O(1)-space
+*/
+// Solution 1: HashMap solution
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for(int key = 1; key <= nums.length; key++) {
+            if(!map.containsKey(key)) {
+                result[1] = key;
+            } else if(map.get(key) > 1) {
+                result[0] = key;
+            }
+        }
+        return result;
+    }
+}
