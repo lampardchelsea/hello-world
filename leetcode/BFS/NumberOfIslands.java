ce9daab7d88955de1e46a4e04587dac8a6912d6b
lampardchelsea <zqqsxdd@gmail.com>
2018-07-18 16:17:23 -0700

    Update NumberOfIslands.java

--- leetcode/BFS/NumberOfIslands.java
+++ leetcode/BFS/NumberOfIslands.java
@@ -191,3 +191,37 @@ public class NumberOfIslands {
     
 }
 
+// Solution 3: DFS
+// Refer to
+// leetcode.com/problems/number-of-islands/discuss/56359/Very-concise-Java-AC-solution/57905
+class Solution {
+    int[] dx = {0,1,-1,0};
+    int[] dy = {1,0,0,-1};
+    int result = 0;
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
+            return 0;
+        }
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == '1') {
+                    result++;
+                    dfs(grid, i, j);    
+                }
+            }
+        }
+        return result;
+    }
+    
+    private void dfs(char[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        for(int k = 0; k < 4; k++) {
+            int next_i = i + dx[k];
+            int next_j = j + dy[k];
+            dfs(grid, next_i, next_j);
+        }
+    }
+}
