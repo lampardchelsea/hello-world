5143a904879d3b2fddf1bfe29fbd1131d8b5fb68
lampardchelsea <zqqsxdd@gmail.com>
2019-04-03 12:31:27 -0700

    Update StringToInteger.java

--- leetcode/String/StringToInteger.java
+++ leetcode/String/StringToInteger.java
@@ -53,3 +53,56 @@ public class Solution {
         return base * sign;
     }
 }
+
+// Another style (More easy to understand for checking Integer Max / Min overflow part)
+// Refer to
+// https://www.programcreek.com/2012/12/leetcode-string-to-integer-atoi/
+class Solution {
+    public int myAtoi(String str) {
+        // Situation need to handle
+        // (1) Remove leading white space
+        // (2) Check significant
+        // (3) Check char valid or not
+        // (4) Integer max or min overflow (by using double first then convert to int)
+        if(str == null || str.length() < 1) {
+            return 0;
+        }
+        // trim white spaces
+        str = str.trim();
+        // In case after removing leading and backing space left as empty string
+        if(str.length() > 0) {
+            char flag = '+';
+            // check negative or positive
+            int i = 0;
+            if(str.charAt(0) == '-') {
+                flag = '-';
+                i++;
+            } else if(str.charAt(0) == '+') {
+                i++;
+            }
+            // Why was double used for result?
+            // Integer has range [-2147483648 , 2147483647] in Java. If the number is
+            // -214748364, the algorithm processes it as a positive integer and then change
+            // sign. The number would exceed the integer range if an integer is used.
+            // use double to store result
+            double result = 0;
+            // calculate value
+            while(str.length() > i && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                result = result * 10 + (str.charAt(i) - '0');
+                i++;
+            }
+            if(flag == '-') {
+                result = -result;
+            }
+            // handle max and min
+            if(result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if(result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            return (int) result;  
+        }
+        return 0;
+    }
+}
