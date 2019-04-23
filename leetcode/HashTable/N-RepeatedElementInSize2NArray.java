2ca5c5e907d329f3881e4bbf5b00de5f6c68d41a
lampardchelsea <zqqsxdd@gmail.com>
2019-04-22 21:18:47 -0700

    Update N-RepeatedElementInSize2NArray.java

--- leetcode/HashTable/N-RepeatedElementInSize2NArray.java
+++ leetcode/HashTable/N-RepeatedElementInSize2NArray.java
@@ -39,3 +39,32 @@ class Solution {
         return (sum - sumOfUniqueN) / (A.length / 2 - 1);
     }
 }
+
+// Solution 2:
+// Refer to
+// https://leetcode.com/problems/n-repeated-element-in-size-2n-array/solution/
+/**
+ Approach 2: Compare
+Intuition and Algorithm
+If we ever find a repeated element, it must be the answer. Let's call this answer the major element.
+Consider all subarrays of length 4. There must be a major element in at least one such subarray.
+This is because either:
+There is a major element in a length 2 subarray, or;
+Every length 2 subarray has exactly 1 major element, which means that a length 4 subarray that 
+begins at a major element will have 2 major elements.
+Thus, we only have to compare elements with their neighbors that are distance 1, 2, or 3 away
+*/
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for N-Repeated Element in Size 2N Array.
+// Memory Usage: 41.8 MB, less than 13.27% of Java online submissions for N-Repeated Element in Size 2N Array
+class Solution {
+    public int repeatedNTimes(int[] A) {
+        for(int i = 1; i <= 3; i++) {
+            for(int j = 0; j < A.length - i; j++) {
+                if(A[j] == A[j + i]) {
+                    return A[j];
+                }
+            }
+        }
+        return -1;
+    }
+}
