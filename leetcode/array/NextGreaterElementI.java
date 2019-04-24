73e201446033c866092dac1866c3bf81813a7484
lampardchelsea <zqqsxdd@gmail.com>
2019-04-23 20:43:13 -0700

    Update NextGreaterElementI.java

--- leetcode/array/NextGreaterElementI.java
+++ leetcode/array/NextGreaterElementI.java
@@ -61,3 +61,33 @@ class Solution {
 }
 
 // Solution 2: Elegant using stack to scan array with O(n) time
+// Refer to
+// https://leetcode.com/problems/next-greater-element-i/discuss/97595/Java-10-lines-linear-time-complexity-O(n)-with-explanation
+/**
+ Key observation:
+Suppose we have a decreasing sequence followed by a greater number
+For example [5, 4, 3, 2, 1, 6] then the greater number 6 is the next greater element 
+for all previous numbers in the sequence
+We use a stack to keep a decreasing sub-sequence, whenever we see a number x greater than stack.peek() 
+we pop all elements less than x and for all the popped ones, their next greater element is x
+For example [9, 8, 7, 3, 2, 1, 6]
+The stack will first contain [9, 8, 7, 3, 2, 1] and then we see 6 which is greater than 1 so we pop 
+1 2 3 whose next greater element should be 6
+*/
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // Map from x to next greater element of x
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); 
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int num : nums2) {
+            while(!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        for(int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return nums1;
+    }
+}
