eb31e068f24b61940f8446c7a1d1b8a58e83fb91
lampardchelsea <zqqsxdd@gmail.com>
2019-04-29 01:47:29 -0700

    Update FindLargestValueInEachTreeRow.java

--- leetcode/Tree/FindLargestValueInEachTreeRow.java
+++ leetcode/Tree/FindLargestValueInEachTreeRow.java
@@ -53,3 +53,38 @@ class Solution {
         return result;
     }
 }
+
+// Solution 2: DFS
+// Refer to
+// https://leetcode.com/problems/find-largest-value-in-each-tree-row/discuss/98971/9ms-JAVA-DFS-solution 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        helper(root, res, 0);
+        return res;
+    }
+    
+    private void helper(TreeNode root, List<Integer> res, int d){
+        if(root == null) {
+            return;
+        }
+        //expand list size
+        if(d == res.size()) {
+            res.add(root.val);
+        } else {
+        //or set value
+            res.set(d, Math.max(res.get(d), root.val));
+        }
+        helper(root.left, res, d+1);
+        helper(root.right, res, d+1);
+    }
+}
