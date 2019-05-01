113a09975639ea316918ac2b4b9f0bb9946f6d29
lampardchelsea <zqqsxdd@gmail.com>
2019-05-01 02:25:06 -0700

    Update TargetSum.java

--- leetcode/DynamicProgramming/TargetSum.java
+++ leetcode/DynamicProgramming/TargetSum.java
@@ -95,3 +95,23 @@ class Solution {
     }
 }
 
+// Solution 2: 1D array DP
+// Refer to
+// https://leetcode.com/problems/target-sum/discuss/97334/Java-(15-ms)-C++-(3-ms)-O(ns)-iterative-DP-solution-using-subset-sum-with-explanation/165404
+class Solution {
+    public int findTargetSumWays(int[] nums, int s) {
+        int sum = 0;
+        for (int n : nums)
+            sum += n;
+        return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s + sum) >>> 1); 
+    }   
+
+    public int subsetSum(int[] nums, int s) {
+        int[] dp = new int[s + 1]; 
+        dp[0] = 1;
+        for (int n : nums)
+            for (int i = s; i >= n; i--)
+                dp[i] += dp[i - n]; 
+        return dp[s];
+    } 
+}
