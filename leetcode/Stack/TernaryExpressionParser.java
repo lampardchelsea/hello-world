4b053b4cbb691d18f6cec2439ad7a42ba966e56b
lampardchelsea <zqqsxdd@gmail.com>
2019-05-06 00:02:05 -0700

    Update TernaryExpressionParser.java

--- leetcode/Stack/TernaryExpressionParser.java
+++ leetcode/Stack/TernaryExpressionParser.java
@@ -34,8 +34,45 @@ Explanation: The conditional expressions group right-to-left. Using parenthesis,
              "(T ? (T ? F : 5) : 3)"                   "(T ? (T ? F : 5) : 3)"
           -> "(T ? F : 3)"                 or       -> "(T ? F : 5)"
           -> "F"                                    -> "F"
+*/
 
-Solution:
+// Style 1: Stack but not insert '?' when encounter, need change i condition 
+// to >= 1 since char[i - 1] means the character before '?' which used to
+// make judgement
+public class Solution {
+    public static String parseTernary(String expression) {
+        if(expression == null || expression.length() == 0) {
+            return "";
+        }
+        char[] chars = expression.toCharArray();
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = chars.length - 1; i >= 1; i--) {
+            char c = chars[i];
+            if(c != '?') {
+                stack.push(c);
+            } else {
+                char temp1 = stack.pop();
+                stack.pop();
+                char temp2 = stack.pop();
+                if(chars[i - 1] == 'T') {
+                    stack.push(temp1);
+                    i--;
+                } else {
+                    stack.push(temp2);
+                    i--;
+                }
+            }
+        }
+        return String.valueOf(stack.pop());
+	   }
+}
+
+
+// Style 2: Stack insert '?' but pop it out after insert
+// Refer to
+// https://yeqiuquan.blogspot.com/2017/07/439-ternary-expression-parser.html
+/**
+ Solution:
 We push each character of the expression into a stack reversely.
 If the last character in the stack is ''?", it means the answer to evaluate the current character is in the stack.
 We pop up the two potential answer and evaluate it.
@@ -43,3 +80,29 @@ After that, we push back the answer.
 If the last character in the stack is not "?", we push the current character into the stack.
 Finally, the answer is the last element left in the stack.
 */
+public class Solution {
+    public String parseTernary(String expression) {
+        if (expression == null || expression.length() == 0) {
+            return "";
+        }
+        Stack<Character> stack = new Stack<>();
+        for (int i = expression.length() - 1; i >= 0; i--) {
+            char c = expression.charAt(i);
+            if (!stack.isEmpty() && stack.peek() == '?') {
+                stack.pop(); // pop out '?'
+                char first = stack.pop();
+                stack.pop();
+                char second = stack.pop();
+                if (c == 'T') {
+                    stack.push(first);
+                }
+                else {
+                    stack.push(second);
+                }
+            } else {
+                stack.push(c); // also push '?' onto stack first but later pop out
+            }
+        }
+        return String.valueOf(stack.pop());
+    }
+}
