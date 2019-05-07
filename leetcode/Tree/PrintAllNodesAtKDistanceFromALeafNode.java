347ec6751f222864d25b97597ee00d6cbf023a47
lampardchelsea <zqqsxdd@gmail.com>
2019-05-07 04:32:54 -0700

    Create PrintAllNodesAtKDistanceFromALeafNode.java

---
+++ leetcode/Tree/PrintAllNodesAtKDistanceFromALeafNode.java
@@ -0,0 +1,10 @@
+/**
+ Refer to
+ https://www.geeksforgeeks.org/print-nodes-distance-k-leaf-node/
+ Given a Binary Tree and a positive integer k, print all nodes that are distance k from a leaf node.
+
+Here the meaning of distance is different from previous post. Here k distance from a leaf means 
+k levels higher than a leaf node. For example if k is more than height of Binary Tree, then nothing 
+should be printed. Expected time complexity is O(n) where n is the number nodes in the given Binary Tree.
+*/
+// Solution 1:
