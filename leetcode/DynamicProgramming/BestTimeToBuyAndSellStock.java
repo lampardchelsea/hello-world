d32d2f16993bebcc1c91587165db85322ef78215
lampardchelsea <zqqsxdd@gmail.com>
2019-07-16 19:36:05 -0700

    Update BestTimeToBuyAndSellStock.java

--- leetcode/DynamicProgramming/BestTimeToBuyAndSellStock.java
+++ leetcode/DynamicProgramming/BestTimeToBuyAndSellStock.java
@@ -19,6 +19,66 @@ Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 // Refer to
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/39038/Kadane's-Algorithm-Since-no-one-has-mentioned-about-this-so-far-%3A)-(In-case-if-interviewer-twists-the-input)
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/39038/Kadane's-Algorithm-Since-no-one-has-mentioned-about-this-so-far-:)-(In-case-if-interviewer-twists-the-input)/36798
+/**
+ The logic to solve this problem is same as "max subarray problem" using Kadane's Algorithm. 
+ Since no body has mentioned this so far, I thought it's a good thing for everybody to know.
+
+All the straight forward solution should work, but if the interviewer twists the question 
+slightly by giving the difference array of prices, Ex: for {1, 7, 4, 11}, if he gives 
+{0, 6, -3, 7}, you might end up being confused.
+
+Here, the logic is to calculate the difference (maxCur += prices[i] - prices[i-1]) of the 
+original array, and find a contiguous subarray giving maximum profit. If the difference falls 
+below 0, reset it to zero.
+
+*maxCur = current maximum value
+*maxSoFar = maximum value found so far
+
+A more clear explanation on why sum of subarray works.:
+
+Suppose we have original array:
+[a0, a1, a2, a3, a4, a5, a6]
+
+what we are given here(or we calculate ourselves) is:
+[b0, b1, b2, b3, b4, b5, b6]
+
+where,
+b[i] = 0, when i == 0
+b[i] = a[i] - a[i - 1], when i != 0
+
+suppose if a2 and a6 are the points that give us the max difference (a2 < a6)
+then in our given array, we need to find the sum of sub array from b3 to b6.
+
+b3 = a3 - a2
+b4 = a4 - a3
+b5 = a5 - a4
+b6 = a6 - a5
+
+adding all these, all the middle terms will cancel out except two
+i.e.
+
+b3 + b4 + b5 + b6 = a6 - a2
+
+a6 - a2 is the required solution.
+
+so we need to find the largest sub array sum to get the most profit
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxCur = 0;
+        int maxSoFar = 0;
+        for(int i = 1; i < prices.length; i++) {
+            maxCur = Math.max(0, maxCur += (prices[i] - prices[i - 1]));
+            maxSoFar = Math.max(maxSoFar, maxCur);
+        }
+        return maxSoFar;
+    }
+}
+
+
+// Refer to
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solution/
 // Approach 1: Brute Force
 public class Solution {
