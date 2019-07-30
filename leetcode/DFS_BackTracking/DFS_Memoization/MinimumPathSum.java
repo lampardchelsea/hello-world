c2bf9777cc998b77bdbce60a39f861b685593128
lampardchelsea <zqqsxdd@gmail.com>
2019-07-29 22:08:13 -0700

    Update MinimumPathSum.java

--- leetcode/DFS_BackTracking/DFS_Memoization/MinimumPathSum.java
+++ leetcode/DFS_BackTracking/DFS_Memoization/MinimumPathSum.java
@@ -17,6 +17,33 @@ Output: 7
 Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 */
 
+// New try
+// Refer to
+// https://leetcode.com/problems/minimum-path-sum/discuss/23471/DP-with-O(N*N)-space-complexity/135764
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if(grid == null || grid.length == 0) {
+            return 0;
+        }
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for(int i = 1; i < rows; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+        for(int i = 1; i < cols; i++) {
+            grid[0][i] += grid[0][i - 1];
+        }
+        for(int i = 1; i < rows; i++) {
+            for(int j = 1; j < cols; j++) {
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[rows - 1][cols - 1];
+    }
+}
+
+
+
 // Solution 1: DP (backtracking) + Memoization
 // Refer to
 // https://loveforprogramming.quora.com/Backtracking-Memoization-Dynamic-Programming
