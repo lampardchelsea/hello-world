ead7a50c291dd920201b59a6bb645f08c9ae9f93
lampardchelsea <zqqsxdd@gmail.com>
2019-08-12 03:40:34 -0700

    Create NumberOfDiceRollsWithTargetSum.java

---
+++ leetcode/DynamicProgramming/NumberOfDiceRollsWithTargetSum.java
@@ -0,0 +1,137 @@
+/**
+ Refer to
+ https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/
+ You have d dice, and each die has f faces numbered 1, 2, ..., f.
+
+Return the number of possible ways (out of fd total ways) modulo 10^9 + 7 to roll the dice so 
+the sum of the face up numbers equals target.
+
+Example 1:
+Input: d = 1, f = 6, target = 3
+Output: 1
+Explanation: 
+You throw one die with 6 faces.  There is only one way to get a sum of 3.
+
+Example 2:
+Input: d = 2, f = 6, target = 7
+Output: 6
+Explanation: 
+You throw two dice, each with 6 faces.  There are 6 ways to get a sum of 7:
+1+6, 2+5, 3+4, 4+3, 5+2, 6+1.
+
+Example 3:
+Input: d = 2, f = 5, target = 10
+Output: 1
+Explanation: 
+You throw two dice, each with 5 faces.  There is only one way to get a sum of 10: 5+5.
+
+Example 4:
+Input: d = 1, f = 2, target = 3
+Output: 0
+Explanation: 
+You throw one die with 2 faces.  There is no way to get a sum of 3.
+
+Example 5:
+Input: d = 30, f = 30, target = 500
+Output: 222616187
+Explanation: 
+The answer must be returned modulo 10^9 + 7.
+
+Constraints:
+1 <= d, f <= 30
+1 <= target <= 1000
+*/
+
+// Solution 1 and 2: Two styles of TLE native DFS
+// Refer to
+// https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/discuss/355841/Java-Memo-DFS
+// Style 1: 'result' as member variable with void return helper
+class Solution {
+    //int result = 0;
+    int MOD = 1000000007;
+    
+    int result = 0;
+    public int numRollsToTarget(int d, int f, int target) {
+        helper(d, f, target);
+        return result;
+    }
+    
+    private void helper(int d, int f, int target) {
+        if(d == 0 && target == 0) {
+            result++;
+            return;
+        }
+        if(d == 0 || target == 0) {
+            return;
+        }
+        for(int i = 1; i <= f; i++) {
+            if(target >= i) {
+                helper(d - 1, f, target - i); 
+            } else {
+                break;
+            }
+            
+        }
+        return;
+    }
+}
+
+// Style 2: 'result' as return variable on helper method
+class Solution {
+    public int numRollsToTarget(int d, int f, int target) {
+        return helper(d, f, target);
+    }
+    
+    private int helper(int d, int f, int target) {
+        int result = 0;
+        if(d == 0 && target == 0) {
+            return 1;
+        }
+        if(d == 0 || target == 0) {
+            return 0;
+        }
+        for(int i = 1; i <= f; i++) {
+            if(target >= i) {
+                result = (result + (helper(d - 1, f, target - i) % MOD)) % MOD; 
+            } else {
+                break;
+            }
+            
+        }
+        return result;
+    }
+}
+
+// DFS Memoization (downgrade from 2D DP to 1D DP)
+// Refer to
+// https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/discuss/355841/Java-Memo-DFS
+class Solution {    
+    int MOD = 1000000007;
+    Map<String, Integer> memo = new HashMap<>();
+    public int numRollsToTarget(int d, int f, int target) {
+        if (d == 0 && target == 0) {
+            return 1;
+        }
+        if (d == 0) {
+            return 0;
+        }
+        if (target == 0) {
+            return 0;
+        }
+        String str = "" + d + " " + target;
+        if (memo.containsKey(str)) {
+           return memo.get(str);
+        }
+        int res = 0;
+        for (int i = 1; i <= f; i++) {
+            if (target >= i) {
+                res = (res + (numRollsToTarget(d - 1, f, target - i) % MOD)) % MOD;
+            } else {
+                break;
+            }
+        }
+        memo.put(str, res);
+        return res;
+    }
+}
+
