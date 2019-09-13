be0de3745f8c54079ab0bad2fa48f299ae1a98fe
lampardchelsea <zqqsxdd@gmail.com>
2019-09-13 12:35:50 -0700

    Update RestoreIPAddress.java

--- leetcode/String/RestoreIPAddress.java
+++ leetcode/String/RestoreIPAddress.java
@@ -55,3 +55,74 @@ public class RestoreIPAddress {
     	
     }
 }
+
+
+
+// New try with DFS + backtracking
+// Definition of valid IP address
+// Refer to
+// https://leetcode.com/problems/restore-ip-addresses/discuss/31165/What-is-the-definition-of-a-valid-IP-address
+/**
+ Definition of valid IP address:
+1. The length of the ip without '.' should be equal to the length of s;
+2. The digit order of ip should be same as the digit order of s;
+3. Each part separated by the '.' should not start with '0' except only '0';
+4. Each part separared by the '.' should not larger than 255;
+*/
+// Solution 1: DFS + backtracking + StringBuilder + index increasing from 0 to string length
+// Refer to
+// https://leetcode.com/problems/restore-ip-addresses/discuss/31095/Backtracking-solution-in-Java-easy-to-understand/29885
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> result = new ArrayList<String>();
+        if(s == null || s.length() == 0 || s.length() > 12) {
+            return result;
+        }
+        helper(result, new StringBuilder(), s, 0, 0);
+        return result;
+    }
+    
+    private void helper(List<String> result, StringBuilder sb, String s, int index, int dotCount) {
+        if(dotCount > 3) {
+            return;
+        }
+        if(index == s.length() && dotCount == 3) {
+            result.add(sb.toString());
+            return;
+        }
+        for(int i = index; i < s.length(); i++) {
+            String token = s.substring(index, i + 1);
+            if(isValid(token)) {
+                int len = sb.length();
+                sb.append(token);
+                if(i + 1 != s.length()) {
+                    sb.append(".");
+                    helper(result, sb, s, i + 1, dotCount + 1);                   
+                } else {
+                    helper(result, sb, s, i + 1, dotCount);
+                }
+                sb.setLength(len);
+            }
+        }
+    }
+    
+    /**
+     Definition of valid IP address:
+     1. The length of the ip without '.' should be equal to the length of s;
+     2. The digit order of ip should be same as the digit order of s;
+     3. Each part separated by the '.' should not start with '0' except only '0';
+     4. Each part separared by the '.' should not larger than 255;
+     5. Valid IP address should contain 3 dots (4 tokens)
+    */
+    private boolean isValid(String token) {
+        if(token.length() > 3 || Integer.valueOf(token) > 255 
+           || (token.length() > 1 && token.charAt(0) == '0')) {
+            return false;
+        }
+        return true;
+    }
+}
+
+// Style 2:
+// Refer to
+// https://massivealgorithms.blogspot.com/2014/06/leetcode-restore-ip-addresses-darrens.html
