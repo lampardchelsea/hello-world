2d03751b199bedaf55b6b7475954822cc1416231
lampardchelsea <zqqsxdd@gmail.com>
2019-09-13 02:25:12 -0700

    Update WordSearch.java

--- lintcode/DFS/VideoExamples/WordSearch.java
+++ lintcode/DFS/VideoExamples/WordSearch.java
@@ -69,6 +69,47 @@ class Solution {
 }
 
 // Solution without additional space required
+// Style 1:
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        if(board == null || board.length == 0) {
+            return false;
+        }
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(helper(board, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    int[] dx = new int[]{0,0,1,-1};
+    int[] dy = new int[]{-1,1,0,0};
+    private boolean helper(char[][] board, int i, int j, String word, int index) {
+        if(index == word.length()) {
+            return true;
+        }
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length 
+           || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+        char c = board[i][j];
+        board[i][j] = '#';
+        for(int k = 0; k < 4; k++) {
+            if(helper(board, i + dx[k], j + dy[k], word, index + 1)) {
+                return true;
+            }
+        }
+        board[i][j] = c;
+        return false;
+    }
+}
+
+
+
+// Style 2:
 class Solution {
     int[] dx = {0, 0, 1, -1};
     int[] dy = {1, -1, 0, 0};
