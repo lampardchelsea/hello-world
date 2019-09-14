1e5396bc3d0144cb04fecac557a6a3aec844a304
lampardchelsea <zqqsxdd@gmail.com>
2019-09-14 15:01:46 -0700

    Update DifferentWaysToAddParentheses.java

--- leetcode/DFS_BackTracking/DifferentWaysToAddParentheses.java
+++ leetcode/DFS_BackTracking/DifferentWaysToAddParentheses.java
@@ -71,3 +71,79 @@ class Solution {
         return result;
     }
 }
+
+
+// New try with DFS, similar way as 95. Unique Binary Search Tree II
+// Refer to
+// https://leetcode.com/problems/different-ways-to-add-parentheses/discuss/66333/Java-recursive-(9ms)-and-dp-(4ms)-solution
+/**
+ I think it's more efficient to pre-parse the string because String.substring() is costly. 
+ I store the parsed string in a list, for example, if the string is 1+2+3+4, then the list will contain:
+
+"1", "+", "2", "+", "3", "+", "4"
+Personally I feel this is also more convenient because all integers occurs at even indices (0, 2, 4, 6) 
+and all operators are at odd indices (1, 3, 5).
+
+Then the problem is very similar to "Unique Binary Search Trees II". For each operator in the list, 
+we compute all possible results for entries to the left of that operator, which is List<Integer> left, 
+and also all possible results for entries to the right of that operator, namely List<Integer> right, 
+and combine the results. It can be achieved by recursion or more efficiently by dp.
+*/
+class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(input == null || input.length() == 0) {
+            return result;    
+        }
+        // Split string into numbers and operations
+        // be careful the numbers could be continuous digits
+        List<String> tokens = new ArrayList<String>();
+        for(int i = 0; i < input.length(); i++) {
+            String token = "";
+            while(i < input.length() && Character.isDigit(input.charAt(i))) {
+                token += input.charAt(i);
+                i++;
+            }
+            // Add number section
+            tokens.add(token);
+            // Add operate (and it could not be the last character in string)
+            if(i < input.length()) {
+            	tokens.add(String.valueOf(input.charAt(i)));
+            }         
+        }
+        result = helper(tokens, 0, tokens.size() - 1);
+        return result;
+    }
+    
+    private List<Integer> helper(List<String> tokens, int lo, int hi) {
+        List<Integer> result = new ArrayList<Integer>();
+        // Base case: only one number left when lo = hi
+        if(lo == hi) {
+            result.add(Integer.valueOf(tokens.get(lo)));
+            return result;
+        }
+        // Same process as 95.Unique Binary Search Tree II
+        // Scan all combinations between left and right sub-list divide by operate
+        for(int i = lo; i <= hi; i++) {
+            if(i % 2 == 1) {
+                String ops = tokens.get(i);
+                List<Integer> left = helper(tokens, lo, i - 1);
+                List<Integer> right = helper(tokens, i + 1, hi);
+                for(int l : left) {
+                    for(int r : right) {
+                        if(ops.equals("+")) {
+                            result.add(l + r);
+                        }
+                        if(ops.equals("*")) {
+                            result.add(l * r);
+                        }
+                        if(ops.equals("-")) {
+                            result.add(l - r);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
