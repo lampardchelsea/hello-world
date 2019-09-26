7b194c608c7b2e054009dff5e851f62a1f354dd3
lampardchelsea <zqqsxdd@gmail.com>
2019-09-26 03:46:18 -0700

    Update EqualSubsetSumPartition.java

--- leetcode/DynamicProgramming/Document/Pattern_1_0_1_Knapsack/EqualSubsetSumPartition.java
+++ leetcode/DynamicProgramming/Document/Pattern_1_0_1_Knapsack/EqualSubsetSumPartition.java
@@ -60,3 +60,63 @@ class Solution {
         return false;
     }
 }
+
+// Solution 2: Top-down Dynamic Programming with Memoization
+/**
+ Since we need to store the results for every subset and for every possible sum, therefore we 
+ will be using a two-dimensional array to store the results of the solved sub-problems. The 
+ first dimension of the array will represent different subsets and the second dimension will 
+ represent different ‘target’ that we can calculate from each subset. These two dimensions of 
+ the array can also be inferred from the two changing values (target and index) in our 
+ recursive function helper().
+ The above algorithm has time and space complexity of O(N*S), where ‘N’ represents total 
+ numbers and ‘S’ is the total sum of all the numbers.
+ Runtime: 3 ms, faster than 86.55% of Java online submissions for Partition Equal Subset Sum.
+ Memory Usage: 38 MB, less than 50.79% of Java online submissions for Partition Equal Subset Sum.
+*/
+class Solution {
+    public boolean canPartition(int[] nums) {
+        if(nums.length == 0) {
+            return false;
+        }
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i]; 
+        }         
+        // if 'sum' is a an odd number, we can't have two subsets with equal sum
+        if(sum % 2 != 0) {
+            return false;  
+        }
+        Boolean[][] dp = new Boolean[1 + nums.length][1 + sum / 2];
+        return helper(nums, sum/2, 0, dp);
+    }
+    
+    private boolean helper(int[] nums, int target, int index, Boolean[][] dp) {
+        if(target == 0) {
+            return true;
+        }
+        if(target != 0 && index >= nums.length) {
+            return false;
+        }
+        if(dp[index][target] != null) {
+            return dp[index][target];
+        }
+        if(nums[index] <= target) {
+            if(helper(nums, target - nums[index], index + 1, dp)) {
+                dp[index][target] = true;
+                return true;
+            }
+        }
+        if(helper(nums, target, index + 1, dp)) {
+            dp[index][target] = true;
+            return true;
+        }
+        dp[index][target] = false;
+        return false;
+    }
+}
+
+// Solution 3: Bottom-up Dynamic Programming
+/**
+ 
+*/
