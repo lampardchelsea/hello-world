d82ba02c595805a1c615f0af74879649ae66cef6
lampardchelsea <zqqsxdd@gmail.com>
2019-09-28 22:23:39 -0700

    Create CountOfSubsetSum.java

---
+++ leetcode/DynamicProgramming/Document/Pattern_1_0_1_Knapsack/CountOfSubsetSum.java
@@ -0,0 +1,28 @@
+/**
+ This issue similar to leetcode
+ Combination Sum IV
+ This Combination Sum question a little different than I, II, III, since it only requires counting the numbers of result,
+ not like I, II, III must use backtracking DFS to getting combination sets, so we can use DP (top down and bottom up) here
+ Refer to
+ https://leetcode.com/problems/combination-sum-iv/
+ Given an integer array with all positive numbers and no duplicates, find the number of possible combinations 
+ that add up to a positive integer target.
+
+Example:
+
+nums = [1, 2, 3]
+target = 4
+
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+
+Note that different sequences are counted as different combinations.
+Therefore the output is 7.
+*/
+
