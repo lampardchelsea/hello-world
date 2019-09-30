0ca7a06b8d01b39b49f0827a9130fb3c6273466d
lampardchelsea <zqqsxdd@gmail.com>
2019-09-30 03:12:53 -0700

    Update MinimumSubsetSumDifference.java

--- leetcode/DynamicProgramming/Document/Pattern_1_0_1_Knapsack/MinimumSubsetSumDifference.java
+++ leetcode/DynamicProgramming/Document/Pattern_1_0_1_Knapsack/MinimumSubsetSumDifference.java
@@ -47,4 +47,66 @@ class Solution {
     }
 }
 
-// Solution 2:
+// Solution 2: Native DFS (Another style)
+// Refer to
+// https://www.techiedelight.com/minimum-sum-partition-problem/
+class MinPartition {
+    public int findMin(int arr[]) {
+        return helper(arr, arr.length - 1, 0, 0);
+    }
+ 
+    // Partition the set S into two subsets S1, S2 such that the
+    // difference between the sum of elements in S1 and the sum
+    // of elements in S2 is minimized
+    public static int helper(int[] S, int n, int S1, int S2) {
+        // base case: if list becomes empty, return the absolute
+        // difference between two sets
+        if (n < 0) {
+            return Math.abs(S1 - S2);
+        }
+        // Case 1. include current item in the subset S1 and recur
+        // for remaining items (n - 1)
+        int include = helper(S, n - 1, S1 + S[n], S2);
+        // Case 2. exclude current item from subset S1 and recur for
+        // remaining items (n - 1)
+        int exclude = helper(S, n - 1, S1, S2 + S[n]);
+        return Math.min(include, exclude);
+    }
+}
+
+// Solution 3: Top-Down DP (DFS + Memoization)
+// Refer to
+// https://www.techiedelight.com/minimum-sum-partition-problem/
+class MinPartition {
+    public int findMin(int arr[]) {
+        Map<String, Integer> memo = new HashMap<String, Integer>();
+        return helper(arr, arr.length - 1, 0, 0, memo);
+    }    
+ 
+    // Partition the set S into two subsets S1, S2 such that the
+    // difference between the sum of elements in S1 and the sum
+    // of elements in S2 is minimized
+    public static int minPartition(int[] S, int n, int S1, int S2, Map<String, Integer> memo) {
+        // base case: if list becomes empty, return the absolute
+        // difference between two sets
+        if(n < 0) {
+            return Math.abs(S1 - S2);
+        }
+        // construct a unique map key from dynamic elements of the input
+        // Note that can uniquely identify the subproblem with n & S1 only,
+        // as S2 is nothing but S - S1 where S is sum of all elements
+        String key = n + "|" + S1;
+        // if sub-problem is seen for the first time, solve it and
+        // store its result in a map
+        if(!memo.containsKey(key)) {
+            // Case 1. include current item in the subset S1 and recur
+            // for remaining items (n - 1)
+            int include = minPartition(S, n - 1, S1 + S[n], S2, memo);
+            // Case 2. exclude current item from subset S1 and recur for
+            // remaining items (n - 1)
+            int exclude = minPartition(S, n - 1, S1, S2 + S[n], memo);
+            memo.put(key, Math.min(include, exclude));
+        }
+        return memo.get(key);
+    }
+}
