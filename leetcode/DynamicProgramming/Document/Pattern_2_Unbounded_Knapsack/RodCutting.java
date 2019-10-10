117a9a5453c35c7a36f24d0c9ac4f618dacd9d81
lampardchelsea <zqqsxdd@gmail.com>
2019-10-09 22:04:16 -0700

    Update RodCutting.java

--- leetcode/DynamicProgramming/Document/Pattern_2_Unbounded_Knapsack/RodCutting.java
+++ leetcode/DynamicProgramming/Document/Pattern_2_Unbounded_Knapsack/RodCutting.java
@@ -23,5 +23,33 @@ price    | 3   5   8   9  10  17  17  20
 // Refer to
 // https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
 /**
- 
+ A naive solution for this problem is to generate all configurations of different pieces and 
+ find the highest priced configuration. This solution is exponential in term of time complexity. 
+ Let us see how this problem possesses both important properties of a Dynamic Programming (DP) 
+ Problem and can efficiently solved using Dynamic Programming.
+
+1) Optimal Substructure:
+We can get the best price by making a cut at different positions and comparing the values 
+obtained after a cut. We can recursively call the same function for a piece obtained after a cut.
+
+Let cutRod(n) be the required (best possible price) value for a rod of length n. cutRod(n) can 
+be written as following.
+
+cutRod(n) = max(price[i] + cutRod(n-i-1)) for all i in {0, 1 .. n-1}
+
+2) Overlapping Subproblems
+Following is simple recursive implementation of the Rod Cutting problem. The implementation 
+simply follows the recursive structure mentioned above.
 */
+class Solution {
+    public int cutRod(int[] prices, int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, prices[i] + cutRod(prices, n - 1 - i));
+        }
+        return max;
+    }
+}
