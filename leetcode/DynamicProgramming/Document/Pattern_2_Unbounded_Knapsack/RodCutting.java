17bfcf6e1dcd7967bbbbc88a4e1fd4d74d2c468c
lampardchelsea <zqqsxdd@gmail.com>
2019-10-10 21:00:31 -0700

    Update RodCutting.java

--- leetcode/DynamicProgramming/Document/Pattern_2_Unbounded_Knapsack/RodCutting.java
+++ leetcode/DynamicProgramming/Document/Pattern_2_Unbounded_Knapsack/RodCutting.java
@@ -17,11 +17,12 @@ in eight pieces of length 1)
 length   | 1   2   3   4   5   6   7   8  
 --------------------------------------------
 price    | 3   5   8   9  10  17  17  20
+
+Output:
+Maximum Obtainable Value is 22
 */
 
-// Solution 1: Native DFS
-// Refer to
-// https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
+
 /**
  A naive solution for this problem is to generate all configurations of different pieces and 
  find the highest priced configuration. This solution is exponential in term of time complexity. 
@@ -40,7 +41,36 @@ cutRod(n) = max(price[i] + cutRod(n-i-1)) for all i in {0, 1 .. n-1}
 2) Overlapping Subproblems
 Following is simple recursive implementation of the Rod Cutting problem. The implementation 
 simply follows the recursive structure mentioned above.
+
+Considering the above implementation, following is recursion tree for a Rod of length 4.
+
+cR() ---> cutRod() 
+
+                          cR(4)
+                  /        /           
+                 /        /              
+             cR(3)       cR(2)     cR(1)   cR(0)
+            /  |         /         |
+           /   |        /          |  
+      cR(2) cR(1) cR(0) cR(1) cR(0) cR(0)
+     /        |          |
+    /         |          |   
+  cR(1) cR(0) cR(0)      cR(0)
+   /
+ /
+CR(0)
+
+In the above partial recursion tree, cR(2) is being solved twice. We can see that there are many 
+subproblems which are solved again and again. Since same suproblems are called again, this problem 
+has Overlapping Subprolems property. So the Rod Cutting problem has both properties (see this and this) 
+of a dynamic programming problem. Like other typical Dynamic Programming(DP) problems, recomputations 
+of same subproblems can be avoided by constructing a temporary array val[] in bottom up manner.
 */
+
+// Solution 1: Native DFS
+// Refer to
+// https://web.stanford.edu/class/archive/cs/cs161/cs161.1168/lecture12.pdf
+// https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
 class Solution {
     public int cutRod(int[] prices, int[] length, int n) {
         if (n <= 0) {
@@ -57,3 +87,10 @@ class Solution {
     // int prices[] = {1, 5, 8, 9, 10, 17, 17, 20}; 
     // int length[] = {1, 2, 3, 4, 5, 6, 7, 8};
 }
+
+// Solution 2: DP
+// Refer to
+// https://web.stanford.edu/class/archive/cs/cs161/cs161.1168/lecture12.pdf
+// https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
+
+
