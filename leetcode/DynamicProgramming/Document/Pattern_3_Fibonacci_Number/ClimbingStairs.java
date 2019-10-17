62b4fc59bcbed809819aad0b5085dd5eadc2483a
lampardchelsea <zqqsxdd@gmail.com>
2019-10-17 11:02:43 -0700

    Update ClimbingStairs.java

--- leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/ClimbingStairs.java
+++ leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/ClimbingStairs.java
@@ -21,6 +21,45 @@ Explanation: There are three ways to climb to the top.
 3. 2 steps + 1 step
 */
 
-// Solution 1:
+// Solution 1: Native DFS (TLE)
 // Refer to
-// 
+// https://leetcode.com/problems/climbing-stairs/discuss/163347/Python-3000DP-or-tm
+// https://leetcode.com/problems/climbing-stairs/discuss/25299/Basically-it's-a-fibonacci.
+class Solution {
+    public int climbStairs(int n) {
+        return helper(n);
+    }
+    
+    private int helper(int n) {
+        if(n <= 2) {
+            return n;
+        }
+        return helper(n - 1) + helper(n - 2);
+    }
+}
+
+// Solution 2: Top-down DP (DFS + Memoization)
+// Refer to
+// https://github.com/lampardchelsea/hello-world/blob/master/leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/FibonacciNumber.java
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Climbing Stairs.
+// Memory Usage: 32.9 MB, less than 5.26% of Java online submissions for Climbing Stairs.
+class Solution {
+    public int climbStairs(int n) {
+        Integer[] memo = new Integer[n + 1];
+        return helper(n, memo);
+    }
+    
+    private int helper(int n, Integer[] memo) {
+        if(memo[n] != null) {
+            return memo[n];
+        }
+        if(n <= 2) {
+            return n;
+        }
+        int result = helper(n - 1, memo) + helper(n - 2, memo);
+        memo[n] = result;
+        return result;
+    }
+}
+
+// Solution 3: 
