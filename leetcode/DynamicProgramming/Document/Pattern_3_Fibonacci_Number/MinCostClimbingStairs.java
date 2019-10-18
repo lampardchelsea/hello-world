816c6f790b9cf5c5c89b6f81ce49edaa84b62836
lampardchelsea <zqqsxdd@gmail.com>
2019-10-18 16:48:47 -0700

    Update MinCostClimbingStairs.java

--- leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/MinCostClimbingStairs.java
+++ leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/MinCostClimbingStairs.java
@@ -71,3 +71,20 @@ class Solution {
 // Solution 3: Bottom up DP
 // Refer to
 // https://leetcode.com/problems/min-cost-climbing-stairs/discuss/?currentPage=1&orderBy=most_votes&query=
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        if(cost == null || cost.length == 0) {
+            return 0;
+        }
+        if(cost.length == 1) {
+            return cost[0];
+        }
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+    }
+}
