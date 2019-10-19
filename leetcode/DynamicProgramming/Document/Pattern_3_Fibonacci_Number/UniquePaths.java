b523157adf983fe8a14b8c1a310f5292de652e72
lampardchelsea <zqqsxdd@gmail.com>
2019-10-19 13:11:26 -0700

    Update UniquePaths.java

--- leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/UniquePaths.java
+++ leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/UniquePaths.java
@@ -73,3 +73,31 @@ class Solution {
         return result;
     }
 }
+
+// Solution 3: Bottom up DP
+// Refer to
+// https://leetcode.com/problems/unique-paths/discuss/22954/C%2B%2B-DP
+// https://leetcode.com/problems/unique-paths/discuss/182143/Recursive-memoization-and-dynamic-programming-solutions
+/**
+ Since the robot can only move right and down, when it arrives at a point, it either arrives from left or above. 
+ If we use dp[i][j] for the number of unique paths to arrive at the point (i, j), then the state equation is 
+ dp[i][j] = dp[i][j - 1] + dp[i - 1][j]. Moreover, we have the base cases dp[0][j] = dp[i][0] = 1 for all valid i and j.
+*/
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for(int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
+
