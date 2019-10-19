515c0695da13b66119071c8a0a35b37e72353ec3
lampardchelsea <zqqsxdd@gmail.com>
2019-10-19 03:09:36 -0700

    Update DecodeWays.java

--- leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/DecodeWays.java
+++ leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/DecodeWays.java
@@ -84,4 +84,27 @@ class Solution {
 
 // Solution 3: Bottom up DP
 // Refer to
-// 
+// https://massivealgorithms.blogspot.com/2014/06/leetcode-decode-ways-darrens-blog.html
+class Solution {
+    public int numDecodings(String s) {
+        // I used a dp array of size n + 1 to save subproblem solutions. 
+        // dp[0] means an empty string will have one way to decode, 
+        // dp[1] means the way to decode a string of size 1. 
+        // I then check one digit and two digit combination and save the 
+        // results along the way. In the end, dp[n] will be the end result.
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for(int i = 2; i <= s.length(); i++) {
+            int oneDigit = Integer.valueOf(s.substring(i - 1, i));
+            int twoDigits = Integer.valueOf(s.substring(i - 2, i));
+            if(oneDigit >= 1 && oneDigit <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if(twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[s.length()];
+    }
+}
