367d78684df744ebdd9eb9504bd4b9c5049b5fc5
lampardchelsea <zqqsxdd@gmail.com>
2019-10-19 12:08:47 -0700

    Update UniquePaths.java

--- leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/UniquePaths.java
+++ leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/UniquePaths.java
@@ -26,4 +26,22 @@ Output: 28
 
 // Solution 1: Native DFS
 // Refer to
-// 
+// https://leetcode.com/problems/unique-paths/discuss/182143/Recursive-memoization-and-dynamic-programming-solutions
+class Solution {
+    public int uniquePaths(int m, int n) {
+        // Minus 1 because the real distance between
+        // top left corner and bottom right corner
+        // is m - 1 and n - 1
+        return helper(m - 1, n - 1);
+    }
+    
+    private int helper(int m, int n) {
+        if(m < 0 || n < 0) {
+            return 0;
+        }
+        if(m == 0 || m == 0) {
+            return 1;
+        }
+        return helper(m - 1, n) + helper(m, n - 1);
+    }
+}
