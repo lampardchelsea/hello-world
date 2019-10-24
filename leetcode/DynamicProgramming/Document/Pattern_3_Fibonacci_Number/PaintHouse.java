62781d401821eeb5de3a77708b163149d4569f5a
lampardchelsea <zqqsxdd@gmail.com>
2019-10-24 04:45:47 -0700

    Update PaintHouse.java

--- leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/PaintHouse.java
+++ leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/PaintHouse.java
@@ -21,4 +21,22 @@ Explanation: Paint house 0 into blue, paint house 1 into green, paint house 2 in
 
 // Solution 1:
 // Refer to
-// 
+// https://github.com/lampardchelsea/hello-world/blob/master/lintcode/DP/PaintHouse.java
+class Solution {
+    // A more clear way
+    public int minCost_2(int[][] costs) {
+        if (costs == null || costs.length == 0) {
+            return 0;
+        }
+        for (int i = 1; i < costs.length; i++) {
+            // 涂第一种颜色的话，上一个房子就不能涂第一种颜色，这样我们要在上一个
+            // 房子的第二和第三个颜色的最小开销中找最小的那个加上
+            costs[i][0] = costs[i][0] + Math.min(costs[i - 1][1], costs[i - 1][2]);
+            // 涂第二或者第三种颜色同理
+            costs[i][1] = costs[i][1] + Math.min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] = costs[i][2] + Math.min(costs[i - 1][0], costs[i - 1][1]);
+        }
+        // 返回涂三种颜色中开销最小的那个
+        return Math.min(Math.min(costs[costs.length - 1][0], costs[costs.length - 1][1]), costs[costs.length - 1][2]);
+    }
+}
