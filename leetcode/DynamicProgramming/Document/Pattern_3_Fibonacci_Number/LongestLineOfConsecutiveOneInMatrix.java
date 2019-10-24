5d34803a2dd22ec572854f985b3b1543056562fe
lampardchelsea <zqqsxdd@gmail.com>
2019-10-23 17:05:24 -0700

    Update LongestLineOfConsecutiveOneInMatrix.java

--- leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/LongestLineOfConsecutiveOneInMatrix.java
+++ leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/LongestLineOfConsecutiveOneInMatrix.java
@@ -65,3 +65,48 @@ class Solution {
 // Solution 2: 3D DP
 // Refer to
 // https://massivealgorithms.blogspot.com/2017/04/leetcode-562-longest-line-of.html
+// https://discuss.leetcode.com/topic/87197/java-o-nm-time-dp-solution
+/**
+ dp[i][j][k]表示从开头遍历到数字nums[i][j]为止，第k种情况的连续1的个数. 
+ k的值为0，1，2，3，分别对应水平，竖直，对角线和逆对角线这四种情况。
+ 更新dp数组过程:
+ 如果如果数字为0的情况直接跳过，然后水平方向就加上前一个的dp值，竖直方向加上上面一个数字的dp值，
+ 对角线方向就加上右上方数字的dp值，逆对角线就加上左上方数字的dp值，然后每个值都用来更新结果res.
+*/
+class Solution {
+    public int longestLine(int[][] M) {
+        if (M == null || M.length == 0) {
+            return 0;
+        }
+        int result = 0;
+        int[][][] dp = new int[M.length][M[0].length][4];
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M[0].length; j++) {
+                if (M[i][j] == 1) {
+                    for (int k = 0; k < 4; k++) {
+                        dp[i][j][k] = 1;
+                    }
+                    // Horizontal line
+                    if (j > 0 && M[i][j - 1] == 1) {
+                        dp[i][j][0] += dp[i][j - 1][0];
+                    }
+                    // Vertical line
+                    if (i > 0 && M[i - 1][j] == 1) {
+                        dp[i][j][1] += dp[i - 1][j][1];
+                    }
+                    // Diagonal line
+                    if (i > 0 && j > 0 && M[i - 1][j - 1] == 1) {
+                        dp[i][j][2] += dp[i - 1][j - 1][2];
+                    }
+                    // Anti-diagonal line
+                    if (i > 0 && j < M[0].length - 1 && M[i - 1][j + 1] == 1) {
+                        dp[i][j][3] += dp[i - 1][j + 1][3];
+                    }
+                    result = Math.max(result, Math.max(dp[i][j][0], dp[i][j][1]));
+                    result = Math.max(result, Math.max(dp[i][j][2], dp[i][j][3]));
+                }
+            }
+        }
+        return result;
+    }
+}
