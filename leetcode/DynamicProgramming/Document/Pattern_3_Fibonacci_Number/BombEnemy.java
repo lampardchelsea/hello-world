18396f482b18365325f0b39a2eb2829690b18bba
lampardchelsea <zqqsxdd@gmail.com>
2019-10-24 02:58:48 -0700

    Update BombEnemy.java

--- leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/BombEnemy.java
+++ leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/BombEnemy.java
@@ -15,6 +15,46 @@
  return 3. (Placing a bomb at (1,1) kills 3 enemies)
 */
 
-// Solution 1:
+// Solution 1: Native DFS
 // Refer to
 // https://massivealgorithms.blogspot.com/2016/06/leetcode-361-bomb-enemy.html
+class Solution {
+    public int maxKilledEnemies(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '0') {
+                    result = Math.max(result, helper(grid, i, j));
+                }
+            }
+        }
+        return result;
+    }
+
+    int[] dx = new int[] {0, 0, 1, -1};
+    int[] dy = new int[] {1, -1, 0, 0};
+    private int helper(char[][] grid, int i, int j) {
+        int result = 0;
+        for (int k = 0; k < 4; k++) {
+            int new_i = i + dx[k];
+            int new_j = j + dy[k];
+            while (new_i >= 0 && new_i < grid.length &&
+                new_j >= 0 && new_j < grid[0].length &&
+                grid[new_i][new_j] != 'W') {
+                if (grid[new_i][new_j] == 'E') {
+                    result++;
+                }
+                new_i += dx[k];
+                new_j += dy[k];
+            }
+        }
+        return result;
+    }
+}
+
+// Solution 2: DP
+// Refer to
+// 
