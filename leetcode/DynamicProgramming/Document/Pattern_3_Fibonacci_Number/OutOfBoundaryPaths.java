a87c62b4271c4dd2ea59d98289ef8cf14eba4200
lampardchelsea <zqqsxdd@gmail.com>
2019-10-26 04:02:15 -0700

    Update OutOfBoundaryPaths.java

--- leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/OutOfBoundaryPaths.java
+++ leetcode/DynamicProgramming/Document/Pattern_3_Fibonacci_Number/OutOfBoundaryPaths.java
@@ -255,5 +255,62 @@ class Solution {
 // Refer to
 // https://leetcode.com/articles/out-of-boundary-paths/
 /**
- 
+ The idea behind this approach is that if we can reach some position in x moves, we can reach all its adjacent 
+ positions in x+1 moves. Based on this idea, we make use of a 2-D dp array to store the number of ways in which 
+ a particular position can be reached. dp[i][j] refers to the number of ways the position corresponding to the 
+ indices (i,j) can be reached given some particular number of moves.
+
+Now, if the current dp array stores the number of ways the various positions can be reached by making use of x-1 
+moves, in order to determine the number of ways the position (i,j) can be reached by making use of xx moves, we 
+need to update the corresponding dpdp entry as dp[i][j] = dp[i-1][j] + dp[i+1][j] + dp[i][j-1] + dp[i][j+1] taking 
+care of boundary conditions. This happens because we can reach the index (i,j) from any of the four adjacent 
+positions and the total number of ways of reaching the index (i,j) in x moves is the sum of the ways of reaching 
+the adjacent positions in x-1 moves.
+
+But, if we alter the dp array, now some of the entries will correspond to x-1 moves and the updated ones will 
+correspond to x moves. Thus, we need to find a way to tackle this issue. So, instead of updating the dp array for 
+the current(x) moves, we make use of a temporary 2-D array temptemp to store the updated results for x moves, 
+making use of the results obtained for dp array corresponding to x-1 moves. After all the entries for all the 
+positions have been considered for x moves, we update the dp array based on temp. Thus, dp now contains the 
+entries corresponding to x moves.
+
+Thus, we start off by considering zero move available for which we make an initial entry of dp[x][y] = 1
+((x,y)is the initial position), since we can reach only this position in zero move. Then, we increase the number 
+of moves to 1 and update all the dp entries appropriately. We do so for all the moves possible from 1 to N.
+
+In order to update count, which indicates the total number of possible moves which lead an out of boundary path, 
+we need to perform the update only when we reach the boundary. We update the count as count = count + dp[i][j], 
+where (i,j) corresponds to one of the boundaries. But, if (i,j) is simultaneously a part of multiple boundaries, 
+we need to add the dp[i][j] factor multiple times(same as the number of boundaries to which (i,j) belongs).
+
+After we are done with all the N moves, count gives the required result.
 */
+// Runtime: 9 ms, faster than 46.75% of Java online submissions for Out of Boundary Paths.
+// Memory Usage: 36.3 MB, less than 25.00% of Java online submissions for Out of Boundary Paths.
+class Solution {
+    public int findPaths(int m, int n, int N, int x, int y) {
+        int M = 1000000007;
+        int dp[][] = new int[m][n];
+        dp[x][y] = 1;
+        int count = 0;
+        for (int moves = 1; moves <= N; moves++) {
+            int[][] temp = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == m - 1)
+                        count = (count + dp[i][j]) % M;
+                    if (j == n - 1)
+                        count = (count + dp[i][j]) % M;
+                    if (i == 0)
+                        count = (count + dp[i][j]) % M;
+                    if (j == 0)
+                        count = (count + dp[i][j]) % M;
+                    temp[i][j] = (((i > 0 ? dp[i - 1][j] : 0) + (i < m - 1 ? dp[i + 1][j] : 0)) % M 
+                                  + ((j > 0 ? dp[i][j - 1] : 0) + (j < n - 1 ? dp[i][j + 1] : 0)) % M) % M;
+                }
+            }
+            dp = temp;
+        }
+        return count;
+    }
+}
