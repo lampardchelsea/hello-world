b3181d90e30aaa9a43b5e3a9926661f99b72dd92
lampardchelsea <zqqsxdd@gmail.com>
2019-10-31 15:31:43 -0700

    Update PalindromicSubstrings.java

--- leetcode/DynamicProgramming/Document/Pattern_4_Palindromic_Subsequence/PalindromicSubstrings.java
+++ leetcode/DynamicProgramming/Document/Pattern_4_Palindromic_Subsequence/PalindromicSubstrings.java
@@ -1,1 +1,24 @@
+/**
+ Refer to
+ https://leetcode.com/problems/palindromic-substrings/
+ Given a string, your task is to count how many palindromic substrings in this string.
 
+The substrings with different start indexes or end indexes are counted as different 
+substrings even they consist of same characters.
+
+Example 1:
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+ 
+Example 2:
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+Note:
+The input string length won't exceed 1000.
+*/
+// Solution 1:
+// Refer to
+// 
