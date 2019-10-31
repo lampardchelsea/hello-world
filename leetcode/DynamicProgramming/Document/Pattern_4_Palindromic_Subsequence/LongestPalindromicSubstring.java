6d3b965a9b8bc148c996d5b9987fe67fa735fbd1
lampardchelsea <zqqsxdd@gmail.com>
2019-10-30 20:44:53 -0700

    Update LongestPalindromicSubstring.java

--- leetcode/DynamicProgramming/Document/Pattern_4_Palindromic_Subsequence/LongestPalindromicSubstring.java
+++ leetcode/DynamicProgramming/Document/Pattern_4_Palindromic_Subsequence/LongestPalindromicSubstring.java
@@ -15,6 +15,39 @@ Output: "bb"
 */
 
 // Solution 1: Brute Force
+// Runtime: 1298 ms, faster than 5.00% of Java online submissions for Longest Palindromic Substring.
+// Memory Usage: 35.9 MB, less than 100.00% of Java online submissions for Longest Palindromic Substring.
+class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        int maxLen = 0;
+        int start = 0;
+        // i is start index, j is (real end index + 1)
+        // to get substring need s.substring(i, j)
+        for(int i = 0; i < len; i++) {
+            for(int j = i + 1; j <= len; j++) {
+                if(isPalindrome(s, i, j - 1)) {
+                    if(j - i > maxLen) {
+                        maxLen = j - i;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substring(start, start + maxLen);
+    }
+    
+    private boolean isPalindrome(String s, int m, int n) {
+        while(m < n) {
+            if(s.charAt(m) != s.charAt(n)) {
+                return false;
+            }
+            m++;
+            n--;
+        }
+        return true;
+    }
+}
 
 // Solution 2: Spread From Center
 
