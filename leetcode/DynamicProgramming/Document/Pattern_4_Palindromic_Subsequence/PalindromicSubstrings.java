007338940e5fe1bc9ba0e50f94a6d95d68c3e54c
lampardchelsea <zqqsxdd@gmail.com>
2019-10-31 16:17:44 -0700

    Update PalindromicSubstrings.java

--- leetcode/DynamicProgramming/Document/Pattern_4_Palindromic_Subsequence/PalindromicSubstrings.java
+++ leetcode/DynamicProgramming/Document/Pattern_4_Palindromic_Subsequence/PalindromicSubstrings.java
@@ -19,6 +19,36 @@ Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 Note:
 The input string length won't exceed 1000.
 */
-// Solution 1:
+// Solution 1: Same way as Longest Palindrome Substring, instead of find maximum length, find how many substrings
 // Refer to
-// 
+// https://leetcode.com/problems/palindromic-substrings/discuss/105707/Java-DP-solution-based-on-longest-palindromic-substring
+// Runtime: 6 ms, faster than 37.86% of Java online submissions for Palindromic Substrings.
+// Memory Usage: 36.1 MB, less than 45.57% of Java online submissions for Palindromic Substrings.
+class Solution {
+    public int countSubstrings(String s) {
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        int count = 0;
+        for(int i = len - 1; i >= 0; i--) {
+            for(int j = i; j < len; j++) {
+                if(j - i <= 2) {
+                    if(s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = true;
+                    }
+                } else {
+                    if(s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                if(dp[i][j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+// Solution 2: Expand from center
+// Refer to
+// https://leetcode.com/problems/palindromic-substrings/discuss/105688/Very-Simple-Java-Solution-with-Detail-Explanation
