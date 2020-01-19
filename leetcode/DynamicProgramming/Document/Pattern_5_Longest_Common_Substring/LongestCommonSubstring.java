79d25745e70fe3ac36c4b2d5c74ad81753bad291
lampardchelsea <zqqsxdd@gmail.com>
2020-01-19 15:01:09 -0800

    Update LongestCommonSubstring.java

--- leetcode/DynamicProgramming/Document/Pattern_5_Longest_Common_Substring/LongestCommonSubstring.java
+++ leetcode/DynamicProgramming/Document/Pattern_5_Longest_Common_Substring/LongestCommonSubstring.java
@@ -28,44 +28,44 @@ The longest common substring is “abcdez” and is of length 6.
  So overall time complexity of this method would be O(n * m^2)
 */
 class Solution {
-	public static int longestCommonSubstring(String s1, String s2) {
-		// Check all the substrings from first string with second string 
-		// and keep track of the maximum.
-		// Find all substring of s1
-		List < String > list = new ArrayList < String > ();
-		for (int i = 0; i < s1.length(); i++) {
-			for (int j = i + 1; j <= s1.length(); j++) {
-				list.add(s1.substring(i, j));
-			}
-		}
-		int max = 0;
-		// Check if a string is substring of another
-		for (String s: list) {
-			if (isSubstring(s, s2)) {
-				if (max < s.length()) {
-					max = s.length();
-				}
-			}
-		}
-		return max;
-	}
+    public static int longestCommonSubstring(String s1, String s2) {
+        // Check all the substrings from first string with second string 
+        // and keep track of the maximum.
+        // Find all substring of s1
+        List < String > list = new ArrayList < String > ();
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = i + 1; j <= s1.length(); j++) {
+                list.add(s1.substring(i, j));
+            }
+        }
+        int max = 0;
+        // Check if a string is substring of another
+        for (String s: list) {
+            if (isSubstring(s, s2)) {
+                if (max < s.length()) {
+                    max = s.length();
+                }
+            }
+        }
+        return max;
+    }
 
-	private static boolean isSubstring(String s1, String s2) {
-		int m = s1.length();
-		int n = s2.length();
-		for (int i = 0; i <= n - m; i++) {
-			int j;
-			for (j = 0; j < m; j++) {
-				if (s2.charAt(i + j) != s1.charAt(j)) {
-					break;
-				}
-			}
-			if (j == m) {
-				return true;
-			}
-		}
-		return false;
-	}
+    private static boolean isSubstring(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+            for (j = 0; j < m; j++) {
+                if (s2.charAt(i + j) != s1.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == m) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 // Solution 2: DP
