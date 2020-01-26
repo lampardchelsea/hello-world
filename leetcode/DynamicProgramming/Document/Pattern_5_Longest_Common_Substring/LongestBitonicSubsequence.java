6032aa86da0e67478d66b59b3b8a89a2ffbfa36e
lampardchelsea <zqqsxdd@gmail.com>
2020-01-25 18:11:45 -0800

    Update LongestBitonicSubsequence.java

--- leetcode/DynamicProgramming/Document/Pattern_5_Longest_Common_Substring/LongestBitonicSubsequence.java
+++ leetcode/DynamicProgramming/Document/Pattern_5_Longest_Common_Substring/LongestBitonicSubsequence.java
@@ -24,3 +24,56 @@ Source: Microsoft Interview Question
 // Solution 1
 // Refer to
 // https://www.geeksforgeeks.org/longest-bitonic-subsequence-dp-15/
+/**
+ Solution
+ This problem is a variation of standard Longest Increasing Subsequence (LIS) problem. Let the input array 
+ be arr[] of length n. We need to construct two arrays lis[] and lds[] using Dynamic Programming solution 
+ of LIS problem. lis[i] stores the length of the Longest Increasing subsequence ending with arr[i]. 
+ lds[i] stores the length of the longest Decreasing subsequence starting from arr[i]. Finally, we need to 
+ return the max value of lis[i] + lds[i] – 1 where i is from 0 to n-1.
+*/
+class Solution {
+    /* lbs() returns the length of the Longest Bitonic Subsequence in 
+    arr[] of size n. The function mainly creates two temporary arrays 
+    lis[] and lds[] and returns the maximum lis[i] + lds[i] - 1. 
+
+    lis[i] ==> Longest Increasing subsequence ending with arr[i] 
+    lds[i] ==> Longest decreasing subsequence starting with arr[i] 
+    */
+    public int lbs(int arr[], int n) {
+        int i, j;
+
+        /* Allocate memory for LIS[] and initialize LIS values as 1 for 
+        	all indexes */
+        int[] lis = new int[n];
+        for (i = 0; i < n; i++)
+            lis[i] = 1;
+
+        /* Compute LIS values from left to right */
+        for (i = 1; i < n; i++)
+            for (j = 0; j < i; j++)
+                if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                    lis[i] = lis[j] + 1;
+
+        /* Allocate memory for lds and initialize LDS values for 
+        	all indexes */
+        int[] lds = new int[n];
+        for (i = 0; i < n; i++)
+            lds[i] = 1;
+
+        /* Compute LDS values from right to left */
+        for (i = n - 2; i >= 0; i--)
+            for (j = n - 1; j > i; j--)
+                if (arr[i] > arr[j] && lds[i] < lds[j] + 1)
+                    lds[i] = lds[j] + 1;
+
+
+        /* Return the maximum value of lis[i] + lds[i] - 1*/
+        int max = lis[0] + lds[0] - 1;
+        for (i = 1; i < n; i++)
+            if (lis[i] + lds[i] - 1 > max)
+                max = lis[i] + lds[i] - 1;
+
+        return max;
+    }
+}
