47ee6cd6f9cd2f20c05cedab6507829aa20966f5
lampardchelsea <zqqsxdd@gmail.com>
2020-02-20 02:35:29 -0800

    Update CountServersThatCommunicate.java

--- leetcode/DFS_BackTracking/CountServersThatCommunicate.java
+++ leetcode/DFS_BackTracking/CountServersThatCommunicate.java
@@ -54,7 +54,6 @@ class Solution {
     public int countServers(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int totalServers = 0;
         int[] rowCount = new int[m];
         int[] colCount = new int[n];
         for(int i = 0; i < m; i++) {
@@ -62,7 +61,6 @@ class Solution {
                 if(grid[i][j] == 1) {
                     rowCount[i]++;
                     colCount[j]++;
-                    totalServers++;
                 }
             }
         }
