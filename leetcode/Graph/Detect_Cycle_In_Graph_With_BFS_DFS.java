b22f3937e948f727c3126fbf9db615282e7ca4ee
lampardchelsea <zqqsxdd@gmail.com>
2020-03-18 00:17:18 -0700

    Create Detect_Cycle_In_Graph_With_BFS_DFS.java

---
+++ leetcode/Graph/Detect_Cycle_In_Graph_With_BFS_DFS.java
@@ -0,0 +1,14 @@
+/**
+ Refer to
+ Detect Cycle in a Directed Graph using BFS
+ https://www.geeksforgeeks.org/detect-cycle-in-a-directed-graph-using-bfs/
+ 
+ Detect cycle in an undirected graph using BFS
+ https://www.geeksforgeeks.org/detect-cycle-in-an-undirected-graph-using-bfs/
+ 
+ Detect cycle in an undirected graph
+ https://www.geeksforgeeks.org/detect-cycle-undirected-graph/
+ 
+ Detect Cycle in a Directed Graph
+ https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
+*/
