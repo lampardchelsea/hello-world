5d4ee12413dfadb18aaf4bef667c8ab97d00344b
lampardchelsea <zqqsxdd@gmail.com>
2020-03-24 00:40:05 -0700

    Update Detect_Cycle_In_Graph_With_BFS_DFS.java

--- leetcode/Graph/Detect_Cycle_In_Graph_With_BFS_DFS.java
+++ leetcode/Graph/Detect_Cycle_In_Graph_With_BFS_DFS.java
@@ -42,6 +42,85 @@
  
  2. Detect cycle in an undirected graph using BFS
  https://www.geeksforgeeks.org/detect-cycle-in-an-undirected-graph-using-bfs/
+
+// Java program to detect cycle in 
+// an undirected graph using BFS. 
+/**
+ * In this article, BFS based solution is discussed. We do a BFS traversal of the given graph. 
+ * For every visited vertex ‘v’, if there is an adjacent ‘u’ such that u is already visited 
+ * and u is not parent of v, then there is a cycle in graph. If we don’t find such an adjacent 
+ * for any vertex, we say that there is no cycle. The assumption of this approach is that 
+ * there are no parallel edges between any two vertices.
+class Cycle {
+    public static void main(String arg[]) {
+        int V = 4;
+        ArrayList < Integer > adj[] = new ArrayList[V];
+        for (int i = 0; i < 4; i++)
+            adj[i] = new ArrayList < Integer > ();
+
+        addEdge(adj, 0, 1);
+        addEdge(adj, 1, 2);
+        addEdge(adj, 2, 0);
+        addEdge(adj, 2, 3);
+
+        if (isCyclicDisconntected(adj, V))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+
+    static void addEdge(ArrayList < Integer > adj[], int u, int v) {
+        adj[u].add(v);
+        adj[v].add(u);
+    }
+
+    static boolean isCyclicConntected(ArrayList < Integer > adj[], int s, int V, boolean visited[]) {
+        // Set parent vertex for every vertex as -1.
+        int parent[] = new int[V];
+        Arrays.fill(parent, -1);
+
+        // Create a queue for BFS
+        Queue < Integer > q = new LinkedList < > ();
+
+        // Mark the current node as
+        // visited and enqueue it
+        visited[s] = true;
+        q.add(s);
+
+        while (!q.isEmpty()) {
+            // Dequeue a vertex from
+            // queue and print it
+            int u = q.poll();
+            // Get all adjacent vertices
+            // of the dequeued vertex u.
+            // If a adjacent has not been
+            // visited, then mark it visited
+            // and enqueue it. We also mark parent
+            // so that parent is not considered
+            // for cycle.
+            for (int i = 0; i < adj[u].size(); i++) {
+                int v = adj[u].get(i);
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.add(v);
+                    parent[v] = u;
+                } else if (parent[u] != v)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean isCyclicDisconntected(ArrayList < Integer > adj[], int V) {
+        // Mark all the vertices as not visited
+        boolean visited[] = new boolean[V];
+        Arrays.fill(visited, false);
+        for (int i = 0; i < V; i++)
+            if (!visited[i] && isCyclicConntected(adj, i, V, visited))
+                return true;
+        return false;
+    }
+}
  
  3. Detect cycle in an undirected graph
  https://www.geeksforgeeks.org/detect-cycle-undirected-graph/
