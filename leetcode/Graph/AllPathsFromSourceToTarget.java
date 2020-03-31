c19e940256d4a29b55d973d01b4e97149d56160f
lampardchelsea <zqqsxdd@gmail.com>
2020-03-30 19:31:04 -0700

    Update AllPathsFromSourceToTarget.java

--- leetcode/Graph/AllPathsFromSourceToTarget.java
+++ leetcode/Graph/AllPathsFromSourceToTarget.java
@@ -38,6 +38,28 @@ To generate all of these paths you need to use Backtracking:
 Time Complexity: O(2^n), because there will be 2^n number of paths
 Space Complexity: O(2^n), because you will need to return 2^n number of paths
 */
+
+// Why the time complexity is O(2^n)
+// Refer to
+// https://leetcode.com/problems/all-paths-from-source-to-target/discuss/118713/Java-DFS-Solution/198489
+/**
+ I think the time complexity is O(2^n).
+Think about this worst case scenario:
+Suppose we have n nodes, labeled 0 to n-1.
+Think of it as an array: [0, 1, 2, 3, 4, 5, 6, ..., n-1]
+For each pair of nodes i and j, if i < j, let there be an edge between node i and node j.
+(So, there are O(n^2) edges, though this fact is not important.)
+Now, we want to calculate how many paths there are from the 0th node to the (n-1)th node.
+Well, notice that each path of length k corresponds to some choice of (k - 1) nodes between 0 and (n-1).
+For example, here are two paths of length 2:
+0->3->(n-1)
+0->5->(n-1)
+Here is a path of length 3:
+0->1->5->(n-1)
+How many paths of length k are there? The answer is (n-2 choose k-1) because we pick k - 1 nodes between 0 and (n - 1).
+The total number of paths is the sum of (n-2 choose k-1) for k = 1, 2, ..., (n-1).
+Using the binomial theorem, this sum is equal to 2^(n-2) which is O(2^n).
+*/
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
