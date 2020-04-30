3af6925e2ccc43b5d22d0fadbc03bd827f3b4a24
lampardchelsea <zqqsxdd@gmail.com>
2020-04-30 01:17:39 -0700

    Update FriendCircles.java

--- leetcode/DFS_BackTracking/FriendCircles.java
+++ leetcode/DFS_BackTracking/FriendCircles.java
@@ -71,7 +71,7 @@ class Solution {
     }
 }
 
-// Solution 2: Union find
+// Solution 2: Union find native link
 // Refer to
 // https://leetcode.com/problems/friend-circles/discuss/101336/Java-solution-Union-Find
 // Style 1:
@@ -167,5 +167,64 @@ class Solution {
             parent[x] = find(parent, find(parent, parent[x]));
         }
         return parent[x];
+    }
+}
+
+// Solution 3: Union Find link by size
+// Refer to
+// https://leetcode.com/problems/friend-circles/discuss/101336/Java-solution-Union-Find/266043
+class Solution {
+    class UF {
+        private int[] parent, size;
+        private int count;
+        
+        public UF(int n) {
+            parent = new int[n];
+            size = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+
+        }
+        
+        public int find(int p) {
+            // path compression
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+        
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP == rootQ) 
+                return;
+            // union by size
+            if (size[rootP] > size[rootQ]) {
+                parent[rootQ] = rootP;
+                size[rootP] += size[rootQ];
+            } else {
+                parent[rootP] = rootQ;
+                size[rootQ] += size[rootP];
+            }
+            count--;
+        }
+        
+        public int count() { return count; }
+    }
+    
+    
+    public int findCircleNum(int[][] M) {
+        int n = M.length;
+        UF uf = new UF(n);
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (M[i][j] == 1)
+                    uf.union(i, j);
+        return uf.count();
     }
 }
