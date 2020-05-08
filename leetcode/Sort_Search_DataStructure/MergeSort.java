61d0a767fdac1f51df2e73a0d29b17c0153e5b98
lampardchelsea <zqqsxdd@gmail.com>
2020-05-08 12:51:21 -0700

    Update MergeSort.java

--- leetcode/Sort_Search_DataStructure/MergeSort.java
+++ leetcode/Sort_Search_DataStructure/MergeSort.java
@@ -4,111 +4,106 @@
  * 
  * Refer to
  * http://algs4.cs.princeton.edu/22mergesort/Merge.java.html
- *
- */
-/**
- * Refer to
- * http://algs4.cs.princeton.edu/22mergesort/Merge.java.html
- *
+ * https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/tutorial/
  */
 public class Merge {
-	public Merge() {}
-	
-	public static void sort(Comparable[] a) {
-		Comparable[] aux = new Comparable[a.length];
-		sort(a, aux, 0, a.length - 1);
-		
-	}
-	
-	public static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
-		// Base case, when two pointers encounter, stop sorting
-		if(hi <= lo) {
-			return;
-		}
-		
-		// Recursive steps
-		int mid = (lo + hi) / 2;
-		sort(a, aux, lo, mid);
-		sort(a, aux, mid + 1, hi);
-		merge(a, aux, lo, mid, hi);
-	}
-	
-	
-	public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
-//		if(hi <= lo) {
-//			return;
-//		}
-		
-        // Precondition: a[lo .. mid] and a[mid+1 .. hi] are sorted subarrays
-		// Before we start real merge, we need to verify each smallest section is being sorted
-		// In this example, the smallest section is every 2 items in array, like first and
-		// second item combine as one smallest section ("M", "E"), before used on merge, it
-		// needs to sort into ("E", "M")
-		// Refer to 
-		// http://faculty.simpson.edu/lydia.sinapova/www/cmsc250/LN250_Weiss/L17-ExternalSortEX1.htm
-		// http://pages.cs.wisc.edu/~jignesh/cs564/notes/lec07-sorting.pdf --> Pass 1(sort on current 
-		// Page required before merge) 
-        assert isSorted(a, lo, mid);
-        assert isSorted(a, mid+1, hi);
-		
-		
-		// First copy all items into auxiliary array
-		for(int k = 0; k < a.length; k++) {
-			aux[k] = a[k];
-		}
-		
-		// Set up two pointers separately point to the first item in first and second half section of input array
-		int i = lo;
-		int j = mid + 1;
+    public Merge() {}
 
-		// Then with logic below after comparing and exchange to copy items back
-		// The condition here is very important, exchange range is between index "lo" to index "hi",
-		// because every time we only merge the current section declared between "lo" and "hi",
-		// not the full length array.
-		for(int k = lo; k <= hi; k++) {
-			// First two conditions i > mid and j > hi are tricky part, because as observe debug,
-			// as i++ or j++ will promote i or j over mid or hi, need to handle the case
-			if(i > mid) {
-				a[k] = aux[j++];
-			} else if(j > hi) {
-				a[k] = aux[i++];
-			} else if(less(aux[j], aux[i])) {
-				a[k] = aux[j++];
-			} else {
-				a[k] = aux[i++];
-			}
-		}
-		
-		// Postcondition: a[lo .. hi] is sorted
+    public static void sort(Comparable[] a) {
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length - 1);
+
+    }
+
+    public static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        // Base case, when two pointers encounter, stop sorting
+        if (hi <= lo) {
+            return;
+        }
+
+        // Recursive steps
+        int mid = (lo + hi) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+
+
+    public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        //		if(hi <= lo) {
+        //			return;
+        //		}
+
+        // Precondition: a[lo .. mid] and a[mid+1 .. hi] are sorted subarrays
+        // Before we start real merge, we need to verify each smallest section is being sorted
+        // In this example, the smallest section is every 2 items in array, like first and
+        // second item combine as one smallest section ("M", "E"), before used on merge, it
+        // needs to sort into ("E", "M")
+        // Refer to 
+        // http://faculty.simpson.edu/lydia.sinapova/www/cmsc250/LN250_Weiss/L17-ExternalSortEX1.htm
+        // http://pages.cs.wisc.edu/~jignesh/cs564/notes/lec07-sorting.pdf --> Pass 1(sort on current 
+        // Page required before merge) 
+        assert isSorted(a, lo, mid);
+        assert isSorted(a, mid + 1, hi);
+
+
+        // First copy all items into auxiliary array
+        for (int k = 0; k < a.length; k++) {
+            aux[k] = a[k];
+        }
+
+        // Set up two pointers separately point to the first item in first and second half section of input array
+        int i = lo;
+        int j = mid + 1;
+
+        // Then with logic below after comparing and exchange to copy items back
+        // The condition here is very important, exchange range is between index "lo" to index "hi",
+        // because every time we only merge the current section declared between "lo" and "hi",
+        // not the full length array.
+        for (int k = lo; k <= hi; k++) {
+            // First two conditions i > mid and j > hi are tricky part, because as observe debug,
+            // as i++ or j++ will promote i or j over mid or hi, need to handle the case
+            if (i > mid) { // Checks if first part comes to an end or not
+                a[k] = aux[j++];
+            } else if (j > hi) { // Checks if second part comes to an end or not
+                a[k] = aux[i++];
+            } else if (less(aux[j], aux[i])) { // Checks which part has smaller element
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+
+        // Postcondition: a[lo .. hi] is sorted
         assert isSorted(a, lo, hi);
-	}
-	
-	public static boolean less(Comparable v, Comparable w) {
-		return v.compareTo(w) < 0;
-	}
-	
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
     private static void show(Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
             StdOut.println(a[i]);
         }
     }
-	
+
     /***************************************************************************
      *  Check if array is sorted - useful for debugging.
      ***************************************************************************/
     private static boolean isSorted(Comparable[] a) {
-       return isSorted(a, 0, a.length - 1);
+        return isSorted(a, 0, a.length - 1);
     }
 
     private static boolean isSorted(Comparable[] a, int lo, int hi) {
         for (int i = lo + 1; i <= hi; i++)
-            if (less(a[i], a[i-1])) return false;
+            if (less(a[i], a[i - 1])) return false;
         return true;
     }
-    
+
     public static void main(String[] args) {
         //String[] a = StdIn.readAllStrings();
-    	String[] a = {"M", "E", "R", "G", "E", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+        String[] a = {"M", "E", "R", "G", "E", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
         Merge.sort(a);
         show(a);
     }
