2a0864b1a2b1f0d77fbe518d443569bfb3bb5a04
lampardchelsea <zqqsxdd@gmail.com>
2020-05-22 03:45:41 -0700

    Update ConvertSortedArrayToBinarySearchTree.java

--- leetcode/array/ConvertSortedArrayToBinarySearchTree.java
+++ leetcode/array/ConvertSortedArrayToBinarySearchTree.java
@@ -2,7 +2,7 @@
 // https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/#/description
 // Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
 
-// Solution 1:
+// Solution 1: Recursive
 // Refer to
 // https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/#/description
 // https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/#/description
@@ -37,3 +37,42 @@ public class Solution {
         return root;
     }
 }
+
+// Solution 2: Iterative
+// Refer to
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/discuss/35218/Java-Iterative-Solution/241775
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(0);
+        Stack<Object> stack = new Stack<Object>();
+        stack.push(nums.length - 1);
+        stack.push(0);
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode node = (TreeNode)stack.pop();
+            int start = (int)stack.pop();
+            int end = (int)stack.pop();
+            int mid = start + (end - start) / 2;
+            node.val = nums[mid];
+            if(mid - 1 >= start) {
+                node.left = new TreeNode(0);
+                stack.push(mid - 1);
+                stack.push(start);
+                stack.push(node.left);
+            }
+            if(mid + 1 <= end) {
+                node.right = new TreeNode(0);
+                stack.push(end);
+                stack.push(mid + 1);
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+}
+
+
+
