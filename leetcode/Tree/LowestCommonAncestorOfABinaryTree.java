8fa87463151cff8db8d4011444d5205cd7e20c09
lampardchelsea <zqqsxdd@gmail.com>
2020-05-23 17:56:30 -0700

    Update and rename LowestCommonAncestorBT.java to LowestCommonAncestorOfABinaryTree.java

--- leetcode/Tree/LowestCommonAncestorOfABinaryTree.java
+++ leetcode/Tree/LowestCommonAncestorOfABinaryTree.java
@@ -1,4 +1,6 @@
 /**
+ Refer to
+ https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
  * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
  * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between 
  * two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow 
