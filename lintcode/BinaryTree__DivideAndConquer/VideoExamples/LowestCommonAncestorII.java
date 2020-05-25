428a9ad20e039e76d999ee8052822c8cb8721a8f
lampardchelsea <zqqsxdd@gmail.com>
2020-05-25 10:18:15 -0700

    Update LowestCommonAncestorII.java

--- lintcode/BinaryTree__DivideAndConquer/VideoExamples/LowestCommonAncestorII.java
+++ lintcode/BinaryTree__DivideAndConquer/VideoExamples/LowestCommonAncestorII.java
@@ -28,4 +28,43 @@
       5   6
  LCA(5, 6) = 7
 */
+/**
+ * Definition of ParentTreeNode:
+ * 
+ * class ParentTreeNode {
+ *     public ParentTreeNode parent, left, right;
+ * }
+ */
 
+// Solution 1: 
+// Refer to
+// https://xuyiruan.com/2019/02/06/Lowest-Common-Ancestor-Series/
+/**
+ Thought
+ The follow up problem provides extra parent pointer for each TreeNode. With this, we can eliminate the 
+ time requires to traverse the entire tree to locate p and q node. Instead, we can trace directly from p 
+ node to root and add all its ancester to a Set in ordder. Then trace from q to root and return the first 
+ shared node as LCA
+*/
+public class Solution {
+    /*
+     * @param root: The root of the tree
+     * @param A: node in the tree
+     * @param B: node in the tree
+     * @return: The lowest common ancestor of A and B
+     */
+    public ParentTreeNode lowestCommonAncestorII(ParentTreeNode root, ParentTreeNode A, ParentTreeNode B) {
+        if(root == null) {
+            return null;
+        }
+        Set<ParentTreeNode> parents_A = new HashSet<ParentTreeNode>();
+        while(A != null) {
+            parents_A.add(A);
+            A = A.parent;
+        }
+        while(!parents_A.contains(B)) {
+            B = B.parent;
+        }
+        return B;
+    }
+}
