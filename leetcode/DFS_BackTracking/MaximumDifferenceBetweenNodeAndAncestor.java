8491a31992512f45859ad207b308cbbad1958a5e
lampardchelsea <zqqsxdd@gmail.com>
2020-05-29 13:19:59 -0700

    Update MaximumDifferenceBetweenNodeAndAncestor.java

--- leetcode/DFS_BackTracking/MaximumDifferenceBetweenNodeAndAncestor.java
+++ leetcode/DFS_BackTracking/MaximumDifferenceBetweenNodeAndAncestor.java
@@ -47,14 +47,16 @@ class Solution {
     public int maxAncestorDiff(TreeNode root) {
         return helper(root, root.val, root.val);
     }
-    
+    // Top-down by using pre-order traverse
     private int helper(TreeNode root, int min, int max) {
         if(root == null) {
             return max - min;
         }
         min = Math.min(min, root.val);
         max = Math.max(max, root.val);
-        return Math.max(helper(root.left, min, max), helper(root.right, min, max));
+        int leftDiff = helper(root.left, min, max);
+        int rightDiff = helper(root.right, min, max)
+        return Math.max(leftDiff, rightDiff);
     }
 }
 
@@ -86,5 +88,113 @@ class Solution {
         result = Math.max(result, max - min);
         helper(root.left, min, max);
         helper(root.right, min, max);
+    }
+}
+
+// Testing
+/**
+                     3
+                 /       \
+                5         1
+              /   \	    /   \
+             6     2   0     8
+                 /   \
+                7     4
+				
+Top-Down pre-order traverse
+root(3) -> min = 3, max = 3
+  -> root(5) -> min = 3, max = 5
+    -> root(6) -> min = 3, max = 6
+      -> return 6 - 3 
+      -> leftDiff = 3
+      -> return 6 - 3
+      -> rightDiff = 3
+      -> return Math.max(3, 3)
+    -> root(2) -> min = 2, max = 5
+      -> root(7) -> min = 2, max = 7
+        -> return 7 - 2
+        -> leftDiff = 5
+        -> return 7 - 2
+        -> rightDiff = 5
+        -> return Math.max(5, 5)
+      -> root(4) -> min = 2, max = 5
+        -> return 5 - 2
+        -> leftDiff = 3
+        -> return 5 - 2
+        -> rightDiff = 3
+        -> return Math.max(3, 3)
+      -> return Math.max(5, 3)
+    -> return Math.max(3, 5)
+  -> root(1) -> min = 1, max = 3
+    -> root(0) -> min = 0, max = 3
+    -> return 3 - 0
+    -> leftDiff = 3
+    -> return 3 - 0
+    -> rightDiff = 3
+    -> return Math.max(3, 3)
+    -> root(8) -> min = 1, max = 8
+    -> return 8 - 1
+    -> leftDiff = 7
+    -> return 8 - 1
+    -> rightDiff = 7
+    -> return Math.max(7, 7)
+  -> return Math.max(5, 7)
+-> return 7
+*/
+public class Solution {
+    public static void main(String[] args) {
+        /**
+         * Test with below binary tree
+         * 
+         *           3
+         *       /       \
+                5         1
+              /   \	    /   \
+             6     2   0     8
+                 /   \
+                7     4
+         */
+        Solution q = new Solution();
+        TreeNode root = q.new TreeNode(3);
+        root.left = q.new TreeNode(5);
+        root.right = q.new TreeNode(1);
+        root.left.left = q.new TreeNode(6);
+        root.left.right = q.new TreeNode(2);
+        root.left.right.left = q.new TreeNode(7);
+        root.left.right.right = q.new TreeNode(4);
+        root.right.left = q.new TreeNode(0);
+        root.right.right = q.new TreeNode(8);
+        int result = q.maxAncestorDiff(root);
+        System.out.println(result);
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxAncestorDiff(TreeNode root) {
+        return helper(root, root.val, root.val);
+    }
+
+    private int helper(TreeNode root, int min, int max) {
+        if (root == null) {
+            return max - min;
+        }
+        min = Math.min(min, root.val);
+        max = Math.max(max, root.val);
+        int leftDiff = helper(root.left, min, max);
+        int rightDiff = helper(root.right, min, max);
+        return Math.max(leftDiff, rightDiff);
     }
 }
