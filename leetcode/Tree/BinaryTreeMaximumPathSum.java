0817d4942b79e8d2b0626bad32d72cc66176083a
lampardchelsea <zqqsxdd@gmail.com>
2020-05-29 21:02:37 -0700

    Update BinaryTreeMaximumPathSum.java

--- leetcode/Tree/BinaryTreeMaximumPathSum.java
+++ leetcode/Tree/BinaryTreeMaximumPathSum.java
@@ -128,6 +128,16 @@ public class BinaryTreeMaximumPathSum {
 // Re-work
 // Solution 1: Recursive
 // Refer to
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/discuss/39775/Accepted-short-solution-in-Java
+/**
+ A path from start to end, goes up on the tree for 0 or more steps, then goes down for 0 or more steps. 
+ Once it goes down, it can't go up. Each path has a highest node, which is also the lowest common ancestor 
+ of all other nodes on the path.
+ A recursive method maxPathDown(TreeNode node) 
+ (1) computes the maximum path sum with highest node is the input node, update maximum if necessary 
+ (2) returns the maximum sum of the path that can be extended to input node's parent.
+*/
+
 // https://leetcode.com/problems/binary-tree-maximum-path-sum/discuss/39775/Accepted-short-solution-in-Java/37681
 /**
  Just want to add a explanation about the last two lines based on my comprehension.
