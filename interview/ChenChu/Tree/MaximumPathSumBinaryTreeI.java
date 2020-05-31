fe61e64474275f8f0acf049829608fd1540e5963
lampardchelsea <zqqsxdd@gmail.com>
2020-05-30 19:02:17 -0700

    Update MaximumPathSumBinaryTreeI.java

--- interview/ChenChu/Tree/MaximumPathSumBinaryTreeI.java
+++ interview/ChenChu/Tree/MaximumPathSumBinaryTreeI.java
@@ -2,15 +2,19 @@
  Refer to
  https://hjweds.gitbooks.io/leetcode/maximum-path-sum-binary-tree-i.html
  https://www.geeksforgeeks.org/find-maximum-path-sum-two-leaves-binary-tree/
+ Find the maximum path sum between two leaves of a binary tree
  Given a binary tree in which each node element contains a number. Find the maximum possible sum 
  from one leaf node to another.
  The maximum sum path may or may not go through root. For example, in the following binary tree, 
  the maximum sum is 27(3 + 6 + 9 + 0 – 1 + 10). Expected time complexity is O(n).
  If one side of root is empty, then function should return minus infinite (INT_MIN in case of C/C++)
          -15
-			5       6
-   -8   1   3   9
- 2   6
+        /    \
+      5        6
+     / \      / \
+   -8   1    3   9
+  /  \
+ 2    6
 */
 
 // Solution 1:
