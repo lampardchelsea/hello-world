f63473394bd3d0fb786e7be3795cb8902aadddd6
lampardchelsea <zqqsxdd@gmail.com>
2020-05-31 02:34:55 -0700

    Update MaximumPathSumBinaryTreeI.java

--- interview/ChenChu/Tree/MaximumPathSumBinaryTreeI.java
+++ interview/ChenChu/Tree/MaximumPathSumBinaryTreeI.java
@@ -24,3 +24,73 @@
 // Solution 1:
 // Refer to
 // https://www.geeksforgeeks.org/find-maximum-path-sum-two-leaves-binary-tree/
+public class Solution {
+    public static void main(String[] args) {
+        /**
+         * Test with below binary tree
+         * 
+         *           3
+         *       /       \
+                5         1
+              /   \	    /   \
+             6     2   0     8
+                 /   \
+                7     4
+         */
+        Solution q = new Solution();
+        TreeNode root = q.new TreeNode(3);
+        root.left = q.new TreeNode(5);
+        root.right = q.new TreeNode(1);
+        root.left.left = q.new TreeNode(6);
+        root.left.right = q.new TreeNode(2);
+        root.left.right.left = q.new TreeNode(7);
+        root.left.right.right = q.new TreeNode(4);
+        root.right.left = q.new TreeNode(0);
+        root.right.right = q.new TreeNode(8);
+        int result = q.maxPathSum(root);
+        System.out.println(result);
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        if (root == null || root.left == null || root.right == null) {
+            return max;
+        }
+        helper(root);
+        return max;
+    }
+
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = helper(root.left);
+        int right = helper(root.right);
+        //left和right非空时才更新max
+        if (root.left != null && root.right != null) {
+            int sum = root.val + left + right;
+            max = Math.max(max, sum);
+            return Math.max(left, right) + root.val;
+        }
+        //注意不是return Math.max(left, right) + root.key; eg:  
+        //     1
+        //   /  \
+        //      -1 应该返回非空的那支路径
+        return root.left == null ? right + root.val : left + root.val;
+    }
+}
