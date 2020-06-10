d1a40dabee4c4dffc2103965b9ec03d6ba9af0b9
lampardchelsea <zqqsxdd@gmail.com>
2020-06-10 01:47:49 -0700

    Update BinaryTreeUpsideDown.java

--- leetcode/Tree/BinaryTreeUpsideDown.java
+++ leetcode/Tree/BinaryTreeUpsideDown.java
@@ -143,7 +143,135 @@ public class BinaryTreeUpsideDown {
 // https://github.com/lampardchelsea/hello-world/blob/master/leetcode/Tree/Document/Binary_Tree_Upside_Down.pdf
 // http://rainykat.blogspot.com/2017/01/leetcodelinkedin-156-binary-tree-upside.html
 // Solution 1: Recursive
+/**
+Recursive Solution
+We already compared this problem to the simple linked list reversal problem, so it’s also supposed to be viable 
+to be solved by recursion. Recall the linked list problem, we first recursively call the sub-problem and try to 
+get the final head of the reversed result list. And then in current call, we make the current node last one in 
+the list. Similarly, we can definitely use the same strategy. First, we don’t think too much, which means that 
+we can directly call the sub-problem and try to get the eventual new root from those recursive calls. Next, we 
+make the current root as the right child of its left child, and the current root’s right child as the left child 
+of the root’s left child. The wording is a bit confusing, but the logic is almost the same as the list problem.
+Now, let’s analyze the recursive rules:
+Base case: When the input root is empty or it does not have left child.
+Recursive Rule: 1) Retrieve the final new root from recursive calls. 2) Make the current node and its right child 
+be the children of the current node’s left child. More specifically, Left point the left child to the current 
+root’s right child and right point the left child to the current node.
+TreeNode* upsideDownBinaryTree(TreeNode* root) {
+    if (!root || !root->left) {
+        return root;
+    }
+    
+    TreeNode* newRoot = upsideDownBinaryTree(root->left);
+    root->left->left = root->right;
+    root->left->right = root;
+    root->left = NULL;
+    root->right = NULL;
+    
+    return newRoot;
+}
+We can also compare this recursive solution to the linked list reversal problem’s one. The base case is that when 
+the input root is empty or it does not have left child, we can directly return the root. Note that the empty case 
+here is also a corner case instead of the base case since if the tree is not empty, we will never reach the case 
+that the recursive call gets a NULL pointer. This is also because we need to return the final new root to the 
+former function calls, so we have to return the last node but not the empty one. If the current root has no left 
+child, it will be the eventual new root after flip, so we return that one.
+For the recursive rule, note that in the linked list problem, we directly go fetch the final result in the subsequent 
+sub-list. Here, we can definitely apply the same strategy to fetch the final result at first, and then complete the 
+task in our current function. Recall that in that list problem, in each function, we set the current node to the 
+last node in the list. Now, in a binary tree, we need to set the current node and its right child to the last level 
+in the tree. And everything is almost the same. We let the left child point to the current root and its right child, 
+and then set the children of the current root to empty.
 
+Comparison Between Flip Binary Tree And Reverse Singly Linked List Problem
+It’s time to go through the example:
+Suppose that we are given a binary tree:
+    
+    1
+   / \
+  2   3
+ / \
+4   5
+Binary Tree:
+    1
+   / \
+  2   3
+ / \
+4   5
+In Main Function:               call upsideDownBinaryTree(1)
+Binary Tree:
+    1
+   / \
+  2   3
+ / \
+4   5
+In upsideDownBinaryTree(1):     call upsideDownBinaryTree(2)
+Binary Tree:
+    1
+   / \
+  2   3
+ / \
+4   5
+In upsideDownBinaryTree(2):     call upsideDownBinaryTree(4)
+Binary Tree:
+    1
+   / \
+  2   3
+ / \
+4   5
+In upsideDownBinaryTree(4):     hit base case
+                                return 4
+Binary Tree:
+    1
+   / \
+  2   3
+ / \
+4   5
+In upsideDownBinaryTree(2):     let 4 left point to 5
+                                let 4 right point to 2
+                                let 2 left point to NULL
+                                let 2 right point to NULL
+                                return 4
+Binary Tree:
+    1
+   / \
+  2   3
+ / 
+4 - 5
+In upsideDownBinaryTree(1):     let 2 left point to 3
+                                let 2 right point to 1
+                                let 1 left point to NULL
+                                let 1 right point to NULL
+                                return 4
+Binary Tree:
+    1
+   / 
+  2 - 3
+ / 
+4 - 5
+In Main Function:               get 4 
+Time Complexity: O(n) — The time complexity is the same as the iterative solution since we also need to dive into 
+                        the deepest node on the most straight left path from root.
+Space Complexity: O(n) — With recursion we need to consider the usage of call stacks, so in this problem the space 
+                         complexity is also O(n) since we need n levels of call stacks for the deepest recursion path.
+*/
+public class Solution {
+    /**
+     * @param root: the root of binary tree
+     * @return: new root
+     */
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root == null || root.left == null) {
+            return root;
+        }
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
 
 // Solution 2: Iterative
 /**
@@ -258,6 +386,10 @@ Binary Tree:
 4 - 5
 Data Structure:     prev = 4, cur = NULL, next = NULL, lastRight = NULL
 return prev = 4
+
+Time Complexity: O(n) — n represents the length of the path stretching from root all the way down to its left children.
+Space Complexity: O(1) — The only main data structure we use in this implementation are those four pointers, 
+                         which is definitely constant space complexity.
 */
 public class Solution {
     /**
