cd8fec22a3189b2063d0f557593abbdbff914fa0
lampardchelsea <zqqsxdd@gmail.com>
2020-06-12 18:23:12 -0700

    Update SameTree.java

--- leetcode/Tree/SameTree.java
+++ leetcode/Tree/SameTree.java
@@ -86,4 +86,41 @@ class Solution {
   均有各自的迭代和递归的写法，这里我们先来看先序的迭代写法，相当于同时
   遍历两个数，然后每个节点都进行比较, 可参见之间那道 Binary Tree Preorder Traversal
 */
-
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Stack<TreeNode> stack_p = new Stack<TreeNode>();
+        Stack<TreeNode> stack_q = new Stack<TreeNode>();
+        if(p != null) {
+            stack_p.push(p);            
+        }
+        if(q != null) {
+            stack_q.push(q);            
+        }
+        while(!stack_p.isEmpty() && !stack_q.isEmpty()) {
+            TreeNode pn = stack_p.pop();
+            TreeNode qn = stack_q.pop();
+            if(pn.val != qn.val) {
+                return false;
+            }
+            if(pn.right != null) {
+                stack_p.push(pn.right);
+            }
+            if(qn.right != null) {
+                stack_q.push(qn.right);
+            }
+            if(stack_p.size() != stack_q.size()) {
+                return false;
+            }
+            if(pn.left != null) {
+                stack_p.push(pn.left);
+            }
+            if(qn.left != null) {
+                stack_q.push(qn.left);
+            }
+            if(stack_p.size() != stack_q.size()) {
+                return false;
+            }
+        }
+        return stack_p.size() == stack_q.size();
+    }
+}
