859442120d9cc9b9c01c237c546b49bcb7081977
lampardchelsea <zqqsxdd@gmail.com>
2020-06-14 13:39:48 -0700

    Update FlipEquivalentBinaryTrees.java

--- leetcode/Tree/FlipEquivalentBinaryTrees.java
+++ leetcode/Tree/FlipEquivalentBinaryTrees.java
@@ -140,4 +140,39 @@ class Solution {
     }
 }
 
+// Solution 3: Iterative (DFS) --> Just replace 'Queue' in Solution 2 with 'Stack'
+// Refer to
+// https://leetcode.com/problems/flip-equivalent-binary-trees/discuss/200514/JavaPython-3-DFS-3-liners-and-BFS-with-explanation-time-and-space%3A-O(n).
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        Stack<TreeNode> s1 = new Stack<TreeNode>();
+        Stack<TreeNode> s2 = new Stack<TreeNode>();
+        s1.push(root1);
+        s2.push(root2);
+        while(!s1.isEmpty() && !s2.isEmpty()) {
+            TreeNode n1 = s1.pop();
+            TreeNode n2 = s2.pop();
+            if(n1 == null || n2 == null) {
+                if(n1 == n2) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            if(n1.val != n2.val) {
+                return false;
+            }
+            if((n1.left == null && n2.left == null) || (n1.left != null && n2.left != null && n1.left.val == n2.left.val)) {
+                s1.push(n1.left);
+                s1.push(n1.right);
+            } else {
+                s1.push(n1.right);
+                s1.push(n1.left);
+            }
+            s2.push(n2.left);
+            s2.push(n2.right);
+        }
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
 
