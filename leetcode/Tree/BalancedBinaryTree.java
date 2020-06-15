e05d870854fda878b06c5726a7cfc1a9da4b553f
lampardchelsea <zqqsxdd@gmail.com>
2020-06-15 02:02:54 -0700

    Update BalancedBinaryTree.java

--- leetcode/Tree/BalancedBinaryTree.java
+++ leetcode/Tree/BalancedBinaryTree.java
@@ -130,6 +130,73 @@ public class BlancedBinrayTree {
 }
 
 
+// Re-work
+// Solution 1: Two Pass DFS (Time Complexity O(NlogN))
+// Refer to
+// https://leetcode.com/problems/balanced-binary-tree/discuss/35691/The-bottom-up-O(N)-solution-would-be-better
+/**
+ For the current node root, calling depth() for its left and right children actually has to access all of its children, 
+ thus the complexity is O(N). We do this for each node in the tree, so the overall complexity of isBalanced will be O(N^2). 
+ This is a top down approach.
+*/
+// https://leetcode.com/problems/balanced-binary-tree/discuss/35691/The-bottom-up-O(N)-solution-would-be-better/33913
+/**
+ O(NlogN) is only true for the best case. The first approach is O(N^2) for the worst case,
+ that is the tree has N nodes and N levels, it take O(N^2) to return false.
+*/
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        if(Math.abs(lh - rh) <= 1 && isBalanced(root.left) && isBalanced(root.right)) {
+            return true;
+        }
+        return false;
+    }
+    
+    private int height(TreeNode node) {
+        if(node == null) {
+            return -1;
+        }
+        return Math.max(height(node.left), height(node.right)) + 1;
+    }
+}
+
+// Solution 2: One Pass DFS (Time Complexity O(N))
+// Refer to
+// https://leetcode.com/problems/balanced-binary-tree/discuss/35691/The-bottom-up-O(N)-solution-would-be-better
+/**
+ The second method is based on DFS. Instead of calling depth() explicitly for each child node, we return 
+ the height of the current node in DFS recursion. When the sub tree of the current node (inclusive) is 
+ balanced, the function dfsHeight() returns a non-negative value as the height. Otherwise -1 is returned. 
+ According to the leftHeight and rightHeight of the two children, the parent node could check if the sub tree
+ is balanced, and decides its return value.
+*/
+// https://leetcode.com/problems/balanced-binary-tree/discuss/35691/The-bottom-up-O(N)-solution-would-be-better/198436
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        return helper(root) != -1;
+    }
+    
+    private int helper(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int left = helper(root.left);
+        int right = helper(root.right);
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
+
 
 
 
