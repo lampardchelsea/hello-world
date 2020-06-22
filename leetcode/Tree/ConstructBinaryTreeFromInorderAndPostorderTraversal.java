7f74b70824504f048ffb72e77208a9afdd6d09f0
lampardchelsea <zqqsxdd@gmail.com>
2020-06-22 03:21:29 -0700

    Update ConstructBinaryTreeFromInorderAndPostorderTraversal.java

--- leetcode/Tree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ leetcode/Tree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -20,6 +20,7 @@ Return the following binary tree:
 
 // Solution 1: Recursive
 // Refer to
+// https://github.com/lampardchelsea/hello-world/blob/master/leetcode/Tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
 // https://discuss.leetcode.com/topic/3296/my-recursive-java-code-with-o-n-time-and-o-n-space
 // https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/discuss/34782/My-recursive-Java-code-with-O(n)-time-and-O(n)-space/154363
 class Solution {
@@ -47,5 +48,28 @@ class Solution {
 
 // Solution 2: Recursive with HashMap
 // Refer to
-// 
-
+// https://github.com/lampardchelsea/hello-world/blob/master/leetcode/Tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(postorder == null || postorder.length == 0) {
+            return null;
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return helper(postorder.length - 1, 0, inorder.length - 1, postorder, map);
+    }
+    
+    private TreeNode helper(int rootIndexInPostorder, int inStart, int inEnd, int[] postorder, Map<Integer, Integer> map) {
+        if(inStart > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[rootIndexInPostorder]);
+        int rootIndexInInorder = map.get(root.val);
+        int rightChildNum = inEnd - rootIndexInInorder;
+        root.left = helper(rootIndexInPostorder - rightChildNum - 1, inStart, rootIndexInInorder - 1, postorder, map);
+        root.right = helper(rootIndexInPostorder - 1, rootIndexInInorder + 1, inEnd, postorder, map);
+        return root;
+    }
+}
