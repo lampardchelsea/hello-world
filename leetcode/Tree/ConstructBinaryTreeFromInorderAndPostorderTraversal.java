fedf685eb3f8cf206b31b251adf9899942d36d91
lampardchelsea <zqqsxdd@gmail.com>
2020-06-21 21:07:21 -0700

    Create ConstructBinaryTreeFromInorderAndPostorderTraversal.java

---
+++ leetcode/Tree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -0,0 +1,23 @@
+/**
+ Refer to
+ https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+ Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
+Return the following binary tree:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+*/
+
+// Solution 1: Recursive
+// Refer to
+// https://discuss.leetcode.com/topic/3296/my-recursive-java-code-with-o-n-time-and-o-n-space
