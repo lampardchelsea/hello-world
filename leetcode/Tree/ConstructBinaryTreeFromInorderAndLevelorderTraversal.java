9f5b745de0c0bdd03bc1f52d8c66f6425085345b
lampardchelsea <zqqsxdd@gmail.com>
2020-06-24 23:34:22 -0700

    Update ConstructBinaryTreeFromInorderAndLevelorderTraversal.java

--- leetcode/Tree/ConstructBinaryTreeFromInorderAndLevelorderTraversal.java
+++ leetcode/Tree/ConstructBinaryTreeFromInorderAndLevelorderTraversal.java
@@ -108,5 +108,63 @@ class Solution {
 // Solution 2: 
 // Refer to
 // https://www.techiedelight.com/construct-binary-tree-from-inorder-level-order-traversals/
+class Solution {
+    // Recursive function to construct a binary tree from in-order and level-order traversals
+    public static Node buildTree(int[] inorder, int start, int end, Map < Integer, Integer > map) {
+        // base case
+        if (start > end) {
+            return null;
+        }
+        // find the index of root node in inorder[] to determine the
+        // boundary of left and right subtree
+        int index = start;
+        for (int j = start + 1; j <= end; j++) {
+            // find node with minimum index in level order traversal
+            // That would be the root node of inorder[start, end] ???
+            /**
+             This should be the most tricky part of solution:
+             Based on below test case
+             int[] inorder = { 4, 2, 5, 1, 6, 3, 7 };
+        	    int[] level =   { 1, 2, 3, 4, 5, 6, 7 };
+                          1
+                        /   \
+                       2     3
+                      / \   / \
+                     4   5 6   7  
+             Looks like try to find pair [1 / 4] and the discipline is [1 / 4] is
+             the first pair which value in inorder array < value in levelorder array ???
+            */
+            if (map.get(inorder[j]) < map.get(inorder[index])) {
+                index = j;
+            }
+        }
 
+        // construct the root node
+        Node root = new Node(inorder[index]);
+        // recursively construct the left subtree
+        root.left = buildTree(inorder, start, index - 1, map);
+        // recursively construct the right subtree
+        root.right = buildTree(inorder, index + 1, end, map);
+        // return root node
+        return root;
+    }
+
+    // Construct a binary tree from in-order and level-order traversals
+    public static Node buildTree(int[] in , int[] level) {
+        // create a map to efficiently find index of an element in
+        // level-order sequence
+        Map < Integer, Integer > map = new HashMap < > ();
+        for (int i = 0; i < in .length; i++) {
+            map.put(level[i], i);
+        }
+        // Construct the tree and return it
+        return buildTree( in , 0, in .length - 1, map);
+    }
+
+    public static void main(String[] args) {
+        int[] inorder = { 4, 2, 5, 1, 6, 3, 7 };
+        int[] level	= { 1, 2, 3, 4, 5, 6, 7 };
+        Node root = buildTree(inorder, level);
+    }
+}
 
