17abb4339572b38a523756bcc0468065bf7b33a6
lampardchelsea <zqqsxdd@gmail.com>
2020-06-25 17:40:17 -0700

    Update AddOneRowToTree.java

--- leetcode/Tree/AddOneRowToTree.java
+++ leetcode/Tree/AddOneRowToTree.java
@@ -105,4 +105,60 @@ class Solution {
     }
 }
 
+// Solution 2: Recursive with Stack
+// Refer to
+// https://leetcode.com/problems/add-one-row-to-tree/solution/
+/**
+We can do the same task as discussed in the last approach by making use of a stackstack as well. 
+But, we need to make use of a new data structure, NodeNode here, to keep a track of the depth of the current node along with its value.
+We start by pushing the root NodeNode onto the stackstack. Then, at every step we do as follows:
+Pop an element from the stackstack.
+For every Node popped, check if its depth corresponds to one prior to the depth at which the new node needs to be inserted.
+If yes, insert the new nodes appropriately as in the last approach.
+If no, we push both the left and the right child Node(value+depth) of the current node onto the stackstack.
+Continue the popping and pushing process till the stackstack becomes empty.
+*/
+class Solution {
+    class Node {
+        TreeNode node;
+        int depth;
+        public Node(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+    
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if(d == 1) {
+            TreeNode n = new TreeNode(v);
+            n.left = root;
+            return n;
+        }
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(new Node(root, 1));
+        while(!stack.isEmpty()) {
+            Node n = stack.pop();
+            // Important ! Check whether current node is null, it may introduced by inserting
+            // e.g input as root = {4,2,null,3,1}, v = 1, d = 3
+            if(n.node == null) {
+                continue;
+            }
+            if(n.depth == d - 1) {
+                TreeNode original_left = n.node.left;
+                TreeNode original_right = n.node.right;
+                TreeNode new_left = new TreeNode(v);
+                TreeNode new_right = new TreeNode(v);
+                n.node.left = new_left;
+                new_left.left = original_left;
+                n.node.right = new_right;
+                new_right.right = original_right;
+            } else {
+                stack.push(new Node(n.node.left, n.depth + 1));
+                stack.push(new Node(n.node.right, n.depth + 1));
+            }
+        }
+        return root;
+    }
+}
+
 
