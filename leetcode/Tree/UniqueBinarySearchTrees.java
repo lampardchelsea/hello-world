0bfd45643802eb46dfad900902e1a37d4a7523bb
lampardchelsea <zqqsxdd@gmail.com>
2020-08-06 02:13:54 -0700

    Update UniqueBinarySearchTrees.java

--- leetcode/Tree/UniqueBinarySearchTrees.java
+++ leetcode/Tree/UniqueBinarySearchTrees.java
@@ -111,8 +111,18 @@ Hope it will help you to understand :
     
 And  so on...
 */
-
-
+class Solution {
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = G[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+}
 
 
 
