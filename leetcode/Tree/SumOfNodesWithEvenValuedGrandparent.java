20bd60d4ddafadeababc20415270c22842696c59
lampardchelsea <zqqsxdd@gmail.com>
2020-08-31 17:49:38 -0700

    Update SumOfNodesWithEvenValuedGrandparent.java

--- leetcode/Tree/SumOfNodesWithEvenValuedGrandparent.java
+++ leetcode/Tree/SumOfNodesWithEvenValuedGrandparent.java
@@ -77,4 +77,37 @@ class Solution {
 
 // Solution 2: DFS
 // Refer to
-// 
+// https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/discuss/477095/Easy-DFS-solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public int sumEvenGrandparent(TreeNode root) {
+        helper(root, null, null);
+        return sum;
+    }
+    
+    private void helper(TreeNode curr, TreeNode parent, TreeNode grandParent) {
+        if(curr == null) {
+            return;
+        }
+        if(grandParent != null && grandParent.val % 2 == 0) {
+            sum += curr.val;
+        }
+        helper(curr.left, curr, parent);
+        helper(curr.right, curr, parent);
+    }
+}
