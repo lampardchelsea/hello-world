18a12d857850f459de3b13efce962b0041732801
lampardchelsea <zqqsxdd@gmail.com>
2020-09-04 16:48:47 -0700

    Update CloneBinaryTreeWithRandomPointer.java

--- leetcode/Tree/CloneBinaryTreeWithRandomPointer.java
+++ leetcode/Tree/CloneBinaryTreeWithRandomPointer.java
@@ -55,3 +55,107 @@
  The number of nodes in the tree is in the range [0, 1000].
  Each node’s value is between [1, 10^6].
 */
+
+// Solution 1:
+// Refer to
+// https://www.techiedelight.com/clone-a-binary-tree-with-random-pointers/
+/**
+ To clone a binary tree with reandom pointers, the intuitive solution is to maintain a hash table
+ for each node in the binary tree, the idea is to traverse the binary tree in preorder fashion
+ and for each encountered node, we create a new node with same data and create a mapping from the
+ orignial node to the duplicate node in the hash table. After creating the mapping, we recursively
+ set its left and right pointers. Finally, we traverse the original binary tree again and update
+ random pointers of the duplicate nodes using the hash table
+*/
+class Node {
+    int data;
+    Node left, right, random;
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+class Solution {
+    // Function to print the preorder traversal of a binary tree
+    public void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        // print data
+        System.out.print(root.data + " -> (");
+        // print left child's data
+        System.out.print((root.left != null ? root.left.data : "X") + ", ");
+        // print right child's data
+        System.out.print((root.right != null ? root.right.data : "X") + ", ");
+        // print random child's data
+        System.out.println((root.random != null ? root.random.data : "X") + ")");
+        // recur for the left and right subtree
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public Node copyRandomBinaryTree(Node root) {
+        // Create a map to store mappings from a node to its clone
+        Map < Node, Node > map = new HashMap < Node, Node > ();
+        // Clone data, left and right pointers for each node of
+        // original binary tree and put references into the map
+        cloneLeftRightPointers(root, map);
+        // Update random pointers from the original binary tree
+        // into the map
+        updateRandomPointers(root, map);
+        // Return the cloned root node
+        return map.get(root);
+    }
+
+    private Node cloneLeftRightPointers(Node root, Map < Node, Node > map) {
+        // Base case
+        if (root == null) {
+            return null;
+        }
+        // Clone all fields of the root node except random pointer
+        // Create a new node with same data as root node
+        Node newRoot = new Node(root.data);
+        map.put(root, newRoot);
+        // Clone the left and right subtree
+        newRoot.left = cloneLeftRightPointers(root.left, map);
+        newRoot.right = cloneLeftRightPointers(root.right, map);
+        // Return cloned root node
+        return newRoot;
+    }
+
+    private void updateRandomPointers(Node root, Map < Node, Node > map) {
+        // Base case
+        if (map.get(root) == null) {
+            return;
+        }
+        // Update the random pointer of cloned node
+        map.get(root).random = map.get(root.random);
+        // Update for left and right subtree
+        updateRandomPointers(root.left, map);
+        updateRandomPointers(root.right, map);
+    }
+
+    public static void main(String[] args) {
+        Node root = q.new Node(1);
+        root.left = q.new Node(2);
+        root.right = q.new Node(3);
+        root.left.left = q.new Node(4);
+        root.left.right = q.new Node(5);
+        root.right.left = q.new Node(6);
+        root.right.right = q.new Node(7);
+
+        root.random = root.right.left.random;
+        root.left.left.random = root.right;
+        root.left.right.random = root;
+        root.right.left.random = root.left.left;
+        root.random = root.left;
+
+        System.out.println("Preorder traversal of the original tree:");
+        q.preorder(root);
+
+        Node clone = q.copyRandomBinaryTree(root);
+
+        System.out.println("\nPreorder traversal of the cloned tree:");
+        q.preorder(clone);
+    }
+}
