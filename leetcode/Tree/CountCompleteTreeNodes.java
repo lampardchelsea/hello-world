b8cb9406c9d53df1daa2538b2c46235040d65dd1
lampardchelsea <zqqsxdd@gmail.com>
2020-09-17 12:32:37 -0700

    Update CountCompleteTreeNodes.java

--- leetcode/Tree/CountCompleteTreeNodes.java
+++ leetcode/Tree/CountCompleteTreeNodes.java
@@ -72,8 +72,26 @@ class Solution {
     }
 }
 
-// Solution 3:
+// Solution 3: Use the nature of complete binary tree / perfect binary tree / full binary tree
 // Refer to
+// https://github.com/lampardchelsea/hello-world/blob/master/leetcode/Tree/Document/Full_Compelete_Perfect_Tree.pdf
+/**
+ 完全二叉树 (Complete Binary Tree)：
+ A Complete Binary Tree （CBT) is a binary tree in which every level, except possibly the last, is completely filled, 
+ and all nodes are as far left as possible.
+ 对于一颗二叉树，假设其深度为d（d>1）。除了第d层外，其它各层的节点数目均已达最大值，且第d层所有节点从左向右连续地紧密排列，
+ 这样的二叉树被称为完全二叉树；换句话说，完全二叉树从根结点到倒数第二层满足完美二叉树，最后一层可以不完全填充，其叶子结点都靠左对齐。
+ 
+ 完美二叉树 (Perfect Binary Tree)：
+ A Perfect Binary Tree(PBT) is a tree with all leaf nodes at the same depth. All internal nodes have degree 2.
+ 二叉树的第i层至多拥有 (2 ^ i) - 1 个节点数；深度为k的二叉树至多总共有 (2 ^ (k + 1)) -1 个节点数，
+ 而总计拥有节点数匹配的，称为“满二叉树”；
+
+ 完满二叉树 (Full Binary Tree):
+ A Full Binary Tree (FBT) is a tree in which every node other than the leaves has two children.
+ 换句话说，所有非叶子结点的度都是2。（只要你有孩子，你就必然是有两个孩子）
+*/
+
 // https://www.cnblogs.com/grandyang/p/4567827.html
 /**
  我们还是要来利用一下完全二叉树这个条件，不然感觉对出题者不太尊重。通过上面对完全二叉树跟完美二叉树的定义比较，可以看出二者的关系是，
