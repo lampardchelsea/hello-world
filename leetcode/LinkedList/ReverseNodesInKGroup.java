331923581b0f72f8ba31ed1b4e8019f4ab61d5e9
lampardchelsea <zqqsxdd@gmail.com>
2020-09-23 04:06:03 -0700

    Update ReverseNodesInKGroup.java

--- leetcode/LinkedList/ReverseNodesInKGroup.java
+++ leetcode/LinkedList/ReverseNodesInKGroup.java
@@ -254,3 +254,55 @@ class Solution {
 // Solution 2: Recursive
 // Refer to
 // https://leetcode.com/problems/reverse-nodes-in-k-group/discuss/11423/Short-but-recursive-Java-code-with-comments
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while(curr != null && count != k) { // find the k+1 node
+            curr = curr.next;
+            count++;
+        }
+        if(count == k) { // if k+1 node is found
+            curr = reverseKGroup(curr, k); // reverse list with k+1 node as head
+            // head - head-pointer to direct part, 
+            // curr - head-pointer to reversed part;
+            while(count-- > 0) { // reverse current k-group: 
+                ListNode tmp = head.next; // tmp - next head in direct part
+                head.next = curr; // preappending "direct" head to the reversed list 
+                curr = head; // move head of reversed part to a new node
+                head = tmp; // move "direct" head to the next node in direct part
+            }
+            head = curr;
+        }
+        return head;
+    }
+}
+
+// https://leetcode.com/problems/reverse-nodes-in-k-group/discuss/11423/Short-but-recursive-Java-code-with-comments/12134
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        // Test weather we have more then k node left, 
+        // if less then k node left we just return head 
+        ListNode node = head;
+        int count = 0;
+        while(count < k) {
+            if(node == null) {
+                return head;
+            }
+            node = node.next;
+            count++;
+        }
+        // Reverse k node at current level
+        // pre node point to the the answer of sub-problem
+        ListNode pre = reverseKGroup(node, k);
+        while(count > 0) {  
+            ListNode next = head.next; 
+            head.next = pre; 
+            pre = head; 
+            head = next;
+            count--;
+        }
+        return pre;
+    }
+}
+
