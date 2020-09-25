9b2d46eb344cf1f97704c7b002cba8f1b68ff9d3
lampardchelsea <zqqsxdd@gmail.com>
2020-09-25 03:07:33 -0700

    Update WebCrawler.java

--- leetcode/BFS/WebCrawler.java
+++ leetcode/BFS/WebCrawler.java
@@ -1,6 +1,7 @@
 /**
  Refer to
  https://code.dennyzhang.com/web-crawler
+ https://zhang0peter.com/2020/02/12/LeetCode-1242-Web-Crawler-Multithreaded/
  Given a url startUrl, implement a web crawler to crawl all links that are under the same hostname as startUrl.
  
  Your crawler should:
@@ -9,8 +10,30 @@
  3. Do not crawl the same link twice.
  4. Only crawl the links that are under the same hostname as startUrl.
  
- As shown in the example url above, the hostname is example.org. For simplicity sake, you may assume all urls 
- use http protocol without any port specified.
+ As shown in the example url above, the hostname is example.org. For simplicity sake, you may assume all urls use 
+ http protocol without any port specified. For example, the urls http://leetcode.com/problems and http://leetcode.com/contest 
+ are under the same hostname, while urls http://example.org/test and http://example.com/abc are not under the same hostname.
+
+ The HtmlParser interface is defined as such:
+ interface HtmlParser {
+   // Return a list of all urls from a webpage of given url.
+   // This is a blocking call, that means it will do HTTP request and return when this request is finished.
+   public List<String> getUrls(String url);
+ }
+ Note that getUrls(String url) simulates performing a HTTP request. You can treat it as a blocking function call which waits 
+ for a HTTP request to finish. It is guaranteed that getUrls(String url) will return the urls within 15ms. Single-threaded 
+ solutions will exceed the time limit so, can your multi-threaded web crawler do better?
+
+ Below are two examples explaining the functionality of the problem, for custom testing purposes you’ll have three variables urls, 
+ edges and startUrl. Notice that you will only have access to startUrl in your code, while urls and edges are not directly 
+ accessible to you in code.
+
+ Follow up:
+ Assume we have 10,000 nodes and 1 billion URLs to crawl. We will deploy the same software onto each node. The software can know 
+ about all the nodes. We have to minimize communication between machines and make sure each node does equal amount of work. 
+ How would your web crawler design change?
+ What if one node fails or does not work?
+ How do you know when the crawler is done?
  
 Example 1:
 Input:
