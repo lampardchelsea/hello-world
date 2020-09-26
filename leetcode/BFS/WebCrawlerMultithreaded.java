c6a57c52e92deb453a4033c0e7205b55f56db8d4
lampardchelsea <zqqsxdd@gmail.com>
2020-09-25 22:25:26 -0700

    Update WebCrawlerMultithreaded.java

--- leetcode/BFS/WebCrawlerMultithreaded.java
+++ leetcode/BFS/WebCrawlerMultithreaded.java
@@ -82,6 +82,88 @@ See: https://en.wikipedia.org/wiki/Hostname#Restrictions_on_valid_hostnames
 You may assume there’re no duplicates in url library.
 */
 
+// Points:
+// Refer to
+// https://zhang0peter.com/2020/02/12/LeetCode-1242-Web-Crawler-Multithreaded/
+/**
+这道题目很新颖，意思是写一个模拟爬虫程序，从一个开始页面，爬取属于这个域名的所有网页，LeetCode提供了爬虫的接口。
+题目要求必须使用多线程爬虫，不然会超时。
+1.用set来存储已经爬取过的网页,这个set需要支持多线程并发修改，我使用ConcurrentHashMap
+2.存储结果的List也只要支持多线程并发，我使用Collections.synchronizedList
+这个解法的缺点是可能会在运行过程中产生大量的无用线程，可以使用线程池进行并发操作，减少创建线程的开销。
+使用线程池的最大难点在于如何确定所有任务都执行完成，可以关闭线程池。
+我尝试使用 Future, CountDownLatch，但效果都不好。最关键的是运行速度没有直接新建线程快！！
+
+如果建太多线程不是会导致所有线程都很慢吗，是不是可以试一试Executors.newCachedThreadPool()提供的线程池，
+这样每当一个线程结束，建造一个新线程的时间和资源就免了
+*/
+
+// Refer to
+// https://yifei.me/note/818/
+/**
+1242 题要求使用多线程来实现。在现实生活中，爬虫作为一个 IO 密集型的任务，使用多线程是一项必须的优化。
+
+在上述的单线程版本中，我们使用了 visited 这个数组来存放已经访问过的节点，如果我们采用多线程的话，并且在每个线程中并发判断某个 URL 
+是否被访问过，那么势必需要给这个变量加一个锁。而我们知道，在多线程程序中，加锁往往造成性能损失最大，最容易引起潜在的 bug。
+那么有没有一种办法可以不用显式加锁呢？
+
+其实也很简单，我们只要把需要把并发访问的部分放到一个线程里就好了。这个想法是最近阅读 The Go Programming Language 得到的启发。全部代码如下：
+
+import threading
+import queue
+from urllib.parse import urlsplit
+
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: "HtmlParser") -> List[str]:
+        domain = urlsplit(startUrl).netloc
+        requestQueue = queue.Queue()
+        resultQueue = queue.Queue()
+        requestQueue.put(startUrl)
+        for _ in range(5):
+            t = threading.Thread(target=self._crawl, 
+                args=(domain, htmlParser, requestQueue, resultQueue))
+            t.daemon = True
+            t.start()
+        running = 1
+        visited = set([startUrl])
+        while running > 0:
+            urls = resultQueue.get()
+            for url in urls:
+                if url in visited:
+                    continue
+                visited.add(url)
+                requestQueue.put(url)
+                running += 1
+            running -= 1
+        return list(visited)
+
+    def _crawl(self, domain, htmlParser, requestQueue, resultQueue):
+        while True:
+            url = requestQueue.get()
+            urls = htmlParser.getUrls(url)
+            newUrls = []
+            for url in urls:
+                u = urlsplit(url)
+                if u.netloc == domain:
+                    newUrls.append(url)
+            resultQueue.put(newUrls)
+在上面的代码中，我们开启了 5 个线程并发请求，每个 worker 线程都做同样的事情：
+
+从 requestQueue 中读取一个待访问的 url；
+执行一个很耗时的网络请求：htmlParser.getUrls；
+然后把获取到的新的 url 处理后放到 resultQueue 中。
+而在主线程中：
+
+从 resultQueue 中读取一个访问的结果
+判断每个 URL 是否已经被访问过
+并分发到 requestQueue 中。
+我们可以看到在上述的过程中并没有显式使用锁（当然 queue 本身是带锁的）。原因就在于，我们把对于需要并发访问的结构限制在了一个线程中。
+
+当然如果可以用锁的话，也可以在每个 worker 线程中计数。而这种情况下，为了使用 running > 0 这个条件，
+一定要首先在发现新的 url 的时候 running++，在处理完整个页面之后再 running–。
+*/
+
+
 // Solution 1: Extends Thread
 // Refer to
 // https://leetcode.jp/leetcode-1242-web-crawler-multithreaded-%e8%a7%a3%e9%a2%98%e6%80%9d%e8%b7%af%e5%88%86%e6%9e%90/
@@ -254,6 +336,74 @@ class Solution {
             return false;
         }
         return url.length() == hostname.length() || url.charAt(hostname.length()) == '/';
+    }
+}
+*/
+
+// Solution 3: Using Executor
+// Refer to
+// https://blog.csdn.net/u013325815/article/details/104355226
+// 就是用queue，bfs，不过要用多线程写，我抄的答案
+/** 
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.locks.*;
+import java.net.*;
+ 
+public class Solution {    
+    ExecutorService pool = Executors.newFixedThreadPool(4);
+    AtomicLong numTasks = new AtomicLong(0);
+    Lock lock = new ReentrantLock();
+    List<String> result = new ArrayList<String>();
+    Set<String> visited = new HashSet<>();
+    
+    private class crawlTask implements Runnable {
+        String url;
+        public crawlTask(String url) {
+            this.url = url;
+        }
+        
+        @Override
+        public void run() {
+            try {
+                for(String neighbor: HtmlHelper.parseUrls(url)) {
+                    URL neighborURL = new URL(neighbor);
+                    if(!neighborURL.getHost().endsWith("wikipedia.org")) {
+                        continue;
+                    }
+                    lock.lock();
+                    if(!visited.contains(neighbor)) {
+                        visited.add(neighbor);
+                        result.add(neighbor);
+                        pool.execute(new crawlTask(neighbor));
+                        numTasks.incrementAndGet();
+                    }
+                    lock.unlock();
+                }
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                numTasks.decrementAndGet();
+            }
+        }
+    }
+     
+    public List<String> crawler(String url) {
+        visited.add(url);
+        result.add(url);
+        pool.execute(new crawlTask(url));
+        numTasks.incrementAndGet();
+        try{
+            while(numTasks.get() != 0){
+                Thread.sleep(10);
+            } 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        pool.shutdown();
+        return result;
     }
 }
 */
