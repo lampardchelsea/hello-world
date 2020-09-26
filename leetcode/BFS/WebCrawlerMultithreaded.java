00fa0ce04ffcc73a0a88425d2fdaaa5a24b2ea34
lampardchelsea <zqqsxdd@gmail.com>
2020-09-25 22:15:30 -0700

    Update WebCrawlerMultithreaded.java

--- leetcode/BFS/WebCrawlerMultithreaded.java
+++ leetcode/BFS/WebCrawlerMultithreaded.java
@@ -157,3 +157,103 @@ class Crawler extends Thread {
 Runtime: 2 ms, faster than 97.83% of Java online submissions for Web Crawler Multithreaded.
 Memory Usage: 89.5 MB, less than 100.00% of Java online submissions for Web Crawler Multithreaded.
 */
+
+// Solution 2: Implement Runnable
+// Refer to
+// http://guanzhou.pub/2020/07/01/LeetCode1/
+/**
+ Style 1:
+ class Solution {
+    public List<String> crawl(String startUrl, HtmlParser htmlParser) {
+
+        // find hostname
+        int index = startUrl.indexOf('/', 7);
+        String hostname = (index != -1) ? startUrl.substring(0, index) : startUrl;
+
+        // multi-thread
+        Crawler crawler = new Crawler(startUrl, hostname, htmlParser);
+        crawler.map = new ConcurrentHashMap<>(); // reset result as static property belongs to class, it will go through all of the test cases
+        crawler.result = crawler.map.newKeySet();
+        Thread thread = new Thread(crawler);
+        thread.start();
+
+        crawler.joinThread(thread); // wait for thread to complete
+        return new ArrayList<>(crawler.result);
+    }
+}
+
+class Crawler implements Runnable {
+    String startUrl;
+    String hostname;
+    HtmlParser htmlParser;
+    public static ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+    public static Set<String> result = map.newKeySet();
+
+    public Crawler(String startUrl, String hostname, HtmlParser htmlParser) {
+        this.startUrl = startUrl;
+        this.hostname = hostname;
+        this.htmlParser = htmlParser;
+    }
+
+    @Override
+    public void run() {
+        if (this.startUrl.startsWith(hostname) && !this.result.contains(this.startUrl)) {
+            this.result.add(this.startUrl);
+            List<Thread> threads = new ArrayList<>();
+            for (String s : htmlParser.getUrls(startUrl)) {
+			    if(result.contains(s)) continue;
+                Crawler crawler = new Crawler(s, hostname, htmlParser);
+                Thread thread = new Thread(crawler);
+                thread.start();
+                threads.add(thread);
+            }
+            for (Thread t : threads) {
+                joinThread(t); // wait for all threads to complete
+            }
+        }
+    }
+
+    public static void joinThread(Thread thread) {
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+Sytle 2:
+class Solution {
+    public List<String> crawl(String startUrl, HtmlParser htmlParser) {
+        String hostname = getHostname(startUrl);
+
+        Set<String> visited = ConcurrentHashMap.newKeySet();
+        visited.add(startUrl);
+
+        return crawl(startUrl, htmlParser, hostname, visited)
+            .collect(Collectors.toList());
+    }
+
+    private Stream<String> crawl(String startUrl, HtmlParser htmlParser, String hostname, Set<String> visited) {
+        Stream<String> stream = htmlParser.getUrls(startUrl)
+            .parallelStream()
+            .filter(url -> isSameHostname(url, hostname))
+            .filter(url -> visited.add(url))
+            .flatMap(url -> crawl(url, htmlParser, hostname, visited));
+
+        return Stream.concat(Stream.of(startUrl), stream);
+    }
+
+    private String getHostname(String url) {
+        int idx = url.indexOf('/', 7);
+        return (idx != -1) ? url.substring(0, idx) : url;
+    }
+
+    private boolean isSameHostname(String url, String hostname) {
+        if (!url.startsWith(hostname)) {
+            return false;
+        }
+        return url.length() == hostname.length() || url.charAt(hostname.length()) == '/';
+    }
+}
+*/
