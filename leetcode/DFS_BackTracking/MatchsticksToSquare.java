aed339d7bd9516497a5dcb20b0d11d3422a8bfa2
lampardchelsea <zqqsxdd@gmail.com>
2020-10-12 20:25:03 -0700

    Update MatchsticksToSquare.java

--- leetcode/DFS_BackTracking/MatchsticksToSquare.java
+++ leetcode/DFS_BackTracking/MatchsticksToSquare.java
@@ -88,6 +88,17 @@ class Solution {
  We can either reverse array as one more function or we can traverse indexes from end to start index
  Here we choose the 2nd way since it will take even less time and boost the time from 1095ms to 21ms
 */
+// 中文解释
+// https://www.cnblogs.com/grandyang/p/7733098.html
+/**
+ 下面这种方法也挺巧妙的，思路是建立长度为k的数组v，只有当v里面所有的数字都是target的时候，才能返回true。我们还需要给数组排个序，
+ 由于题目中限制了全是正数，所以数字累加只会增大不会减小，一旦累加超过了target，这个子集合是无法再变小的，所以就不能加入这个数。
+ 实际上相当于贪婪算法，由于题目中数组数字为正的限制，有解的话就可以用贪婪算法得到。我们用一个变量idx表示当前遍历的数字，排序后，
+ 我们从末尾大的数字开始累加，我们遍历数组v，当前位置加上nums[idx]，如果超过了target，我们掉过继续到下一个位置，否则就调用递归，
+ 此时的idx为idx-1，表示之前那个数字已经成功加入数组v了，我们尝试着加下一个数字。如果递归返回false了，我们就将nums[idx]从数组v
+ 中对应的位置减去，还原状态，然后继续下一个位置。如果某个递归中idx等于-1了，表明所有的数字已经遍历完了，此时我们检查数组v中k个
+ 数字是否都为target，是的话返回true，否则返回false
+*/
 // Runtime: 21 ms, faster than 68.12% of Java online submissions for Matchsticks to Square.
 // Memory Usage: 36.4 MB, less than 10.47% of Java online submissions for Matchsticks to Square.
 class Solution {
