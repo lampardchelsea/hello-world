1fe628ad429524a6c521499bbb9801c362a31622
lampardchelsea <zqqsxdd@gmail.com>
2020-10-17 03:17:55 -0700

    Update AccountsMerge.java

--- leetcode/DFS_BackTracking/AccountsMerge.java
+++ leetcode/DFS_BackTracking/AccountsMerge.java
@@ -204,3 +204,87 @@ class Solution {
     }
 }
 
+// Solution 2: DFS (Connect all emails as mutual way as graph + group another map for mapping onwers only)
+// Refer to
+// https://leetcode.com/problems/accounts-merge/discuss/109158/Java-Solution-(Build-graph-%2B-DFS-search)
+/**
+I have tried my best to make my code clean. Hope the basic idea below may help you. Happy coding!
+
+Basicly, this is a graph problem. Notice that each account[ i ] tells us some edges. What we have to do is as follows:
+
+Use these edges to build some components. Common email addresses are like the intersections that connect each single component for each account.
+Because each component represents a merged account, do DFS search for each components and add into a list. 
+Before add the name into this list, sort the emails. Then add name string into it.
+
+Examples: Assume we have three accounts, we connect them like this in order to use DFS.
+{Name, 1, 2, 3} => Name -- 1 -- 2 -- 3
+{Name, 2, 4, 5} => Name -- 2 -- 4 -- 5 (The same graph node 2 appears)
+{Name, 6, 7, 8} => Name -- 6 -- 7 -- 8
+(Where numbers represent email addresses).
+*/
+class Solution {
+    // Test with 3 lists:
+    // {a,b,c} -> owner = a, 1st email = b, 2nd email = c
+    // {d,e,f} -> owner = d, 1st email = e, 2nd email = f
+    // {g,a,d} -> owner = g, 1st email = a, 2nd email = b
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        Map<String, Set<String>> graph = new HashMap<>();    // <email node, neighbor nodes>
+        Map<String, String> owners = new HashMap<>();        // <email, owner>
+        // Build the graph;
+        // After building:
+        // owners: {a=g, b=a, c=a, d=g, e=d, f=d}
+        // graph: {a=[d], b=[c], c=[b], d=[a], e=[f], f=[e]}
+        for (List<String> account : accounts) {
+            String owner = account.get(0);
+            for (int i = 1; i < account.size(); i++) {
+                if (!graph.containsKey(account.get(i))) {
+                    graph.put(account.get(i), new HashSet<>());
+                }
+                owners.put(account.get(i), owner);
+                // No need to connect owner with first email
+                // e.g in our test we have 3 lists, after connect only on all emails:
+                // a: b -- c (not directed, mutual way)
+                // d: e -- f (not directed, mutual way)
+                // g: a -- d (not directed, mutual way)
+                if (i == 1) continue;
+                graph.get(account.get(i)).add(account.get(i - 1));
+                graph.get(account.get(i - 1)).add(account.get(i));
+            }
+        }
+        
+        Set<String> visited = new HashSet<>();
+        List<List<String>> res = new LinkedList<>();
+        // DFS search the graph;
+        // owners.keySet() --> get all unique email values
+        for (String email : owners.keySet()) {
+            List<String> list = new LinkedList<>();
+            if (visited.add(email)) {
+                // The 'email' is the link to search between different groups
+                // note: one group means --> in map <email node, neighbor nodes>
+                // the same 'email' value can present on different group,
+                // and since we connect all emails in a group and store all groups
+                // as a graph, which means if we find one email in different group,
+                // we will able to group them into one single combination as a list
+                dfs(graph, email, visited, list);
+                Collections.sort(list);
+                list.add(0, owners.get(email));
+                res.add(list);
+            }
+        }
+        return res;
+    }
+    
+    public void dfs(Map<String, Set<String>> graph, String email, Set<String> visited, List<String> list) {
+        list.add(email);
+        // Use 'email' to find all its neighbor (connected) emails
+        // Since mutual connected, use 'visited' to prevent trace
+        // back on previous email
+        for (String next : graph.get(email)) {
+            if (visited.add(next)) {
+                dfs(graph, next, visited, list);
+            }
+        }
+    }
+}
+
+
