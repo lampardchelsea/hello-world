3541ed44e67785c24882673ccd7fd6bba42c83db
lampardchelsea <zqqsxdd@gmail.com>
2020-10-25 14:37:24 -0700

    Update TheMazeII.java

--- leetcode/DFS_BackTracking/TheMazeII.java
+++ leetcode/DFS_BackTracking/TheMazeII.java
@@ -118,3 +118,233 @@ class Solution {
         }
     }
 }    
+
+// Re-work
+// Solution 1:
+// Dijkstra
+// https://cheonhyangzhang.gitbooks.io/leetcode-solutions/content/solutions-501-550/505-the-maze-ii.html
+/**
+public class Solution {
+    private class Point implements Comparable<Point>{
+        public int x;
+        public int y;
+        public int step;
+        public Point(int x, int y, int step) {
+            this.x = x;
+            this.y = y;
+            this.step = step;
+        }
+        public int compareTo(Point p) {
+            return this.step - p.step;
+        }
+    }
+    public int shortestDistance(int[][] maze, int[] start, int[] destination) {
+        int[][] dirs = new int[4][2];
+        dirs[0] = new int[]{1, 0};
+        dirs[1] = new int[]{0, 1};
+        dirs[2] = new int[]{-1, 0};
+        dirs[3] = new int[]{0, -1};
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        PriorityQueue<Point> q = new PriorityQueue<Point>();
+        q.add(new Point(start[0], start[1], 0));
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+            if (p.x == destination[0] && p.y == destination[1]) {
+                return p.step;
+            }
+            visited[p.x][p.y] = true;
+            for (int k = 0; k < dirs.length; k ++) {
+                int i = p.x;
+                int j = p.y;
+                int count = 0;
+                while (i >= 0 && i < maze.length && j >= 0 && j < maze[0].length && maze[i][j] == 0) {
+                    i += dirs[k][0];
+                    j += dirs[k][1];
+                    count ++;
+                }
+                i -= dirs[k][0];
+                j -= dirs[k][1];
+                count --;
+                if (visited[i][j] == false) {
+                    q.add(new Point(i, j, p.step + count));
+                }
+            }
+        }
+        return -1;
+    }
+}
+*/
+// Style 1: Use boolean visited which is not exactly same as definition of Dijkstra, but shortest path
+// pick up all depends on minPQ already
+public class Solution {
+    /**
+     * @param maze: the maze
+     * @param start: the start
+     * @param destination: the destination
+     * @return: the shortest distance for the ball to stop at the destination
+     */
+    public int shortestDistance(int[][] maze, int[] start, int[] destination) {
+        // base case
+        if(Arrays.equals(start, destination)) {
+            return 0;
+        }
+        return shortestPath(maze, start, destination);
+    }
+    
+    int[] dx = new int[]{0,0,1,-1};
+    int[] dy = new int[]{-1,1,0,0};
+    private int shortestPath(int[][] maze, int[] start, int[] destination) {
+        // get the vertice has the minimum distance to start
+        PriorityQueue<int[]> minPQ = new PriorityQueue<int[]>((a, b) -> (a[2] - b[2]));
+        minPQ.offer(new int[] {start[0], start[1], 0});
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        while(!minPQ.isEmpty()) {
+            int[] cur = minPQ.poll();
+            // Find the shortest path
+            if(cur[0] == destination[0] && cur[1] == destination[1]) {
+                return cur[2];
+            }
+            visited[cur[0]][cur[1]] = true;
+            for(int k = 0; k < 4; k++) {
+                int x = cur[0];
+                int y = cur[1];
+                int step = 0;
+                // Keep rolling till hit the wall
+                while(x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] == 0) {
+                    x += dx[k];
+                    y += dy[k];
+                    step++;
+                }
+                // One step back to the stop position
+                x -= dx[k];
+                y -= dy[k];
+                step--;
+                // Get current rolling result(after it stopped) distance and update to PQ and visited 2D array
+                int distance = cur[2] + step;
+                if(!visited[x][y]) {
+                    minPQ.offer(new int[] {x, y, distance});
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+// https://github.com/cherryljr/LeetCode/blob/master/The%20Maze%20II.java
+/**
+* Approach 3: Dijkstra's Algorithm
+ * 求单源到到某一个节点的最短路径可以使用 Dijkstra's Algorithm 来进行解决。
+ * 虽然在这是属于杀鸡用牛刀的感觉了，但是也是一种挺好的解法。
+ * 关于 Dijkstra's Algorithm 的模板以及相关解析可以参考：
+ * Network Delay Time -- Approach 3:
+ *  https://github.com/cherryljr/LeetCode/blob/master/Network%20Delay%20Time.java
+ *
+ * 这里使用了 distance[][] 替代了模板中 HashMap 的实现，速度会更快一些。
+ * 两种做法都可以，大家可以自行选择。
+ class Solution {
+    private static final int[][] DIRS = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+
+    public int shortestDistance(int[][] maze, int[] start, int[] dest) {
+        int[][] distance = new int[maze.length][maze[0].length];
+        // Initialize the distance array
+        for (int[] row: distance) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        // Sort the minHeap according to distance
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[2] - b[2]));
+        // Put the start position into minHeap
+        pq.offer(new int[]{start[0], start[1], 0});
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            if (distance[curr[0]][curr[1]] != Integer.MAX_VALUE) {
+                continue;
+            }
+
+            distance[curr[0]][curr[1]] = curr[2];
+            for (int[] dir : DIRS) {
+                int x = curr[0], y = curr[1];
+                int count = 0;
+                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                    x += dir[0];
+                    y += dir[1];
+                    count++;
+                }
+                x -= dir[0];
+                y -= dir[1];
+                count--;
+                
+                if (distance[x][y] == Integer.MAX_VALUE) {
+                    pq.offer(new int[]{x, y, curr[2] + count});
+                }
+            }
+        }
+
+        return distance[dest[0]][dest[1]] == Integer.MAX_VALUE ? -1 : distance[dest[0]][dest[1]];
+    }
+}
+*/
+// Style 2: Use int array distance which exactly same as definition of Dijkstra, check shortest path
+// depends on comparison between original distance and new calculated distance 
+// (if u -> v has dist[u] + step < dist[v] then we update)
+public class Solution {
+    /**
+     * @param maze: the maze
+     * @param start: the start
+     * @param destination: the destination
+     * @return: the shortest distance for the ball to stop at the destination
+     */
+    public int shortestDistance(int[][] maze, int[] start, int[] destination) {
+        // base case
+        if(Arrays.equals(start, destination)) {
+            return 0;
+        }
+        return shortestPath(maze, start, destination);
+    }
+    
+    int[] dx = new int[]{0,0,1,-1};
+    int[] dy = new int[]{-1,1,0,0};
+    private int shortestPath(int[][] maze, int[] start, int[] destination) {
+        // get the vertice has the minimum distance to start
+        PriorityQueue<int[]> minPQ = new PriorityQueue<int[]>((a, b) -> (a[2] - b[2]));
+        minPQ.offer(new int[] {start[0], start[1], 0});
+        // Strictly follow Dijkstra definition
+        int[][] distance = new int[maze.length][maze[0].length];
+        for(int[] d : distance) {
+            Arrays.fill(d, Integer.MAX_VALUE);
+        }
+        while(!minPQ.isEmpty()) {
+            int[] cur = minPQ.poll();
+            // Find the shortest path
+            if(cur[0] == destination[0] && cur[1] == destination[1]) {
+                return cur[2];
+            }
+            distance[cur[0]][cur[1]] = cur[2];
+            for(int k = 0; k < 4; k++) {
+                int x = cur[0];
+                int y = cur[1];
+                int step = 0;
+                // Keep rolling till hit the wall
+                while(x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] == 0) {
+                    x += dx[k];
+                    y += dy[k];
+                    step++;
+                }
+                // One step back to the stop position
+                x -= dx[k];
+                y -= dy[k];
+                step--;
+                // Get current rolling result(after it stopped) distance and update to PQ and visited 2D array
+                int newDistance = cur[2] + step;
+                // Comparison new distance with original value to make sure we need to update based on Dijkstra definition
+                // (if u -> v has dist[u] + step < dist[v] then we update)
+                if(distance[x][y] > newDistance) {
+                    minPQ.offer(new int[] {x, y, newDistance});
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+
