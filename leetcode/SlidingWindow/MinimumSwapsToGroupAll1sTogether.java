05e1b2d8452a043cc3c7f74a2e4d5dc8f2c743c1
lampardchelsea <zqqsxdd@gmail.com>
2020-11-11 17:41:04 -0800

    Update MinimumSwapsToGroupAll1sTogether.java

--- leetcode/SlidingWindow/MinimumSwapsToGroupAll1sTogether.java
+++ leetcode/SlidingWindow/MinimumSwapsToGroupAll1sTogether.java
@@ -31,6 +31,10 @@ Note:
 */
 
 // Solution 1: Fixed length sliding window
+// Template same as Find K-Length Substrings With No Repeated Characters
+// Refer to
+// https://github.com/lampardchelsea/hello-world/blob/master/leetcode/SlidingWindow/FindKLengthSubstringsWithNoRepeatedCharacters.java
+
 // Refer to
 // https://code.dennyzhang.com/minimum-swaps-to-group-all-1s-together
 /**
@@ -39,8 +43,60 @@ We know the size of the final group all 1's
 The window can start from any position
 Use a sliding window. And the find the minimum count of 0s in the sliding window.
 Complexity: Time O(n), Space O(k)
+func minSwaps(data []int) int {
+    k:=0
+    for _, v := range data {
+        if v == 1 {  k++ }
+    }
+    res := 1<<31-1
+    count := 0
+    // sliding window. left: i-k+1, right: i
+    for i:=0; i<len(data); i++ {
+        // move right pointer: add a new element
+        if data[i] == 0 { count++ }
+        // move left pointer: remove the old element
+        if i-k >= 0 {
+            if data[i-k] == 0 { count-- }
+        }
+        // collect result
+        if i-k+1 >= 0  {
+            if count < res { res = count }
+        }
+    }
+    return res
+}
 */
-
-
-
-
+class Solution {
+    public int minSwaps(int[] data) {
+        // Sliding window size K will be number of 1's in the given array
+        int K = 0;
+        for(int i = 0; i < data.length; i++) {
+            if(data[i] == 1) {
+                K++;
+            }
+        }
+        int minSwap = Integer.MAX_VALUE;
+        int curSwap = 0;
+        // Create sliding window as right boundary pointer index = i and since fixed length
+        // window, left boundary pointer automatically obtained as index = i - K + 1
+        for(int i = 0; i < data.length; i++) {
+            // If data[i] is 0 and still in sliding window, swap count increase 1
+            if(data[i] == 0) {
+                curSwap++;
+            }
+            // If move out of sliding window and data[i - K] is 0, swap count decrease 1,
+            // since we move position index = i - K element out of sliding window
+            if(i >= K) {
+                if(data[i - K] == 0) {
+                    curSwap--;
+                }
+            }
+            // Since i > K means at least we have got 2 candiates to compare, then 
+            // find the minimum swap required count by comparison
+            if(i > K) {
+                minSwap = Math.min(minSwap, curSwap);
+            }
+        }
+        return minSwap;
+    }
+}
