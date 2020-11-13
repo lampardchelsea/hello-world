49efc38300bb241805eeff1bda41c7de786ee79c
lampardchelsea <zqqsxdd@gmail.com>
2020-11-12 17:42:04 -0800

    Create LongestSubstringWithAtMostTwoDistinctCharacters.java

---
+++ leetcode/SlidingWindow/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -0,0 +1,30 @@
+/**
+Refer to
+https://www.lintcode.com/problem/longest-substring-with-at-most-two-distinct-characters/description
+Given a string, find the length of the longest substring T that contains at most 2 distinct characters.
+Example
+Example 1
+Input: “eceba”
+Output: 3
+Explanation:
+T is "ece" which its length is 3.
+
+Example 2
+Input: “aaa”
+Output: 3
+*/
+
+// Solution 1: Not fixed length window using two pointers + criteria
+// Refer to
+// Template from
+// https://github.com/lampardchelsea/hello-world/blob/master/leetcode/SlidingWindow/MaxConsecutiveOnesIII.java
+
+// Refer to
+// https://medium.com/@zengruiwang/sliding-window-technique-360d840d5740
+/**
+Two pointers + criteria: the window size is not fixed, usually it asks you to find the subarray that meets the criteria, 
+for example, given an array of integers, find the number of subarrays whose sum is equal to a target value.
+*/
+
+// Refer to
+// 
