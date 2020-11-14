93354b0226cdba3f9f3b96be0ba65656d3a89a4d
lampardchelsea <zqqsxdd@gmail.com>
2020-11-14 00:05:22 -0800

    Update LongestTurbulentSubarray.java

--- leetcode/SlidingWindow/LongestTurbulentSubarray.java
+++ leetcode/SlidingWindow/LongestTurbulentSubarray.java
@@ -27,6 +27,35 @@ Note:
 0 <= A[i] <= 10^9
 */
 
-// Solution 1:
+// Solution 1: Two flag to record
 // Refer to
-// 
+// https://leetcode.com/problems/longest-turbulent-subarray/discuss/221935/Java-O(N)-time-O(1)-space
+class Solution {
+    public int maxTurbulenceSize(int[] A) {
+        // For each A[i]
+        // inc: The length of current valid sequence which ends with two increasing numbers
+        // dec: The length of current valid sequence which ends with two decreasing numbers
+        int inc = 1;
+        int dec = 1;
+        int result = 1;
+        for(int i = 1; i < A.length; i++) {
+            // Current valid sequence ending with two decreasing numbers
+            if(A[i - 1] > A[i]) {
+                // Previous valid sequence length record in 'inc'
+                dec = inc + 1;
+                // Reset previous valid sequence length to 1
+                inc = 1;
+            } else if(A[i - 1] < A[i]) {
+                inc = dec + 1;
+                dec = 1;
+            } else {
+                // When encounter 2 adjacent equal numbers reset both to 1
+                dec = 1;
+                inc = 1;
+            }
+            // Since for each A[i] we get one pair of dec and inc, so we need to find out max one among all pairs
+            result = Math.max(result, Math.max(dec, inc));
+        }
+        return result;
+    }
+}
