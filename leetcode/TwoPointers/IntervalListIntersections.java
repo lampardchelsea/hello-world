d53602611358a0ec265f4685a01566b3552f4df8
lampardchelsea <zqqsxdd@gmail.com>
2020-11-25 13:03:24 -0800

    Update IntervalListIntersections.java

--- leetcode/TwoPointers/IntervalListIntersections.java
+++ leetcode/TwoPointers/IntervalListIntersections.java
@@ -21,4 +21,75 @@ Note:
 
 // Solution 1: Two Pointers
 // Refer to
-// 
+// https://leetcode.com/problems/interval-list-intersections/discuss/647482/Python-Two-Pointer-Approach-%2B-Thinking-Process-Diagrams
+/**
+Detail refer to
+https://github.com/lampardchelsea/hello-world/blob/master/leetcode/TwoPointers/Document/Interval_List_Intersections_Two_Points_Thinking_Process.docx
+*/
+
+// https://leetcode.com/problems/interval-list-intersections/solution/
+/**
+Approach 1: Merge Intervals
+Intuition
+In an interval [a, b], call b the "endpoint".
+Among the given intervals, consider the interval A[0] with the smallest endpoint. (Without loss of generality, this interval occurs in array A.)
+Then, among the intervals in array B, A[0] can only intersect one such interval in array B. (If two intervals in B intersect A[0], then they 
+both share the endpoint of A[0] -- but intervals in B are disjoint, which is a contradiction.)
+
+Algorithm
+If A[0] has the smallest endpoint, it can only intersect B[0]. After, we can discard A[0] since it cannot intersect anything else.
+Similarly, if B[0] has the smallest endpoint, it can only intersect A[0], and we can discard B[0] after since it cannot intersect anything else.
+We use two pointers, i and j, to virtually manage "discarding" A[0] or B[0] repeatedly.
+
+class Solution {
+  public int[][] intervalIntersection(int[][] A, int[][] B) {
+    List<int[]> ans = new ArrayList();
+    int i = 0, j = 0;
+
+    while (i < A.length && j < B.length) {
+      // Let's check if A[i] intersects B[j].
+      // lo - the startpoint of the intersection
+      // hi - the endpoint of the intersection
+      int lo = Math.max(A[i][0], B[j][0]);
+      int hi = Math.min(A[i][1], B[j][1]);
+      if (lo <= hi)
+        ans.add(new int[]{lo, hi});
+
+      // Remove the interval with the smallest endpoint
+      if (A[i][1] < B[j][1])
+        i++;
+      else
+        j++;
+    }
+
+    return ans.toArray(new int[ans.size()][]);
+  }
+}
+
+Complexity Analysis
+Time Complexity: O(M + N), where M, N are the lengths of A and B respectively.
+Space Complexity: O(M + N), the maximum size of the answer.
+*/
+class Solution {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> result = new ArrayList<int[]>();
+        int i = 0;
+        int j = 0;
+        while(i < A.length && j < B.length) {
+            int lo = Math.max(A[i][0], B[j][0]);
+            int hi = Math.min(A[i][1], B[j][1]);
+            if(lo <= hi) {
+                result.add(new int[] {lo, hi});
+            }
+            if(A[i][1] < B[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+}
+
+
+
