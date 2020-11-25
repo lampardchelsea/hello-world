61c89face45f3e0e4897c611eb8f9171ccecdaf5
lampardchelsea <zqqsxdd@gmail.com>
2020-11-25 11:25:38 -0800

    Update MergeIntervals.java

--- leetcode/array/MergeIntervals.java
+++ leetcode/array/MergeIntervals.java
@@ -160,3 +160,97 @@ public class MergeIntervals {
     
 }
 
+
+// Re-work
+// Refer to
+// https://leetcode.com/problems/merge-intervals/discuss/21222/A-simple-Java-solution
+/**
+The idea is to sort the intervals by their starting points. Then, we take the first interval and compare its end with the next intervals starts. 
+As long as they overlap, we update the end to be the max end of the overlapping intervals. Once we find a non overlapping interval, we can add 
+the previous "extended" interval and start over.
+
+Sorting takes O(n log(n)) and merging the intervals takes O(n). So, the resulting algorithm takes O(n log(n)).
+
+I used a lambda comparator (Java 8) and a for-each loop to try to keep the code clean and simple.
+
+EDIT: The function signature changed in april 2019.
+Here is a new version of the algorithm with arrays. To make more memory efficient, I reused the initial array (sort of "in-place") but it would 
+be easy to create new subarrays if you wanted to keep the initial data.
+It takes less memory than 99% of the other solutions (sometimes 90% depending on the run) and is more than 10 times faster than the previous 
+version with lists.
+
+class Solution {
+	public int[][] merge(int[][] intervals) {
+		if (intervals.length <= 1)
+			return intervals;
+
+		// Sort by ascending starting point
+		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
+		List<int[]> result = new ArrayList<>();
+		int[] newInterval = intervals[0];
+		result.add(newInterval);
+		for (int[] interval : intervals) {
+			if (interval[0] <= newInterval[1]) // Overlapping intervals, move the end if needed
+				newInterval[1] = Math.max(newInterval[1], interval[1]);
+			else {                             // Disjoint intervals, add the new interval to the list
+				newInterval = interval;
+				result.add(newInterval);
+			}
+		}
+
+		return result.toArray(new int[result.size()][]);
+	}
+}
+Previous version with lists.
+
+public List<Interval> merge(List<Interval> intervals) {
+    if (intervals.size() <= 1)
+        return intervals;
+    
+    // Sort by ascending starting point using an anonymous Comparator
+    intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
+    
+    List<Interval> result = new LinkedList<Interval>();
+    int start = intervals.get(0).start;
+    int end = intervals.get(0).end;
+    
+    for (Interval interval : intervals) {
+        if (interval.start <= end) // Overlapping intervals, move the end if needed
+            end = Math.max(end, interval.end);
+        else {                     // Disjoint intervals, add the previous one and reset bounds
+            result.add(new Interval(start, end));
+            start = interval.start;
+            end = interval.end;
+        }
+    }
+    
+    // Add the last interval
+    result.add(new Interval(start, end));
+    return result;
+}
+EDIT: Updated with Java 8 lambda comparator.
+EDIT 25/05/2019: Updated for new method signature.
+*/
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals.length == 1) {
+            return intervals;
+        }
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> result = new ArrayList<int[]>();
+        int[] newInterval = intervals[0];
+        result.add(newInterval);
+        for(int[] interval : intervals) {
+            // Overlapping intervals, move the end if needed
+            if(interval[0] <= newInterval[1]) {
+                newInterval[1] = Math.max(interval[1], newInterval[1]);
+            // Disjoint intervals, add the new interval to the list
+            } else {
+                newInterval = interval;
+                result.add(newInterval);
+            }
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+}
