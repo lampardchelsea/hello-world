c3037ea7d9d589646458f362446d54f290702d01
lampardchelsea <zqqsxdd@gmail.com>
2020-11-27 13:23:43 -0800

    Update RemoveAllAdjacentDuplicatesInStringII.java

--- leetcode/Stack/RemoveAllAdjacentDuplicatesInStringII.java
+++ leetcode/Stack/RemoveAllAdjacentDuplicatesInStringII.java
@@ -130,6 +130,34 @@ Complexity
 Time O(N) for one pass
 Space O(N)
 */
+// Refer to
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/discuss/392933/JavaC++Python-Two-Pointers-and-Stack-Solution/773393
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        // Array to hold the count of character at particular index
+        int[] count = new int[sb.length()];
+        for(int i = 0; i < sb.length(); i++) {
+            // For first character and when previous character is not equal to 
+            // current character then start count again for current character
+            if(i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                count[i] = 1;
+            // When character are equal increase the count by 1 of previous 
+            // count as they are same characters
+            } else {
+                count[i] = count[i - 1] + 1;
+                // IF current count == k, time to delete in buffer
+                if(count[i] == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    // Change the i position after deleting
+                    i = i - k;
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
+
 
 // Solution 2: Two Pointers
 // Refer to
