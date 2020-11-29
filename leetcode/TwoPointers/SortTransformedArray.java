b4f53fd468e3a65e17c7aa2fb813f634747953d1
lampardchelsea <zqqsxdd@gmail.com>
2020-11-29 02:25:11 -0800

    Create SortTransformedArray.java

---
+++ leetcode/TwoPointers/SortTransformedArray.java
@@ -0,0 +1,23 @@
+/**
+Refer to
+https://www.lintcode.com/problem/sort-transformed-array/description
+Given a sorted array of integers nums and integer values a, b and c. Apply a quadratic function of the 
+form f(x)=ax^2+bx+c to each element x in the array.
+
+The returned array must be in sorted order.
+
+Example1
+Input: nums = [-4, -2, 2, 4], a = 1, b = 3, c = 5
+Output: [3, 9, 15, 33]
+
+Example2
+Input: nums = [-4, -2, 2, 4], a = -1, b = 3, c = 5
+Output: [-23, -5, 1, 7]
+
+Notice
+Expected time complexity: O(n)
+*/
+
+// Solution 1:
+// Refer to
+// 
