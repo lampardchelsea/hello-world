8a6db8e79f60c049f35df891bf1632bbc6b1388c
lampardchelsea <zqqsxdd@gmail.com>
2020-11-29 14:09:31 -0800

    Update 3SumSmaller.java

--- leetcode/TwoPointers/3SumSmaller.java
+++ leetcode/TwoPointers/3SumSmaller.java
@@ -29,7 +29,6 @@ Could you solve it in O(n2) runtime?
 // Refer to
 // https://www.cnblogs.com/grandyang/p/5235086.html
 /**
-Since we only care about number of triplet combinations, we don't care about the indexes value, we can sort the array
 这道题是 3Sum 问题的一个变形，让我们求三数之和小于一个目标值，那么最简单的方法就是穷举法，将所有的可能的三个数字的组合都遍历一遍，
 比较三数之和跟目标值之间的大小，小于的话则结果自增1，参见代码如下:
 // O(n^3)
@@ -58,7 +57,6 @@ public class Solution {
      */
     public int threeSumSmaller(int[] nums, int target) {
         int count = 0;
-        Arrays.sort(nums);
         for(int i = 0; i < nums.length - 2; i++) {
             int sum = target - nums[i];
             for(int j = i + 1; j < nums.length - 1; j++) {
@@ -73,4 +71,32 @@ public class Solution {
     }
 }
 
-// Solution 2: 
+// Solution 2: Two Pointers
+// https://www.cnblogs.com/grandyang/p/5235086.html
+/**
+Since we only care about number of triplet combinations, we don't care about the indexes value, we can sort the array
+题目中的 Follow up 让我们在 O(n^2) 的时间复杂度内实现，那么借鉴之前那两道题 3Sum Closest 和 3Sum 中的方法，采用双指针来做，
+这里面有个 trick 就是当判断三个数之和小于目标值时，此时结果应该加上 right-left，因为数组排序了以后，如果加上 num[right] 
+小于目标值的话，那么加上一个更小的数必定也会小于目标值，然后将左指针右移一位，否则将右指针左移一位，参见代码如下：
+// O(n^2)
+class Solution {
+public:
+    int threeSumSmaller(vector<int>& nums, int target) {
+        if (nums.size() < 3) return 0;
+        int res = 0, n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n - 2; ++i) {
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] < target) {
+                    res += right - left;
+                    ++left;
+                } else {
+                    --right;
+                }
+            }
+        }
+        return res;
+    }
+};
+*/
