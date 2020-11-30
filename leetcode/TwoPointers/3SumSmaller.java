8589b6b1e4ad680a367c746fbd8d26b0064bb043
lampardchelsea <zqqsxdd@gmail.com>
2020-11-29 16:40:20 -0800

    Update 3SumSmaller.java

--- leetcode/TwoPointers/3SumSmaller.java
+++ leetcode/TwoPointers/3SumSmaller.java
@@ -137,5 +137,28 @@ public class Solution {
     }
 }
 */
-
+public class Solution {
+    /**
+     * @param nums:  an array of n integers
+     * @param target: a target
+     * @return: the number of index triplets satisfy the condition nums[i] + nums[j] + nums[k] < target
+     */
+    public int threeSumSmaller(int[] nums, int target) {
+        int count = 0;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while(j < k) {
+                if(nums[j] + nums[k] < target - nums[i]) {
+                    count += k - j;
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return count;
+    }
+}
 
