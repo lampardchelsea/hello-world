8f3a1dd9e2b8d9713adef2e37fe5ccfc88e8001a
lampardchelsea <zqqsxdd@gmail.com>
2020-12-02 12:08:24 -0800

    Update SubarraySumEqualsK.java

--- leetcode/array/SubarraySumEqualsK.java
+++ leetcode/array/SubarraySumEqualsK.java
@@ -113,6 +113,8 @@ class Solution {
         int count = 0;
         int sum = 0;
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // Why it is necessary to put (0,1) in the map before the loop. { map.put(0,1)} ?
+        // consider the case k = 3 and nums = [3]
         map.put(0, 1);
         for(int i = 0; i < nums.length; i++) {
             sum += nums[i];
@@ -122,5 +124,42 @@ class Solution {
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
+    }
+}
+
+
+// Why does hashmap + presum solution work ?
+// Refer to
+// https://leetcode.com/problems/subarray-sum-equals-k/discuss/102106/Java-Solution-PreSum-+-HashMap/172014
+// Some comments on "Why does this work?"
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        // Edge cases
+        if(nums.length == 0)    return 0;
+        
+        // Sliding window -- No, contains negative number
+        // hashmap + preSum
+        /*
+            1. Hashmap<sum[0,i - 1], frequency>
+            2. sum[i, j] = sum[0, j] - sum[0, i - 1]    --> sum[0, i - 1] = sum[0, j] - sum[i, j]
+                   k           sum      hashmap-key     -->  hashmap-key  =  sum - k
+            3. now, we have k and sum.  
+                  As long as we can find a sum[0, i - 1], we then get a valid subarray
+                 which is as long as we have the hashmap-key,  we then get a valid subarray
+            4. Why don't map.put(sum[0, i - 1], 1) every time ?
+                  if all numbers are positive, this is fine
+                  if there exists negative number, there could be preSum frequency > 1
+        */
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int result = 0;
+        map.put(0, 1);
+        for(int cur : nums) {
+            sum += cur;
+            if(map.containsKey(sum - k))  // there exist a key, that [hashmap-key  =  sum - k]
+                result += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return result; 
     }
 }
