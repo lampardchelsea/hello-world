474f80d19ca7201f8dfe1c89e8fce45181962759
lampardchelsea <zqqsxdd@gmail.com>
2020-12-07 03:41:31 -0800

    Update LongestMountainInArray.java

--- leetcode/TwoPointers/LongestMountainInArray.java
+++ leetcode/TwoPointers/LongestMountainInArray.java
@@ -58,4 +58,69 @@ Java:
         return res;
     }
 */
+class Solution {
+    public int longestMountain(int[] arr) {
+        int N = arr.length;
+        int[] up = new int[N];
+        int[] down = new int[N];
+        int count = 0;
+        for(int i = 0; i < N; i++) {
+            if(i > 0 && arr[i] > arr[i - 1]) {
+                up[i] = up[i - 1] + 1;
+            }
+        }
+        for(int i = N - 2; i >= 0; i--) {
+            if(arr[i] > arr[i + 1]) {
+                down[i] = down[i + 1] + 1;
+            }
+            if(up[i] > 0 && down[i] > 0) {
+                count = Math.max(count, up[i] + down[i] + 1);
+            }
+        }
+        return count;
+    }
+}
+
+// Follow up:
+// Can you solve it using only one pass?
+// Can you solve it in O(1) space?
+// In this solution, I count up length and down length.
+// Both up and down length are clear to 0 when A[i - 1] == A[i] or down > 0 && A[i - 1] < A[i].
+/**
+Java:
+    public int longestMountain(int[] A) {
+        int res = 0, up = 0, down = 0;
+        for (int i = 1; i < A.length; ++i) {
+            if (down > 0 && A[i - 1] < A[i] || A[i - 1] == A[i]) up = down = 0;
+            if (A[i - 1] < A[i]) up++;
+            if (A[i - 1] > A[i]) down++;
+            if (up > 0 && down > 0 && up + down + 1 > res) res = up + down + 1;
+        }
+        return res;
+    }
+*/
+class Solution {
+    public int longestMountain(int[] arr) {
+        int up = 0;
+        int down = 0;
+        int count = 0;
+        for(int i = 1; i < arr.length; i++) {
+            if(down > 0 && arr[i - 1] < arr[i] || arr[i - 1] == arr[i]) {
+                up = 0;
+                down = 0;
+            }
+            if(arr[i - 1] < arr[i]) {
+                up++;
+            }
+            if(arr[i - 1] > arr[i]) {
+                down++;
+            }
+            if(up > 0 && down > 0 && up + down + 1 > count) {
+                count = up + down + 1;
+            }
+        }
+        return count;
+    }
+}
+
 
