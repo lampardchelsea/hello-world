5e8be24919e463d048841aef086116aec11edc9e
lampardchelsea <zqqsxdd@gmail.com>
2020-12-06 22:55:58 -0800

    Create LongestMountainInArray.java

---
+++ leetcode/TwoPointers/LongestMountainInArray.java
@@ -0,0 +1,61 @@
+/**
+Refer to
+https://leetcode.com/problems/longest-mountain-in-array/
+You may recall that an array arr is a mountain array if and only if:
+
+arr.length >= 3
+There exists some index i (0-indexed) with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+Given an integer array arr, return the length of the longest subarray, which is a mountain. Return 0 if there is no mountain subarray.
+
+Example 1:
+Input: arr = [2,1,4,7,3,2,5]
+Output: 5
+Explanation: The largest mountain is [1,4,7,3,2] which has length 5.
+
+Example 2:
+Input: arr = [2,2,2]
+Output: 0
+Explanation: There is no mountain.
+
+Constraints:
+1 <= arr.length <= 104
+0 <= arr[i] <= 104
+
+Follow up:
+Can you solve it using only one pass?
+Can you solve it in O(1) space?
+*/
+
+// Solution 1: Two Pass
+// Refer to
+// https://leetcode.com/problems/longest-mountain-in-array/discuss/135593/C%2B%2BJavaPython-1-pass-and-O(1)-space
+/**
+Intuition:
+We have already many 2-pass or 3-pass problems, like 821. Shortest Distance to a Character.
+They have almost the same idea.
+One forward pass and one backward pass.
+Maybe another pass to get the final result, or you can merge it in one previous pass.
+
+Explanation:
+In this problem, we take one forward pass to count up hill length (to every point).
+We take another backward pass to count down hill length (from every point).
+Finally a pass to find max(up[i] + down[i] + 1) where up[i] and down[i] should be positives.
+
+Time Complexity:
+O(N)
+
+Java:
+    public int longestMountain(int[] A) {
+        int N = A.length, res = 0;
+        int[] up = new int[N], down = new int[N];
+        for (int i = N - 2; i >= 0; --i) if (A[i] > A[i + 1]) down[i] = down[i + 1] + 1;
+        for (int i = 0; i < N; ++i) {
+            if (i > 0 && A[i] > A[i - 1]) up[i] = up[i - 1] + 1;
+            if (up[i] > 0 && down[i] > 0) res = Math.max(res, up[i] + down[i] + 1);
+        }
+        return res;
+    }
+*/
+
