e4c23af2dedd41e440f79f888bbc2aced360c434
lampardchelsea <zqqsxdd@gmail.com>
2020-12-09 16:20:31 -0800

    Update 3SumWithMultiplicity.java

--- leetcode/TwoPointers/3SumWithMultiplicity.java
+++ leetcode/TwoPointers/3SumWithMultiplicity.java
@@ -29,7 +29,54 @@ Constraints:
 0 <= target <= 300
 */
 
-// Solution 1: Two Pointers + HashMap
+// Solution 1: Same style as what we did for 15. 3 Sum
+// Refer to
+// https://leetcode.com/problems/3sum-with-multiplicity/discuss/181080/Java-sorting-solution-O(N2)
+/**
+The solution is similar to 3Sum problem (https://leetcode.com/submissions/detail/128547721/).
+The key is how to skip duplicate numbers:
+Case 1: A[l] == A[r], the total possible answer is num * (num-1) / 2, where num = r-l+1.
+Case 2: A[l] != A[r], find how many duplicates in the left-hand side (which is cntL) and how many duplicates in the 
+        right-hand side (which is cntR). Then, search for the next possible answers.
+
+class Solution {
+    public int threeSumMulti(int[] A, int target) {
+        int mod = 1000000007;
+        int ans = 0;
+        Arrays.sort(A);
+        for(int i = 0; i < A.length-2; i++) {
+            long cnt = 0;
+            int l = i+1, r = A.length-1;
+            while(l < r) {
+                if(A[i] + A[l] + A[r] > target) r--;
+                else if(A[i] + A[l] + A[r] < target) l++;
+                else {
+                    if(A[l] != A[r]) {
+                        long cntL = 1, cntR = 1;
+                        while(l+1 < r && A[l] == A[l+1]) { cntL++; l++; }
+                        while(l < r-1 && A[r] == A[r-1]) { cntR++; r--; }
+                        cnt += (cntL * cntR) % mod;
+                        l++;
+                        r--;
+                    } else {
+                        long n = r-l+1;
+                        cnt += (n * (n-1) / 2) % mod;
+                        break;
+                    }
+                }
+            }
+            ans = (int) (ans + cnt) % mod;
+        }
+        return ans;
+    }
+}
+Time complexity: O(N^2)
+Space complexity: O(1)
+*/
+
+
+
+// Solution 2: Two Pointers + HashMap
 // Refer to
 // https://leetcode.com/problems/3sum-with-multiplicity/discuss/181128/10-lines-Super-Super-Easy-Java-Solution
 /**
