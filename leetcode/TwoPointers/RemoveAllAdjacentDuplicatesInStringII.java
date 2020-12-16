e59c30592d6dbd30ebbeadb0f97633389a0d14a0
lampardchelsea <zqqsxdd@gmail.com>
2020-12-16 03:10:46 -0800

    Create RemoveAllAdjacentDuplicatesInStringII.java

---
+++ leetcode/TwoPointers/RemoveAllAdjacentDuplicatesInStringII.java
@@ -0,0 +1,56 @@
+/**
+Refer to
+https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
+Given a string s, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them causing 
+the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made.
+
+It is guaranteed that the answer is unique. 
+
+Example 1:
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+
+Example 2:
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation: 
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+
+Example 3:
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+
+Constraints:
+1 <= s.length <= 10^5
+2 <= k <= 10^4
+s only contains lower case English letters.
+*/
+
+// Solution 1: Two Pointers
+// Refer to
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/discuss/392933/JavaC%2B%2BPython-Two-Pointers-and-Stack-Solution
+/**
+Intuition
+If you read my post last month about 1047. Remove All Adjacent Duplicates In String
+you cannot solve this problem easier.
+
+Solution 1: Two Pointers
+java
+    public String removeDuplicates(String s, int k) {
+        int i = 0, n = s.length(), count[] = new int[n];
+        char[] stack = s.toCharArray();
+        for (int j = 0; j < n; ++j, ++i) {
+            stack[i] = stack[j];
+            count[i] = i > 0 && stack[i - 1] == stack[j] ? count[i - 1] + 1 : 1;
+            if (count[i] == k) i -= k;
+        }
+        return new String(stack, 0, i);
+    }
+*/
