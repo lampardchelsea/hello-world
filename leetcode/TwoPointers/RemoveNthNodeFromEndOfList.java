f1602be1c486ffe7d4e9d4c64e9bc5ef34c50e2c
lampardchelsea <zqqsxdd@gmail.com>
2020-12-16 11:59:19 -0800

    Update RemoveNthNodeFromEndOfList.java

--- leetcode/TwoPointers/RemoveNthNodeFromEndOfList.java
+++ leetcode/TwoPointers/RemoveNthNodeFromEndOfList.java
@@ -30,6 +30,11 @@ First we will add an auxiliary "dummy" node, which points to the list head. The 
 such as a list with only one node, or removing the head of the list. On the first pass, we find the list length L. Then we set a 
 pointer to the dummy node and start to move it through the list till it comes to the (L - n) th node. We relink next pointer 
 of the (L - n) th node to the (L - n + 2) th node and we are done.
+
+Complexity Analysis
+Time complexity : O(L). The algorithm makes two traversal of the list, first to calculate list length L and second to find the (L - n) th node. 
+There are 2L-n operations and time complexity is O(L).
+Space complexity : O(1). We only used constant extra space.
 */
 /**
  * Definition for singly-linked list.
