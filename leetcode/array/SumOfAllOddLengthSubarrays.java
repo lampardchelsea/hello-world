2a1a5fb9196857f42ce5c464adf613949acf2a77
lampardchelsea <zqqsxdd@gmail.com>
2020-12-27 05:26:16 -0800

    Update SumOfAllOddLengthSubarrays.java

--- leetcode/array/SumOfAllOddLengthSubarrays.java
+++ leetcode/array/SumOfAllOddLengthSubarrays.java
@@ -172,3 +172,13 @@ Java
         return res;
     }
 */
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int n = arr.length;
+        int result = 0;
+        for(int i = 0; i < n; i++) {
+            result += ((i + 1) * (n - i) + 1) / 2 * arr[i];
+        }
+        return result;
+    }
+}
