849b0dac425bfd4ce6dca0090bf880c4929e28a4
lampardchelsea <zqqsxdd@gmail.com>
2020-12-27 05:11:57 -0800

    Update SumOfAllOddLengthSubarrays.java

--- leetcode/array/SumOfAllOddLengthSubarrays.java
+++ leetcode/array/SumOfAllOddLengthSubarrays.java
@@ -104,3 +104,71 @@ class Solution {
         return result;
     }
 }
+
+// Solution 3: 
+// Refer to
+// https://helloacm.com/algorithms-to-sum-of-all-odd-length-subarrays/
+/**
+O(N) LINEAR ALGORITHM TO COMPUTE THE SUM OF SUBARRAY OF ODD LENGTH
+We can sum up the contribution of A[i] – then it will be a linear solution.
+*/
+
+// https://leetcode.com/problems/sum-of-all-odd-length-subarrays/discuss/854184/JavaC%2B%2BPython-O(N)-Time-O(1)-Space
+/**
+Intuition
+Hmmm, totally not an easy problem.
+That where it's misleading:
+It lets brute force get accepted,
+and mark it as easy.
+
+Solution 1: Brute Force
+Enumerate all possible odd length of subarray.
+Time O(n^3)
+Space O(1)
+
+Solution 2: Consider the contribution of A[i]
+Also suggested by @mayank12559 and @simtully.
+
+Consider the subarray that contains A[i],
+we can take 0,1,2..,i elements on the left,
+from A[0] to A[i],
+we have i + 1 choices.
+
+we can take 0,1,2..,n-1-i elements on the right,
+from A[i] to A[n-1],
+we have n - i choices.
+
+In total, there are (i + 1) * (n - i) subarrays, that contains A[i].
+And there are ((i + 1) * (n - i) + 1) / 2 subarrays with odd length, that contains A[i].
+A[i] will be counted ((i + 1) * (n - i) + 1) / 2 times.
+
+Example of array [1,2,3,4,5]
+1 2 3 4 5 subarray length 1
+1 2 X X X subarray length 2
+X 2 3 X X subarray length 2
+X X 3 4 X subarray length 2
+X X X 4 5 subarray length 2
+1 2 3 X X subarray length 3
+X 2 3 4 X subarray length 3
+X X 3 4 5 subarray length 3
+1 2 3 4 X subarray length 4
+X 2 3 4 5 subarray length 4
+1 2 3 4 5 subarray length 5
+
+5 8 9 8 5 total times each index was added.
+3 4 5 4 3 total times in odd length array with (x + 1) / 2
+2 4 4 4 2 total times in even length array with x / 2
+
+Complexity
+Time O(N)
+Space O(1)
+
+Java
+    public int sumOddLengthSubarrays(int[] A) {
+        int res = 0, n = A.length;
+        for (int i = 0; i < n; ++i) {
+            res += ((i + 1) * (n - i) + 1) / 2 * A[i];
+        }
+        return res;
+    }
+*/
