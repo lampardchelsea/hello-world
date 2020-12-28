1d765b19c44ca9982f21a8138e9a6adf64ec88c6
lampardchelsea <zqqsxdd@gmail.com>
2020-12-27 21:30:38 -0800

    Update CellsWithOddValuesInAMatrix.java

--- leetcode/array/CellsWithOddValuesInAMatrix.java
+++ leetcode/array/CellsWithOddValuesInAMatrix.java
@@ -82,4 +82,101 @@ class Solution {
     }
 }
 
-// Solution 2: 
+// Solution 2:
+// Refer to
+// https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/discuss/426647/C%2B%2B-0ms-9.2MB.-Faster-than-100-and-uses-lesser-memory-than-100-submissions
+/**
+This can be done in O(m+n) time and O(m+n) space
+Firstly notice that the order in which we add 1 does not matter. You could add 1 to all rows and then proceed to adding 1 to all columns.
+
+Now suppose you add 1 to the same row twice, the resultant elements in that row are even. So applying the operation 
+to the same row/col is as good as not applying anything there.
+
+Applying operation to a same row/col any odd number of times is as good as applying the operation only once.
+
+So now suppose there are r such rows where you apply the operation odd number of times. And c such cols where you apply operation odd number of times.
+
+Then
+All m elements in each row are odd => r*m
+All n elements in each row are odd => c*n
+You double count r*c elements => -1 * r * c
+Also, these r*c elements are even => -1 * r * c
+So total odd elements is r*m + c*n - 2*r*c
+
+class Solution {
+public:
+    int oddCells(int n, int m, vector<vector<int>>& indices) {
+        vector<bool> N(n, false);
+        vector<bool> M(m, false);
+        
+        for(int i=0; i<indices.size(); i++) {
+            N[indices[i][0]] = N[indices[i][0]]^true;
+            M[indices[i][1]] = M[indices[i][1]]^true;
+        }
+        
+        int r = 0;
+        int c = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(N[i])
+                r++;
+        }
+        
+        for(int j=0; j<m; j++) {
+            if(M[j])
+                c++;
+        }
+        
+        return  r*m + c*n - 2*r*c;
+    }
+};
+*/
+
+// Refer to
+// https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/discuss/425100/JavaPython-3-2-methods%3A-time-O(m-*-n-%2B-L)-and-O(L)-codes-w-comment-and-analysis.
+/**
+Method 2:
+We actually only care about the number of rows and columns with odd times of increments respectively.
+
+Count the rows and columns that appear odd times;
+Compute the number of cells in aforementioned rows and columns respectively, then both deduct the cells located on odd rows 
+and odd columns (they become evens, because odd + odd results even).
+Note: Easier alternative way for 2 is odd_cols * even_rows + even_cols * odd_rows -- credit to @lenchen1112.
+    public int oddCells(int n, int m, int[][] indices) {
+        boolean[] oddRows = new boolean[n], oddCols = new boolean[m];
+        int cntRow = 0, cntCol = 0;
+        for (int[] idx : indices) {
+            oddRows[idx[0]] ^= true;
+            oddCols[idx[1]] ^= true;
+        }
+        for (boolean r : oddRows)
+            cntRow += r ? 1 : 0;
+        for (boolean c : oddCols)
+            cntCol += c ? 1 : 0;
+        // return m * cntRow + n * cntCol - 2 * cntRow * cntCol;
+        return (m - cntCol) * cntRow + (n - cntRow) * cntCol;
+    }
+Analysis:
+Time: O(L + m + n), space: O(m + n), where L = indices.length.
+*/
+class Solution {
+    public int oddCells(int n, int m, int[][] indices) {
+        boolean[] oddRows = new boolean[n];
+        boolean[] oddCols = new boolean[m];
+        int cntRow = 0;
+        int cntCol = 0;
+        // XOR - Sets each bit to 1 if only one of the two bits is 1
+        for(int[] idx : indices) {
+            oddRows[idx[0]] ^= true;
+            oddCols[idx[1]] ^= true;
+        }
+        for(boolean r : oddRows) {
+            cntRow += r ? 1 : 0;
+        }
+        for(boolean c : oddCols) {
+           cntCol += c ? 1 : 0; 
+        }
+        // return (m - cntCol) * cntRow + (n - cntRow) * cntCol;
+        return m * cntRow + n * cntCol - 2 * cntRow * cntCol;
+    }
+}
