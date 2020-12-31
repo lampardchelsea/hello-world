2d6297a7cbb3d96108924658bce9c8b2489f2eb3
lampardchelsea <zqqsxdd@gmail.com>
2020-12-31 05:21:13 -0800

    Update ToeplitzMatrix.java

--- leetcode/array/ToeplitzMatrix.java
+++ leetcode/array/ToeplitzMatrix.java
@@ -46,3 +46,72 @@ class Solution {
         return true;
     }
 }
+
+// Follow Up:
+// Refer to
+// https://leetcode.com/problems/toeplitz-matrix/discuss/271388/Java-Solution-for-Follow-Up-1-and-2
+/**
+Here are what I've thought so far, please let me know if you have any better ideas!
+
+For the follow-up 1, we are only able to load one row at one time, so we have to use a buffer (1D data structure) to store the row info. 
+When next row comes as a streaming flow, we can compare each value (say, matrix[i][j], i>=1, j>=1) to the "upper-left" value in the 
+buffer (buffer[j - 1]); meanwhile, we update the buffer by inserting the 1st element of the current row (matrix[i][0]) to buffer at 
+position 0 (buffer[0]), and removing the last element in the buffer.
+
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) return true;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        List<Integer> buffer = new LinkedList<>();
+        for(int j = 0; j < col; j++) buffer.add(matrix[0][j]);
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < col; j++){
+                if(buffer.get(j - 1) != matrix[i][j]) return false;
+            }
+            buffer.remove(buffer.size() - 1);
+            buffer.add(0, matrix[i][0]);
+        }
+        return true;
+    }
+}
+For the follow-up 2, we can only load a partial row at one time. We could split the whole matrix vertically into several sub-matrices, 
+while each sub-matrix should have one column overlapped. We repeat the same method in follow-up 1 for each sub-matrix.
+
+For example:
+[1 2 3 4 5 6 7 8 9 0]              [1 2 3 4]              [4 5 6 7]              [7 8 9 0]
+[0 1 2 3 4 5 6 7 8 9]              [0 1 2 3]              [3 4 5 6]              [6 7 8 9]
+[1 0 1 2 3 4 5 6 7 8]     -->      [1 0 1 2]       +      [2 3 4 5]       +      [5 6 7 8]
+[2 1 0 1 2 3 4 5 6 7]              [2 1 0 1]              [1 2 3 4]              [4 5 6 7]
+[3 2 1 0 1 2 3 4 5 6]              [3 2 1 0]              [0 1 2 3]              [3 4 5 6]
+*/
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        // Initial buffer by filling first row of matrix
+        List<Integer> buffer = new LinkedList<Integer>();
+        for(int i = 0; i < cols; i++) {
+            buffer.add(matrix[0][i]);
+        }
+        // Read in new row (start from row 1) and compare with previous
+        // row which stored in buffer, update buffer itself with new row
+        // value by removing last element and insert first element from
+        // new row ahead (at index 0)
+        for(int i = 1; i < rows; i++) {
+            // Compare all element on buffer with all element on current 
+            // row, if any mismatch then not toeplitz
+            for(int j = 1; j < cols; j++) {
+                if(buffer.get(j - 1) != matrix[i][j]) {
+                    return false;
+                }
+            }
+            // If match then update buffer by removing last element on
+            // current buffer and insert first element from current row
+            // at buffer's first position
+            buffer.remove(buffer.size() - 1);
+            buffer.add(0, matrix[i][0]);
+        }
+        return true;
+    }
+}
