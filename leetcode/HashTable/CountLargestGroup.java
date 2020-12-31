85b0ab899751224d606549d46833e80c9628eb74
lampardchelsea <zqqsxdd@gmail.com>
2020-12-31 11:50:57 -0800

    Update CountLargestGroup.java

--- leetcode/HashTable/CountLargestGroup.java
+++ leetcode/HashTable/CountLargestGroup.java
@@ -57,3 +57,50 @@ class Solution {
         return count;
     }
 }
+
+// Solution 2: Promote two for loop into one
+// Refer to
+// https://leetcode.com/problems/count-largest-group/discuss/678377/Java-simple-HashMap
+/**
+public int countLargestGroup(int n) {
+        int maxFreq = 0, dp[] = new int[n + 1], res = 1;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i % 10 + dp[i / 10];
+            map.put(dp[i], map.getOrDefault(dp[i], 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.get(key) > maxFreq) {
+                maxFreq = map.get(key);
+                res = 1;
+            } else if (map.get(key) == maxFreq) res++;
+        }
+        return res;
+    }
+*/
+class Solution {
+    public int countLargestGroup(int n) {
+        Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+        for(int i = 1; i <= n; i++) {
+            int val = i;
+            int sum = 0;
+            while(val > 0) {
+                sum += val % 10;
+                val /= 10;
+            }
+            map.putIfAbsent(sum, new ArrayList<Integer>());
+            map.get(sum).add(i);
+        }
+        int max_size = 0;
+        int count = 0;
+        for(Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            if(max_size < entry.getValue().size()) {
+                max_size = entry.getValue().size();
+                count = 1;
+            } else if(max_size == entry.getValue().size()) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
