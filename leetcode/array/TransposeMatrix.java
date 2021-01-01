ce9b28a3df749f6e4a0f5a68e0b3e7fda303ea93
lampardchelsea <zqqsxdd@gmail.com>
2020-12-31 22:45:56 -0800

    Create TransposeMatrix.java

---
+++ leetcode/array/TransposeMatrix.java
@@ -0,0 +1,32 @@
+/**
+Refer to
+https://leetcode.com/problems/transpose-matrix/
+Given a matrix A, return the transpose of A.
+
+The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
+
+Example 1:
+Input: [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+Example 2:
+Input: [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+
+Note:
+1 <= A.length <= 1000
+1 <= A[0].length <= 1000
+*/
+class Solution {
+    public int[][] transpose(int[][] A) {
+        int rows = A.length;
+        int cols = A[0].length;
+        int[][] result = new int[cols][rows];
+        for(int i = 0; i < cols; i++) {
+            for(int j = 0; j < rows; j++) {
+                result[i][j] = A[j][i];
+            }
+        }
+        return result;
+    }
+}
