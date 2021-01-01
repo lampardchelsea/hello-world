afa8dd76fff2e4b6d17505fb74a0840ac37c7113
lampardchelsea <zqqsxdd@gmail.com>
2020-12-31 16:43:17 -0800

    Update DefuseTheBomb.java

--- leetcode/array/DefuseTheBomb.java
+++ leetcode/array/DefuseTheBomb.java
@@ -64,3 +64,30 @@ class Solution {
         return result;
     }
 }
+
+// Solution 2: Sliding Window
+// Refer to
+// https://leetcode.com/problems/defuse-the-bomb/discuss/935398/JAVA-o(n)-100-time-and-space-short-and-concise-sliding-window
+/**
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int[] res = new int[code.length];
+        if (k == 0) return res;
+        //Define the initial window and initial sum
+        int start = 1, end = k, sum = 0;
+        if (k < 0) {//If k < 0, the starting point will be end of the array.
+            k = -k;
+            start = code.length - k;
+            end = code.length - 1;
+        }
+        for (int i = start; i <= end; i++) sum += code[i];
+        //Scan through the code array as i moving to the right, update the window sum.
+        for (int i = 0; i < code.length; i++) {
+            res[i] = sum;
+            sum -= code[(start++) % code.length];
+            sum += code[(++end) % code.length];
+        }
+        return res;
+    }
+}
+*/
