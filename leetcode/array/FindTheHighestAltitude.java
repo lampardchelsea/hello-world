f19aaa1bf1fddf2fe5fbc0a9493e719266576043
lampardchelsea <zqqsxdd@gmail.com>
2021-01-24 15:41:56 -0800

    Update FindTheHighestAltitude.java

--- leetcode/array/FindTheHighestAltitude.java
+++ leetcode/array/FindTheHighestAltitude.java
@@ -52,3 +52,16 @@ class Solution {
         return Math.max(0, max);
     }
 }
+
+// Solution 3: Wihout aditional comparison
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int cur = 0;
+        int max = 0;
+        for(int i = 0; i < gain.length; i++) {
+            cur += gain[i];
+            max = Math.max(cur, max);
+        }
+        return max;
+    }
+}
