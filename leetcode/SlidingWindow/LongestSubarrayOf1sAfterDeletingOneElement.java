8f36f8f28337a94b4dd0e0c7fb1d53b5dc184465
lampardchelsea <zqqsxdd@gmail.com>
2021-02-11 05:47:13 -0800

    Update LongestSubarrayOf1sAfterDeletingOneElement.java

--- leetcode/SlidingWindow/LongestSubarrayOf1sAfterDeletingOneElement.java
+++ leetcode/SlidingWindow/LongestSubarrayOf1sAfterDeletingOneElement.java
@@ -86,3 +86,21 @@ class Solution {
     }
 }
 
+// Note: Why we can use only if condition to find maximum length of subarray ?
+/**
+Take example as: nums = [0,1,1,1,0,1,1,0,1]
+
+If debug with console, we find in solution 1, when i = 8, j stop at 3, for solution 2, when i = 8, j stop at 5.
+The thing is we don't actually need to while loop to find exactly previous 0 index, since we are trying to find
+maximum length between i and j, if i keeps move forward (0 to length - 1) by 1 step each round, if we able to 
+find maximum size sliding window, j should either not move forward (0 to length - 1) OR just keep the same pace
+as i that move foward by 1 step each round, if we use while loop, even we find exactly previous 0 index that will
+not help we find maximum size sliding window since j will move fast then i, in another word, when i just move 1
+step, j may move n >= 1 steps to find previous 0 index, it will not help maximum (i - j).
+
+Instead of moving j with while loop, just use if condition will guarantee we find maximum size sliding window since
+j at most move 0 or 1 step each round when i must move 1 step each round, during this kind of iteration, if we are
+lucky enough to remove the previous 0 when j move forward then we find the answer, we don't have to move j till
+actual previous 0 index, such as here when i move up to 8, in if condition we only stop j at 3, but in while loop
+you have to move j till 5 to find 2nd 0 in nums array and try to remove it.
+*/
