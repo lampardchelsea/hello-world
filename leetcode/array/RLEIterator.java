0c59fe233432bfd3c446d0a6a69f395631b8feea
lampardchelsea <zqqsxdd@gmail.com>
2021-02-13 18:23:16 -0800

    Update RLEIterator.java

--- leetcode/array/RLEIterator.java
+++ leetcode/array/RLEIterator.java
@@ -72,4 +72,59 @@ class RLEIterator {
  * int param_1 = obj.next(n);
  */
  
-// Solution 2: 
+// Solution 2: index no need actual array
+// Style 1:
+class RLEIterator {
+    int[] A;
+    int index;
+    public RLEIterator(int[] A) {
+        this.A = A;
+        this.index = 0;
+    }
+    
+    public int next(int n) {
+        while(index < A.length) { // all elements exhausted?
+            if(n <= A[index]) { // find the corresponding value.
+                A[index] -= n; // deduct n from A[index].
+                return A[index + 1]; // A[index + 1] is the nth value.
+            }
+            n -= A[index]; // not find the value yet, deduct A[index] from n.
+            index += 2; // move on to next group of same values.
+        }
+        return -1;
+    }
+}
+
+/**
+ * Your RLEIterator object will be instantiated and called as such:
+ * RLEIterator obj = new RLEIterator(A);
+ * int param_1 = obj.next(n);
+ */
+
+// Style 2:
+class RLEIterator {
+    int[] A;
+    int index;
+    public RLEIterator(int[] A) {
+        this.A = A;
+        this.index = 0;
+    }
+    
+    public int next(int n) {
+        while(index < A.length && n > A[index]) { // exhaust as many terms as possible.
+            n -= A[index]; // exhaust A[idx + 1] for A[idx] times. 
+            index += 2; // move to next term
+        }
+        if(index < A.length) { // not exhaust all terms.
+            A[index] -= n;
+            return A[index + 1];
+        }
+        return -1; // exhaust all terms but still not enough.
+    }
+}
+
+/**
+ * Your RLEIterator object will be instantiated and called as such:
+ * RLEIterator obj = new RLEIterator(A);
+ * int param_1 = obj.next(n);
+ */
