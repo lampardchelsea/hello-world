317aa2ea72298b0fa2a83670517a5308645b5ffb
lampardchelsea <zqqsxdd@gmail.com>
2021-02-14 06:19:01 -0800

    Update CorporateFlightBookings.java

--- leetcode/array/CorporateFlightBookings.java
+++ leetcode/array/CorporateFlightBookings.java
@@ -113,3 +113,18 @@ class Solution:
             result[i] = tmp
         return result[:n]
 */
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] result = new int[n];
+        for(int[] b : bookings) {
+            result[b[0] - 1] += b[2];
+            if(b[1] < n) {
+                result[b[1]] -= b[2];
+            }
+        }
+        for(int i = 1; i < n; i++) {
+            result[i] += result[i - 1];
+        }
+        return result;
+    }
+}
