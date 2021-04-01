53832c04e01ff0afe25b092f8c52c6ff7f79b51c
lampardchelsea <zqqsxdd@gmail.com>
2021-03-31 21:08:32 -0700

    Update FirstBadVersion.java

--- leetcode/BinarySearch/FirstBadVersion.java
+++ leetcode/BinarySearch/FirstBadVersion.java
@@ -37,3 +37,25 @@ public class Solution extends VersionControl {
         }
     }
 }
+
+
+// Solution 2: Anther template
+// Refer to
+// https://leetcode.com/discuss/general-discussion/786126/python-powerful-ultimate-binary-search-template-solved-many-problems
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int lo = 1;
+        int hi = n;
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(isBadVersion(mid)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+}
