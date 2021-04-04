4f1187928318053b7c2c7ae618a442f48b65517e
lampardchelsea <zqqsxdd@gmail.com>
2021-04-04 04:36:08 -0700

    Update CapacityToShipPackagesWithinDDays.java

--- leetcode/BinarySearch/CapacityToShipPackagesWithinDDays.java
+++ leetcode/BinarySearch/CapacityToShipPackagesWithinDDays.java
@@ -180,3 +180,113 @@ class Solution {
         return days;
     }
 }
+
+// Style 2:
+// Refer to
+// https://leetcode.com/discuss/general-discussion/786126/python-powerful-ultimate-binary-search-template-solved-many-problems
+/**
+The above problems are quite easy to solve, because they already give us the array to be searched. We'd know that we should 
+use binary search to solve them at first glance. However, more often are the situations where the search space and search 
+target are not so readily available. Sometimes we won't even realize that the problem should be solved with binary search -- 
+we might just turn to dynamic programming or DFS and get stuck for a very long time.
+
+As for the question "When can we use binary search?", my answer is that, If we can discover some kind of monotonicity, 
+for example, if condition(k) is True then condition(k + 1) is True, then we can consider binary search.
+
+1011. Capacity To Ship Packages Within D Days [Medium]
+A conveyor belt has packages that must be shipped from one port to another within D days. The i-th package on the conveyor 
+belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order given by weights). 
+We may not load more weight than the maximum weight capacity of the ship.
+
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within D days.
+
+Example :
+Input: weights = [1,2,3,4,5,6,7,8,9,10], D = 5
+Output: 15
+
+Explanation: 
+A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
+1st day: 1, 2, 3, 4, 5
+2nd day: 6, 7
+3rd day: 8
+4th day: 9
+5th day: 10
+
+Note that the cargo must be shipped in the order given, so using a ship of capacity 14 and splitting the packages into parts 
+like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) is not allowed. 
+Binary search probably would not come to our mind when we first meet this problem. We might automatically treat weights as search 
+space and then realize we've entered a dead end after wasting lots of time. In fact, we are looking for the minimal one among all 
+feasible capacities. We dig out the monotonicity of this problem: if we can successfully ship all packages within D days with 
+capacity m, then we can definitely ship them all with any capacity larger than m. Now we can design a condition function, let's 
+call it feasible, given an input capacity, it returns whether it's possible to ship all packages within D days. This can run in 
+a greedy way: if there's still room for the current package, we put this package onto the conveyor belt, otherwise we wait for 
+the next day to place this package. If the total days needed exceeds D, we return False, otherwise we return True.
+
+Next, we need to initialize our boundary correctly. Obviously capacity should be at least max(weights), otherwise the conveyor 
+belt couldn't ship the heaviest package. On the other hand, capacity need not be more thansum(weights), because then we can ship 
+all packages in just one day.
+
+Now we've got all we need to apply our binary search template:
+
+def shipWithinDays(weights: List[int], D: int) -> int:
+    def feasible(capacity) -> bool:
+        days = 1
+        total = 0
+        for weight in weights:
+            total += weight
+            if total > capacity:  # too heavy, wait for the next day
+                total = weight
+                days += 1
+                if days > D:  # cannot ship within D days
+                    return False
+        return True
+
+    left, right = max(weights), sum(weights)
+    while left < right:
+        mid = left + (right - left) // 2
+        if feasible(mid):
+            right = mid
+        else:
+            left = mid + 1
+    return left
+*/
+class Solution {
+    public int shipWithinDays(int[] weights, int D) {
+        int lo = 0;
+        int hi = 0;
+        for(int weight : weights) {
+            hi += weight;
+            lo = Math.max(lo, weight);
+        }
+        while(lo < hi) { // quit loop when lo == hi
+            int mid = lo + (hi - lo) / 2;
+            // If total days equal or smaller than D means threshold = mid
+            // still valid, we can continue shrink the upper bound 'hi',
+            // 'mid' itself is also valid in this case
+            if(totalDays(weights, mid) <= D) {
+                hi = mid;
+            } else {
+            // If total days larger than D means threshold = mid not valid,
+            // we have to increase lower bound 'lo' to 'mid + 1', '+1' means
+            // 'mid' itself is also not valid in this case
+                lo = mid + 1;
+            }
+        }
+        // Return either 'lo' or 'hi' is same thing, since when lo == hi
+        // we exist while loop
+        return lo;
+    }
+    
+    private int totalDays(int[] weights, int maxCapacity) {
+        int total = 0;
+        int count = 1;
+        for(int weight : weights) {
+            total += weight;
+            if(total > maxCapacity) {
+                total = weight;
+                count++;
+            }
+        }
+        return count;
+    }
+}
