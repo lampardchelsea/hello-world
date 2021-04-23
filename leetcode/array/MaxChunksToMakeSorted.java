2f325ec20187751eb33f1a55f1cac0ebe81f83b0
lampardchelsea <zqqsxdd@gmail.com>
2021-04-22 22:10:28 -0700

    Update MaxChunksToMakeSorted.java

--- leetcode/array/MaxChunksToMakeSorted.java
+++ leetcode/array/MaxChunksToMakeSorted.java
@@ -74,3 +74,55 @@ class Solution {
     }
 }
 
+// Solution 2: Compare left max and right min.
+// Refer to
+// https://leetcode.com/problems/max-chunks-to-make-sorted-ii/discuss/113462/Java-solution-left-max-and-right-min.
+/**
+Algorithm: Iterate through the array, each time all elements to the left are smaller (or equal) to all elements to the right, there is a new chunck.
+Use two arrays to store the left max and right min to achieve O(n) time complexity. Space complexity is O(n) too.
+This algorithm can be used to solve ver1 too.
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int[] maxOfLeft = new int[n];
+        int[] minOfRight = new int[n];
+        maxOfLeft[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            maxOfLeft[i] = Math.max(maxOfLeft[i-1], arr[i]);
+        }
+        minOfRight[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            minOfRight[i] = Math.min(minOfRight[i + 1], arr[i]);
+        }
+        int res = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (maxOfLeft[i] <= minOfRight[i + 1]) res++;
+        }
+        return res + 1;
+    }
+}
+*/
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int[] maxLeft = new int[n];
+        int[] minRight = new int[n];
+        maxLeft[0] = arr[0];
+        for(int i = 1; i < n; i++) {
+            maxLeft[i] = Math.max(maxLeft[i - 1], arr[i]);
+        }
+        minRight[n - 1] = arr[n - 1];
+        for(int i = n - 2; i >= 0; i--) {
+            minRight[i] = Math.min(minRight[i + 1], arr[i]);
+        }
+        // At least one chunk (original arr)
+        int count = 1;
+        for(int i = 0; i < n - 1; i++) {
+            // At index i, compare max(0,..., i) with min(i + 1, ..., len - 1).
+            if(maxLeft[i] <= minRight[i + 1]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
