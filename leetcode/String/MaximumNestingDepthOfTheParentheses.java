8e58ae0e5ef8163e596e07762480a2084a0646a5
lampardchelsea <zqqsxdd@gmail.com>
2021-04-24 17:29:48 -0700

    Update MaximumNestingDepthOfTheParentheses.java

--- leetcode/String/MaximumNestingDepthOfTheParentheses.java
+++ leetcode/String/MaximumNestingDepthOfTheParentheses.java
@@ -84,13 +84,6 @@ https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/discuss/8
 241. Different Ways to Add Parentheses
 301. Remove Invalid Parentheses
 678. Valid Parenthesis String
-856. Score of Parentheses
-921. Minimum Add to Make Parentheses Valid
-1021. Remove Outermost Parentheses
-1111. Maximum Nesting Depth of Two Valid Parentheses Strings
-1190. Reverse Substrings Between Each Pair of Parentheses
-1249. Minimum Remove to Make Valid Parentheses
-1541. Minimum Insertions to Balance a Parentheses String
 */
 class Solution {
     public int maxDepth(String s) {
