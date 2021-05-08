fcc9d06bae0bae4d51f03e359db5d4ca9316ced9
lampardchelsea <zqqsxdd@gmail.com>
2021-05-08 12:50:12 -0700

    Update EvaluateReversePolishNotation.java

--- leetcode/Stack/EvaluateReversePolishNotation.java
+++ leetcode/Stack/EvaluateReversePolishNotation.java
@@ -36,6 +36,17 @@ Constraints:
 tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
 */
 
+// What is Reverse Polish notation ?
+// https://en.wikipedia.org/wiki/Reverse_Polish_notation 
+/**
+In reverse Polish notation, the operators follow their operands; for instance, to add 3 and 4 together, one would write 3 4 + rather 
+than 3 + 4. If there are multiple operations, operators are given immediately after their second operands; so the expression written 
+3 − 4 + 5 in conventional notation would be written 3 4 − 5 + in reverse Polish notation: 4 is first subtracted from 3, then 5 is 
+added to it. An advantage of reverse Polish notation is that it removes the need for parentheses that are required by infix notation. 
+While 3 − 4 × 5 can also be written 3 − (4 × 5), that means something quite different from (3 − 4) × 5. In reverse Polish notation, 
+the former could be written 3 4 5 × −, which unambiguously means 3 (4 5 ×) − which reduces to 3 20 − (which can further be reduced 
+to -17); the latter could be written 3 4 − 5 × (or 5 3 4 − ×, if keeping similar formatting), which unambiguously means (3 4 −) 5 ×.
+*/
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<Integer>();
