b9a92cda5056b4b29691038b2526e7373d04059e
lampardchelsea <zqqsxdd@gmail.com>
2021-05-20 00:10:44 -0700

    Update CamelcaseMatching.java

--- leetcode/TwoPointers/CamelcaseMatching.java
+++ leetcode/TwoPointers/CamelcaseMatching.java
@@ -78,6 +78,93 @@ Use new ArrayList<>(queries.length) to allocate capacity up-front. This can avoi
 queryArr[i] >= 'A' && queryArr[i] <= 'Z' can be replaced by built-in static method Character.isUpperCase().
 */
 
+// Given two strings, find if first string is a subsequence of second
+// Refer to
+// https://www.geeksforgeeks.org/given-two-strings-find-first-string-subsequence-second/
+/**
+Following is Recursive Implementation of the above idea.  
+Recursive Java program to check if a string is subsequence of another string
+class SubSequence {
+	// Returns true if str1[] is a subsequence of str2[]
+	// m is length of str1 and n is length of str2
+	static boolean isSubSequence(String str1, String str2,
+								int m, int n)
+	{
+		// Base Cases
+		if (m == 0)
+			return true;
+		if (n == 0)
+			return false;
+
+		// If last characters of two strings are matching
+		if (str1.charAt(m - 1) == str2.charAt(n - 1))
+			return isSubSequence(str1, str2, m - 1, n - 1);
+
+		// If last characters are not matching
+		return isSubSequence(str1, str2, m, n - 1);
+	}
+
+	// Driver program
+	public static void main(String[] args)
+	{
+		String str1 = "gksrek";
+		String str2 = "geeksforgeeks";
+		int m = str1.length();
+		int n = str2.length();
+		boolean res = isSubSequence(str1, str2, m, n);
+		if (res)
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+	}
+}
+
+Following is the Iterative Implementation:
+Iterative Java program to check if a string is subsequence of another string
+
+class GFG {
+
+	// Returns true if str1[] is a subsequence
+	// of str2[] m is length of str1 and n is
+	// length of str2
+	static boolean isSubSequence(String str1, String str2,
+								int m, int n)
+	{
+		int j = 0;
+
+		// Traverse str2 and str1, and compare
+		// current character of str2 with first
+		// unmatched char of str1, if matched
+		// then move ahead in str1
+		for (int i = 0; i < n && j < m; i++)
+			if (str1.charAt(j) == str2.charAt(i))
+				j++;
+
+		// If all characters of str1 were found
+		// in str2
+		return (j == m);
+	}
+
+	// Driver program to test methods of
+	// graph class
+	public static void main(String[] args)
+	{
+		String str1 = "gksrek";
+		String str2 = "geeksforgeeks";
+		int m = str1.length();
+		int n = str2.length();
+		boolean res = isSubSequence(str1, str2, m, n);
+
+		if (res)
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+	}
+}
+
+// This code is contributed by Pramod Kumar
+*/
+
 // Similar problem: 925. Long Pressed Name
 // Refer to
 // https://github.com/lampardchelsea/hello-world/blob/master/leetcode/TwoPointers/LongPressedName.java
