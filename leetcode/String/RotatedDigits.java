f40c272ad5781798480e801f90ecc202d73d6412
lampardchelsea <zqqsxdd@gmail.com>
2021-06-08 13:50:05 -0700

    Update RotatedDigits.java

--- leetcode/String/RotatedDigits.java
+++ leetcode/String/RotatedDigits.java
@@ -77,3 +77,66 @@ class Solution {
         return result;
     } 
 }
+
+// Solution 2: Improvement
+// Refer to
+// https://leetcode.com/problems/rotated-digits/discuss/116547/Easily-Understood-Java-Solution/116844
+class Solution {
+    public int rotatedDigits(int n) {
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+            if(isValid(i)) {
+                count++;
+            }
+            i += incrementIfNeeded(i);
+        }
+        return count;
+    }
+    
+    /**
+    Improved efficiency. Increments i if the leading digit of i is 3, 4 or 7.
+    For example, if i = 300, then we know 300, 301, 302 ... 399 are all invalid.
+    IncrementIfNeeded(int i) returns 99 to set i to 399 and 400 after i++ from loop increment.
+    The same thing happens for 400. 
+    Therefore, we only check 300 and 400, rather than 300, 301, 302, ..., 399, 400, 401, 402, ..., 499.
+    Improved efficiency. Previous improvement checked all values between say 
+    1,700,000 and 1,799,999 because 7 was not the most significant digit. New
+    incrementIfNeeded method finds the FIRST occurrence of 3 or 7 and returns an 
+    increment accordingly. 4 is not checked because we can skip it when a 3 is found.
+    */
+    private int incrementIfNeeded(int i) {
+        int inc = 1;
+        while(i >= 10) {
+            int mod = i % 10;
+            if(mod == 3) {
+                return 2 * inc - 1;
+            }
+            if(mod == 7) {
+                return inc - 1;
+            }
+            inc *= 10;
+            i /= 10;
+        }
+        if(i == 3) {
+            return 2 * inc - 1;
+        } else if(i == 7) {
+            return inc - 1;
+        } else {
+            return 0;
+        }
+    }
+    
+    private boolean isValid(int n) {
+        boolean result = false;
+        while(n > 0) {
+            if(n % 10 == 2 || n % 10 == 5 || n % 10 == 6 || n % 10 == 9) {
+                result = true;
+            }
+            if(n % 10 == 3 || n % 10 == 4 || n % 10 == 7) {
+                return false;
+            }
+            n /= 10;
+        }
+        return result;
+    } 
+}
