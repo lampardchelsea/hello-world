49e736f64a805f7336e9f73234614b8f727fad25
lampardchelsea <zqqsxdd@gmail.com>
2021-07-10 03:39:59 -0700

    Update BuildingH2O.java

--- leetcode/Concurrency/BuildingH2O.java
+++ leetcode/Concurrency/BuildingH2O.java
@@ -153,6 +153,39 @@ public:
     }
 */
 
+// https://leetcode.com/problems/building-h2o/discuss/894910/Java-SynchronizedSemaphore
+/**
+class H2O {
+    private int cntH;
+    
+    public H2O() {
+        cntH = 0;
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        synchronized(this) {
+            while (cntH == 2) {
+                wait();
+            }
+            releaseHydrogen.run();
+            ++cntH;
+            notifyAll();
+        }
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        synchronized(this) {
+            while (cntH != 2) {
+                wait();
+            }
+            releaseOxygen.run();
+            cntH -= 2;
+            notifyAll();
+        }
+    }
+}
+*/
+
 // https://leetcode.com/problems/building-h2o/discuss/897529/Java-Solution
 class H2O {
     private int hydrogenCount;
