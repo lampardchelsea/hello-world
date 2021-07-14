62bd2b39d3d10c66a487dea6383d966c6a83dc28
lampardchelsea <zqqsxdd@gmail.com>
2021-07-14 00:46:01 -0700

    Update FizzBuzzMultithreaded.java

--- leetcode/Concurrency/FizzBuzzMultithreaded.java
+++ leetcode/Concurrency/FizzBuzzMultithreaded.java
@@ -552,3 +552,72 @@ class FizzBuzz {
         threadD.start();
     }
 }
+
+// OR equal to below format
+class FizzBuzz {
+    private int n;
+    private int counter = 1;
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        synchronized(this) {
+            while(true){
+                while(counter <= n && (counter % 3 != 0 || counter % 5 == 0)){
+                    wait();
+                }
+                if(counter > n) break;
+                printFizz.run();
+                ++counter;
+                notifyAll();
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        synchronized(this) {
+            while(true){
+                while(counter <= n && (counter % 5 != 0 || counter % 3 == 0)){
+                    wait();
+                }
+                if(counter > n) break;
+                printBuzz.run();
+                ++counter;
+                notifyAll();
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        synchronized(this) {
+            while(true){
+                while(counter <= n && !(counter % 3 == 0 && counter % 5 == 0)){
+                    wait();
+                }
+                if(counter > n) break;
+                printFizzBuzz.run();
+                ++counter;
+                notifyAll();
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        synchronized(this) {
+            while(true){
+                while(counter <= n && (counter % 3 == 0 || counter % 5 == 0)){
+                    wait();
+                }
+                if(counter > n) break;
+                printNumber.accept(counter);
+                ++counter;
+                notifyAll();
+            }
+        }
+    }
+}
