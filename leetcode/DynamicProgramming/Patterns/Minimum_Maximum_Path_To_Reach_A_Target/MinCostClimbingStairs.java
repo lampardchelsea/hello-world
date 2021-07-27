ed5ad447032a0691623d63dc2cb45006a6b1a753
lampardchelsea <zqqsxdd@gmail.com>
2021-07-27 12:47:14 -0700

    Update MinCostClimbingStairs.java

--- leetcode/DynamicProgramming/Patterns/Minimum_Maximum_Path_To_Reach_A_Target/MinCostClimbingStairs.java
+++ leetcode/DynamicProgramming/Patterns/Minimum_Maximum_Path_To_Reach_A_Target/MinCostClimbingStairs.java
@@ -20,19 +20,39 @@
     Note:
     cost will have a length in the range [2, 1000].
     Every cost[i] will be an integer in the range [0, 999]
- *
- * Solution
- * https://leetcode.com/problems/min-cost-climbing-stairs/discuss/110111/Easy-to-understand-C++-using-DP-with-detailed-explanation
 */
-class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int[] dp = new int[n];
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-        for(int i = 2; i < n; i++) {
-            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
-        }
-        return Math.min(dp[n - 1], dp[n - 2]);
-    }
+
+// Minimum (Maximum) Path to Reach a Target
+// Refer to
+// https://leetcode.com/discuss/general-discussion/458695/dynamic-programming-patterns#Minimum-(Maximum)-Path-to-Reach-a-Target
+/**
+Problem list: https://leetcode.com/list/55ac4kuc
+
+Generate problem statement for this pattern
+
+Statement
+Given a target find minimum (maximum) cost / path / sum to reach the target.
+
+Approach
+Choose minimum (maximum) path among all possible paths before the current state, then add value for the current state.
+
+routes[i] = min(routes[i-1], routes[i-2], ... , routes[i-k]) + cost[i]
+Generate optimal solutions for all values in the target and return the value for the target.
+
+Top-Down
+for (int j = 0; j < ways.size(); ++j) {
+    result = min(result, topDown(target - ways[j]) + cost/ path / sum);
 }
+return memo[state parameters] = result;
+
+Bottom-Up
+for (int i = 1; i <= target; ++i) {
+   for (int j = 0; j < ways.size(); ++j) {
+       if (ways[j] <= i) {
+           dp[i] = min(dp[i], dp[i - ways[j]] + cost / path / sum) ;
+       }
+   }
+}
+
+return dp[target]
+*/
