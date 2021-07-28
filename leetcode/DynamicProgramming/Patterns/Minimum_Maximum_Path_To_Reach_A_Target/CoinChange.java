da814c1897d17b79e344cf8d0758829b2055a54b
lampardchelsea <zqqsxdd@gmail.com>
2021-07-28 16:46:34 -0700

    Update CoinChange.java

--- leetcode/DynamicProgramming/Patterns/Minimum_Maximum_Path_To_Reach_A_Target/CoinChange.java
+++ leetcode/DynamicProgramming/Patterns/Minimum_Maximum_Path_To_Reach_A_Target/CoinChange.java
@@ -44,6 +44,58 @@ In other words:
     M(v, w, 0) = 0;   (can't packet anything into a 0-capacity knapsack) 
 */
 
+// Solution
+// Refer to
+// https://leetcode.com/discuss/general-discussion/458695/dynamic-programming-patterns#Minimum-(Maximum)-Path-to-Reach-a-Target
+/**
+Minimum (Maximum) Path to Reach a Target
+Problem list: https://leetcode.com/list/55ac4kuc
+
+Generate problem statement for this pattern
+
+Statement
+Given a target find minimum (maximum) cost / path / sum to reach the target.
+
+Approach
+Choose minimum (maximum) path among all possible paths before the current state, then add value for the current state.
+
+routes[i] = min(routes[i-1], routes[i-2], ... , routes[i-k]) + cost[i]
+Generate optimal solutions for all values in the target and return the value for the target.
+
+Top-Down
+for (int j = 0; j < ways.size(); ++j) {
+    result = min(result, topDown(target - ways[j]) + cost/ path / sum);
+}
+return memo[state parameters] = result;
+Bottom-Up
+for (int i = 1; i <= target; ++i) {
+   for (int j = 0; j < ways.size(); ++j) {
+       if (ways[j] <= i) {
+           dp[i] = min(dp[i], dp[i - ways[j]] + cost / path / sum) ;
+       }
+   }
+}
+ 
+return dp[target]
+==============================================================================
+322. Coin Change
+
+Top-Down
+for (int i = 0; i < coins.size(); ++i) {
+    if (coins[i] <= target) { // check validity of a sub-problem
+        result = min(ans, CoinChange(target - coins[i], coins) + 1);
+    }
+}
+return memo[target] = result;
+Bottom-Up
+for (int j = 1; j <= amount; ++j) {
+   for (int i = 0; i < coins.size(); ++i) {
+       if (coins[i] <= j) {
+           dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+       }
+   }
+}
+*/
 
 // Solution 1: Native DFS(TLE) similar to Combination Sum (different on Combination Sum calculate all possibilities by use backtracking, 
 // here just need set up remain < 0 based condition to work with tmp >= 0 and initialize min = Integer.MAX_VALUE to calculate minimum)
