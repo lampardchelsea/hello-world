4872137048fc3f61269ed3323b180c046dd7a7f7
lampardchelsea <zqqsxdd@gmail.com>
2021-08-07 12:43:31 -0700

    Create MaximalSquare.java

---
+++ leetcode/DynamicProgramming/Patterns/Minimum_Maximum_Path_To_Reach_A_Target/MaximalSquare.java
@@ -0,0 +1,15 @@
+/** 
+ * Refer to
+ * https://leetcode.com/problems/maximal-square/description/
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest square 
+   containing only 1's and return its area.
+  For example, given the following matrix:
+  1 0 1 0 0
+  1 0 1 1 1
+  1 1 1 1 1
+  1 0 0 1 0
+  Return 4.
+ *
+ * Solution
+ * https://leetcode.com/articles/maximal-square/
+*/
