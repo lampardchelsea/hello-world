037d23352060f2c192d90355ca76ac14834a8333
lampardchelsea <zqqsxdd@gmail.com>
2022-09-07 15:44:15 -0700

    Update LongestSubstringWithAtMostTwoDistinctCharacters.java

--- leetcode/SlidingWindow/LongestSubstringWithAtMostTwoDistinctCharacters.java
+++ leetcode/SlidingWindow/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -143,7 +143,8 @@ public class Solution {
     }
 }
 
-Attempt 1: 2022-09-07(30min)
+Attempt 1: 2022-09-07
+Solution 1 (30min)
 public class Solution {
     /**
      * @param s: A string
@@ -175,6 +176,53 @@ public class Solution {
                     freq.remove(c1);
                     size--;
                 }
+                i++;
+            }
+            maxLen = Math.max(maxLen, j - i + 1);
+        }
+        return maxLen == Integer.MIN_VALUE ? 0 : maxLen;
+    }
+}
+
+Solution 2 (30min)
+public class Solution {
+    /**
+     * @param s: A string
+     * @param k: An integer
+     * @return: An integer
+     */
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        // write your code here
+        Map<Character, Integer> char_index_map = new HashMap<Character, Integer>();
+        int maxLen = Integer.MIN_VALUE;
+        // 'i' is left end index, 'j' is right end index
+        int i = 0;
+        int len = s.length();
+        // Before over k distinct characters try to expand right end as much as possible
+        for(int j = 0; j < len; j++) {
+            // Instead of store {char, frequency} mapping, we store {char, most recent index},
+            // the most recent index of character means the latest happening position in String s
+            // e.g 'b' in "bbeceba" when j = 3, the most recent index of 'b' is 1 (update from 0)
+            char_index_map.put(s.charAt(j), j);
+            int size = char_index_map.size();
+            // When distinct character number > k, start shrink left end, use while loop
+            // in case current string section (left end indicate by 'i') start with same
+            // character, e.g "bbeceba" start with two 'b's when j = 3, k = 2 and i = 0,
+            // currently substring indicated as "bbec", we have to remove two continuous
+            // 'b' to match k = 2, the while loop does the truncate job, it runs one time
+            // to increase i from 0 to 1, which means skip all continuous 'b' and reach to
+            // the most recent one, the 2nd 'b' is the most recent one since its index = 1
+            // match the stored most recent index = 1 when j = 3 (if we have continuous 'b'
+            // at beginning, it will skip more, e.g "bbbbeceba" it will skip 3 times), now
+            // we can remove distinct character 'b' from map directly by just increase i once,
+            // since the next new character 'e' will be the new start of window (i++ after
+            // size-- indicate to new character 'e'), distinct characters drop back from 3 to 2
+            if(size > k) {
+                while(char_index_map.get(s.charAt(i)) != i) {
+                    i++;
+                }
+                char_index_map.remove(s.charAt(i));
+                size--;
                 i++;
             }
             maxLen = Math.max(maxLen, j - i + 1);
