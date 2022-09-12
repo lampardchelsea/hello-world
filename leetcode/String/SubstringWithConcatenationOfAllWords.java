1a7f0f01430e425b770aca439e12a89d0471bfa9
lampardchelsea <zqqsxdd@gmail.com>
2022-09-12 16:04:54 -0700

    Update SubstringWithConcatenationOfAllWords.java

--- leetcode/String/SubstringWithConcatenationOfAllWords.java
+++ leetcode/String/SubstringWithConcatenationOfAllWords.java
@@ -142,3 +142,142 @@ https://leetcode.com/problems/substring-with-concatenation-of-all-words/discuss/
 HashMap.contains is O(word_len) as it uses equals method internally which is character by character comparison. 
 So O(s_len * num_of_words * word_len) is correct. 
 Space complexity is O(num_of_words + word_len) due to HashMap(words) and substring method(creates a new copy of sub array)
+
+Solution 2 (360min, too long, not understand till now)
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        Map<String, Integer> freqMap = new HashMap<String, Integer>();
+        // 统计数组中每个词出现的次数，放入哈希表中待用
+        for(String word : words) {
+            freqMap.put(word, freqMap.getOrDefault(word, 0) + 1);
+        }
+        List<Integer> result = new ArrayList<Integer>();
+        // 得到每个词的长度
+        int len = s.length();
+        int wordLen = words[0].length();
+        // 以单词的每一位为起始位置分成wordLen种情况错开位来统计
+        for(int i = 0; i < wordLen; i++) {
+            // start标记当前滑动窗口的起始位置
+            int start = i;
+            // count表明当前滑动窗口内有多少词
+            int count = 0;
+            // 建一个新的哈希表，记录本轮搜索中滑动窗口内单词出现次数
+            Map<String, Integer> currFreqMap = new HashMap<String, Integer>();
+            for(int j = i; j <= len - wordLen; j += wordLen) {
+                String sub = s.substring(j, j + wordLen);
+                // 判断当前词是否是给定数组中的
+                if(freqMap.containsKey(sub)) {
+                    currFreqMap.put(sub, currFreqMap.getOrDefault(sub, 0) + 1);
+                    count++;
+                    // 如果该单词出现次数已经超过给定数组中的次数了，说明多来了一个该单词，
+                    // 所以要把滑动窗口中该单词上次出现的位置及之前所有单词给去掉
+                    while(currFreqMap.get(sub) > freqMap.get(sub)) {
+                        String leftMost = s.substring(start, start + wordLen);
+                        currFreqMap.put(leftMost, currFreqMap.get(leftMost) - 1);
+                        start += wordLen;
+                        count--;
+                    }
+                    // 如果滑动窗口内单词数和总单词数一样，则找到结果
+                    if(count == words.length) {
+                        result.add(start);
+                        // 找到结果以后把当前滑动窗口最左边的单词去掉，更新滑动窗口起始位置，
+                        // 为可能的下一次找到结果预留位置(这一步比较困难，不太自然) 
+                        String leftMost = s.substring(start, start + wordLen);
+                        currFreqMap.put(leftMost, currFreqMap.get(leftMost) - 1);
+                        start += wordLen;
+                        count--;
+                    }
+                // 如果截出来的单词都不在数组中，前功尽弃，重新开始
+                } else {
+                    currFreqMap.clear();
+                    start = j + wordLen;
+                    count = 0;
+                }
+            }
+        }
+        return result;
+    }
+}
+
+Space Complexity: O(num_of_words + word_len)
+Time Complexity: O(s_len * num_of_words * word_len) sub-optimal
+
+Below is an explicit example how above code running:
+Refer to:
+https://segmentfault.com/a/1190000003903467
+https://leetcode.wang/leetCode-30-Substring-with-Concatenation-of-All-Words.html
+
+s="foobarfoobarfoothebarman"
+words = ["foo","bar","foo","the"]
+用这个来跑一遍代码
+=====================================
+freqMap={foo,2}{bar,1}{the,1}
+words.length=4
+wordLen=3
+=====================================
+s="foobarfoobarfoothebarman"
+i=0 foo|bar|foo
+start=0 ==> 'start' used for recording window start index
+-------------------------------------
+j=0 -> j=3 -> j=6 -> j=9
+sub="foo" -> sub="bar" -> sub="foo" -> sub="bar"
+currFreqMap={foo,1} -> {foo,1}{bar,1} -> {foo,2}{bar,1} -> {foo,2}{bar,2}
+count=0 -> count=1 -> count=2 -> count=3 -> count=4
+-------------------------------------
+When j=9, sub="bar", currFreqMap has {bar,2} > freqMap {bar,1} trigger while loop to remove previous "bar" and all words of this previous "bar"
+while loop round 1:
+leftMost=s.subtring(0,0+3)="foo"
+currFreqMap={foo,1}{bar,2}
+start=3
+count=3
+while loop round 2:
+leftMost=s.subtring(3,3+3)="bar"
+currFreqMap={foo,1}{bar,1}
+start=6
+count=2
+Now currFreqMap has {bar,1} == freqMap {bar,1}, while loop end
+-------------------------------------
+Since count < words.length=4, directly back to inner for loop for next iteration
+j=12 -> j=15
+sub="foo" -> sub="the"
+currFreqMap={foo,2}{bar,1} -> {foo,2}{bar,1}{the,1}
+count=3 -> count=4
+Now count == words.length=4, find a solution subtring as "foobarfoothe", its start index record by 'start=6', then go into if(count == words.length) block
+leftMost=s.subtring(6,6+3)="foo"
+currFreqMap={foo,1}{bar,1}{the,1}
+result.add(6)
+start=9
+count=3
+The if block remove the left most word as "foo" of solution substring "foobarfoothe", update 'start' to 9, update 'count' to 3, operation as update 'start' and 'count' is for preparing next iteration, especially update 'start' from 6 to 9 is preparing the next potential solution substring start index, then if block end, go to next iteration of inner for loop
+-------------------------------------
+j=18
+sub="bar"
+currFreqMap={foo,1}{bar,2}{the,1}
+count=4
+When j=18, sub="bar", currFreqMap has {bar,2} > freqMap {bar,1} trigger while loop to remove previous "bar" and all words of previous "bar" (but different than while loop triggered before, current previous "bar" is the one start at index=9 recorded and updated by 'start=9' in previous iteration's if block, not the already removed one start at index=3)
+while loop round 1:
+leftMost=s.subtring(9,9+3)="bar"
+currFreqMap={foo,1}{bar,1}{the,1}
+start=12
+count=3
+Now currFreqMap has {bar,1} == freqMap {bar,1}, while loop end
+-------------------------------------
+Since count < words.length=4, directly back to inner for loop for next iteration
+j=21
+sub="man"
+since freqMap not contains "man", all remain stuff in currFreqMap(for example here as {foo,1}{bar,1}{the,1}) are in vain, we have to skip this word "man" to start a fresh lookup
+currFreqMap.clear() to clean up remain stuff
+start=j+wordLen=21+3=24 to skip "man" on original string and record as a new substring start index
+count=0 to reset matched word count in window
+After reset then go back to inner for loop for next itearation
+-------------------------------------
+j=24 is the end of original string s="foobarfoobarfoothebarman", so first outside for loop done, we only find a solution subtring as "foobarfoothe" start at index=6
+=====================================
+s="foobarfoobarfoothebarman"
+oob|arf|oo
+=====================================
+s="barfoothefoobarman"
+oba|rfo|o
+=====================================
+For the late 2 secanrio we can use outside for loop to do the same
