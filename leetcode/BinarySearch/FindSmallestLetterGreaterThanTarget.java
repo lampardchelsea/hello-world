bba53e188b735e22a0574141a3b16b6255e8b3be
lampardchelsea <zqqsxdd@gmail.com>
2022-09-22 02:02:04 -0700

    Update FindSmallestLetterGreaterThanTarget.java

--- leetcode/BinarySearch/FindSmallestLetterGreaterThanTarget.java
+++ leetcode/BinarySearch/FindSmallestLetterGreaterThanTarget.java
@@ -65,3 +65,32 @@ class Solution {
 Space Complexity: O(1)         
 Time Complexity: O(logn)
 ```
+
+Refer to
+https://leetcode.com/problems/find-smallest-letter-greater-than-target/discuss/110005/Easy-Binary-Search-in-Java-O(log(n))-time/111739
+```
+ public char nextGreatestLetter(char[] letters, char target) {
+    	if (letters[0] > target) return letters[0];
+    	if (letters[letters.length - 1] <= target) return letters[0];
+    	
+    	return binarySearch(letters, target);
+    }
+    
+    private char binarySearch(char[] letters, char target) {
+    	int start = 0;
+    	int end = letters.length - 1;
+    	while (start <= end) {
+    		int mid = start + (end - start) / 2;
+    		if (letters[mid] <= target) {
+    			start = mid + 1;
+    		}
+    		else if (letters[mid] > target) {
+    			end = mid - 1;
+    		}
+    	}
+    	return letters[start];	
+    }
+```
+
+https://leetcode.com/problems/find-smallest-letter-greater-than-target/discuss/110005/Easy-Binary-Search-in-Java-O(log(n))-time
+Binary search for the character which comes immediately after character target in the alphabets, or if the target is greater than or equal to the last character in the input list, then search for the first character in the list.
