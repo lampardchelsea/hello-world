d5820e7c34df5f17555e776d5f282092114e4a0e
lampardchelsea <zqqsxdd@gmail.com>
2022-10-29 04:15:39 -0700

    Update BinarySearchTreeIterator.java

--- leetcode/Stack/BinarySearchTreeIterator.java
+++ leetcode/Stack/BinarySearchTreeIterator.java
@@ -55,3 +55,180 @@ public class BSTIterator {
  * BSTIterator i = new BSTIterator(root);
  * while (i.hasNext()) v[f()] = i.next();
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+https://leetcode.com/problems/binary-search-tree-iterator/
+
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+- BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+- boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+- int next() Moves the pointer to the right, then returns the number at the pointer.
+
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+Example 1:
+
+
+```
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+```
+
+Constraints:
+- The number of nodes in the tree is in the range [1, 105].
+- 0 <= Node.val <= 106
+- At most 105 calls will be made to hasNext, and next.
+
+Follow up:
+- Could you implement next() and hasNext() to run in average O(1) time and use O(h) memory, where h is the height of the tree?
+---
+Attempt 1: 2022-10-28
+
+Solution 1:  Partial inorder traversal with Stack which only store left subtree (30min)
+```
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<TreeNode>();
+        while(root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+    
+    public int next() {
+        TreeNode node = stack.pop();
+        TreeNode cur = node.right;
+        while(cur != null) {
+            stack.push(cur);
+            cur = cur.left;
+        }
+        return node.val;
+    }
+    
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+```
+
+Refer to
+https://leetcode.com/problems/binary-search-tree-iterator/discuss/52526/Ideal-Solution-using-Stack-(Java)
+My idea comes from this: My first thought was to use inorder traversal to put every node into an array, and then make an index pointer for the next() and hasNext(). That meets the O(1) run time but not the O(h) memory. O(h) is really much more less than O(n) when the tree is huge.
+
+This means I cannot use a lot of memory, which suggests that I need to make use of the tree structure itself. And also, one thing to notice is the "average O(1) run time". It's weird to say average O(1), because there's nothing below O(1) in run time, which suggests in most cases, I solve it in O(1), while in some cases, I need to solve it in O(n) or O(h). These two limitations are big hints.
+
+Before I come up with this solution, I really draw a lot binary trees and try inorder traversal on them. We all know that, once you get to a TreeNode, in order to get the smallest, you need to go all the way down its left branch. So our first step is to point to pointer to the left most TreeNode. The problem is how to do back trace. Since the TreeNode doesn't have father pointer, we cannot get a TreeNode's father node in O(1) without store it beforehand. Back to the first step, when we are traversal to the left most TreeNode, we store each TreeNode we met ( They are all father nodes for back trace).
+
+After that, I try an example, for next(), I directly return where the pointer pointing at, which should be the left most TreeNode I previously found. What to do next? After returning the smallest TreeNode, I need to point the pointer to the next smallest TreeNode. When the current TreeNode has a right branch (It cannot have left branch, remember we traversal to the left most), we need to jump to its right child first and then traversal to its right child's left most TreeNode. When the current TreeNode doesn't have a right branch, it means there cannot be a node with value smaller than itself father node, point the pointer at its father node.
+
+The overall thinking leads to the structure Stack, which fits my requirement so well.
+https://leetcode.com/problems/binary-search-tree-iterator/discuss/52526/Ideal-Solution-using-Stack-(Java)/142668
+```
+public class BSTIterator {
+    private final Stack<TreeNode> stack;
+    
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.left;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode node = stack.pop();
+        
+        // Traversal cur node's right branch
+        TreeNode cur = node.right;
+        while (cur != null){
+            stack.push(cur);
+            cur = cur.left;
+        }
+        
+        return node.val;
+    }
+}
+```
+
+Refer to
+https://leetcode.com/problems/binary-search-tree-iterator/discuss/1430547/C%2B%2B-Simple-Solution-using-Stack-O(h)-Time-Complexity-(-with-Diagrammatic-Explanation-)
+We could have easily done this question by using inorder traversal and storing it in a vector after that iterating every index of vector for next() value and if the index is equal to vector size then hasnext() becomes false else it would be true.
+
+But in question, it is given that we have to do it in 0(h) memory, where h is the height of the tree. So now we will make use of the stack and instead of inorder traversal, we would use partial inorder traversal so that at any instant of time the stack contains elements equal to the height of the tree.
+
+Step 1: We will push the left part of the tree into a stack which is 7 and then 3 is inserted. Now is next() is called 3 is stored as the top and then popped now we will push the right of 3 but it does not contain any so we just return the top-> val i.e. 3.
+
+Step 2: We will now pop() 7 from the stack and see if it has the right children yes it has so we push 15 and then 9 into the stack. Observe here you'll see at a time stack contains elements equal to the height of the tree.
+
+Step 3: Now 9 is popped and 9 does contain any right child so we move on 15. If hasnext() is called it would return true as traversal is still left and the stack is also not empty.
+
+Step 4: Now 15 is popped and checked if it has the right child yes it has i.e. 20 so 20 is pushed inside the stack.
+
+Step 5: Lastly 20 is popped and returned and after that is hasnext() is called it will return false as there are no more elements or children inside the stack.
+
+
