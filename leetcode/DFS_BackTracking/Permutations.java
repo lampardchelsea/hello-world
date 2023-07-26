979683a5502ba78e932290bab2c184cb0998dd5d
lampardchelsea <zqqsxdd@gmail.com>
2023-07-26 00:06:41 -0700

    Update Permutations.java

--- leetcode/DFS_BackTracking/Permutations.java
+++ leetcode/DFS_BackTracking/Permutations.java
@@ -127,7 +127,7 @@ Constraints:
 ---
 Attempt 1: 2022-10-18
 
-Solution 1: Backtracking style 1 (10min)
+Solution 1: Backtracking style 1 (10min, no boolean[] visited array required just use ArrayList.contains() to identify if current number been used or not before is because one condition: All the integers of nums are unique.)
 ```
 class Solution { 
     public List<List<Integer>> permute(int[] nums) { 
@@ -142,6 +142,9 @@ class Solution {
             return; 
         } 
         for(int i = index; i < nums.length; i++) { 
+            // No boolean[] visited array required just use ArrayList.contains() to 
+            // identify if current number been used or not before is because one condition: 
+            // All the integers of nums are unique.
             if(tmp.contains(nums[i])) { 
                 continue; 
             } 
@@ -152,6 +155,48 @@ class Solution {
             tmp.remove(tmp.size() - 1); 
         } 
     } 
+}
+
+----------------------------------------------------------------------------------------------------------
+Remove redundant 'index' from parameter
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) { 
+        List<List<Integer>> result = new ArrayList<List<Integer>>(); 
+        // No need 'index = 0' as recursion parameter anymore, since 
+        // when recursion happens in "permutation", it always have to
+        // start with first element to scan the whole input again, so
+        // the for loop in recursion always start with i = 0, no need
+        // receive value passed in from parameter 'index = 0'
+        // That's the major difference than "combination" which requires
+        // next recursion level always move 1 index ahead of current index
+        //helper(nums, result, new ArrayList<Integer>(), 0); 
+        helper(nums, result, new ArrayList<Integer>()); 
+        return result; 
+    } 
+     
+    //private void helper(int[] nums, List<List<Integer>> result, List<Integer> tmp, int index) { 
+    private void helper(int[] nums, List<List<Integer>> result, List<Integer> tmp) { 
+        if(tmp.size() == nums.length) { 
+            result.add(new ArrayList<Integer>(tmp)); 
+            return; 
+        } 
+        //for(int i = index; i < nums.length; i++) { 
+        for(int i = 0; i < nums.length; i++) { 
+            // No boolean[] visited array required just use ArrayList.contains() to 
+            // identify if current number been used or not before is because one condition: 
+            // All the integers of nums are unique.
+            if(tmp.contains(nums[i])) { 
+                continue; 
+            } 
+            tmp.add(nums[i]);
+            // Differ than L77.Combinations statement which pass local variable 'i' 
+            // plus 1('i + 1') into next recursion
+            //helper(nums, result, tmp, index); 
+            helper(nums, result, tmp); 
+            tmp.remove(tmp.size() - 1); 
+        } 
+    }
 }
 
 Refer to
@@ -204,7 +249,7 @@ https://medium.com/algorithms-and-leetcode/backtracking-e001561b9f28
 The traversal and backtrack process is below
 
 
-Compare to L77.Combinations Solution 1: Backtracking style 1, the critical difference is L46. Permutations Solution 1: Backtracking style 1 pass 'index' into next recursion level, whereas L77. Combinations pass local variable 'i' plus 1 as 'i + 1' into next recursion level 
+Compare to L77.Combinations Solution 1: Backtracking style 1, the critical difference is L46. Permutations Solution 1: Backtracking style 1 pass 'index(=0)' or no need to pass 'index' and always for loop start from 0 into next recursion level, whereas L77. Combinations pass local variable 'i' plus 1 as 'i + 1' into next recursion level 
 ```
 // L77. Combination pass local variable 'i' plus 1 as 'i + 1' into next recursion level instead of passing 'index'
 class Solution {  
@@ -234,7 +279,7 @@ class Solution {
 ```
 
 ---
-Solution 2: Backtracking style 2 (10min, instead of contains() method check, use boolean array)
+Solution 2: Backtracking style 2 (10min, instead of contains() method check, use boolean[] visited array)
 ```
 class Solution { 
     public List<List<Integer>> permute(int[] nums) { 
@@ -255,6 +300,38 @@ class Solution {
             tmp.add(nums[i]); 
             visited[i] = true; 
             helper(nums, result, tmp, index, visited); 
+            visited[i] = false; 
+            tmp.remove(tmp.size() - 1); 
+        } 
+    } 
+}
+
+----------------------------------------------------------------------------------------------------------
+Remove redundant 'index' from parameter
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) { 
+        List<List<Integer>> result = new ArrayList<List<Integer>>(); 
+        //helper(nums, result, new ArrayList<Integer>(), 0, new boolean[nums.length]); 
+        helper(nums, result, new ArrayList<Integer>(), new boolean[nums.length]);
+        return result; 
+    } 
+     
+    //private void helper(int[] nums, List<List<Integer>> result, List<Integer> tmp, int index, boolean[] visited) {
+    private void helper(int[] nums, List<List<Integer>> result, List<Integer> tmp, boolean[] visited) { 
+        if(tmp.size() == nums.length) { 
+            result.add(new ArrayList<Integer>(tmp)); 
+            return; 
+        } 
+        //for(int i = index; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) { 
+            if(visited[i]) { 
+                continue; 
+            } 
+            tmp.add(nums[i]); 
+            visited[i] = true; 
+            //helper(nums, result, tmp, index, visited); 
+            helper(nums, result, tmp, visited); 
             visited[i] = false; 
             tmp.remove(tmp.size() - 1); 
         } 
@@ -371,4 +448,101 @@ n    actual    e*n!      n*n!
 8    109600    109601    322560 
 9    986409    986410    3265920 
 10   9864100   9864101   36288000
+```
+
+---
+L77. Combinations 和 L46. Permutations & L47. Permutations II应用回溯法最大的区别就是L77真的需要在每一层递归的传递的时候都需要向前（从左向右视为前）移动坐标一位，因为是求Combinations，不能折返回已经用过的坐标的数（包括当前坐标），所有的坐标和坐标所代表的数值只能最多使用一次或者不用，但L46，L47不一样，因为是Permutations，所以可以折返回已经用过的所有坐标再次选取，所以每一层递归的时候传递进去的坐标都必须从0开始，重新从头开始选取数值，除了当前选取集合中已经包含的坐标之外的所有坐标都可以再次选取（例如，从{1,2,3,4}中选2个，在当前递归过程中在到达“底”之前已经形成的集合已经包含了{2,3}，除了数值4可选之外，还可以回头选择坐标为0的数值1，但是坐标位于1,2的数值2,3都不能再选了，因为已经在当前集合中出现过了）
+
+不同于L77, 本题没有优化为Memoization和DP的意义, 所以此处只保留了一种Recursion的方法
+Additional solution: Recursion (60min, insert new digit into all potential intervals on existing combination after each recursion)
+```
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        return helper(nums, nums.length - 1);
+    }
+ 
+
+    // index表示当前新增的数字在原数组nums中的位置
+    private List<List<Integer>> helper(int[] nums, int index) {
+        // 只有一个数字的时候
+        if(index == 0) {
+            List<List<Integer>> tmp = new ArrayList<>();
+            List<Integer> one_num = new ArrayList<>();
+            one_num.add(nums[0]);
+            tmp.add(one_num);
+            return tmp;
+        }
+        List<List<Integer>> cur_result = helper(nums, index - 1);
+        // 遍历每一种情况
+        int cur_size = cur_result.size();
+        for(int i = 0; i < cur_size; i++) {
+            // 在数字的缝隙插入新的数字, j是在已有数字的间隙中插入新数字时的可选位置[0到index]
+            // e.g 比如在{1,2}这个组合中插入新数字3会有三个可选坐标[0到2]
+            // 插入后的效果, 插入位置0,{3,1,2}, 插入位置1,{1,3,2}, 插入位置2,{1,2,3}
+            for(int j = 0; j <= index; j++) {
+                List<Integer> comb = new ArrayList<>(cur_result.get(i));
+                comb.add(j, nums[index]);
+                // 添加到结果中
+                cur_result.add(comb);
+            }
+        }
+        // 由于result此时既保存了之前的结果，和添加完的结果，所以把之前的结果要删除
+        for(int i = 0; i < cur_size; i++) {
+            cur_result.remove(0);
+        }
+        return cur_result;
+    }
+}
+```
+
+Refer to
+https://leetcode.wang/leetCode-46-Permutations.html
+
+解法一 插入
+
+这是自己开始想到的一个方法，考虑的思路是，先考虑小问题怎么解决，然后再利用小问题去解决大问题。没错，就是递归的思路。比如说，
+
+如果只有 1 个数字 [ 1 ]，那么很简单，直接返回 [ [ 1 ] ] 就 OK 了。
+
+如果加了 1 个数字 2， [ 1 2 ] 该怎么办呢？我们只需要在上边的情况里，在 1 的空隙，也就是左边右边插入 2 就够了。变成 [ [ 2 1 ], [ 1 2 ] ]。
+
+如果再加 1 个数字 3，[ 1 2 3 ] 该怎么办呢？同样的，我们只需要在上边的所有情况里的空隙里插入数字 3 就行啦。例如 [ 2 1 ] 在左边，中间，右边插入 3 ，变成 3 2 1，2 3 1，2 1 3。同理，1 2 在左边，中间，右边插入 3，变成 3 1 2，1 3 2，1 2 3，所以最后的结果就是 [ [ 3 2 1]，[ 2 3 1]，[ 2 1 3 ], [ 3 1 2 ]，[ 1 3 2 ]，[ 1 2 3 ] ]。
+
+如果再加数字也是同样的道理，只需要在之前的情况里，数字的空隙插入新的数字就够了。
+
+思路有了，直接看代码吧。
+```
+public List<List<Integer>> permute(int[] nums) {
+    return permute_end(nums,nums.length-1);
+}
+// end 表示当前新增的数字的位置
+private List<List<Integer>> permute_end(int[] nums, int end) {
+    // 只有一个数字的时候
+    if(end == 0){
+        List<List<Integer>> all = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        temp.add(nums[0]);
+        all.add(temp);
+        return all;
+    }
+    //得到上次所有的结果
+    List<List<Integer>> all_end = permute_end(nums,end-1);
+    int current_size = all_end.size();
+    //遍历每一种情况
+    for (int j = 0; j < current_size; j++) { 
+        //在数字的缝隙插入新的数字
+        for (int k = 0; k <= end; k++) {
+            List<Integer> temp = new ArrayList<>(all_end.get(j));
+            temp.add(k, nums[end]);
+            //添加到结果中
+            all_end.add(temp);
+        };
+
+    }
+    //由于 all_end 此时既保存了之前的结果，和添加完的结果，所以把之前的结果要删除
+    for (int j = 0; j < current_size; j++) {
+        all_end.remove(0);
+    }
+    return all_end;
+}
 ```
