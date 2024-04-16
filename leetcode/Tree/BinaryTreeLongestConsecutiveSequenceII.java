2bf48630f31769bcdaf91e4f66dfb1f6cb279394
lampardchelsea <zqqsxdd@gmail.com>
2024-04-15 23:41:06 -0700

    Update BinaryTreeLongestConsecutiveSequenceII.java

--- leetcode/Tree/BinaryTreeLongestConsecutiveSequenceII.java
+++ leetcode/Tree/BinaryTreeLongestConsecutiveSequenceII.java
@@ -1,41 +1,29 @@
+
 https://leetcode.ca/all/549.html
-
 Given a binary tree, you need to find the length of Longest Consecutive Path in Binary Tree.
-
 Especially, this path can be either increasing or decreasing. For example, [1,2,3,4] and [4,3,2,1] are both considered valid, but the path [1,2,4,3] is not valid. On the other hand, the path can be in the child-Parent-child order, where not necessarily be parent-child order.
 
 Example 1:
-```
 Input:
         1
        / \
       2   3
-
 Output: 2
 Explanation: The longest consecutive path is [1, 2] or [2, 1].
-```
  
 Example 2:
-```
 Input:
         2
        / \
       1   3
-
 Output: 3
 Explanation: The longest consecutive path is [1, 2, 3] or [3, 2, 1].
-```
-
 Note: All the values of tree nodes are in the range of [-1e7, 1e7].
----
+--------------------------------------------------------------------------------
 Attempt 1: 2022-12-29
-
 Solution 1: Divide and Conquer (120min, similar as L124/P9.7.Binary Tree Maximum Path Sum, the difference is L549 has both candidate increasing and decreasing paths, we have to record both)
-
 这道题的helper函数的思路是，从当前根节点出发，最长连续路径 = 左子树的最长递增路径 + 右子树的最长递减路径 - 1 或者 = 左子树的最长递减路径 + 右子树的最长递增路径 - 1，换言之，最终就是寻找最长递增路径 + 最长递减路径 -1 （不分左右）
-
 Style 1: Create helper class Node {increase, decrease} to record current level recursion maximum count on path
-```
 class TreeSolution { 
     private class TreeNode { 
         public int val; 
@@ -134,13 +122,10 @@ class TreeSolution {
 }
 
 Time Complexity: O(n), where n is the number of nodes in the tree. 
-
 Space Complexity: O(logn), on average for the recursion stack since this is a binary tree.
-```
 
 Refer to
 https://cheonhyangzhang.gitbooks.io/leetcode-solutions/content/solutions-501-550/549-binary-tree-longest-consecutive-sequence-ii.html
-```
 public class Solution { 
     private class Node { 
         private int incr; 
@@ -189,13 +174,11 @@ public class Solution {
         return curr; 
     } 
 }
-```
 
 Refer to
 No need  'sum_incr' or 'sum_decr'
 https://massivealgorithms.blogspot.com/2017/04/leetcode-549-binary-tree-longest.html
-```
-    int max = 0; 
+   int max = 0; 
      
     class Result { 
         TreeNode node; 
@@ -241,10 +224,8 @@ https://massivealgorithms.blogspot.com/2017/04/leetcode-549-binary-tree-longest.
          
         return curr; 
     }
-```
 
 Style 2: Return int[] array instead of create class Node {increase, decrease} to record current level recursion maximum count on path
-```
 class TreeSolution {
     private class TreeNode {
         public int val;
@@ -254,8 +235,6 @@ class TreeSolution {
             this.left = this.right = null;
         }
     }
-
-
 
     public static void main(String[] args) {
         /**
@@ -300,17 +279,12 @@ class TreeSolution {
         System.out.println(result);
     }
 
-
-
-
     int count = 0;
     public int longestConsecutive(TreeNode root) {
         helper(root);
         return count;
     }
-
-
-
+    
     private int[] helper(TreeNode root) {
         // returns [longest_decreasing_length_from_root, longest_increasing_length_from_root]
         if(root == null) {
@@ -343,28 +317,21 @@ class TreeSolution {
 }
 
 Time Complexity: O(n), where n is the number of nodes in the tree. 
-
 Space Complexity: O(logn), on average for the recursion stack since this is a binary tree.
-```
 
 Refer to
 https://github.com/YaokaiYang-assaultmaster/LeetCode/blob/master/LeetcodeAlgorithmQuestions/549.%20Binary%20Tree%20Longest%20Consecutive%20Sequence%20II.md
 采用bottom-up的方法dfs (也就是Divide and Conquer). 每个点同时维护能向下延展的最大increasing 和 decreasing长度.
 Compared with 298. Binary Tree Longest Consecutive Sequence, this question includes more different conditions since it allows for:
-1. both increasing and decreasing order from a follows the parent-child path.
-2. child-parent-child path.
-
+1.both increasing and decreasing order from a follows the parent-child path.
+2.child-parent-child path.
 Hence this question actually contains 2 subproblems to solve:
-1. what is the longest increasing consecutive parent-child path sequence given a root node?
-2. what is the longest decreasing consecutive parent-child path sequence given a root node?
-
+1.what is the longest increasing consecutive parent-child path sequence given a root node?
+2.what is the longest decreasing consecutive parent-child path sequence given a root node?
 Based on the above 2 sub-solution, we know that the longest consecutive sequence for a given root is longest_increasing_sequence + longest_decreasing_sequence from this root. We can simply add up this 2 value because the longest increasing consecutive sequence and longest decreasing consecutive sequence is guaranteed to showed up in different child path (otherwise there will be a contradiction--a child's value cannot be greater than and less than the root's value at the same time).
-
 If the root's value's value is not consecutive with a child's value, then the length of current sequence is simply 1.
-
 Time complexity: O(n) where nis the number of nodes in the tree.
 Space complexity: O(logn) on average for the recursion stack since this is a binary tree.
-```
 class Solution { 
     int max = 0; 
     public int longestConsecutive(TreeNode root) { 
@@ -397,4 +364,8 @@ class Solution {
         return new int[]{dcr, icr}; 
     } 
 }
-```
+ 
+Refer to
+L124.P9.7.Binary Tree Maximum Path Sum
+L257.Binary Tree Paths (Ref.L1430,L549,L124)
+L298.Binary Tree Longest Consecutive Sequence (Ref.L257)
