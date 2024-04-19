be053a88800bcda2d7b161769c7924897fe0417c
lampardchelsea <zqqsxdd@gmail.com>
2024-04-19 02:33:18 -0700

    Update LowestCommonAncestorofaBinaryTreeII.java

--- leetcode/Tree/LowestCommonAncestorofaBinaryTreeII.java
+++ leetcode/Tree/LowestCommonAncestorofaBinaryTreeII.java
@@ -1,50 +1,41 @@
+
 https://leetcode.ca/all/1644.html
-
 Given the root of a binary tree, return the lowest common ancestor (LCA) of two given nodes, p and q. If either node p or q does not exist in the tree, return null. All values of the nodes in the tree are unique.
-
 According to the definition of LCA on Wikipedia: "The lowest common ancestor of two nodes p and q in a binary tree T is the lowest node that has both p and q as descendants (where we allow a node to be a descendant of itself)". A descendant of a node x is a node y that is on the path from node x to some leaf node.
 
 Example 1:
 
 
-```
 Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
 Output: 3
 Explanation: The LCA of nodes 5 and 1 is 3.
-```
 
 Example 2:
 
 
-```
 Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
 Output: 5
 Explanation: The LCA of nodes 5 and 4 is 5. A node can be a descendant of itself according to the definition of LCA.
-```
 
 Example 3:
 
 
-```
 Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 10
 Output: null
 Explanation: Node 10 does not exist in the tree, so return null.
-```
  
 Constraints:
-- The number of nodes in the tree is in the range [1, 104].
-- -109 <= Node.val <= 109
+- The number of nodes in the tree is in the range [1, 10^4].
+- -10^9 <= Node.val <= 10^9
 - All Node.val are unique.
 - p != q
  
 Follow up: Can you find the LCA traversing the tree, without checking nodes existence?
----
+--------------------------------------------------------------------------------
 Note: The difference between L1644.Lowest Common Ancestor of a Binary Tree II and L236.Lowest Common Ancestor of a Binary Tree is L1644 allow p and q may not exist in the tree.
 
 Attempt 1: 2022-12-28
-
 Solution 1:  Divide and Conquer (30 min)
-```
 class TreeSolution { 
     private class TreeNode { 
         public int val; 
@@ -90,6 +81,7 @@ class TreeSolution {
             return null; 
         } 
     } 
+
     public TreeNode helper(TreeNode root, TreeNode p, TreeNode q) { 
         if(root == null) { 
             return null; 
@@ -114,7 +106,19 @@ class TreeSolution {
     } 
 }
 
-Complexity Analysis 
-Time Complexity: O(N). Where N is the number of nodes in the binary tree. In the worst case we might be visiting all the nodes of the binary tree. 
-Space Complexity: O(N). This is because the maximum amount of space utilized by the recursion stack would be N since the height of a skewed binary tree could be N.
-```
+Complexity Analysis  
+Time Complexity: O(N). Where N is the number of nodes in the binary tree. 
+In the worst case we might be visiting all the nodes of the binary tree.  
+Space Complexity: O(N). This is because the maximum amount of space utilized 
+by the recursion stack would be N since the height of a skewed binary tree could be N.
+
+Refer to
+https://blog.csdn.net/qq_46105170/article/details/109699655
+给定一棵二叉树，再给定两个节点（未必在树中），求这两个节点的最近公共祖先。题目保证节点的数字各不相同。
+
+思路是DFS。如果两个节点都在树中，则可以参考https://blog.csdn.net/qq_46105170/article/details/104141292的做法。而这道题中不能保证两个节点都在树中，所以DFS的时候，如果遇到root等于p pp或q qq的时候，是不能立刻返回root的，因为不能判断另一个节点是否在树中。所以我们的方案是采取后序遍历，保证每个节点都被遍历到。然后的逻辑就和两个节点都在树中一样了。并且，当搜索到p或q的时候，我们做个计数。最后如果计数等于2，说明两个节点都找到了，就可以返回答案了。
+      
+    
+Refer to
+L235.Lowest Common Ancestor of a Binary Search Tree
+L236.Lowest Common Ancestor of a Binary Tree (Ref.L865,L235)
