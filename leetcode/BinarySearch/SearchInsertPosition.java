06d78e4e255e286eecc8a6a84c841862173aeb9c
lampardchelsea <zqqsxdd@gmail.com>
2024-05-27 15:47:03 -0700

    Update SearchInsertPosition.java

--- leetcode/BinarySearch/SearchInsertPosition.java
+++ leetcode/BinarySearch/SearchInsertPosition.java
@@ -1,8 +1,7 @@
-/**
-Refer to
+
 https://leetcode.com/problems/search-insert-position/
-Given a sorted array of distinct integers and a target value, return the index if the target is found. 
-If not, return the index where it would be if it were inserted in order.
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
 
 Example 1:
 Input: nums = [1,3,5,6], target = 5
@@ -16,79 +15,14 @@ Example 3:
 Input: nums = [1,3,5,6], target = 7
 Output: 4
 
-Example 4:
-Input: nums = [1,3,5,6], target = 0
-Output: 0
-
-Example 5:
-Input: nums = [1], target = 0
-Output: 0
-
 Constraints:
-1 <= nums.length <= 104
--104 <= nums[i] <= 104
-nums contains distinct values sorted in ascending order.
--104 <= target <= 104
-*/
-
-// Solution 1: 
-// Refer to
-// https://leetcode.com/discuss/general-discussion/786126/python-powerful-ultimate-binary-search-template-solved-many-problems
-/**
-35. Search Insert Position [Easy]
-Given a sorted array and a target value, return the index if the target is found. If not, return the index 
-where it would be if it were inserted in order. You may assume no duplicates in the array.
-
-Example:
-Input: [1,3,5,6], 5
-Output: 2
-Input: [1,3,5,6], 2
-Output: 1
-Very classic application of binary search. We are looking for the minimal k value satisfying nums[k] >= target, 
-and we can just copy-paste our template. Notice that our solution is correct regardless of whether the input 
-array nums has duplicates. Also notice that the input target might be larger than all elements in nums and 
-therefore needs to placed at the end of the array. That's why we should initialize right = len(nums) instead 
-of right = len(nums) - 1.
-
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums)
-        while left < right:
-            mid = left + (right - left) // 2
-            if nums[mid] >= target:
-                right = mid
-            else:
-                left = mid + 1
-        return left
-*/
-class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int lo = 0;
-        int hi = nums.length; // we might need to inseart at the end
-        while(lo < hi) { // breaks if lo == hi
-            int mid = lo + (hi - lo) / 2; // always gives the lower mid
-            // try to find the first index which value on that index 
-            // larger or equal to target
-            if(nums[mid] >= target) {
-                hi = mid; // it might be possible to insert @ mid
-            } else {
-                lo = mid + 1; // no way mid is a valid option
-            }
-        }
-        return lo;
-    }
-}
-
-
-
-
-
-
-
+- 1 <= nums.length <= 10^4
+- -10^4 <= nums[i] <= 10^4
+- nums contains distinct values sorted in ascending order.
+- -10^4 <= target <= 10^4
+--------------------------------------------------------------------------------
 Attempt 1: 2022-10-02
-
 Solution 1: 5min, Binary Search Find Upper Boundary (template based on while(lo <= hi), refer L704.Binary Search)
-```
 class Solution { 
     // Convert problem into find the most recent value position smaller 
     // or equal to target 
@@ -115,11 +49,9 @@ class Solution {
 
 Space Complexity: O(1)         
 Time Complexity: O(logn)
-```
 
----
+--------------------------------------------------------------------------------
 Solution 2: 10min, refer to L34.Find First and Last Position of Element in Sorted Array Solution 2 while(lo < hi) Right Biased template
-```
 class Solution { 
     // Convert problem into find the most recent value position smaller 
     // or equal to target 
@@ -149,11 +81,9 @@ class Solution {
 
 Space Complexity: O(1)          
 Time Complexity: O(logn)
-```
 
----
+--------------------------------------------------------------------------------
 Solution 3: 60min, too long to get the trick on setting hi = len rather than hi = len - 1, follow L34.Find First and Last Position of Element in Sorted Array Solution 2 while(lo < hi) template
-```
 class Solution { 
     // Convert problem into find the most recent value position smaller 
     // or equal to target 
@@ -167,10 +97,10 @@ class Solution {
         int hi = len; 
         while(lo < hi) { 
             int mid = lo + (hi - lo) / 2; 
-            if(nums[mid] < target) { 
-                lo = mid + 1; 
-            } else { 
+            if(nums[mid] >= target) { 
                 hi = mid; 
+            } else {
+                lo = mid + 1; 
             } 
         } 
         return lo; 
@@ -179,11 +109,9 @@ class Solution {
 
 Space Complexity: O(1)          
 Time Complexity: O(logn)
-```
 
 Refer to
 https://leetcode.com/problems/search-insert-position/discuss/15110/Very-concise-and-efficient-solution-in-Java
-```
 public class Solution { 
     public int searchInsert(int[] nums, int target) { 
         int low = 0, high = nums.length; 
@@ -197,11 +125,9 @@ public class Solution {
         return low; 
     }
 }
-```
 
 https://leetcode.wang/leetCode-35-Search-Insert-Position.html
 给定一个有序数组，依旧是二分查找，不同之处是如果没有找到指定数字，需要返回这个数字应该插入的位置。这道题比较简单，在二分查找的基础上，只要想清楚返回啥就够了。想的话，就考虑最简单的情况如果数组只剩下 2 5，target 是 1, 3, 6 的时候，此时我们应该返回什么就行。
-```
 public int searchInsert(int[] nums, int target) { 
     int start = 0; 
     int end = nums.length - 1; 
@@ -227,7 +153,6 @@ public int searchInsert(int[] nums, int target) {
         return start; 
     } 
 }
-```
 这道题不难，但是对于二分查找又有了一些新认识。
 
 首先，一定要注意，数组剩下偶数个元素的时候，中点取的是左端点。例如 1 2 3 4，中点取的是 2。正因为如此，我们更新 start 的时候不是直接取 mid ，而是 mid + 1。因为剩下两个元素的时候，mid 和 start 是相同的，如果不进行加 1 会陷入死循环。
@@ -236,7 +161,6 @@ public int searchInsert(int[] nums, int target) {
 我们开始更新 start 的时候，是 mid + 1，如果剩两个元素，例如 2 4，target = 6 的话，此时 mid = 0，start = mid + 1 = 1，我们返回 start + 1 = 2。如果 mid 是右端点，那么 mid = 1，start = mid + 1 = 2，这样就可以直接返回 start 了，不需要在返回的时候加 1 了
 
 怎么做到呢？最最开始的时候我们取 end 的时候是 end = nums.length - 1。如果我们改成 end = nums.length，这样每次取元素的时候，如果和之前对比，取到的就是右端点了。这样的话，最后返回的时候就不需要多加 1 了。选用 end = nums.length 的效果类似于 Solution 2 中的 Right Biased ==> int mid = lo + (hi - lo) / 2 + 1; 
-```
 public int searchInsert(int[] nums, int target) { 
     int start = 0; 
     int end = nums.length; 
@@ -255,11 +179,9 @@ public int searchInsert(int[] nums, int target) {
     } 
     return start; 
 }
-```
 
----
-Solution 4: 60min, intuitive solution no template based, not follow the template from L34.Find First and Last Position of Element in Sorted Array Solution 2
-```
+--------------------------------------------------------------------------------
+Solution 4: 60min, intuitive solution No Template Based, not follow the template from L34.Find First and Last Position of Element in Sorted Array Solution 2
 class Solution { 
     // Convert problem into find the most recent value position smaller 
     // or equal to target 
@@ -290,7 +212,6 @@ class Solution {
         return lo; 
     } 
 }
-```
 
 Refer to
 https://leetcode.com/problems/search-insert-position/discuss/249092/Come-on-forget-the-binary-search-patterntemplate!-Try-understand-it!
@@ -299,79 +220,74 @@ l means left index, r means right index, m means middle index
 Comments
 I have seen many different styles of binary search solution, I saw some people tend to use some pattern whenever solving binary search problems, i.e.
 
-1. while(l <= r){...}. Same while condition
-2. r = m - 1;, right is always m - 1
-3. l = m + 1: , left is always m + 1
+1.while(l <= r){...}. Same while condition
+2.r = m - 1;, right is always m - 1
+3.l = m + 1: , left is always m + 1
 
-It is a good idea to use pattern/template for binary search?
-I can only speak for myself. At least for me, I do not think so. I prefer to choose the binary search rules based on the context of the problem to decide how to play with l and r.If you are using pattern, you have to remember it. You are not understanding the core of binary search! This is a big issue I think.
+It is a good idea to use pattern/template for binary search?I can only speak for myself. At least for me, I do not think so. I prefer to choose the binary search rules based on the context of the problem to decide how to play with l and r.If you are using pattern, you have to remember it. You are not understanding the core of binary search! This is a big issue I think.
 
 Algorithms
 I want to give my explanation below:
-1. First of all, we assume [l, r] is the possible answer range(inclusive) for this question. So initially l = 0; and r = n - 1;
-2. we calculate int m = l + (r - l)/2; rather than int m = (l + r)/2; to avoid overflow.
-3. Clearly, if A[m] = target, m is exactly the position we should insert the target
-4. if A[m] < target, then the insertion position should be at least at m + 1, because it can be even larger, i.e. to the right of m + 1(including m + 1), we set l = m + 1;
-5. if A[m] > target, the insertion position should be at most m, because it can even smaller. So we would set r = m. 
+1.First of all, we assume [l, r] is the possible answer range(inclusive) for this question. So initially l = 0; and r = n - 1;
+2.we calculate int m = l + (r - l)/2; rather than int m = (l + r)/2; to avoid overflow.
+3.Clearly, if A[m] = target, m is exactly the position we should insert the target
+4.if A[m] < target, then the insertion position should be at least at m + 1, because it can be even larger, i.e. to the right of m + 1(including m + 1), we set l = m + 1;
+5.if A[m] > target, the insertion position should be at most m, because it can even smaller. So we would set r = m. 
 An example:
-```
 target: 6 and m = 7
-1 	5 	7 	9
-	        m
-	      result
-```
-6. Then how to determine the while loop condition, l < r, l <= r or l < r - 1? The answer is that we analyze and conclude with the one which will not cause deal while loop in our code while we are moving our left and right pointers.l = m + 1 and r = m
+1     5     7     9
+            m
+          result
+6.Then how to determine the while loop condition, l < r, l <= r or l < r - 1? 
+The answer is that we analyze and conclude with the one which will not cause deal while loop in our code while we are moving our left and right pointers
+a. l = m + 1
+b. r = m
 We will analyze which would happen when we have 3, 2 and 1 elements at the end below
 First let's assume there are 3 elements left at last as shown below:
-```
-x   x   x   5 	7   9   x   x   x
-            l	m   r
-```
+x   x   x   5     7   9   x   x   x
+            l    m   r
 we can see that l = m + 1 can help shrink the search space by 2:
-```
-x   x   x   5 	7   9  x   x   x 
-            l	m   r
-```
+x   x   x   5     7   9  x   x   x 
+            l    m   r
 and r = m can help shrink the size by 1:
-```
-x   x   x   5 	7   9   x   x   x
-            l	r
-```
+x   x   x   5     7   9   x   x   x
+            l    r
 so 3 elements will not cause dead while loop. we should be fine.
 Next we reduce to 2 elements:
-```
 x   x   x   5   7   x   x   x
            l/m  r
-```
 Same way, we can see that l = m + 1 and r = m can both shrink the size by 1, no dead loop as well.
 Then we can safely go to the cases when we have only 1 element at the end:
-```
 x   x   x   5   x   x   x
           l/m/r
-```
 We can see that l = m + 1 will not cause dead loop, but with r = m we cannot shrink the size anymore, so it might lead to a dead while loop. So we need to jump out of the while loop when we have 1 element. The condition of 1 element is i = j, so we choose our while condition: while (l < r)
-7. At the end, we need to check the last element: nums[l/r] which has not been checked in binary search loop with target to determine the index. We call it the post processing.
+7.At the end, we need to check the last element: nums[l/r] which has not been checked in binary search loop with target to determine the index. We call it the post processing.
 
 Summary
 Actually I always do my analysis start from 2 elements instead of 3 elements, because for most of binary search problems, we always can shrink the search space to 2 or 1 element at the end. With 2 elements, it is pretty quick to identify the condition based on your rule.
 
 Final Code
-```
-class Solution { 
-	public int searchInsert(int[] nums, int target) { 
-		if(nums == null || nums.length == 0) return 0; 
-		int n = nums.length; 
-		int l = 0; 
-		int r = n - 1; 
-		while(l < r){ 
-			int m = l + (r - l)/2; 
-			if(nums[m] == target) return m; 
-			else if(nums[m] > target) r = m; // right could be the result 
-			else l = m + 1; // m + 1 could be the result 
-		} 
-		// 1 element left at the end 
-		// post-processing 
-		return nums[l] < target ? l + 1: l; 
-	} 
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return 0;
+
+        int n = nums.length;
+        int l = 0;
+        int r = n - 1;
+        while(l < r){
+            int m = l + (r - l)/2;
+
+            if(nums[m] == target) return m;
+            else if(nums[m] > target) r = m; // right could be the result
+            else l = m + 1; // m + 1 could be the result
+        }
+
+        // 1 element left at the end
+        // post-processing
+        return nums[l] < target ? l + 1: l;
+    }
 }
-```
+
+Refer to
+L34.Find First and Last Position of Element in Sorted Array (Ref.L278,L704)
+L704.Binary Search
