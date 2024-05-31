312535fa312937d2333763177f5240e8596f29d6
lampardchelsea <zqqsxdd@gmail.com>
2024-05-31 00:11:24 -0700

    Update KthLargestElementinanArray.java

--- leetcode/Heap/KthLargestElementinanArray.java
+++ leetcode/Heap/KthLargestElementinanArray.java
@@ -197,34 +197,26 @@ public int findKthLargest(int[] nums, int k) {
 
 
 
+
 https://leetcode.com/problems/kth-largest-element-in-an-array/
-
 Given an integer array nums and an integer k, return the kth largest element in the array.
-
 Note that it is the kth largest element in the sorted order, not the kth distinct element.
-
 Can you solve it without sorting?
 
 Example 1:
-```
 Input: nums = [3,2,1,5,6,4], k = 2
 Output: 5
-```
 
 Example 2:
-```
 Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
 Output: 4
-```
 
 Constraints:
-- 1 <= k <= nums.length <= 105
-- -104 <= nums[i] <= 104
----
+- 1 <= k <= nums.length <= 10^5
+- -10^4 <= nums[i] <= 10^4
+--------------------------------------------------------------------------------
 Attempt 1: 2023-08-03
-
 Solution 1: Sort (10min)
-```
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
@@ -237,20 +229,16 @@ Sorting nums requires O(n⋅log⁡n) time.
 Space Complexity: O(log⁡n) 
 The space complexity of the sorting algorithm depends on the implementation of each programming language: 
 - In Java, Arrays.sort() for primitives is implemented using a variant of the Quick Sort algorithm, which has a space complexity of O(log⁡n)
-```
 
 Refer to
 https://leetcode.com/problems/kth-largest-element-in-an-array/editorial/
-
 Approach 1: Sort
-
 Intuition
 Sort the array in descending order and then return the kth
 element. Note that this is the "trivial" approach and if asked this question in an interview, you would be expected to come up with a better solution than this.
 
 Implementation
 Note: kis 1-indexed, not 0-indexed. As such, we need to return the element at index k - 1after sorting, not index k.
-```
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
@@ -259,22 +247,18 @@ class Solution {
         return nums[nums.length - k];
     }
 }
-```
 
 Complexity Analysis
 Given n as the length of nums,
 - Time complexity: O(n⋅log⁡n)
-  Sorting nums requires O(n⋅log⁡n) time.
+Sorting nums requires O(n⋅log⁡n) time.
 - Space Complexity: O(log⁡n)
-  The space complexity of the sorting algorithm depends on the implementation of each programming language:
-	- In Java, Arrays.sort() for primitives is implemented using a variant of the Quick Sort algorithm, which has a space complexity of O(log⁡n)
----
+The space complexity of the sorting algorithm depends on the implementation of each programming language:
+- In Java, Arrays.sort() for primitives is implemented using a variant of the Quick Sort algorithm, which has a space complexity of O(log⁡n)
+--------------------------------------------------------------------------------
 Solution 2: Min-heap or Max-heap (10min)
-
 Style 1: Use build in Java PriorityQueue
-```
 Minimum PriorityQueue
-
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         // Java default PriorityQueue as minimum PriorityQueue
@@ -318,10 +302,8 @@ Given n as the length of nums. Operations on a heap cost logarithmic time relati
 We iterate n times, performing up to log⁡k work at each iteration, giving us a time complexity of O(n⋅log⁡k) 
 Because k≤n , this is an improvement on the previous approach. 
 Space complexity: O(k) The heap uses O(k) space.
-```
 
 Style 2: Use customized PriorityQueue
-```
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         int length = nums.length;
@@ -387,19 +369,16 @@ class Solution {
             // return pq[v] - pq[w] < 0;
             // but as this question will find the kth largest number, and -2147483648 is surely
             // sit at the last position, so will not reflect problem here
-    		// Refer to
-    		// https://github.com/lampardchelsea/hello-world/blob/master/leetcode/array/ThirdMaximumNumber.java
+            // Refer to
+            // https://github.com/lampardchelsea/hello-world/blob/master/leetcode/array/ThirdMaximumNumber.java
             return pq[v] < pq[w];
         }
     }
 }
-```
 
 Refer to
 https://leetcode.com/problems/kth-largest-element-in-an-array/editorial/
-
 Approach 2: Min-Heap
-
 Intuition
 A heap is a very powerful data structure that allows us to efficiently find the maximum or minimum value in a dynamic dataset.
 
@@ -412,15 +391,14 @@ It is impossible for one of the green elements to be popped because that would i
 After we handle all the elements, we can just check the top of the heap. Because the heap is holding the k largest elements and the top of the heap is the smallest element, the top of the heap would be the kth largest element, which is what the problem is asking for.
 
 Algorithm
-1. Initialize a min-heap heap.
-2. Iterate over the input. For each num:
-	- Push num onto the heap.
-	- If the size of heap exceeds k, pop from heap.
-3. Return the top of the heap.
+1.Initialize a min-heap heap.
+2.Iterate over the input. For each num:
+- Push num onto the heap.
+- If the size of heap exceeds k, pop from heap.
+3.Return the top of the heap.
 
 Implementation
-	Note: C++ std::priority_queue implements a max-heap. To achieve min-heap functionality, we will multiply the values by -1 before pushing them onto the heap.
-```
+Note: C++ std::priority_queue implements a max-heap. To achieve min-heap functionality, we will multiply the values by -1 before pushing them onto the heap.
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> heap = new PriorityQueue<>();
@@ -434,18 +412,16 @@ class Solution {
         return heap.peek();
     }
 }
-```
 Complexity Analysis
 Given n as the length of nums,
 - Time complexity: O(n⋅log⁡k)
-  Operations on a heap cost logarithmic time relative to its size. Because our heap is limited to a size of k, operations cost at most O(log⁡k). We iterate over nums, performing one or two heap operations at each iteration.
-  We iterate n times, performing up to log⁡k work at each iteration, giving us a time complexity of O(n⋅log⁡k)
-  Because k≤n , this is an improvement on the previous approach.
+Operations on a heap cost logarithmic time relative to its size. Because our heap is limited to a size of k, operations cost at most O(log⁡k). We iterate over nums, performing one or two heap operations at each iteration.
+We iterate n times, performing up to log⁡k work at each iteration, giving us a time complexity of O(n⋅log⁡k)
+Because k≤n , this is an improvement on the previous approach.
 - Space complexity: O(k)
-  The heap uses O(k) space.
----
+The heap uses O(k) space.
+--------------------------------------------------------------------------------
 Solution 3: Quick Select (60 min)
-```
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         // Should not use Arrays.asList() to work with primitive 
@@ -526,18 +502,14 @@ class Solution {
 
 Time complexity: O(n) on average, O(n^2) in the worst case
 Space complexity: O(n)
-```
 
 Refer to
 https://leetcode.com/problems/kth-largest-element-in-an-array/editorial/
-
 Approach 3: Quickselect
-
 Intuition
 This is a more advanced/esoteric algorithm. Do not feel discouraged if you are unable to derive it yourself. It is highly unlikely that you would be expected to come up with this solution in an interview without any help from the interviewer.
 
 Quickselect, also known as Hoare's selection algorithm, is an algorithm for finding the kth smallest element in an unordered list. It is significant because it has an average runtime of O(n).
-
 Quickselect uses the same idea as Quicksort. First, we choose a pivot index. The most common way to choose the pivot is randomly. We partition nums into 3 sections: elements equal to the pivot, elements greater than the pivot, and elements less than the pivot.
 
 Next, we count the elements in each section. Let's denote the sections as follows:
@@ -556,21 +528,19 @@ There's one extra step if the answer is in right. When we go to search in right,
 We don't need to modify k when we search in left because when we search in left, we delete elements smaller than the answer, which doesn't affect k.
 
 If the answer is in neither left or right, it must be in mid. Since mid only has elements equal to the pivot, the pivot must be the answer.
-
 The easiest way to implement this repetitive process is by using recursion.
 
 Algorithm
 Note: the implementation we use here is not a standard Quickselect implementation. We will be using slightly more space (still the same complexity), but in exchange, we will be writing significantly less code.
-1. Define a quickSelect function that takes arguments nums and k. This function will return the kth greatest element in nums(the nums and k given to it as input, not the original nums and k).
-	- Select a random element as the pivot.
-	- Create left, mid, and right as described above.
-	- If k <= left.length, return quickSelect(left, k).
-	- If left.length + mid.length < k, return quickSelect(right, k - left.length - mid.length).
-	- Otherwise, return pivot.
-2. Call quickSelect with the original nums and k, and return the answer.
+1.Define a quickSelect function that takes arguments nums and k. This function will return the kth greatest element in nums(the nums and k given to it as input, not the original nums and k).
+- Select a random element as the pivot.
+- Create left, mid, and right as described above.
+- If k <= left.length, return quickSelect(left, k).
+- If left.length + mid.length < k, return quickSelect(right, k - left.length - mid.length).
+- Otherwise, return pivot.
+2.Call quickSelect with the original nums and k, and return the answer.
 
 Implementation
-```
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         List<Integer> list = new ArrayList<>();
@@ -610,7 +580,6 @@ class Solution {
         return pivot;
     }
 }
-```
 
 Complexity Analysis
 Given n as the length of nums,
@@ -623,7 +592,7 @@ On average, the size of nums will decrease by a factor of ~2 on each call. You m
 that is a factor of ~2 smaller. This recurrence can be analyzed using the master theorem with  a = 1, b = 2, k = 1:
 
 - Space complexity: O(n)
-  We need O(n) space to create left, mid, and right. Other implementations of Quickselect can avoid creating these three in memory, but in the worst-case scenario, those implementations would still require O(n) space for the recursion call stack.
+We need O(n) space to create left, mid, and right. Other implementations of Quickselect can avoid creating these three in memory, but in the worst-case scenario, those implementations would still require O(n) space for the recursion call stack.
 
 Bonus
 When we randomly choose pivots, Quickselect has a worst-case scenario time complexity of O(n^2). 
@@ -634,13 +603,12 @@ This approach is way out of scope for an interview, and practically it isn't eve
 
 The median of medians approach should only be appreciated for its theoretical beauty. Those who are interested can read more using the link above.
 
+--------------------------------------------------------------------------------
 Solution 4: Counting Sort (30 min)
-
 Note: 
-Build  a index shift frame to avoid negative index issue since bucket build closely relate to number value itself, if that's a negative index, we have to add shift variable to 'make it up' to non-negative index
+Build a index shift frame to avoid negative index issue since bucket build closely relate to number value itself, if that's a negative index, we have to add shift variable to 'make it up' to non-negative index
 e.g if nums[i] = -10000, when build bucket we cannot directly use -10000 as bucket index, just add 10000 as a make up to correct the bucket index to non-negative
 Also need to avoid index out of boundary issue, bucket[num - min + shift] will break when num - min + shift > boundary, in this case just remove '-min' and change to bucket[num + shift] is fine, which already resolve negative index issue and never over come the pre-defined boundary
-```
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         int min = 10001;
@@ -706,22 +674,19 @@ class Solution {
         return result - 10000;
     }
 }
-```
 
 Refer to
 https://leetcode.com/problems/kth-largest-element-in-an-array/editorial/
-
 Approach 4: Counting Sort
-
 Intuition
 Counting sort is a non-comparison sorting algorithm. It can be used to sort an array of positive integers.
 
 In this approach, we will sort the input using a slightly modified counting sort, and then return the kth element from the end (just like in the first approach).
 
 Here is how we will sort the array:
-1. First, find the maxValue in the array. Create an array count with size maxValue + 1.
-2. Iterate over the array and find the frequency of each element. For each element num, increment count[num].
-3. Now we know the frequency of each element. Each index of count represents a number. Create a new array sortedArr and iterate over count. For each index num, add count[num] copies of num to sortedArr. Because we iterate over the indices in sorted order, we will also add elements to sortedArr in sorted order.
+1.First, find the maxValue in the array. Create an array count with size maxValue + 1.
+2.Iterate over the array and find the frequency of each element. For each element num, increment count[num].
+3.Now we know the frequency of each element. Each index of count represents a number. Create a new array sortedArr and iterate over count. For each index num, add count[num] copies of num to sortedArr. Because we iterate over the indices in sorted order, we will also add elements to sortedArr in sorted order.
 
 The following animation demonstrates this process:
 
@@ -735,26 +700,23 @@ The following animation demonstrates this process:
 
 
 There is one problem: as we are associating indices with numbers, this algorithm will not work if negative numbers are in the input. The constraints state that negative numbers can be in the input, so we need to account for this.
-
-Let's also find minValue, the minimum value in the array. Instead of count having a size of maxValue + 1, we will make it have a size of maxValue - minValue + 1 (if minValue < 0, then this will appropriately increase the size of count).
-
+Let's also find minValue, the minimum value in the array. Instead of count having a size of maxValue + 1, we will make it have a size of 
+maxValue - minValue + 1 (if minValue < 0, then this will appropriately increase the size of count).
 Now, we can just apply an offset of minValue when mapping numbers to indices and vice-versa. When we populate count, given a num we will increment count[num - minValue]. count[num] will represent the frequency of num + minValue.
-
 
 One more small optimization
 Since we don't actually need to sort the array but only need to return the kth largest value, we will iterate over the indices of counting reverse order (get the larger numbers first). At each number, we will decrement k(or a variable we initialize as remain = k to avoid modifying the input) by the frequency. If remain <= 0, we can return the current number. With this optimization, we don't need to create sorted Arr, saving us O(n) space.
 
 Algorithm
-1. Find the maxValue and minValue of nums.
-2. Create count, an array of size maxValue - minValue + 1.
-3. Iterate over nums. For each num, increment count[num - minValue].
-4. Set remain = k and iterate over the indices of count backward. For each index num:
-	- Subtract the frequency of num from remain: remain -= count[num].
-	- If remain <= 0, return the number that the current index represents: num + minValue.
-5. The code should never reach this point. Return any value.
+1.Find the maxValue and minValue of nums.
+2.Create count, an array of size maxValue - minValue + 1.
+3.Iterate over nums. For each num, increment count[num - minValue].
+4.Set remain = k and iterate over the indices of count backward. For each index num:
+- Subtract the frequency of num from remain: remain -= count[num].
+- If remain <= 0, return the number that the current index represents: num + minValue.
+5.The code should never reach this point. Return any value.
 
 Implementation
-```
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         int minValue = Integer.MAX_VALUE;
@@ -781,14 +743,17 @@ class Solution {
         return -1;
     }
 }
-```
 
 Complexity Analysis
 Given n as the length of nums and m as maxValue - minValue,
 - Time complexity: O(n+m)
-  We first find maxValue and minValue, which costs O(n).
-  Next, we initialize count, which costs O(m).
-  Next, we populate count, which costs O(n).
-  Finally, we iterate over the indices of count, which costs up to O(m).
+We first find maxValue and minValue, which costs O(n).
+Next, we initialize count, which costs O(m).
+Next, we populate count, which costs O(n).
+Finally, we iterate over the indices of count, which costs up to O(m).
 - Space complexity: O(m)
-  We create an array count with size O(m).
+We create an array count with size O(m).      
+    
+Refer to
+Counting Sort
+十大经典排序算法(动图演示)
