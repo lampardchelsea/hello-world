850481f6c45fa9dd1ef7760374b41d7d125090b7
lampardchelsea <zqqsxdd@gmail.com>
2024-06-04 19:28:54 -0700

    Update ShortestPathInAGridWithObstaclesElimination.java

--- leetcode/BFS/ShortestPathInAGridWithObstaclesElimination.java
+++ leetcode/BFS/ShortestPathInAGridWithObstaclesElimination.java
@@ -345,3 +345,183 @@ new boolean[m][n][k + 1]; // keeps track of whether or not we visited a node
         return -1; // no valid answer found
     }
 }
+
+--------------------------------------------------------------------------------
+Solution 2: Dijkstra (120 min)
+class Solution {
+    public int shortestPath(int[][] grid, int k) {
+        int[] dx = new int[]{0,0,1,-1};
+        int[] dy = new int[]{1,-1,0,0};
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][][] distances = new int[m][n][k + 1];
+        for(int[][] distance : distances) {
+            for(int[] d : distance) {
+                Arrays.fill(d, Integer.MAX_VALUE);
+            }
+        }
+        // Initialize distance for all potential removing obstacles cases(0 ~ k) as 0
+        Arrays.fill(distances[0][0], 0);
+        // min-heap storing {i, j, # obstacles eliminated, distance}, sorted by distance to (0,0)
+        // Similar strategy as L743.Network Delay Time
+        PriorityQueue<int[]> minPQ = new PriorityQueue<>((a, b) -> a[3] - b[3]);
+        minPQ.offer(new int[]{0, 0, 0, 0});
+        // Using Dijkstra algorithm with PriorityQueue, no 'visited' array required
+        while(!minPQ.isEmpty()) {
+            int[] cur = minPQ.poll();
+            for(int i = 0; i < 4; i++) {
+                int new_x = cur[0] + dx[i];
+                int new_y = cur[1] + dy[i];
+                // Must in boundary
+                if(new_x >= 0 && new_x < m && new_y >= 0 && new_y < n) {
+                    int new_k = cur[2] + grid[new_x][new_y];
+                    // Must under # obstacles eliminated limit
+                    if(new_k <= k) {
+                        // Continue if we have more optimal result:
+                        // Under same number of obstacles removed case, if the 
+                        // new path(new_distance) able to reach longer distance 
+                        // than existing path(distances[new_x][new_y][new_k]), 
+                        // we will update current path to new path, which means 
+                        // addding the node into minPQ
+                        int new_distance = cur[3] + 1;
+                        if(distances[new_x][new_y][new_k] > new_distance) {
+                            distances[new_x][new_y][new_k] = new_distance;
+                            minPQ.offer(new int[]{new_x, new_y, new_k, new_distance});
+                        }
+                    }
+                }
+            }
+        }
+        // Each number of removing obstacle case(0 ~ k) will have a 
+        // minimum distance, scan all of them to find final minimum one
+        int result = distances[m - 1][n - 1][0];
+        for(int i = 1; i <= k; i++) {
+            result = Math.min(result, distances[m - 1][n - 1][i]);
+        }
+        // If not able to find a path to reach {m - 1, n - 1}
+        // just return -1
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+}
+
+Refer to
+Dijkstra Solution
+[Java] Clean O(MNK)-Time BFS Solution || comparing with Dijkstra's
+https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/solutions/1188835/java-clean-o-mnk-time-bfs-solution-comparing-with-dijkstra-s/
+This most optimal BFS Solution is actually not as straightforward as it seems to be. The visited matrix record the minimum obstacles removed to get to that entry. Thus, this Solution is different from the classic BFS Algorithm in these aspects :
+1.a position (x,y) may be visited more than 1 time
+2.a position (newX, newY) should not enter queue if we have a more optimal result
+Namely, we have the following code:
+int[][] visited = new int[m][n];
+for (int[] i: visited) Arrays.fill(i, Integer.MAX_VALUE);
+visited[0][0] = 0;
+instead of
+boolean[][] visited = new boolean[m][n];
+for (boolean[] b : visited) Arrays.fill(b, false);
+visited[0][0] = true;
+And in during the BFS process, we have:
+if (visited[newX][newY] <= newK) continue;
+instead of
+if (visited[newX][newY]) continue;
+The solution below is a direct analogy to the classical BFS Algorithm:
+class Solution {
+    private static final int[][] DIRECTIONS = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+    public int shortestPath(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length;
+        // storing {i, j, # obstacles eliminated}
+        Deque<int[]> queue = new LinkedList<>();
+        queue.offerLast(new int[]{0, 0, 0});
+        
+        int[][] visited = new int[m][n];
+        for (int[] i: visited) Arrays.fill(i, Integer.MAX_VALUE);
+        visited[0][0] = 0;
+        
+        int dist = 0;
+      
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            
+            while (size-- > 0) {
+                int[] curr = queue.poll();
+                if (curr[0] == m-1 && curr[1] == n-1) return dist;
+                
+                for (int[] dir : DIRECTIONS) {
+                    int newX = curr[0] + dir[0];
+                    int newY = curr[1] + dir[1];
+                    
+                    // 1). continue if out of bound
+                    if (newX < 0 || newY < 0 || newX >= m || newY >= n) continue;
+                    
+                    // 2). continue if out of elimation
+                    int newK = curr[2] + grid[newX][newY];
+                    if (newK > k) continue;
+                    
+                    // 3). continue if we have more optimal result
+                    if (visited[newX][newY] <= newK) continue;
+                    
+                    visited[newX][newY] = newK;
+                    queue.offerLast(new int[]{newX, newY, newK});
+                }
+            }
+            
+            dist++;
+        }
+        return -1;
+    }
+}
+However, I still want to point out that the reasoning above is really similar to Dijkstra's Algorithm. This Dijkstra-Solution is actually even more straightforward.
+Before jump into the Dijkstra-Solution, I recommand comparing these problems:
+- 1102. Path With Maximum Minimum Value
+- 1368. Minimum Cost to Make at Least One Valid Path in a Grid
+- 1631. Path With Minimum Effort
+- 1293. Shortest Path in a Grid with Obstacles Elimination
+class Solution {
+    private static final int[][] DIRECTIONS = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+    public int shortestPath(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length;
+        
+        int[][][] dists = new int[m][n][k+1];
+        for (int[][] dist : dists) 
+            for (int[] d : dist)
+                Arrays.fill(d, Integer.MAX_VALUE);
+        
+        Arrays.fill(dists[0][0], 0);
+        
+        // min-heap storing {i, j, # obstacles eliminated, dist}, sorted by distance to (0,0)
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> a[3] - b[3]);
+        heap.offer(new int[]{0, 0, 0, 0});
+        
+        while (!heap.isEmpty()) {
+            int[] curr = heap.poll();
+            if (curr[0] == m-1 && curr[1] == n-1) continue;
+            
+            for (int[] dir : DIRECTIONS) {
+                int newX = curr[0] + dir[0];
+                int newY = curr[1] + dir[1];
+                
+                // 1). continue if out of bound
+                if (newX < 0 || newY < 0 || newX >= m || newY >= n) continue;
+                
+                // 2). continue if out of elimation
+                int newK = curr[2] + grid[newX][newY];
+                if (newK > k) continue;
+                
+                // 3). continue if we have more optimal result
+                int newDist = curr[3] + 1;
+                if (dists[newX][newY][newK] <= newDist) continue;
+                
+                dists[newX][newY][newK] = newDist;
+                heap.offer(new int[]{newX, newY, newK, newDist});
+            }
+        }
+        
+        int res = dists[m-1][n-1][0];
+        for (int i = 1; i <= k; i++) res = Math.min(res, dists[m-1][n-1][i]);
+        return (res == Integer.MAX_VALUE) ? -1 : res;
+    }
+}
+
+Refer to
+L2290.Minimum Obstacle Removal to Reach Corner (Ref.L1293)
+Dijkstra Shortest Path Algorithm - A Detailed and Visual Introduction
+L743.Network Delay Time
