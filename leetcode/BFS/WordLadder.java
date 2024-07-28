17879f1647cce54a24a62259290bc2f6d943b654
lampardchelsea <zqqsxdd@gmail.com>
2024-07-28 14:25:05 -0700

    Update WordLadder.java

--- leetcode/BFS/WordLadder.java
+++ leetcode/BFS/WordLadder.java
@@ -1,41 +1,176 @@
-/**
- Refer to
- https://leetcode.com/problems/word-ladder/
- Given two words (beginWord and endWord), and a dictionary's word list, find the length of 
- shortest transformation sequence from beginWord to endWord, such that:
 
-Only one letter can be changed at a time.
-Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
-Note:
-
-Return 0 if there is no such transformation sequence.
-All words have the same length.
-All words contain only lowercase alphabetic characters.
-You may assume no duplicates in the word list.
-You may assume beginWord and endWord are non-empty and are not the same.
+https://leetcode.com/problems/word-ladder/
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words 
+beginWord -> s1 -> s2 -> ... -> sk such that:
+- Every adjacent pair of words differs by a single letter.
+- Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+- sk == endWord
+Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
 
 Example 1:
-Input:
-beginWord = "hit",
-endWord = "cog",
-wordList = ["hot","dot","dog","lot","log","cog"]
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
 Output: 5
-Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
-return its length 5.
+Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
 
 Example 2:
-Input:
-beginWord = "hit"
-endWord = "cog"
-wordList = ["hot","dot","dog","lot","log"]
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
 Output: 0
-Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
-*/
+Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
 
-// Solution: Bi-directional BFS
-// https://leetcode.com/problems/word-ladder/discuss/40711/Two-end-BFS-in-Java-31ms.
-// http://theoryofprogramming.com/2018/01/21/bidirectional-search/
-/**
+Constraints:
+- 1 <= beginWord.length <= 10
+- endWord.length == beginWord.length
+- 1 <= wordList.length <= 5000
+- wordList[i].length == beginWord.length
+- beginWord, endWord, and wordList[i] consist of lowercase English letters.
+- beginWord != endWord
+- All the words in wordList are unique.
+--------------------------------------------------------------------------------
+Attempt 1: 2023-10-24
+Solution 1: BFS level order traversal (10 min)
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
+        if(!set.contains(endWord)) {
+            return 0;
+        }
+        Set<String> visited = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        q.offer(beginWord);
+        visited.add(beginWord);
+        int step = 1;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                String cur = q.poll();
+                if(cur.equals(endWord)) {
+                    return step;
+                }
+                for(int j = 0; j < cur.length(); j++) {
+                    for(char c = 'a'; c <= 'z'; c++) {
+                        char[] chars = cur.toCharArray();
+                        if(chars[j] != c) {
+                            chars[j] = c;
+                            String next = new String(chars);
+                            if(!visited.contains(next) && set.contains(next)) {
+                                q.offer(next);
+                                visited.add(next);
+                            }
+                        }
+                    }
+                }
+            }
+            step++;
+        }
+        return 0;
+    }
+}
+
+Time Complexity: O(M^2 * N), where M is size of dequeued word & N is size of our word list
+Space Complexity:O(M * N) where M is no. of character that we had in our string & N is the size of our wordList.
+
+Solution 2: Bi-direction BFS level order traversal (30 min)
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
+        if(!set.contains(endWord)) {
+            return 0;
+        }
+        Set<String> visited = new HashSet<>();
+        Queue<String> q1 = new LinkedList<>();
+        Queue<String> q2 = new LinkedList<>();
+        q1.offer(beginWord);
+        q2.offer(endWord);
+        visited.add(beginWord);
+        visited.add(endWord);
+        int step = 1;
+        while(!q1.isEmpty() && !q2.isEmpty()) {
+            // Always check the smaller size queue and keep q1 as smaller one
+            // and q2 is by the way to keep the larger one
+            if(q1.size() > q2.size()) {
+                Queue<String> tmp = q1;
+                q1 = q2;
+                q2 = tmp;
+            }
+            Queue<String> nextQ = new LinkedList<>();
+            int size = q1.size();
+            for(int i = 0; i < size; i++) {
+                String cur = q1.poll();
+                for(int j = 0; j < cur.length(); j++) {
+                    for(char c = 'a'; c <= 'z'; c++) {
+                        char[] chars = cur.toCharArray();
+                        if(chars[j] != c) {
+                            chars[j] = c;
+                            String next = new String(chars);
+                            // If opponent queue contains next string then just need
+                            // 1 more step to reach the target string
+                            if(q2.contains(next)) {
+                                return step + 1;
+                            }
+                            // Instead of use original wordList.contains(next), have
+                            // to use set.contains(next) to improve speed
+                            if(!visited.contains(next) && set.contains(next)) {
+                                nextQ.offer(next);
+                                visited.add(next);
+                            }
+                        }
+                    }
+                }
+            }
+            // q1 already processed and update to nextQ
+            q1 = nextQ;
+            step++;
+        }
+        return 0;
+    }
+}
+
+Time Complexity: O(M^2 * N), where M is size of dequeued word & N is size of our word list
+Space Complexity:O(M * N) where M is no. of character that we had in our string & N is the size of our wordList.
+
+Bi-directional BFS
+Refer to
+https://leetcode.com/problems/word-ladder/solutions/40711/Two-end-BFS-in-Java-31ms./
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        Set<String> beginSet = new HashSet<String>(), endSet = new HashSet<String>();
+        int len = 1;
+        int strLen = beginWord.length();
+        HashSet<String> visited = new HashSet<String>();
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            if (beginSet.size() > endSet.size()) {
+                Set<String> set = beginSet;
+                beginSet = endSet;
+                endSet = set;
+            }
+            Set<String> temp = new HashSet<String>();
+            for (String word : beginSet) {
+                char[] chs = word.toCharArray();
+                for (int i = 0; i < chs.length; i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        char old = chs[i];
+                        chs[i] = c;
+                        String target = String.valueOf(chs);
+                        if (endSet.contains(target)) {
+                            return len + 1;
+                        }
+                        if (!visited.contains(target) && wordList.contains(target)) {
+                            temp.add(target);
+                            visited.add(target);
+                        }
+                        chs[i] = old;
+                    }
+                }
+            }
+            beginSet = temp;
+            len++;
+        }
+        return 0;
+    }
+}
+http://theoryofprogramming.com/2018/01/21/bidirectional-search/
 http://www.noteanddata.com/leetcode-127-Word-Ladder-java-bidirectional-bfs-solution-note.html
  题目 leetcode 127 Word Ladder
 输入一个开始单词和结束单词， 然后在输入一个单词数组，
@@ -59,225 +194,13 @@ java题解代码
 每次遍历的时候，用来一个小优化，就是每次从queue.size比较小的queue开始遍历，没有严格按照一个方向一层的方法来遍历，这样效果会更好一点
 小细节要注意的是，双向BFS，每次还是要完整的做一层遍历，不能只做一个元素的遍历，否则就不对了
 具体例子可以参考http://www.cppblog.com/Yuan/archive/2011/02/23/140553.html
-*/
-class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        // Check if the endWord in wordList or not, if not then no way to reach from beginWord
-        if(!wordList.contains(endWord)) {
-            return 0;
-        }
-        int len = 1;
-        Set<String> set = new HashSet<String>();
-        Queue<String> beginQueue = new LinkedList<String>();
-        Queue<String> endQueue = new LinkedList<String>();
-        beginQueue.offer(beginWord);
-        endQueue.offer(endWord);
-        while(!beginQueue.isEmpty() && !endQueue.isEmpty()) {
-            if(beginQueue.size() > endQueue.size()) {
-                Queue<String> temp = beginQueue;
-                beginQueue = endQueue;
-                endQueue = temp;
-            }
-            Queue<String> nextQueue = new LinkedList<String>();
-            int size = beginQueue.size();
-            for(int i = 0; i < size; i++) {
-                String word = beginQueue.poll();
-                for(String nextWord : getNextWord(word, wordList)) {
-                    if(!set.contains(nextWord)) {
-                        set.add(nextWord);
-                        nextQueue.offer(nextWord);    
-                    }        
-                    if(endQueue.contains(nextWord)) {
-                        return len + 1;
-                    }
-                }
-            }
-            beginQueue = nextQueue;
-            len++;
-        }
-        return 0;
-    }
+
+
+"The idea behind bidirectional search is to run two simultaneous searches—one forward from
+the initial state and the other backward from the goal—hoping that the two searches meet in
+the middle. The motivation is that b^(d/2) + b^(d/2) is much less than b^d. b is branch factor, d is depth. "
+
+----- section 3.4.6 in Artificial Intelligence - A modern approach by Stuart Russel and Peter Norvig
     
-    private List<String> getNextWord(String word, List<String> wordList) {
-        List<String> nextWords = new ArrayList<String>();
-        for(int i = 0; i < word.length(); i++) {
-            for(char c = 'a'; c <= 'z'; c++) {
-                if(word.charAt(i) == c) {
-                    continue;
-                }
-                String nextWord = replace(word, c, i);
-                if(wordList.contains(nextWord)) {
-                    nextWords.add(nextWord);
-                }
-            }
-        }
-        return nextWords;
-    }
-    
-    private String replace(String s, char c, int i) {
-        char[] chars = s.toCharArray();
-        chars[i] = c;
-        return new String(chars);
-    }
-}
-
-// Wrong solution here
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        // Check if the endWord in wordList or not, if not then no way to reach from beginWord
-        if(!wordList.contains(endWord)) {
-            return 0;
-        }
-        int len = 1;
-        Set<String> set = new HashSet<String>();
-        Queue<String> beginQueue = new LinkedList<String>();
-        Queue<String> endQueue = new LinkedList<String>();
-        beginQueue.offer(beginWord);
-        endQueue.offer(endWord);
-        while(!beginQueue.isEmpty() && !endQueue.isEmpty()) {
-            if(beginQueue.size() > endQueue.size()) {
-                Queue<String> temp = beginQueue;
-                beginQueue = endQueue;
-                endQueue = temp;
-            }
-            Queue<String> nextQueue = new LinkedList<String>();
-            int size = beginQueue.size();
-            for(int i = 0; i < size; i++) {
-                String word = beginQueue.poll();
-                for(String nextWord : getNextWord(word, wordList)) {
-                    // Below section not equal to correct solution, since 'continue'
-                    // will also skip the check for next statement as
-                    // 'endQueue.contains(nextWord)' if set already has the nextWord
-                    // and nextWord is exactly in endQueue.
-                    // e.g
-                    // "hit"
-                    // "cog"
-                    // ["hot","dot","dog","lot","log","cog"]
-                    if(set.contains(nextWord)) {
-                        continue; 
-                    }        
-                    if(endQueue.contains(nextWord)) {
-                        return len + 1;
-                    }
-                    set.add(nextWord);
-                    nextQueue.offer(nextWord);   
-                }
-            }
-            beginQueue = nextQueue;
-            len++;
-        }
-        return 0;
-    }
-
-// Re-work
-// BFS
-class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        // Must use hashset to check 'contains' to improve speed
-        Set<String> dict = new HashSet<String>();
-        for(String s : wordList) {
-            dict.add(s);
-        }
-        if(!wordList.contains(endWord)) {
-            return 0;
-        }
-        if(beginWord.equals(endWord)) {
-            return 1;
-        }
-        Set<String> visited = new HashSet<String>();
-        Queue<String> q = new LinkedList<String>();
-        q.offer(beginWord);
-        visited.add(beginWord);
-        int step = 1;
-        while(!q.isEmpty()) {
-            int size = q.size();
-            for(int i = 0; i < size; i++) {
-                String cur = q.poll();
-                if(cur.equals(endWord)) {
-                    return step;
-                }
-                for(int j = 0; j < cur.length(); j++) {
-                    for(char c = 'a'; c <= 'z'; c++) {
-                        char[] chars = cur.toCharArray();
-                        if(chars[j] == c) {
-                            continue;
-                        }
-                        chars[j] = c;
-                        String next = new String(chars);
-                        // Instead of use original wordList.contains(next), have
-                        // to use dict.contains(next) to improve speed
-                        if(!visited.contains(next) && dict.contains(next)) {
-                            visited.add(next);
-                            q.offer(next);
-                        }
-                    }
-                }
-            }
-            step++;
-        }
-        return 0;
-    }
-}
-
-// Bi-direction BFS
-class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        // Must use hashset to check 'contains' to improve speed
-        Set<String> dict = new HashSet<String>();
-        for(String s : wordList) {
-            dict.add(s);
-        }
-        if(!wordList.contains(endWord)) {
-            return 0;
-        }
-        if(beginWord.equals(endWord)) {
-            return 1;
-        }
-        Set<String> visited = new HashSet<String>();
-        Queue<String> q1 = new LinkedList<String>();
-        Queue<String> q2 = new LinkedList<String>();
-        q1.offer(beginWord);
-        visited.add(beginWord);
-        q2.offer(endWord);
-        visited.add(endWord);
-        int step = 1;
-        while(!q1.isEmpty() && !q2.isEmpty()) {
-            // Always check the smaller size queue and keep q1 as smaller one
-            // and q2 is by the way to keep the larger one
-            if(q1.size() > q2.size()) {
-                Queue<String> temp = q2;
-                q2 = q1;
-                q1 = temp;
-            }
-            Queue<String> nextQ = new LinkedList<String>();
-            int size = q1.size();
-            for(int i = 0; i < size; i++) {
-                String cur = q1.poll();
-                for(int j = 0; j < cur.length(); j++) {
-                    for(char c = 'a'; c <= 'z'; c++) {
-                        char[] chars = cur.toCharArray();
-                        if(chars[j] == c) {
-                            continue;
-                        }
-                        chars[j] = c;
-                        String next = new String(chars);
-                        // If opponent queue contains next string then just need
-                        // 1 more step to reach the target string
-                        if(q2.contains(next)) {
-                            return step + 1;
-                        }
-                        // Instead of use original wordList.contains(next), have
-                        // to use dict.contains(next) to improve speed
-                        if(!visited.contains(next) && dict.contains(next)) {
-                            visited.add(next);
-                            nextQ.offer(next);
-                        }
-                    }
-                }
-            }
-            // q1 already processed and update to nextQ
-            q1 = nextQ;
-            step++;
-        }
-        return 0;
-    }
-}
+Refer to
+L126.Word Ladder II (Ref.L127)
