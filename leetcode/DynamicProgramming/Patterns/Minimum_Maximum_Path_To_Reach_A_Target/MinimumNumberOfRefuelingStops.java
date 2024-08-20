8d140184a2034bddbb57a0a4e53e7ee2a29c9e9a
lampardchelsea <zqqsxdd@gmail.com>
2024-08-20 01:12:37 -0700

    Update MinimumNumberOfRefuelingStops.java

--- leetcode/DynamicProgramming/Patterns/Minimum_Maximum_Path_To_Reach_A_Target/MinimumNumberOfRefuelingStops.java
+++ leetcode/DynamicProgramming/Patterns/Minimum_Maximum_Path_To_Reach_A_Target/MinimumNumberOfRefuelingStops.java
@@ -611,7 +611,18 @@ This conversion optimizes the solution and is particularly useful when dealing w
 Below are old attempts but including valuable details and thoughts:
 Refer to
 https://leetcode.com/problems/minimum-number-of-refueling-stops/discuss/613853/Python-5-solutions-gradually-optimizing-from-Naive-DFS-to-O(n)-space-DP
-Solution 1: Native DFS with for loops subset implementation (TLE)
+Notes:
+For a problem with no clear DP idea, always start with DFS/BFS solution
+A DFS solution with return values instead of a self.rst global value can be easier to be rewrotten with memorization
+For knapsack problems, when the value amount is too large or there is a limitation on value to reach, consider to build a dp[i][j] with former i bags, 
+pick j of them instead of the regular way dp[i][j] with former i bags, value j can be constructed or not.
+- Original 0-1 knapsack: maximum value given # of bags limitation: dp[i][j] = bool, in former i bags, j VALUE can be constructed or not.
+- Reversed 0-1 knapsack: minimum # of bags used to reach a given value dp[i][j] = value, in former i bags, j # OF BAGS being picked, what is the maximum value.
+In a space-uncompressed dp solution, inner loop's left to right / right to left updating direction doesn't matter when dp[i][j] not related to dp[i][j +/-1] , 
+but the compressed 1-d dp's updating direction matters a lot, because last row's results might be replaced by current row's ones. Check here for anthoer 
+exapmle where updating direction needs to be modified because of space optimization.
+
+ Solution 1: Native DFS with for loops subset implementation (TLE)
 Python version
 class Solution(object):
     def minRefuelStops(self, target, startFuel, stations):
