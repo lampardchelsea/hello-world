bbe05793a47b23a183f2113aefb4100d1fe04143
lampardchelsea <zqqsxdd@gmail.com>
2024-12-08 14:53:26 -0800

    Update KokoEatingBananas.java

--- leetcode/BinarySearch/KokoEatingBananas.java
+++ leetcode/BinarySearch/KokoEatingBananas.java
@@ -1,15 +1,8 @@
-/**
-Refer to
+
 https://leetcode.com/problems/koko-eating-bananas/
-Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. 
-The guards have gone and will come back in h hours.
-
-Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas 
-and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead 
-and will not eat any more bananas during this hour.
-
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
 Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
-
 Return the minimum integer k such that she can eat all the bananas within h hours.
 
 Example 1:
@@ -23,91 +16,13 @@ Output: 30
 Example 3:
 Input: piles = [30,11,23,4,20], h = 6
 Output: 23
-
+ 
 Constraints:
-1 <= piles.length <= 104
-piles.length <= h <= 109
-1 <= piles[i] <= 109
-*/
-
-// Solution 1:
-// Refer to
-// https://leetcode.com/discuss/general-discussion/786126/python-powerful-ultimate-binary-search-template-solved-many-problems
-/**
-875. Koko Eating Bananas [Medium]
-Koko loves to eat bananas. There are N piles of bananas, the i-th pile has piles[i] bananas. The guards have gone and will come 
-back in H hours. Koko can decide her bananas-per-hour eating speed of K. Each hour, she chooses some pile of bananas, and eats K 
-bananas from that pile. If the pile has less than K bananas, she eats all of them instead, and won't eat any more bananas during this hour.
-
-Koko likes to eat slowly, but still wants to finish eating all the bananas before the guards come back. Return the minimum 
-integer K such that she can eat all the bananas within H hours.
-
-Example :
-
-Input: piles = [3,6,7,11], H = 8
-Output: 4
-Input: piles = [30,11,23,4,20], H = 5
-Output: 30
-Input: piles = [30,11,23,4,20], H = 6
-Output: 23
-Very similar to LC 1011 and LC 410 mentioned above. Let's design a feasible function, given an input speed, determine whether 
-Koko can finish all bananas within H hours with hourly eating speed speed. Obviously, the lower bound of the search space is 1, 
-and upper bound is max(piles), because Koko can only choose one pile of bananas to eat every hour.
-
-def minEatingSpeed(piles: List[int], H: int) -> int:
-    def feasible(speed) -> bool:
-        # return sum(math.ceil(pile / speed) for pile in piles) <= H  # slower        
-        return sum((pile - 1) // speed + 1 for pile in piles) <= H  # faster
-
-    left, right = 1, max(piles)
-    while left < right:
-        mid = left  + (right - left) // 2
-        if feasible(mid):
-            right = mid
-        else:
-            left = mid + 1
-    return left
-*/
-
-// How to round up the result of integer division?
-// https://stackoverflow.com/questions/17944/how-to-round-up-the-result-of-integer-division
-/**
-int pageCount = (records + recordsPerPage - 1) / recordsPerPage;
-*/
-class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int lo = 1;
-        int hi = 0;
-        for(int pile : piles) {
-            hi = Math.max(hi, pile);
-        }
-        while(lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if(totalHours(piles, mid) <= h) {
-                hi = mid;
-            } else {
-                lo = mid + 1;
-            }
-        }
-        return lo;
-    }
-    
-    private int totalHours(int[] piles, int k) {
-        int count = 0;
-        for(int pile : piles) {
-            count += (pile - 1) / k + 1;
-        }
-        return count;
-    }
-}
-
-
-
-
-
+- 1 <= piles.length <= 10^4
+- piles.length <= h <= 10^9
+- 1 <= piles[i] <= 10^9
+--------------------------------------------------------------------------------
 Attempt 1: 2022-09-14 (30min, difficult on how to integrate into Find Lower Boundary template and handle overflow of integer max value limitation)
-
-```
 class Solution { 
     public int minEatingSpeed(int[] piles, int h) { 
         int lo = 1; 
@@ -157,22 +72,17 @@ class Solution {
 }
 
 Space Complexity: O(1)       
-Time Complexity: O(nlogn) 
-where n is no of piles & n is range of K (left to right)
-```
+Time Complexity: O(nlogn), where n is no of piles & n is range of K (left to right)
 
 Refer to
 https://leetcode.com/problems/koko-eating-bananas/discuss/152506/Binary-Search-Java-Python-with-Explanations
 Each hour, Koko chooses some pile of bananas, and eats K bananas from that pile.
-
-There is a limited value range of K: [lo, hi].There is a K' value, such that K(for any K >= K') can enable her to eat all the bananas within H hours: [K', hi].We are asked to find K'.
-
+There is a limited value range of K: [lo, hi].
+There is a K' value, such that K(for any K >= K') can enable her to eat all the bananas within H hours: [K', hi].
+We are asked to find K'.
 Given a linear searching space [lo, hi], [mi, hi] (lo <= mi) satisfy a property, we can use Binary Searc to get mi.
-
 Initially, we know that K belongs to [1, the largest element in piles[]]. And we follow the pattern of lower-bound Binary Search except that if (K == target) is replaced with if (canEatAll(piles, K, H)).
-
-```
- public int minEatingSpeed(int[] piles, int H) {
+    public int minEatingSpeed(int[] piles, int H) {
         int lo = 1, hi = getMaxPile(piles);
         
         // Binary search to find the smallest valid K.
@@ -206,5 +116,11 @@ Initially, we know that K belongs to [1, the largest element in piles[]]. And we
         }
         return maxPile;
     }
-```
 
+Refer to
+L410.Split Array Largest Sum (Ref.L1011,L704,L1482)
+L1011.Capacity To Ship Packages Within D Days (Ref.L410,L1482)
+L1283.Find the Smallest Divisor Given a Threshold (Ref.L410,L1011,L1482)
+L1482.Minimum Number of Days to Make m Bouquets (Ref.L410,L1011)
+L1802.Maximum Value at a Given Index in a Bounded Array (Ref.L410)
+L2064.Minimized Maximum of Products Distributed to Any Store (Ref.L410)
