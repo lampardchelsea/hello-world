09562ec1a287059057af5518d6d244425434c0ee
lampardchelsea <zqqsxdd@gmail.com>
2024-12-11 01:04:10 -0800

    Update LongestRepeatingCharacterReplacement.java

--- leetcode/SlidingWindow/LongestRepeatingCharacterReplacement.java
+++ leetcode/SlidingWindow/LongestRepeatingCharacterReplacement.java
@@ -1,223 +1,24 @@
-/**
-Refer to
-https://leetcode.com/problems/longest-repeating-character-replacement/
-Given a string s that consists of only uppercase English letters, you can perform at most k operations on that string.
-In one operation, you can choose any character of the string and change it to any other uppercase English character.
-Find the length of the longest sub-string containing all repeating letters you can get after performing the above operations.
 
-Note:
-Both the string's length and k will not exceed 104.
+https://leetcode.com/problems/longest-repeating-character-replacement/
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
 
 Example 1:
-Input:
-s = "ABAB", k = 2
-Output:
-4
-Explanation:
-Replace the two 'A's with two 'B's or vice versa.
- 
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+
 Example 2:
-Input:
-s = "AABABBA", k = 1
-Output:
-4
-Explanation:
-Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
 The substring "BBBB" has the longest repeating letters, which is 4.
-*/
 
-// Solution 1: Not fixed length window using two pointers + criteria
-// Refer to
-// https://leetcode.com/problems/longest-repeating-character-replacement/discuss/91271/Java-12-lines-O(n)-sliding-window-solution-with-explanation
-/**
-public int characterReplacement(String s, int k) {
-        int len = s.length();
-        int[] count = new int[26];
-        int start = 0, maxCount = 0, maxLength = 0;
-        for (int end = 0; end < len; end++) {
-            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
-            while (end - start + 1 - maxCount > k) {
-                count[s.charAt(start) - 'A']--;
-                start++;
-            }
-            maxLength = Math.max(maxLength, end - start + 1);
-        }
-        return maxLength;
-    }
-There's no edge case for this question. The initial step is to extend the window to its limit, that is, the longest 
-we can get to with maximum number of modifications. Until then the variable start will remain at 0.
-
-Then as end increase, the whole substring from 0 to end will violate the rule, so we need to update start accordingly 
-(slide the window). We move start to the right until the whole string satisfy the constraint again. Then each time we 
-reach such situation, we update our max length.
-*/
-
-
-// Why we don't update "maxRepeat" ?
-// Refer to
-// https://leetcode.com/problems/longest-repeating-character-replacement/discuss/91271/Java-12-lines-O(n)-sliding-window-solution-with-explanation/95833
-// https://leetcode.com/problems/longest-repeating-character-replacement/discuss/91271/Java-12-lines-O(n)-sliding-window-solution-with-explanation/95822
-/**
-This solution is great, best so far. However, it requires a bit more explanation.
-
-Since we are only interested in the longest valid substring, our sliding windows need not shrink, even if a window may 
-cover an invalid substring. We either grow the window by appending one char on the right, or shift the whole window to 
-the right by one. And we only grow the window when the count of the new char exceeds the historical max count (from a 
-previous window that covers a valid substring).
-
-That is, we do not need the accurate max count of the current window; we only care if the max count exceeds the historical 
-max count; and that can only happen because of the new char.
-
-Here's my implementation that's a bit shorter
-
-public int characterReplacement(String s, int k)
-{
-    int[] count = new int[128];
-    int max=0;
-    int start=0;
-    for(int end=0; end<s.length(); end++)
-    {
-        max = Math.max(max, ++count[s.charAt(end)]);
-        if(max+k<=end-start)
-            count[s.charAt(start++)]--;
-    }
-    return s.length()-start;
-}
-
-Basics:
-We use a window.
-1.The window starts at the left side of the string and the length of the window is initialized to zero.
-2.The window only stays the same or grows in length as the algorithm proceeds.
-3.The algorithm grows the window to the maximum valid length according to the rules of the game.
-4.The algorithm returns the length of the window upon completion.
-
-Setup:
-1.The length of the window is defined by the following equation: end - start + 1.
-2.The values of both start and end are subject to change during the execution of the algorithm.
-3.The value of end starts at 0 and gets incremented by 1 with each execution of the loop.
-4.But unless a certain condition is met, the value of start will also gets incremented with each execution of the loop, 
-  keeping the length of the window unchanged.
-5.That condition is met when the number of the most commonly occuring character in the window + k is at least as large 
-  as the length of the window (the value of k determines how many of the less commonly occurring characters there can be). 
-  This condition would be required to create a string containing all the same characters from the characters contained 
-  within the window.
-
-Execution:
-1.Right in the beginning, the length of the window is going to be able to grow to at least the value of k.
-2.After that initial growth, the algorithm becomes a search to find the window that contains the greatest number of 
-  reoccurring characters.
-3.Whenever including the character at end results in an increase in the greatest number of reoccurring characters ever 
-  encountered in a window tested so far, the window grows by one (by not incrementing start).
-4.When determining whether or not including another character at the end of the window results in the increase described 
-  above, only the occurrence of the newly included character in the window and the running all-time max need to be taken 
-  into account (after all, only the frequency of the newly included character is increasing).
-5.Even if/when the value of start is incremented (i.e. the left side of the window is moved to the right), the all-time 
-  max doesn't need to be reset to reflect what's currently in the window because 1) at this point in the algorithm, the 
-  all-time maximum number of reoccurring characters in a window is what we're using to determine the all-time longest 
-  window; and 2) we only care about positive developments in our search (i.e. we find a window that contains an even greater 
-  number of reoccurring characters than any other window we have tested so far and therefore is longer than any window we 
-  have tested so far). The algorithm becomes a search for the max and we only need to set the max when we have a new max.
-*/
-
-
-// Why we have "j - i + 1 - maxRepeat > k" ?
-// Refer to
-// https://leetcode.com/problems/longest-repeating-character-replacement/discuss/91271/Java-12-lines-O(n)-sliding-window-solution-with-explanation/137008
-/**
-In case anyone is confused by this solution, here's another way of explaining it:
-
-end-start+1 = size of the current window
-maxCount = largest count of a single, unique character in the current window
-
-The main equation is: end-start+1-maxCount
-
-When end-start+1-maxCount == 0, then then the window is filled with only one character
-When end-start+1-maxCount > 0, then we have characters in the window that are NOT the character that occurs the most. 
-end-start+1-maxCount is equal to exactly the # of characters that are NOT the character that occurs the most in that window. 
-Example: For a window "xxxyz", end-start+1-maxCount would equal 2. (maxCount is 3 and there are 2 characters here, "y" and "z" 
-that are not "x" in the window.)
-
-We are allowed to have at most k replacements in the window, so when end-start+1-maxCount > k, then there are more characters 
-in the window than we can replace, and we need to shrink the window.
-
-If we have window with "xxxy" and k = 1, that's fine because end-start+1-maxCount = 1, which is not > k. maxLength gets updated to 4.
-
-But if we then find a "z" after, like "xxxyz", then we need to shrink the window because now end-start+1-maxCount = 2, and 2 > 1. 
-The window becomes "xxyz".
-
-maxCount may be invalid at some points, but this doesn't matter, because it was valid earlier in the string, and all that matters is 
-finding the max window that occurred anywhere in the string. Additionally, it will expand if and only if enough repeating characters 
-appear in the window to make it expand. So whenever it expands, it's a valid expansion.
-
-Hope that helps.
-P.S. Yes, as several other people mentioned already, the while should be replaced with if. The time complexity is exactly the same, 
-because the while-loop only runs once anyway.
-*/
-
-// Style 1: Separately use 2 statements to increase / decrease i and j where i means start index and j means end index
-// Runtime: 6 ms, faster than 54.12% of Java online submissions for Longest Repeating Character Replacement.
-// Memory Usage: 39 MB, less than 70.42% of Java online submissions for Longest Repeating Character Replacement.
-class Solution {
-    public int characterReplacement(String s, int k) {
-        int maxLen = 0;
-        int maxRepeat = 0;
-        int i = 0;
-        int[] freq = new int[26];
-        for(int j = 0; j < s.length(); j++) {
-            freq[s.charAt(j) - 'A']++;
-            maxRepeat = Math.max(maxRepeat, freq[s.charAt(j) - 'A']);
-            if(j - i + 1 - maxRepeat > k) {
-                freq[s.charAt(i) - 'A']--;
-                i++;
-            }
-            maxLen = Math.max(maxLen, j - i + 1);
-        }
-        return maxLen;
-    }
-}
-
-
-// Style 2: Instead of separately use 2 statements to increase / decrease i and j where i means start index and j means end index,
-// we merge into one line statement, we promote to 5ms now
-// Runtime: 5 ms, faster than 68.92% of Java online submissions for Longest Repeating Character Replacement.
-// Memory Usage: 39.2 MB, less than 38.09% of Java online submissions for Longest Repeating Character Replacement.
-class Solution {
-    public int characterReplacement(String s, int k) {
-        int maxLen = 0;
-        int maxRepeat = 0;
-        int i = 0;
-        int[] freq = new int[26];
-        for(int j = 0; j < s.length(); j++) {
-            maxRepeat = Math.max(maxRepeat, ++freq[s.charAt(j) - 'A']);
-            if(j - i + 1 - maxRepeat > k) {
-                freq[s.charAt(i++) - 'A']--;
-            }
-            maxLen = Math.max(maxLen, j - i + 1);
-        }
-        return maxLen;
-    }
-}
-
-// Style 3: Instead of looply calculate the maxLen, we just need s.length() - i where i means the final position of start index
-// to calculate the final window length, we further promote to 4ms now.
-// Runtime: 4 ms, faster than 93.59% of Java online submissions for Longest Repeating Character Replacement.
-// Memory Usage: 39.3 MB, less than 39.09% of Java online submissions for Longest Repeating Character Replacement.
-class Solution {
-    public int characterReplacement(String s, int k) {
-        int maxRepeat = 0;
-        int i = 0;
-        int[] freq = new int[26];
-        for(int j = 0; j < s.length(); j++) {
-            maxRepeat = Math.max(maxRepeat, ++freq[s.charAt(j) - 'A']);
-            if(j - i + 1 - maxRepeat > k) {
-                freq[s.charAt(i++) - 'A']--;
-            }
-        }
-        return s.length() - i;
-    }
-}
-
-
+Constraints:
+- 1 <= s.length <= 10^5
+- s consists of only uppercase English letters.
+- 0 <= k <= s.length
+--------------------------------------------------------------------------------
 Attempt 1: 2022-09-10 (30min, no update of 'maxRepeat' when substring shrinks is tricky)
 class Solution { 
     // Equal to find longest substring has 
@@ -236,7 +37,7 @@ class Solution {
             maxRepeat = Math.max(maxRepeat, freq[c - 'A']); 
             // When (length of substring - max frequency char in substring) > k 
             // we start to shrink the left end 
-            while(j - i + 1 - maxRepeat > k) { 
+            if(j - i + 1 - maxRepeat > k) { 
                 char c1 = s.charAt(i); 
                 freq[c1 - 'A']--; 
                 // Below 'maxRepeat' update is not necessary, because when  
@@ -257,3 +58,110 @@ class Solution {
 
 Space Complexity: O(n) 
 Time Complexity: O(n)
+
+Refer to chatGPT
+Approach:
+This is a sliding window problem where we maintain a window and check its validity by ensuring the condition:
+Window Length − Max Frequency of Any Character in Window ≤ k
+If the condition holds, the window is valid; otherwise, shrink the window.
+Key Idea:
+1.Sliding Window:
+- Use two pointers to define the window: start and end.
+- Expand the window by moving end.
+- Shrink the window from start if the window becomes invalid.
+2.Character Frequency:
+- Keep track of the frequency of each character in the window.
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26]; // Frequency array for characters A-Z
+        int maxFreq = 0; // Maximum frequency of any single character in the window
+        int start = 0; // Start of the sliding window
+        int maxLength = 0; // Maximum length of valid substring
+
+        for (int end = 0; end < s.length(); end++) {
+            // Update frequency of the current character
+            freq[s.charAt(end) - 'A']++;
+            // Update the max frequency in the current window
+            maxFreq = Math.max(maxFreq, freq[s.charAt(end) - 'A']);
+
+            // Check if the window is valid
+            int windowLength = end - start + 1;
+            if (windowLength - maxFreq > k) {
+                // Shrink the window
+                freq[s.charAt(start) - 'A']--;
+                start++;
+            }
+
+            // Update the maximum length of the valid window
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+    }
+}
+Explanation:
+1.Window Validity:
+- If Window Length − Max Frequency ≤ k, the window is valid because the rest of the characters can be replaced to form a repeating substring.
+2.Sliding Window:
+- Expand the window by moving end.
+- If the window becomes invalid, move start to shrink the window until it's valid again.
+3.Frequency Tracking:
+- Use an array freq of size 26 to store the frequency of each character in the current window.
+4.Max Frequency:
+- Track the maximum frequency of a single character in the window to avoid recalculating it every time.
+--------------------------------------------------------------------------------
+Refer to
+https://leetcode.com/problems/longest-repeating-character-replacement/solutions/91271/java-12-lines-o-n-sliding-window-solution-with-explanation/
+In case anyone is confused by this solution, here's another way of explaining it:
+- end - start + 1 = size of the current window
+- maxCount = largest count of a single, unique character in the current window
+The main equation is: end - start + 1 - maxCount
+- When end - start + 1 - maxCount == 0, then the window is filled with only one character
+- When end - start + 1 - maxCount > 0, then we have characters in the window that are NOT the character that occurs the most. end - start + 1 - maxCount is equal to exactly the # of characters that are NOT the character that occurs the most in that window. Example: For a window "xxxyz", end - start + 1 - maxCount would equal 2. (maxCount is 3 and there are 2 characters here, "y" and "z" that are not "x" in the window.)
+We are allowed to have at most k replacements in the window, so when end - start + 1 - maxCount > k, then there are more characters in the window than we can replace, and we need to shrink the window.
+If we have window with "xxxy" and k = 1, that's fine because end - start + 1 - maxCount = 1, which is not > k. maxLength gets updated to 4.
+But if we then find a "z" after, like "xxxyz", then we need to shrink the window because now end - start + 1 - maxCount = 2, and 2 > 1. The window becomes "xxyz".
+maxCount may be invalid at some points, but this doesn't matter, because it was valid earlier in the string, and all that matters is finding the max window that occurred anywhere in the string. Additionally, it will expand if and only if enough repeating characters appear in the window to make it expand. So whenever it expands, it's a valid expansion.
+public int characterReplacement(String s, int k) {
+    int len = s.length();
+    int[] count = new int[26];
+    int start = 0, maxCount = 0, maxLength = 0;
+    for (int end = 0; end < len; end++) {
+        maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+        while (end - start + 1 - maxCount > k) {
+            count[s.charAt(start) - 'A']--;
+            start++;
+        }
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+    return maxLength;
+}
+
+Same code but another explain
+Refer to
+https://leetcode.com/problems/longest-repeating-character-replacement/solutions/91271/java-12-lines-o-n-sliding-window-solution-with-explanation/comments/95833
+This solution is great, best so far. However, it requires a bit more explanation.
+- Since we are only interested in the longest valid substring, our sliding windows need NOT shrink, even if a window may cover an invalid substring. 
+- We either grow the window by appending one char on the right, or shift the whole window to the right by one. 
+- And we only grow the window when the count of the new char exceeds the historical max count (from a previous window that covers a valid substring).
+- That is, we do not need the accurate max count of the current window; we only care if the max count exceeds the historical max count; and that can only happen because of the new char.
+Here's my implementation that's a bit shorter
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int len = s.length();
+        int[] count = new int[26];
+        int maxCount = 0;
+        int start = 0;
+        for(int end = 0; end < len; end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+            if(end - start + 1 - maxCount > k) {
+                count[s.charAt(start) - 'A']--;
+                start++;
+            }   
+        }
+        return len - start;
+    }
+}
+
+Refer to
+L340.P2.3.Longest Substring with At Most K Distinct Characters (Ref.L424)
