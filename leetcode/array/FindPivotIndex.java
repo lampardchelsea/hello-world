a72809c094716716e6773d0ce75588dbbd6996ab
lampardchelsea <zqqsxdd@gmail.com>
2024-12-15 23:18:43 -0800

    Update FindPivotIndex.java

--- leetcode/array/FindPivotIndex.java
+++ leetcode/array/FindPivotIndex.java
@@ -1,16 +1,9 @@
-/**
-Refer to
-https://leetcode.com/problems/find-pivot-index/
+https://leetcode.com/problems/find-pivot-index/description/
 Given an array of integers nums, calculate the pivot index of this array.
-
-The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to 
-the sum of all the numbers strictly to the index's right.
-
-If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. 
-This also applies to the right edge of the array.
-
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
 Return the leftmost pivot index. If no such index exists, return -1.
-
+ 
 Example 1:
 Input: nums = [1,7,3,6,5,6]
 Output: 3
@@ -29,49 +22,18 @@ Example 3:
 Input: nums = [2,1,-1]
 Output: 0
 Explanation:
-The pivot index is 0.
-Left sum = 0 (no elements to the left of index 0)
-Right sum = nums[1] + nums[2] = 1 + -1 = 0
-
+The pivot index is 0.Left sum = 0 (no elements to the left of index 0)Right sum = nums[1] + nums[2] = 1 + -1 = 0
+ 
 Constraints:
-1 <= nums.length <= 104
--1000 <= nums[i] <= 1000
-*/
-
-// Solution 1: PreSum
-// Refer to
-// https://leetcode.com/problems/find-pivot-index/solution/
-/**
-Approach #1: Prefix Sum [Accepted]
-Intuition and Algorithm
-
-We need to quickly compute the sum of values to the left and the right of every index.
-
-Let's say we knew S as the sum of the numbers, and we are at index i. If we knew the sum of numbers leftsum that are to the left of index i, 
-then the other sum to the right of the index would just be S - nums[i] - leftsum.
-
-As such, we only need to know about leftsum to check whether an index is a pivot index in constant time. Let's do that: as we iterate through 
-candidate indexes i, we will maintain the correct value of leftsum.
-*/
-class Solution {
-    public int pivotIndex(int[] nums) {
-        int sum = 0;
-        int leftSum = 0;
-        for(int n : nums) {
-            sum += n;
-        }
-        for(int i = 0; i < nums.length; i++) {
-            if(leftSum == sum - leftSum - nums[i]) {
-                return i;
-            }
-            leftSum += nums[i];
-        }
-        return -1;
-    }
-}
-
-// Wrong Solution: we should not use sum / 2 == ... because / 2 will encounter issue when sum is odd number
-// Below still failed at [-1,-1,-1,-1,0,0], output 4, expected -1.
+- 1 <= nums.length <= 10^4
+- -1000 <= nums[i] <= 1000
+ 
+Note: This question is the same as 1991: https://leetcode.com/problems/find-the-middle-index-in-array/
+--------------------------------------------------------------------------------
+Attempt 1: 2024-05-04
+Wrong Solution
+we should not use sum / 2 == ... because / 2 will encounter issue when sum is odd number
+Below still failed at [-1,-1,-1,-1,0,0], output 4, expected -1.
 class Solution {
     public int pivotIndex(int[] nums) {
         int sum = 0;
@@ -94,3 +56,27 @@ class Solution {
         return -1;
     }
 }
+Solution 1: Brute Force (10 min)
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0; // Total sum of all array elements
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0; // Running sum of elements to the left of the current index
+
+        for (int i = 0; i < nums.length; i++) {
+            // Calculate the right sum as: totalSum - leftSum - nums[i]
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i; // Found the pivot index
+            }
+            leftSum += nums[i]; // Update left sum for the next index
+        }
+
+        return -1; // No pivot index found
+    }
+}
+
+Time Complexity: O(n)
+Space Complexity: O(1)       
