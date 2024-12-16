5913caca107c8caa1a042423c2e632d26af27c2f
lampardchelsea <zqqsxdd@gmail.com>
2024-12-15 16:11:58 -0800

    Update SubarraySumEqualsK.java

--- leetcode/array/SubarraySumEqualsK.java
+++ leetcode/array/SubarraySumEqualsK.java
@@ -198,39 +198,25 @@ class Solution {
 
 
 
-
-
-
-
-
-
 https://leetcode.com/problems/subarray-sum-equals-k/
-
 Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
-
 A subarray is a contiguous non-empty sequence of elements within an array.
 
 Example 1:
-```
 Input: nums = [1,1,1], k = 2
 Output: 2
-```
 
 Example 2:
-```
 Input: nums = [1,2,3], k = 3
 Output: 2
-```
 
 Constraints:
-- 1 <= nums.length <= 2 * 104
+- 1 <= nums.length <= 2 * 10^4
 - -1000 <= nums[i] <= 1000
-- -107 <= k <= 107
----
+- -10^7 <= k <= 10^7
+--------------------------------------------------------------------------------
 Attempt 1: 2023-02-04
-
 Solution 1:  Native for loop with 2 passes (30 min, first pass is create preSum array, second pass is calculate interval with nested for loop)
-```
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int[] preSum = new int[nums.length + 1];
@@ -252,10 +238,8 @@ class Solution {
 
 Time Complexity: O(n^2)  
 Space Complexity: O(n)
-```
 
-Solution 2:  Hash Table (30 min)
-```
+Solution 2:  Hash Table + Auxiliary array (30 min)
 class Solution { 
     public int subarraySum(int[] nums, int k) { 
         Map<Integer, Integer> map = new HashMap<Integer, Integer>(); 
@@ -276,56 +260,36 @@ class Solution {
 
 Time Complexity: O(n) 
 Space Complexity: O(n)
-```
 
 Refer to
 https://leetcode.com/problems/subarray-sum-equals-k/solutions/803317/java-solution-with-detailed-explanation/
-
 Thinking
-
 1. Use an array to store the sum accumulated from the beginning to a certain position.
 Example:
-```
 nums = [1,   2,   3  ]
 sum  = [1, 1+2, 1+2+3]
-```
 
 2. How to create array "sum" ?
-```
 sum[i] = sum[i - 1] + nums[i]
-```
 Q : If i == 0, the index is out of range. How to solve this problem ?
 A : Set the first element of the array "sum" to 0, and initialize the array "sum" from index 1 rather than 0.
-```
 nums = [1,   2,   3  ]
 sum  = [0,   1,   1+2, 1+2+3] // Also, the length of "sum" is one more than "nums"  
-
 sum[i] = sum[i - 1] + nums[i - 1]
-```
 
-```
 // Java Version
 int[] sum = new int[nums.length + 1];
-
 sum[0] = 0;
 for (int i = 1; i < (nums.length + 1); i++)
   sum[i] = sum[i - 1] + nums[i - 1];
-```
 
 3. Using array "sum" to calculate the sum of a subarray
-```
 sumOfSubarray = sum[end] - sum[start];
-```
 For example : Calculate the sum of "nums" means using the last element of "sum" minus the first element of "sum" which is 0.
-```
 nums[0] + nums[1] + nums[2] = sum[3] - sum[0] = 6 - 0
-```
 
 4.Using array "sum"to caculate all possibilities .
-
 Code
-
-```
 public class Solution {
     public int subarraySum(int[] nums, int k) {
         int count = 0;
@@ -345,46 +309,29 @@ public class Solution {
         return count;
     }
 }
-```
-
 Complexity Analysis
-
-- Time complexity : O(n2).
+- Time complexity : O(n^2).
 - Space complexity : O(n).
-
-
 Optimization by Hashmap
-
-
 Thinking
-
 1.In the previous method
 Step 1. The "nums" array is traversed to calculate all the elements of the sum array
 Step 2. Use the nested loop to judge.
 key : Can we judge when the array is traversed(Step 1) ?
 Transposition
-```
 int[] sum = new int[nums.length + 1];
-
 sum[0] = 0;
 for (int end = 1; end < (nums.length + 1); end++)
   sum[end] = sum[end - 1] + nums[end - 1];
-```
 a. Put each element of "sum" array into hashmap according to this format : (sumi, number of occurence)
 b. When constructing the "sum" array, we take the currently constructed element as sum[end], then all the elements before "end" which have been calculated can be regarded as all sum[start] for this "end".
 Transform the judgment condition
 Obviously, when sum[end] is calculated, all its possible sum[start] are already in the map.
-```
 sum[end] - sum[start] == k
-
 sum[end] - k == sum[start]
-```
 c. When sumend is calculated, we only need to determine whether there is key == sumend - k in the hashmap and add the number of occurrence to the answer.
 Attention : In the previous method, we set the first element of sum to 0. Similarly, we put it in the hashmap, which is (0, 1).
-
 Code
-
-```
 public class Solution {
     public int subarraySum(int[] nums, int k) {
         int count = 0, sum = 0;
@@ -400,14 +347,51 @@ public class Solution {
         return count;
     }
 }
-```
-
 Complexity Anaysis
-
 - Time complexity : O(n).
 - Space complexity : O(n).
+--------------------------------------------------------------------------------
+Solution 3:  Hash Table + Single Varaiable (30 min)
+Actually no need preSum array, we can replace its functionality with a single variable 'presSum'
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int preSum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for(int num : nums) {
+            preSum += num;
+            count += map.getOrDefault(preSum - k, 0);
+            map.put(preSum, map.getOrDefault(preSum, 0) + 1);
+        }
+        return count;
+    }
+}
 
+Time Complexity: O(n) 
+Space Complexity: O(1)
+
+Refer to
+https://leetcode.com/problems/subarray-sum-equals-k/solutions/102106/java-solution-presum-hashmap/
+Solution 1. Brute force. We just need two loops (i, j) and test if SUM[i, j] = k. Time complexity O(n^2), Space complexity O(1). I bet this solution will TLE.
+Solution 2. From solution 1, we know the key to solve this problem is SUM[i, j]. So if we know SUM[0, i - 1] and SUM[0, j], then we can easily get SUM[i, j]. To achieve this, we just need to go through the array, calculate the current sum and save number of all seen PreSum to a HashMap. Time complexity O(n), Space complexity O(n).
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0, result = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+        
+        return result;
+    }
+}
 A harder problem
-
 https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/
 https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/discuss/803353/java-solution-with-detailed-explanation
