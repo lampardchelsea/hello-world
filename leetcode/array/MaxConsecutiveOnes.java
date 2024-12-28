151469533ffa904b025906f72fd16efafcddb81e
lampardchelsea <zqqsxdd@gmail.com>
2024-12-28 00:54:10 -0800

    Update MaxConsecutiveOnes.java

--- leetcode/array/MaxConsecutiveOnes.java
+++ leetcode/array/MaxConsecutiveOnes.java
@@ -63,3 +63,80 @@ public class Solution {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+https://leetcode.com/problems/max-consecutive-ones/description/
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+Example 1:
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+Example 2:
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+ 
+Constraints:
+- 1 <= nums.length <= 10^5
+- nums[i] is either 0 or 1.
+--------------------------------------------------------------------------------
+Attempt 1: 2024-12-27
+Solution 1: Array (10 min)
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 1) {
+                count++;
+                // We can also put here since answer only comes from this branch
+                //max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+}
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Refer to
+L487.Max Consecutive Ones II (Ref.L485)
+L1004.P2.7.Max Consecutive Ones III
+L1446.Consecutive Characters
