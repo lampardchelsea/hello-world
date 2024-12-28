0dfb0b4807c3c105d225c15119fea1f9f250f9de
lampardchelsea <zqqsxdd@gmail.com>
2024-12-28 00:51:53 -0800

    Update ConsecutiveCharacters.java

--- leetcode/String/ConsecutiveCharacters.java
+++ leetcode/String/ConsecutiveCharacters.java
@@ -54,3 +54,89 @@ class Solution {
         return max;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+https://leetcode.com/problems/consecutive-characters/description/
+The power of the string is the maximum length of a non-empty substring that contains only one unique character.
+Given a string s, return the power of s.
+
+Example 1:
+Input: s = "leetcode"
+Output: 2
+Explanation: The substring "ee" is of length 2 with the character 'e' only.
+
+Example 2:
+Input: s = "abbcccddddeeeeedcba"
+Output: 5
+Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+ 
+Constraints:
+- 1 <= s.length <= 500
+- s consists of only lowercase English letters.
+--------------------------------------------------------------------------------
+Attempt 1: 2024-12-27
+Solution 1: Array (10 min)
+class Solution {
+    public int maxPower(String s) {
+        int max = 1;
+        int count = 1;
+        char c = s.charAt(0);
+        for(int i = 1; i < s.length(); i++) {
+            if(s.charAt(i) == c) {
+                count++;
+            } else {
+                count = 1;
+                c = s.charAt(i);
+            }
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+}
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Refer to
+L485.Max Consecutive Ones
