0b4a3f9aacd5c654296280ea73d1dcccbcba8ad1
lampardchelsea <zqqsxdd@gmail.com>
2025-08-10 12:33:21 -0700

    Update AllNodesDistanceKInBinaryTree.java

--- leetcode/Tree/AllNodesDistanceKInBinaryTree.java
+++ leetcode/Tree/AllNodesDistanceKInBinaryTree.java
@@ -1,75 +1,25 @@
-/**
- Refer to
- https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
- We are given a binary tree (with root node root), a target node, and an integer value K.
-
-Return a list of the values of all nodes that have a distance K from the target node.  
-The answer can be returned in any order.
-
+https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/description/
+Given the root of a binary tree, the value of a target node target, and an integer k, return an array of the values of all nodes that have a distance k from the target node.
+You can return the answer in any order.
+ 
 Example 1:
-Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, K = 2
-Output: [7,4,1]
-                         3
-                    5         1
-                  6   2     0   8
-                    7   4
-                         
-Explanation: 
-The nodes that are a distance 2 from the target node (with value 5)
-have values 7, 4, and 1.
 
-Note that the inputs "root" and "target" are actually TreeNodes.
-The descriptions of the inputs above are just serializations of these objects.
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, k = 2
+Output: [7,4,1]Explanation: The nodes that are a distance 2 from the target node (with value 5) have values 7, 4, and 1.
 
-Note:
-The given tree is non-empty.
-Each node in the tree has unique values 0 <= node.val <= 500.
-The target node is a node in the tree.
-0 <= K <= 1000
-*/
-// Solution 1: DFS with back travel to ancestor
-// Refer to
-// https://www.geeksforgeeks.org/print-nodes-distance-k-given-node-binary-tree/
-/**
- Given a binary tree, a target node in the binary tree, and an integer value k, 
- print all the nodes that are at distance k from the given target node. 
- No parent pointers are available.
- E.g
-                            20
-                     8             22
-                 4      12
-                     10    14 
-
- Consider the tree shown in diagram
- Input: target = pointer to node with data 8.
- root = pointer to node with data 20.
- k = 2.
- Output : 10 14 22
-
- If target is 14 and k is 3, then output should be 4 20
-
- There are two types of nodes to be considered.
-1) Nodes in the subtree rooted with target node. 
-   For example if the target node is 8 and k is 2, then such nodes are 10 and 14.
-2) Other nodes, may be an ancestor of target, or a node in some other subtree. 
-   For target node 8 and k is 2, the node 22 comes in this category.
-
-Finding the first type of nodes is easy to implement. Just traverse subtrees rooted 
-with the target node and decrement k in recursive call. When the k becomes 0, print 
-the node currently being traversed (See this for more details). Here we call the 
-function as printkdistanceNodeDown().
-
-How to find nodes of second type? For the output nodes not lying in the subtree with 
-the target node as the root, we must go through all ancestors. For every ancestor, 
-we find its distance from target node, let the distance be d, now we go to other 
-subtree (if target was found in left subtree, then we go to right subtree and vice 
-versa) of the ancestor and find all nodes at k-d distance from the ancestor.
-*/
-
-// Also refer to
-// https://www.cnblogs.com/grandyang/p/10686922.html
-// https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/discuss/143798/1ms-beat-100-simple-Java-dfs-using-hashmap-with-explanation
-
+Example 2:
+Input: root = [1], target = 1, k = 3
+Output: []
+ 
+Constraints:
+- The number of nodes in the tree is in the range [1, 500].
+- 0 <= Node.val <= 500
+- All the values Node.val are unique.
+- target is the value of one of the nodes in the tree.
+- 0 <= k <= 1000
+--------------------------------------------------------------------------------
+Attempt 1: 2025-08-07
+Solution 1: DFS + Hash Table (60 min)
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -80,79 +30,241 @@ versa) of the ancestor and find all nodes at k-d distance from the ancestor.
  * }
  */
 class Solution {
-    List<Integer> result = new ArrayList<Integer>();
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
-        if(root == null || target == null || K < 0) {
-            return result;
-        }
-        distanceBetweenCurrentRootAndTargetNode(root, target, K);
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> result = new ArrayList<>();
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        buildParentMap(root, null, parentMap);
+        Set<TreeNode> visited = new HashSet<>();
+        helper(target, k, visited, parentMap, result);
         return result;
     }
-    
-    // Prints all nodes at distance K from a given target node.
-    // The K distance nodes may be upward or downward. 
-    // This function returns distance of root from target node, 
-    // it returns -1 if target node is not present in tree rooted 
-    // with root. 
-    private int distanceBetweenCurrentRootAndTargetNode(TreeNode root, TreeNode target, int K) {
-        // Base Case: If tree is empty, return -1 
-        if(root == null) {
-            return -1;
+
+    private void buildParentMap(TreeNode node, TreeNode parent, 
+        Map<TreeNode, TreeNode> parentMap) {
+        if(node == null) {
+            return;
         }
-        // If target is same as root. Use the downward function 
-	// to add all nodes at distance K in subtree rooted with 
-	// target or root 
-        if(root.val == target.val) {
-            printKDistanceNodeDown(root, K);
-            return 0;
-        }
-        // Recursive for left subtree
-        int dl = distanceBetweenCurrentRootAndTargetNode(root.left, target, K);
-        // Check if target node was found in left subtree
-        if(dl != -1) {
-            // If root is at distance k from target, add root to result
-            // Note that dl is distance of root's left child from target,
-            // for root need plus 1 more edge to match distance K
-            // Else go to right subtree and add all (K - dl -2) distance nodes 
-            // Note that the right child is 2 edges away from left child 
-            if(dl + 1 == K) {
-                result.add(root.val);
-            } else {
-                printKDistanceNodeDown(root.right, K - dl - 2);
-            }
-            return dl + 1;
-        }
-        // MIRROR OF ABOVE CODE FOR RIGHT SUBTREE 
-	// Note that we reach here only when node was not found in left subtree 
-        int dr = distanceBetweenCurrentRootAndTargetNode(root.right, target, K);
-        if(dr != -1) {
-            if(dr + 1 == K) {
-                result.add(root.val);
-            } else {
-                printKDistanceNodeDown(root.left, K - dr - 2);
-            }
-            return dr + 1;
-        }
-        // If target was neither present in left nor in right subtree
-        return -1;
+        parentMap.put(node, parent);
+        buildParentMap(node.left, node, parentMap);
+        buildParentMap(node.right, node, parentMap);
     }
-    
-    // Recursive function to print all the nodes at distance k in 
-    // tree (or subtree) rooted with given root.
-    private void printKDistanceNodeDown(TreeNode root, int K) {
-        // Base case
-        if(root == null || K < 0) {
+
+    private void helper(TreeNode node, int k, Set<TreeNode> visited, 
+        Map<TreeNode, TreeNode> parentMap, List<Integer> result) {
+            if(node == null || visited.contains(node)) {
+                return;
+            }
+            visited.add(node);
+            if(k == 0) {
+                result.add(node.val);
+                return;
+            }
+            // Recurse on left child
+            helper(node.left, k - 1, visited, parentMap, result);
+            // Recurse on right child
+            helper(node.right, k - 1, visited, parentMap, result);
+            // Recurse on parent
+            helper(parentMap.get(node), k - 1, visited, parentMap, result);
+        }
+}
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+Solution 2: BFS + Hash Table (60 min)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> result = new ArrayList<>();
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        buildParentMap(root, null, parentMap);
+        Set<TreeNode> visited = new HashSet<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(target);
+        visited.add(target);
+        int distance = 0;
+        while(!queue.isEmpty() && distance < k) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                // Add left child
+                if(node.left != null && !visited.contains(node.left)) {
+                    queue.offer(node.left);
+                    visited.add(node.left);
+                }
+                // Add right child
+                if(node.right != null && !visited.contains(node.right)) {
+                    queue.offer(node.right);
+                    visited.add(node.right);
+                }
+                // Add parent
+                TreeNode parent = parentMap.get(node);
+                if(parent != null && !visited.contains(parent)) {
+                    queue.offer(parent);
+                    visited.add(parent);
+                }
+            }
+            distance++;
+        }
+        while(!queue.isEmpty()) {
+            result.add(queue.poll().val);
+        }
+        return result;
+    }
+
+    private void buildParentMap(TreeNode node, TreeNode parent, 
+        Map<TreeNode, TreeNode> parentMap) {
+        if(node == null) {
             return;
         }
-        // If we reach a K distant node, add it to result
-        if(K == 0) {
-            result.add(root.val);
-            return;
-        }
-        // Recursive for left and right subtrees
-        printKDistanceNodeDown(root.left, K - 1);
-        printKDistanceNodeDown(root.right, K - 1);
+        parentMap.put(node, parent);
+        buildParentMap(node.left, node, parentMap);
+        buildParentMap(node.right, node, parentMap);
     }
 }
 
-// Solution 2: DFS with 
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+Refer to Deepseek
+Here's a Depth-First Search (DFS) approach to solve this problem, which provides an alternative to the BFS solution.
+DFS Approach
+1.Build Parent Map: First create a mapping from each node to its parent (same as BFS approach)
+2.DFS Traversal: Perform a DFS starting from the target node, tracking the current distance from target
+3.Collect Results: When reaching distance K, collect the nodes
+Solution Code
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        buildParentMap(root, null, parentMap);
+        List<Integer> result = new ArrayList<>();
+        Set<TreeNode> visited = new HashSet<>();
+        dfs(target, k, parentMap, visited, result);
+        return result;
+    }
+    
+    private void dfs(TreeNode node, int k, Map<TreeNode, TreeNode> parentMap, 
+                    Set<TreeNode> visited, List<Integer> result) {
+        if (node == null || visited.contains(node)) {
+            return;
+        }
+        visited.add(node);
+        if (k == 0) {
+            result.add(node.val);
+            return;
+        }
+        // Recurse on left child
+        dfs(node.left, k - 1, parentMap, visited, result);
+        // Recurse on right child
+        dfs(node.right, k - 1, parentMap, visited, result);
+        // Recurse on parent
+        TreeNode parent = parentMap.get(node);
+        dfs(parent, k - 1, parentMap, visited, result);
+    }
+    
+    private void buildParentMap(TreeNode node, TreeNode parent, Map<TreeNode, TreeNode> parentMap) {
+        if (node == null) return;
+        parentMap.put(node, parent);
+        buildParentMap(node.left, node, parentMap);
+        buildParentMap(node.right, node, parentMap);
+    }
+}
+Explanation
+1.Parent Mapping: The buildParentMap function recursively creates a parent-child relationship map, same as in the BFS solution.
+2.DFS Traversal:
+- Start DFS from the target node
+- Track visited nodes to avoid cycles
+- Decrement K at each level of recursion
+- When K reaches 0, add the current node to results
+3.Three-Way Recursion:
+- Explore left child
+- Explore right child
+- Explore parent (using the parent map)
+Key differences from BFS:
+- Uses recursion stack instead of a queue
+- Explores each path fully before backtracking
+- Still maintains O(n) time complexity
+- Call stack depth could be O(n) in worst case (unbalanced tree)
+This DFS solution provides the same result as BFS but with a different traversal order. Both are valid approaches with similar time/space complexity.
+
+Can we have a BFS solution ?
+Approach
+To find all nodes at distance K from a target node in a binary tree:
+1.Build Parent Map: Create a mapping from each node to its parent to enable upward traversal.
+2.BFS Traversal: Perform a breadth-first search starting from the target node, expanding outward to nodes at distance K.
+3.Track Visited Nodes: Avoid revisiting nodes by maintaining a visited set.
+Solution Code
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        buildParentMap(root, null, parentMap);
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        Set<TreeNode> visited = new HashSet<>();
+        queue.offer(target);
+        visited.add(target);
+        
+        int distance = 0;
+        
+        while (!queue.isEmpty() && distance < k) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                
+                // Add left child
+                if (current.left != null && !visited.contains(current.left)) {
+                    queue.offer(current.left);
+                    visited.add(current.left);
+                }
+                
+                // Add right child
+                if (current.right != null && !visited.contains(current.right)) {
+                    queue.offer(current.right);
+                    visited.add(current.right);
+                }
+                
+                // Add parent
+                TreeNode parent = parentMap.get(current);
+                if (parent != null && !visited.contains(parent)) {
+                    queue.offer(parent);
+                    visited.add(parent);
+                }
+            }
+            distance++;
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            result.add(queue.poll().val);
+        }
+        
+        return result;
+    }
+    
+    private void buildParentMap(TreeNode node, TreeNode parent, Map<TreeNode, TreeNode> parentMap) {
+        if (node == null) return;
+        parentMap.put(node, parent);
+        buildParentMap(node.left, node, parentMap);
+        buildParentMap(node.right, node, parentMap);
+    }
+}
+
+Explanation
+1.Parent Mapping: The buildParentMap function recursively creates a mapping of each node to its parent, enabling upward traversal from any node.
+2.BFS Initialization: Start BFS from the target node, marking it as visited.
+3.Level-wise Expansion: For each level (distance from target):
+- Process all nodes at the current distance.
+- Add unvisited left child, right child, and parent to the queue for the next level.
+4.Result Collection: After reaching distance K, collect all remaining nodes in the queue as the result.
+This approach efficiently explores the tree in all directions (children and parent) while maintaining the correct distance from the target node. The time complexity is O(n) where n is the number of nodes, and space complexity is O(n) for the parent map and queue.
+
+Refer to
+L2385.Amount of Time for Binary Tree to Be Infected (Ref.L863)
