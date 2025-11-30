3301f2f845e3308d143f0efb73190ccfc3b1d464
lampardchelsea <zqqsxdd@gmail.com>
2025-11-29 17:29:20 -0800

    Update BinaryTreeZigzagLevelOrderTraversal.java

--- leetcode/Tree/BinaryTreeZigzagLevelOrderTraversal.java
+++ leetcode/Tree/BinaryTreeZigzagLevelOrderTraversal.java
@@ -198,37 +198,28 @@ class Solution {
 
 
 https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
-
 Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
 
 Example 1:
 
 
-```
 Input: root = [3,9,20,null,null,15,7]
 Output: [[3],[20,9],[15,7]]
-```
 
 Example 2:
-```
 Input: root = [1]
 Output: [[1]]
-```
 
 Example 3:
-```
 Input: root = []
 Output: []
-```
 
 Constraints:
 - The number of nodes in the tree is in the range [0, 2000].
 - -100 <= Node.val <= 100
----
+--------------------------------------------------------------------------------
 Attempt 1: 2023-09-10
-
 Solution 1:  BFS + Level order traversal (10 min)
-```
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -276,10 +267,8 @@ class Solution {
         return result;
     }
 }
-```
 
 Solution 2: DFS + Pre-order traversal (30 min)
-```
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -328,15 +317,13 @@ class Solution {
         helper(root.right, result, depth + 1);
     }
 }
-```
 
----
+--------------------------------------------------------------------------------
 Refer to
 https://grandyang.com/leetcode/103/
 这道二叉树的之字形层序遍历是之前那道 Binary Tree Level Order Traversal 的变形，不同之处在于一行是从左到右遍历，下一行是从右往左遍历，交叉往返的之字形的层序遍历。最简单直接的方法就是利用层序遍历，并使用一个变量 cnt 来统计当前的层数（从0开始），将所有的奇数层的结点值进行翻转一下即可，参见代码如下：
 
 解法一：
-```
     class Solution {
         public:
         vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
@@ -359,13 +346,10 @@ https://grandyang.com/leetcode/103/
             return res;
         }
     };
-```
-
 我们可以将上面的解法进行优化一下，翻转数组虽然可行，但是比较耗时，假如能够直接计算出每个结点值在数组中的坐标，就可以直接进行更新了。由于每层的结点数是知道的，就是队列的元素个数，所以可以直接初始化数组的大小。此时使用一个变量 leftToRight 来标记顺序，初始时是 true，当此变量为 true 的时候，每次加入数组的位置就是i本身，若变量为 false 了，则加入到 size-1-i 位置上，这样就直接相当于翻转了数组。每层遍历完了之后，需要翻转 leftToRight 变量，同时不要忘了将 oneLevel 加入结果 res，参见代码如下：
 
 解法二：
-```
-    class Solution {
+  class Solution {
         public:
         vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
             if (!root) return {};
@@ -388,13 +372,11 @@ https://grandyang.com/leetcode/103/
             return res;
         }
     };
-```
 
 我们也可以使用递归的方法来解，这里实际上用的是先序遍历，递归函数需要一个变量 level 来记录当前的深度，由于 level 是从0开始的，假如结果 res 的大小等于 level，就需要在结果 res 中新加一个空集，这样可以保证 res[level] 不会越界。取出 res[level] 之后，判断 level 的奇偶，若其为偶数，则将 node->val 加入 oneLevel 的末尾，若为奇数，则加在 oneLevel 的开头。然后分别对 node 的左右子结点调用递归函数，此时要传入 level+1 即可，参见代码如下：
 
 解法三：
-```
-    class Solution {
+ class Solution {
         public:
         vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
             vector<vector<int>> res;
@@ -413,4 +395,8 @@ https://grandyang.com/leetcode/103/
             helper(node->right, level + 1, res);
         }
     };
-```
+      
+    
+Refer to
+L314.Binary Tree Vertical Order Traversal (Ref.L987)
+L987.Vertical Order Traversal of a Binary Tree (Ref.L103,L199,L314)
